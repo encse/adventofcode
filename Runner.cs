22884df040b1f8bb1515b1bcacb8f50493eed4d2
David Nemeth Csoka <encse@csokavar.hu>
2017-12-03 11:12:09 +0100

    c# and day03

---
+++ Runner.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+
+namespace AdventOfCode2017
+{
+    interface Solver
+    {
+        void Solve(string input);
+    }
+
+    class Runner
+    {
+        static void Main()
+        {
+            RunSolver(new Day03.Solution());
+        }
+
+        static void RunSolver(Solver solver)
+        {
+            var name = solver.GetType().FullName.Split('.')[1].ToLower();
+            Console.WriteLine($"Solving {name}");
+            foreach (var file in Directory.EnumerateFiles(name))
+            {
+                if (file.EndsWith(".in"))
+                {
+                    solver.Solve(File.ReadAllText(file));
+                }
+            }
+        }
+    }
+}
