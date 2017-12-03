4ba987207d36958a6dce8c6848f1b06705ef1b63
David Nemeth Csoka <encse@csokavar.hu>
2017-12-03 15:55:03 +0100

    formatting

--- Day02/Solution.cs
+++ Day02/Solution.cs
@@ -2,33 +2,32 @@
 using System.IO;
 using System.Linq;
 
-namespace AdventOfCode2017.Day02
-{
-    class Solution : Solver
-    {
-        public void Solve(string input)
-        {
+namespace AdventOfCode2017.Day02 {
+
+    class Solution : Solver {
+        
+        public void Solve(string input) {
             Console.WriteLine(PartOne(input));
             Console.WriteLine(PartTwo(input));
         }
 
-        int PartOne(string input)
-        {
-            var diffs = from line in input.Split('\n')
-                        let numbers = line.Split('\t').Select(int.Parse)
-                        select numbers.Max() - numbers.Min();
-            return diffs.Sum();
+        int PartOne(string input) {
+            return (
+                from line in input.Split('\n')
+                let numbers = line.Split('\t').Select(int.Parse)
+                select numbers.Max() - numbers.Min()
+            ).Sum();
         }
 
-        int PartTwo(string input)
-        {
-            var divisions = from line in input.Split('\n')
-                            let numbers = line.Split('\t').Select(int.Parse)
-                            from a in numbers
-                            from b in numbers
-                            where a > b && a % b == 0
-                            select a / b;
-            return divisions.Sum();
+        int PartTwo(string input) {
+            return (
+                from line in input.Split('\n')
+                let numbers = line.Split('\t').Select(int.Parse)
+                from a in numbers
+                from b in numbers
+                where a > b && a % b == 0
+                select a / b
+            ).Sum();
         }
     }
 }
--- Day03/Solution.cs
+++ Day03/Solution.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace AdventOfCode2017.Day03
-{
-    class Solution : Solver
-    {
-        public void Solve(string input)
-        {
+namespace AdventOfCode2017.Day03 {
+
+    class Solution : Solver {
+
+        public void Solve(string input) {
             Console.WriteLine(PartOne(input));
             Console.WriteLine(PartTwo(input));
         }
 
-        int PartOne(string input)
-        {
+        int PartOne(string input) {
             var targetValue = int.Parse(input);
-            var (x, y) = Coordinates().ElementAt(targetValue - 1);
+            var (x, y) = SpiralCoordinates().ElementAt(targetValue - 1);
             return Math.Abs(x) + Math.Abs(y);
         }
 
-        int PartTwo(string input)
-        {
+        int PartTwo(string input) {
             var targetValue = int.Parse(input);
             var mem = new int[1000, 1000];
             Action<int, int, int> setMem = (x, y, t) => mem[x + 500, y + 500] = t;
@@ -29,36 +26,32 @@ namespace AdventOfCode2017.Day03
 
             setMem(0, 0, 1);
 
-            foreach (var (x, y) in Coordinates())
-            {
-                  var v = (
-                        from dx in new []{ -1, 0, 1 }
-                        from dy in new []{ -1, 0, 1 }
-                        select getMem(x + dx, y + dy)
-                    ).Sum();
+            foreach (var (x, y) in SpiralCoordinates()) {
+                var v = (
+                    from dx in new[] { -1, 0, 1 }
+                    from dy in new[] { -1, 0, 1 }
+                    select getMem(x + dx, y + dy)
+                ).Sum();
+
                 setMem(x, y, v);
 
-                if (v > targetValue)
-                {
+                if (v > targetValue) {
                     return v;
                 }
             }
             throw new Exception("never");
         }
 
-        IEnumerable<(int, int)> Coordinates()
-        {
+        IEnumerable<(int, int)> SpiralCoordinates() {
             var (edgeLength, nextEdgeLength) = (1, 1);
             var (x, y) = (0, 0);
             var (dx, dy) = (1, 0);
             var stepsOnEdge = 0;
 
-            while (true)
-            {
+            while (true) {
                 yield return (x, y);
 
-                if (stepsOnEdge == edgeLength)
-                {
+                if (stepsOnEdge == edgeLength) {
                     (dx, dy) = (-dy, dx);
                     (edgeLength, nextEdgeLength) = (nextEdgeLength, nextEdgeLength + (nextEdgeLength == edgeLength ? 1 : 0));
                     stepsOnEdge = 0;
