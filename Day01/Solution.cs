31e31018e5d2245865c3187bf6a4a483c60880de
David Nemeth Csoka <encse@csokavar.hu>
2017-12-03 15:55:03 +0100

    formatting

--- Day01/Solution.cs
+++ Day01/Solution.cs
@@ -2,31 +2,28 @@
 using System.IO;
 using System.Linq;
 
-namespace AdventOfCode2017.Day01
-{
-    class Solution : Solver
-    {
-        public void Solve(string input)
-        {
+namespace AdventOfCode2017.Day01 {
+
+    class Solution : Solver {
+
+        public void Solve(string input) {
             Console.WriteLine(PartOne(input));
             Console.WriteLine(PartTwo(input));
         }
 
-        int InverseCaptcha(string input, int skip)
-        {
-            var numbersToSum = from i in Enumerable.Range(0, input.Length)
-                               where input[i] == input[(i + skip) % input.Length]
-                               select int.Parse(input[i].ToString());
-            return numbersToSum.Sum();
+        int InverseCaptcha(string input, int skip) {
+            return (
+                from i in Enumerable.Range(0, input.Length)
+                where input[i] == input[(i + skip) % input.Length]
+                select int.Parse(input[i].ToString())
+            ).Sum();
         }
 
-        int PartOne(string input)
-        {
+        int PartOne(string input) {
             return InverseCaptcha(input, 1);
         }
 
-        int PartTwo(string input)
-        {
+        int PartTwo(string input) {
             return InverseCaptcha(input, input.Length / 2);
         }
     }
