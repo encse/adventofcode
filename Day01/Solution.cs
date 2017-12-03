9eec1ecc1b7299a8bc41235b60854c26387a54d6
David Nemeth Csoka <encse@csokavar.hu>
2017-12-03 16:46:48 +0100

    cleanup

--- Day01/Solution.cs
+++ Day01/Solution.cs
@@ -11,20 +11,20 @@ namespace AdventOfCode2017.Day01 {
             Console.WriteLine(PartTwo(input));
         }
 
-        int InverseCaptcha(string input, int skip) {
-            return (
-                from i in Enumerable.Range(0, input.Length)
-                where input[i] == input[(i + skip) % input.Length]
-                select int.Parse(input[i].ToString())
-            ).Sum();
-        }
-
         int PartOne(string input) {
             return InverseCaptcha(input, 1);
         }
 
         int PartTwo(string input) {
             return InverseCaptcha(input, input.Length / 2);
+        }
+
+        int InverseCaptcha(string input, int skip) {
+            return (
+                from i in Enumerable.Range(0, input.Length)
+                where input[i] == input[(i + skip) % input.Length]
+                select int.Parse(input[i].ToString())
+            ).Sum();
         }
     }
 }
--- Day03/Solution.cs
+++ Day03/Solution.cs
@@ -8,18 +8,33 @@ namespace AdventOfCode2017.Day03 {
     class Solution : Solver {
 
         public void Solve(string input) {
-            var targetValue = int.Parse(input);
-            Console.WriteLine(PartOne(targetValue));
-            Console.WriteLine(PartTwo(targetValue));
+            var num = int.Parse(input);
+            Console.WriteLine(PartOne(num));
+            Console.WriteLine(PartTwo(num));
         }
 
-        int PartOne(int input) {
-            var (x, y) = SpiralCoordinates().ElementAt(input - 1);
+        int PartOne(int num) {
+            var (x, y) = SpiralCoordinates().ElementAt(num - 1);
             return Math.Abs(x) + Math.Abs(y);
         }
 
-        int PartTwo(int input) {
-            return SpiralSums().First(v => v > input);
+        int PartTwo(int num) {
+            return SpiralSums().First(v => v > num);
+        }
+
+        IEnumerable<(int, int)> SpiralCoordinates() {
+            var (x, y) = (0, 0);
+            var (dx, dy) = (1, 0);
+
+            for (var edgeLength = 1; ; edgeLength++) {
+                for (var run = 0; run < 2; run++) {
+                    for (var step = 0; step < edgeLength; step++) {
+                        yield return (x, y);
+                        (x, y) = (x + dx, y - dy);
+                    }
+                    (dx, dy) = (-dy, dx);
+                }
+            }
         }
 
         IEnumerable<int> SpiralSums() {
@@ -37,21 +52,6 @@ namespace AdventOfCode2017.Day03 {
 
                 mem[(x, y)] = v;
                 yield return v;
-            }
-        }
-
-        IEnumerable<(int, int)> SpiralCoordinates() {
-            var (x, y) = (0, 0);
-            var (dx, dy) = (1, 0);
-
-            for (var edgeLength = 1; ; edgeLength++) {
-                for (var run = 0; run < 2; run++) {
-                    for (var step = 0; step < edgeLength; step++) {
-                        yield return (x, y);
-                        (x, y) = (x + dx, y - dy);
-                    }
-                    (dx, dy) = (-dy, dx);
-                }
             }
         }
     }
