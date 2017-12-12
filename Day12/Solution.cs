d0c000c50eb6ce1f88da7d9dbdbdbcdad2dcc844
David Nemeth Csoka <encse@csokavar.hu>
2017-12-12 09:18:51 +0100

    12

--- Day12/Solution.cs
+++ Day12/Solution.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace AdventOfCode2017.Day00 {
+namespace AdventOfCode2017.Day12 {
+
+    class Node {
+        public string Id;
+        public List<string> Neighbours;
+    }
 
     class Solution : Solver {
 
-        public string GetName() { 
-            return "???"; 
+        public string GetName() {
+            return "Digital Plumber";
         }
 
         public void Solve(string input) {
@@ -16,12 +22,50 @@ namespace AdventOfCode2017.Day00 {
             Console.WriteLine(PartTwo(input));
         }
 
-        int PartOne(string input) {
-            return 0;
+        int PartOne(string input) => GetPartitions(input).Where(x => x.Contains("0")).Single().Count();
+        int PartTwo(string input) => GetPartitions(input).Count();
+
+        IEnumerable<HashSet<string>> GetPartitions(string input) {
+            var nodes = Parse(input);
+            var parent = new Dictionary<string, string>();
+
+            string getRoot(string id) {
+                var root = id;
+                while (parent.ContainsKey(root)) {
+                    root = parent[root];
+                }
+                return root;
+            }
+
+            foreach (var nodeA in nodes) {
+                var rootA = getRoot(nodeA.Id);
+                foreach (var nodeB in nodeA.Neighbours) {
+                    var rootB = getRoot(nodeB);
+                    if (rootB != rootA) {
+                        parent[rootB] = rootA;
+                    }
+                }
+            }
+
+            return
+                from node in nodes
+                let root = getRoot(node.Id)
+                group node.Id by root into partitions
+                select new HashSet<string>(partitions.ToArray());
+            
         }
 
-        string PartTwo(string input) {
-            return "";
+        List<Node> Parse(string input) {
+            return (
+                from line in input.Split('\n')
+                let parts = Regex.Split(line, " <-> ")
+                select
+                    new Node() {
+                        Id = parts[0],
+                        Neighbours = new List<string>(Regex.Split(parts[1], ", "))
+                    }
+            ).ToList();
         }
+
     }
 }
