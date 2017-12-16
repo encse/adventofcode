0d700676e66a1b681ee406deed56f72100070a25
David Nemeth Csoka <encse@csokavar.hu>
2017-12-16 10:00:48 +0100

    cleanip

--- Day16/Solution.cs
+++ Day16/Solution.cs
@@ -9,8 +9,6 @@ namespace AdventOfCode2017.Day16 {
 
     class Solution : Solver {
 
-        const string startState = "abcdefghijklmnop";
-
         public string GetName() => "???";
 
         public void Solve(string input) {
@@ -18,72 +16,78 @@ namespace AdventOfCode2017.Day16 {
             Console.WriteLine(PartTwo(input));
         }
 
-        string PartOne(string input) {
-            return Step(input, startState);
-        }
+        string PartOne(string input) => Parse(input)("abcdefghijklmnop");
 
         string PartTwo(string input) {
-            var mod = Mod(input);
+            var startState = "abcdefghijklmnop";
+            var step = Parse(input);
+            var mod = Mod(step, startState);
 
             var state = startState;
             for (int i = 0; i < 1000000000 % mod; i++) {
-                state = Step(input, state);
+                state = step(state);
             }
             return state;
         }
 
-        string Step(string input, string orderT) {
-            var order = orderT.ToList();
-            var spin = Move("s([0-9]+)", m => {
-                    int n = int.Parse(m[0]);
-                    order = order.Skip(order.Count - n).Concat(order.Take(order.Count - n)).ToList();
-                });
-
-            var exchange = Move("x([0-9]+)/([0-9]+)", m => {
-                    int idx1 = int.Parse(m[0]);
-                    int idx2 = int.Parse(m[1]);
-                    (order[idx1], order[idx2]) = (order[idx2], order[idx1]);
-                });
-
-            var partner = Move("p([a-z])/([a-z])", m => {
-                    var (c1, c2) = (m[0].Single(), m[1].Single());
-                    var (idx1, idx2) = (order.IndexOf(c1), order.IndexOf(c2));
-                    order[idx1] = c2;
-                    order[idx2] = c1;
-                });
-
-            foreach (var stm in input.Split(',')) {
-                spin(stm);
-                exchange(stm);
-                partner(stm);
-            }
-            
-            return string.Join("", order);
-        }
-
-        int Mod(string input) {
+        int Mod(Func<string, string> step, string startState) {
             var seen = new HashSet<string>();
             var state = startState;
             seen.Add(state);
             for (int i = 0; i < 1000000000; i++) {
-                state = Step(input, state);
+                state = step(state);
                 if (seen.Contains(state)) {
                     return i + 1;
                 }
             }
             return 1000000000;
         }
-        Func<string, bool> Move(string pattern, Action<string[]> a) {
-            var rx = new Regex(pattern);
-            return (string stm) => {
-                var match = rx.Match(stm);
-                if (match.Success) {
-                    a(match.Groups.Cast<Group>().Skip(1).Select(g => g.Value).ToArray());
-                    return true;
-                } else {
-                    return false;
+
+        Func<string, string> Parse(string input) {
+            var moves =
+                from stm in input.Split(',')
+                select
+                    Move(stm, "s([0-9]+)", m => {
+                        int n = int.Parse(m[0]);
+                        return (order) => {
+                            return order.Skip(order.Count - n).Concat(order.Take(order.Count - n)).ToList();
+                        };
+                    }) ??
+                    Move(stm, "x([0-9]+)/([0-9]+)", m => {
+                        int idx1 = int.Parse(m[0]);
+                        int idx2 = int.Parse(m[1]);
+                        return (order) => {
+                            (order[idx1], order[idx2]) = (order[idx2], order[idx1]);
+                            return order;
+                        };
+                    }) ??
+                    Move(stm, "p([a-z])/([a-z])", m => {
+                        var (c1, c2) = (m[0].Single(), m[1].Single());
+                        return order => {
+                            var (idx1, idx2) = (order.IndexOf(c1), order.IndexOf(c2));
+                            order[idx1] = c2;
+                            order[idx2] = c1;
+                            return order;
+                        };
+                    }) ??
+                    throw new Exception("Cannot parse " + stm);
+
+            return startOrder => {
+                var order = startOrder.ToList();
+                foreach (var move in moves) {
+                    order = move(order);
                 }
+                return string.Join("", order);
             };
+        }
+
+         Func<List<char>, List<char>> Move(string stm, string pattern, Func<string[], Func<List<char>, List<char>>> a) {
+            var match = Regex.Match(stm , pattern);
+            if (match.Success) {
+                return a(match.Groups.Cast<Group>().Skip(1).Select(g => g.Value).ToArray());
+            } else {
+                return null;
+            }
         }
     }
 }
