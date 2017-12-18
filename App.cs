8086df443bb2ace3da8a6ef96fa4f085bd7b6fba
David Nemeth Csoka <encse@csokavar.hu>
2017-12-18 13:37:13 +0100

    cleanup

--- App.cs
+++ App.cs
@@ -3,51 +3,64 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2017 {
-    interface Solver {
-        string GetName();
-        IEnumerable<object> Solve(string input);
-    }
 
-    class Runner {
+    class App {
+        
         static void Main(string[] args) {
             SplashScreen();
-
-            if (args.Length == 2 && args[0] == "update" && int.TryParse(args[1], out int _)) {
-                Updater.Update(int.Parse(args[1])).Wait();
-                return;
-            } else if (args.Length == 1 || args.Length == 0) {
-
-                Type tSolver = null;
+            
+            Type tSolver = null;
                 var tSolvers = Assembly.GetEntryAssembly().GetTypes()
                     .Where(t => t.GetTypeInfo().IsClass && typeof(Solver).IsAssignableFrom(t))
                     .OrderBy(t => t.FullName);
 
-                if (args.Length == 1) {
-                    int day;
-                    if (int.TryParse(args[0], out day)) {
-                        tSolver = tSolvers.Where(x => x.FullName.Contains($"Day{day.ToString("00")}")).FirstOrDefault();
-                    }
-                } else {
+            var action =
+                Command(args, Args("update", "[0-9]+"), m => {
+                    var day = int.Parse(m[1]);
+                    return () => Updater.Update(day).Wait();
+                }) ??
+                Command(args, Args("[0-9]+"), m => {
+                    var day = int.Parse(m[0]);
+                    tSolver = tSolvers.Where(x => x.FullName.Contains($"Day{day.ToString("00")}")).First();
+                    return () => Runner.RunSolver(Activator.CreateInstance(tSolver) as Solver);
+                }) ??
+                Command(args, Args(), m => {
                     tSolver = tSolvers.Last();
-                }
+                    return () => Runner.RunSolver(Activator.CreateInstance(tSolver) as Solver);
+                }) ??
+                new Action(() => {
+                    Console.WriteLine("USAGE: dotnet [command]");
+                    Console.WriteLine();
+                    Console.WriteLine("Commands:");
+                    Console.WriteLine($"  run update [day]  Prepares a folder for the given day, updates the input, the readme and creates a solution template.");
+                    Console.WriteLine($"  run [day]         Solve the problem of the day");
+                    Console.WriteLine($"  run               Solve the problem of the last day");
+                });
 
-                if (tSolver != null) {
-                    RunSolver(Activator.CreateInstance(tSolver) as Solver);
-                    return;
-                }
-            } 
-
-            Console.WriteLine("USAGE: dotnet [command]");
-            Console.WriteLine();
-            Console.WriteLine("Commands:");
-            Console.WriteLine($"  run update [day]  Prepares a folder for the given day, updates the input, the readme and creates a solution template.");
-            Console.WriteLine($"  run [day]         Solve the problem of the day");
-            Console.WriteLine($"  run               Solve the problem of the last day");
-
+            action();
         }
 
+        static Action Command(string[] args, string[] regexes, Func<string[], Action> parse) {
+            if (args.Length != regexes.Length) {
+                return null;
+            }
+            var matches = Enumerable.Zip(args, regexes, (arg, regex) => new Regex("^" + regex + "$").Match(arg));
+            if (!matches.All(match => match.Success)) {
+                return null;
+            }
+            try {
+                return parse(matches.Select(m => m.Value).ToArray());
+            } catch {
+                return null;
+            }
+        }
+
+        static string[] Args(params string[] regex){
+            return regex;
+        }
         static void SplashScreen(){
              Console.WriteLine(
                 string.Join("\n", @"
@@ -83,25 +96,6 @@ namespace AdventOfCode2017 {
                    /_/ \_\__,_|\_/\___|_||_\__| \___/_|    \___\___/\__,_\___| /___\__/|_|/_/  
                    "
                 .Split('\n').Skip(1).Select(x => x.Substring(19))));
-        }
-        static void RunSolver(Solver solver) {
-            var name = solver.GetType().FullName.Split('.')[1];
-            Console.WriteLine($"Day {Day(solver.GetType())}: {solver.GetName()}");
-            Console.WriteLine();
-            foreach (var file in Directory.EnumerateFiles(name)) {
-                if (file.EndsWith(".in")) {
-                    var dt = DateTime.Now;
-                    foreach (var line in solver.Solve(File.ReadAllText(file))) {
-                        var now = DateTime.Now;
-                        Console.WriteLine($"{line} ({(now - dt).TotalMilliseconds}ms)");
-                        dt = now;
-                    }
-                }
-            }
-        }
-
-        static int Day(Type type) {
-            return int.Parse(type.FullName.Split('.')[1].Substring(3));
         }
     }
 }
