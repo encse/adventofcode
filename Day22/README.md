54f08b2ad0d90c1c3002b4030df7580fa12e21f4
David Nemeth Csoka <encse@csokavar.hu>
2017-12-22 08:37:45 +0100

    22

--- Day22/README.md
+++ Day22/README.md
@@ -111,3 +111,115 @@ After a total of `10000` bursts of activity, `5587` bursts will have caused an i
 Given your actual map, after `10000` bursts of activity, *how many bursts cause a node to become infected*? (Do not count nodes that begin infected.)
 
 
+## --- Part Two ---
+As you go to remove the virus from the infected nodes, it *evolves* to resist your attempt.
+
+Now, before it infects a clean node, it will *weaken* it to disable your defenses. If it encounters an infected node, it will instead *flag* the node to be cleaned in the future.  So:
+
+
+ - *Clean* nodes become *weakened*.
+ - *Weakened* nodes become *infected*.
+ - *Infected* nodes become *flagged*.
+ - *Flagged* nodes become *clean*.
+
+Every node is always in exactly one of the above states.
+
+The virus carrier still functions in a similar way, but now uses the following logic during its bursts of action:
+
+
+ - Decide which way to turn based on the *current node*:
+  
+   - If it is *clean*, it turns *left*.
+   - If it is *weakened*, it does *not* turn, and will continue moving in the same direction.
+   - If it is *infected*, it turns *right*.
+   - If it is *flagged*, it *reverses* direction, and will go back the way it came.
+  
+
+ - Modify the state of the *current node*, as described above.
+ - The virus carrier moves *forward* one node in the direction it is facing.
+
+Start with the same map (still using `.` for *clean* and `#` for infected) and still with the virus carrier starting in the middle and facing *up*.
+
+Using the same initial state as the previous example, and drawing *weakened* as `W` and *flagged* as `F`, the middle of the infinite grid looks like this, with the virus carrier's position again marked with `[ ]`:
+
+```
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . . . . # . . .
+. . . #[.]. . . .
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+```
+
+This is the same as before, since no initial nodes are *weakened* or *flagged*.  The virus carrier is on a clean node, so it still turns left, instead *weakens* the node, and moves left:
+
+```
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . . . . # . . .
+. . .[#]W . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+```
+
+The virus carrier is on an infected node, so it still turns right, instead *flags* the node, and moves up:
+
+```
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . .[.]. # . . .
+. . . F W . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+```
+
+This process repeats three more times, ending on the previously-flagged node and facing right:
+
+```
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . W W . # . . .
+. . W[F]W . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+```
+
+Finding a flagged node, it reverses direction and *cleans* the node:
+
+```
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . W W . # . . .
+. .[W]. W . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+```
+
+The *weakened* node becomes infected, and it continues in the same direction:
+
+```
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . W W . # . . .
+.[.]# . W . . . .
+. . . . . . . . .
+. . . . . . . . .
+. . . . . . . . .
+```
+
+Of the first `100` bursts, `26` will result in *infection*. Unfortunately, another feature of this evolved virus is *speed*; of the first `10000000` bursts, `2511944` will result in *infection*.
+
+Given your actual map, after `10000000` bursts of activity, *how many bursts cause a node to become infected*? (Do not count nodes that begin infected.)
+
+
--- Day22/Solution.cs
+++ Day22/Solution.cs
@@ -7,14 +7,15 @@ using System.Text;
 
 namespace AdventOfCode.Day22 {
 
+    enum State {
+        Clean,
+        Weakened,
+        Infected,
+        Flagged
+    }
+
     class Solution : Solver {
 
-        enum State {
-            Clean,
-            Weakened,
-            Infected,
-            Flagged
-        }
         public string GetName() => "Sporifica Virus";
 
         public IEnumerable<object> Solve(string input) {
@@ -22,73 +23,63 @@ namespace AdventOfCode.Day22 {
             yield return PartTwo(input);
         }
 
-        int PartOne(string input) {
-            var lines = input.Split('\n').Where(line => !string.IsNullOrWhiteSpace(line)).ToArray();
-            var crow = lines.Length;
-            var ccol = lines[0].Length;
-            var infectedCells = new HashSet<(int irow, int icol)>();
-            for (int irowT = 0; irowT < crow; irowT++) {
-                for (int icolT = 0; icolT < ccol; icolT++) {
-                    if (lines[irowT][icolT] == '#') {
-                        infectedCells.Add((irowT, icolT));
-                    }
-                }
-            }
-            var (irow, icol) = ((crow) / 2, (ccol) / 2);
-            var (drow, dcol) = (-1, 0);
-            var infections = 0;
-            for (int i = 0; i < 10000; i++) {
-                if (infectedCells.Contains((irow, icol))) {
-                    infectedCells.Remove((irow, icol));
-                    (drow, dcol) = (dcol, -drow);
-                } else {
-                    infectedCells.Add((irow, icol));
-                    infections++;
-                    (drow, dcol) = (-dcol, drow);
-                }
-                (irow, icol) = (irow + drow, icol + dcol);
-            }
-            return infections;
-        }
-
-        int PartTwo(string input) {
-            var lines = input.Split('\n').Where(line => !string.IsNullOrWhiteSpace(line)).ToArray();
-            var crow = lines.Length;
-            var ccol = lines[0].Length;
-            var infectedCells = new Dictionary<(int irow, int icol), State>();
-            for (int irowT = 0; irowT < crow; irowT++) {
-                for (int icolT = 0; icolT < ccol; icolT++) {
-                    if (lines[irowT][icolT] == '#') {
-                        infectedCells.Add((irowT, icolT), State.Infected);
-                    }
-                }
-            }
-            var (irow, icol) = ((crow) / 2, (ccol) / 2);
-            var (drow, dcol) = (-1, 0);
-            var infections = 0;
-            for (int i = 0; i < 10000000; i++) {
-                var state = infectedCells.ContainsKey((irow, icol)) ? infectedCells[(irow, icol)] : State.Clean;
-                State newState = state;
-                switch(state){
+        int PartOne(string input) =>
+            Iterate(input, 10000, (state, drow, dcol) => {
+                switch (state) {
                     case State.Clean:
-                        newState = State.Weakened;
-                        (drow, dcol) = (-dcol, drow);
-                        break;
-                    case State.Weakened:
-                        newState = State.Infected;
-                        infections++;
-                        break;
-                     case State.Infected:
-                        newState = State.Flagged;
-                        (drow, dcol) = (dcol, -drow);
-                        break;
-                    case State.Flagged:
-                        newState = State.Clean;
-                        (drow, dcol) = (-drow, -dcol);
-                        break;
-                   
+                        return (State.Infected, -dcol, drow);
+                    case State.Infected:
+                        return (State.Clean, dcol, -drow);
+                    default:
+                        throw new Exception();
                 }
-                infectedCells[(irow, icol)] = newState;
+            });
+
+        int PartTwo(string input) =>
+            Iterate(input, 10000000, (state, drow, dcol) => {
+                switch (state) {
+                    case State.Clean:
+                        return (State.Weakened, -dcol, drow);
+                    case State.Weakened:
+                        return (State.Infected, drow, dcol);
+                    case State.Infected:
+                        return (State.Flagged, dcol, -drow);
+                    case State.Flagged:
+                        return (State.Clean, -drow, -dcol);
+                    default:
+                        throw new Exception();
+                }
+            });
+
+
+        int Iterate(string input, int iterations, Func<State, int, int, (State State, int irow, int icol)> update) {
+            var lines = input.Split('\n').Where(line => !string.IsNullOrWhiteSpace(line)).ToArray();
+            var crow = lines.Length;
+            var ccol = lines[0].Length;
+            var cells = new Dictionary<(int irow, int icol), State>();
+            for (int irowT = 0; irowT < crow; irowT++) {
+                for (int icolT = 0; icolT < ccol; icolT++) {
+                    if (lines[irowT][icolT] == '#') {
+                        cells.Add((irowT, icolT), State.Infected);
+                    }
+                }
+            }
+            var (irow, icol) = ((crow) / 2, (ccol) / 2);
+            var (drow, dcol) = (-1, 0);
+            var infections = 0;
+            for (int i = 0; i < iterations; i++) {
+                var state = cells.TryGetValue((irow, icol), out var s) ? s : State.Clean;
+                
+                (state, drow, dcol) = update(state, drow, dcol);
+                
+                if (state == State.Infected) {
+                    infections++;
+                }
+                if (state == State.Clean) {
+                    cells.Remove((irow, icol));
+                } else {
+                    cells[(irow, icol)] = state;
+                }
                 (irow, icol) = (irow + drow, icol + dcol);
             }
             return infections;
