2e4d804510142243c8f34b422e7ba1cd129b438c
David Nemeth Csoka <encse@csokavar.hu>
2017-12-27 08:37:41 +0100

    25 with parser

--- Day25/Solution.cs
+++ Day25/Solution.cs
@@ -8,8 +8,6 @@ using System.Text;
 namespace AdventOfCode.Day25 {
 
     class Solution : Solver {
-        enum State { A, B, C, D, E, F }
-
         public string GetName() => "The Halting Problem";
 
         public IEnumerable<object> Solve(string input) {
@@ -17,141 +15,68 @@ namespace AdventOfCode.Day25 {
         }
 
         int PartOne(string input) {
-            Dictionary<int, int> tape = new Dictionary<int, int>();
-
-            int getV(int p) {
-                if (tape.ContainsKey(p)) {
-                    return tape[p];
-                }
-                return 0;
-            }
-            void setV(int p, int value) {
-                tape[p] = value;
-            }
-            var state = State.A;
+            var machine = Parse(input);
+            var tape = new Dictionary<int, int>();
             var pos = 0;
-            for (int i = 0; i < 12173597; i++) {
-                switch (state) {
-                    case State.A:
-                        if (getV(pos) == 0) {
-                            setV(pos, 1);
-                            pos++;
-                            state = State.B;
-                        } else {
-                            setV(pos, 0);
-                            pos--;
-                            state = State.C;
-                        }
-                        break;
-                    case State.B:
-                        if (getV(pos) == 0) {
-                            setV(pos, 1);
-                            pos--;
-                            state = State.A;
-                        } else {
-                            setV(pos, 1);
-                            pos++;
-                            state = State.D;
-                        }
-                        break;
-                    case State.C:
-                        if (getV(pos) == 0) {
-                            setV(pos, 1);
-                            pos++;
-                            state = State.A;
-                        } else {
-                            setV(pos, 0);
-                            pos--;
-                            state = State.E;
-                        }
-                        break;
-                    case State.D:
-                        if (getV(pos) == 0) {
-                            setV(pos, 1);
-                            pos++;
-                            state = State.A;
-                        } else {
-                            setV(pos, 0);
-                            pos++;
-                            state = State.B;
-                        }
-                        break;
-                    case State.E:
-                        if (getV(pos) == 0) {
-                            setV(pos, 1);
-                            pos--;
-                            state = State.F;
-                        } else {
-                            setV(pos, 1);
-                            pos--;
-                            state = State.C;
-                        }
-                        break;
-                    case State.F:
-                        if (getV(pos) == 0) {
-                            setV(pos, 1);
-                            pos++;
-                            state = State.D;
-                        } else {
-                            setV(pos, 1);
-                            pos++;
-                            state = State.A;
-                        }
-                        break;
-                }
+            while (machine.iterations > 0) {
+                var read = tape.TryGetValue(pos, out var t) ? t : 0;
+                var (write, dir, newState) = machine.prg[(machine.state, read)];
+                machine.state = newState;
+                tape[pos] = write;
+                pos += dir;
+                machine.iterations--;
             }
             return tape.Select(kvp => kvp.Value).Sum();
         }
+
+        Machine Parse(string input) {
+            var lines = input.Split('\n').Where(line => !string.IsNullOrEmpty(line)).ToArray();
+            int iline = 0;
+
+            Machine machine = new Machine();
+
+            String(@"Begin in state (\w).", out machine.state);
+            Int(@"Perform a diagnostic checksum after (\d+) steps.", out machine.iterations);
+
+            while (String(@"In state (\w):", out var state)) {
+                while (Int(@"If the current value is (\d):", out var read)) {
+                    Int(@"- Write the value (\d).", out var write);
+                    String(@"- Move one slot to the (left|right).", out var dir);
+                    String(@" - Continue with state (\w).", out string newState);
+                    machine.prg[(state, read)] = (write, dir == "left" ? -1 : 1, newState);
+                }
+            }
+
+            bool Int(string pattern, out int r) {
+                r = 0;
+                if (String(pattern, out string st)) {
+                    r = int.Parse(st);
+                    return true;
+                }
+                return false;
+            }
+
+            bool String(string pattern, out string st) {
+                st = null;
+                if (iline >= lines.Length) {
+                    return false;
+                }
+                var m = Regex.Match(lines[iline], pattern);
+                if (m.Success) {
+                    iline++;
+                    st = m.Groups[1].Value;
+                }
+                return m.Success;
+            }
+
+            return machine;
+        }
     }
 
-    // class Parser {
-    //     Machine Parse(string input) {
-    //          var lines = input.Split('\n').Where(line => !string.IsNullOrEmpty(line));
-
-    //         Machine problem = new Machine();
-
-    //         string state;
-    //         string newState;
-    //         string dir;
-    //         int read;
-    //         int write;
-
-    //         Literal(@"Begin in state (\w).", out problem.state).
-    //         Literal(@"Perform a diagnostic checksum after (\d+) steps.", out problem.iterations).
-    //         OneOrMore(
-    //             Literal(@"In state (\w):", out state).
-    //             OneOrMore(
-    //                 Literal(@"If the current value is (\d):", out read).
-    //                 Literal(@"- Write the value (\d).", out write).
-    //                 Literal(@"- Move one slot to the (left|right).", out dir).
-    //                 Literal(@" - Continue with state (\w).", out newState).
-    //                 Then(() => problem.prg[(state, read)] = (write, dir == "left" ? -1 : 1, newState))
-    //             ));
-
-    //         return problem;
-    //     }
-
-    //     private Parser OneOrMore(Parser p) {
-    //         return this;
-    //     }
-    //     private Parser Literal(string q, out string st) {
-    //         st = null;
-    //         return this;
-    //     }
-    //     private Parser Literal(string q, out int st) {
-    //         st = 0;
-    //         return this;
-    //     }
-    //     private Parser Then(Action cb) {
-    //         cb();
-    //         return this;
-    //     }
-    // }
-
-    // class Machine {
-    //     public string state;
-    //     public int iterations;
-    //     public Dictionary<(string, int), (int, int, string)> prg =
-    //         new Dictionary<(string state, int r), (int w, int dir, string state)>();
-    // }
+    class Machine {
+        public string state;
+        public int iterations;
+        public Dictionary<(string state, int read), (int write, int dir, string state)> prg =
+            new Dictionary<(string, int), (int, int, string)>();
+    }
 }
