ab5c0e1be99b68bbe31068aa07d1e7f4caf5a44a
David Nemeth Csoka <encse@csokavar.hu>
2017-12-28 16:00:56 +0100

    7,8

---
+++ 2016/Day07/Solution.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2016.Day07 {
+
+    class Solution : Solver {
+
+        public string GetName() => "Internet Protocol Version 7";
+
+        public IEnumerable<object> Solve(string input) {
+            yield return PartOne(input);
+            yield return PartTwo(input);
+        }
+
+        int PartOne(string input) =>
+            input.TrimEnd().Split('\n').Count(TLS);
+
+        int PartTwo(string input) =>
+            input.TrimEnd().Split('\n').Count(SSL);
+
+
+        bool TLS(string st) =>
+            Classify(st).Any(c => !c.f && Abba(c.st).Any()) &&
+            Classify(st).All(c => !c.f || !Abba(c.st).Any());
+
+        bool SSL(string st) => (
+            from c1 in Classify(st)
+            from c2 in Classify(st)
+            where !c1.f && c2.f
+            from aba in Aba(c1.st)
+            let bab = $"{aba[1]}{aba[0]}{aba[1]}"
+            where c2.st.Contains(bab)
+            select true
+        ).Any();
+
+        IEnumerable<(string st, bool f)> Classify(string st) {
+            var part = "";
+            for (var i = 0; i < st.Length; i++) {
+                var ch = st[i];
+                if (ch == '[') {
+                    yield return (part, false);
+                    part = "";
+                } else if (ch == ']') {
+                    yield return (part, true);
+                    part = "";
+                } else {
+                    part += ch;
+                }
+            }
+            if (part != "")
+                yield return (part, false);
+        }
+
+        IEnumerable<string> Abba(string st) {
+            for (var i = 0; i < st.Length - 3; i++) {
+                if(st[i + 2] == st[i + 1] && st[i] == st[i + 3] && st[i] != st[i + 2])
+                    yield return st.Substring(i, 4);
+            }
+        }
+        
+        IEnumerable<string> Aba(string st) {
+            for (var i = 0; i < st.Length -2; i++) {
+                if(st[i] == st[i + 2] && st[i] != st[i + 1])
+                    yield return st.Substring(i, 3);
+            }
+        }
+    }
+}
