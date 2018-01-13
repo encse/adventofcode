a300c39282829d184a36d744229258545f4f1cc7
David Nemeth Csoka <encse@csokavar.hu>
2018-01-13 14:55:52 +0100

    11

---
+++ 2015/Day11/Solution.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2015.Day11 {
+
+    class Solution : Solver {
+
+        public string GetName() => "Corporate Policy";
+
+        public IEnumerable<object> Solve(string input) {
+            yield return PartOne(input);
+            yield return PartTwo(input);
+        }
+
+        string PartOne(string input) => Passwords(input).First();
+        string PartTwo(string input) => Passwords(input).Skip(1).First();
+
+        IEnumerable<string> Passwords(string pwd) =>
+            from word in Words(pwd) 
+            let straigth = Enumerable.Range(0, word.Length - 2).Any(i => word[i] == word[i + 1] - 1 && word[i] == word[i + 2] - 2)
+            let reserved = "iol".Any(ch => word.Contains(ch))
+            let pairs = Enumerable.Range(0, word.Length - 1).Select(i => word.Substring(i, 2)).Where(sword => sword[0] == sword[1]).Distinct()
+            where straigth && !reserved && pairs.Count() > 1
+            select word;
+        
+        IEnumerable<string> Words(string word) {
+            while (true) {
+                var sb = new StringBuilder();
+                for (var i = word.Length - 1; i >= 0; i--) {
+                    var ch = word[i] + 1;
+                    if (ch > 'z') {
+                        ch = 'a';
+                        sb.Insert(0, (char)ch);
+                    } else {
+                        sb.Insert(0, (char)ch);
+                        sb.Insert(0, word.Substring(0, i));
+                        i = 0;
+                    }
+                }
+                word = sb.ToString();
+                yield return word;
+            }
+        }
+    }
+}
---
+++ 2015/Day11/input.refout
@@ -0,0 +1,2 @@
+cqjxxyzz
+cqkaabcc
