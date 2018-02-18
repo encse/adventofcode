310fc736055f4ec09f405cd91d304513c7eb63f2
David Nemeth Csoka <encse@csokavar.hu>
2018-02-18 13:18:23 +0100

    readme

--- Lib/Generator/ProjectReadmeGenerator.cs
+++ Lib/Generator/ProjectReadmeGenerator.cs
@@ -4,7 +4,7 @@ using AdventOfCode.Model;
 
 namespace AdventOfCode.Generator {
     
-    public class ProjectReadmeGenerator {
+    public class ReadmeGeneratorForYear {
         public string Generate(Calendar calendar) {
             var calendarLines =
                 string.Join("\n",
@@ -13,26 +13,12 @@ namespace AdventOfCode.Generator {
                     
             return $@"
                > # Advent of Code
-               > C# solutions to the advent of code problems (2015-2017).
-               > Check out http://adventofcode.com.
-               > 
-               > ## Dependencies
-               > 
-               > - This library is based on `.NET Core 2.0`. It should work on Windows, Linux and OS X.
-               > - `Newtonsoft.Json` for JSON parsing
-               > - `HtmlAgilityPack.NetCore` is used for problem download.
-               > 
-               > ## Running
-               > 
-               > To run the project:
-               > 
-               > 1. Install .NET Core.
-               > 2. Download the code.
-               > 3. Run `dotnet run <year>/<day>`.
-               > 
-               > To prepare for the next day:
-               > 
-               > 1. Run `dotnet run update <year>/<day>`.".StripMargin("> ");
+               > C# solutions to the advent of code problems ({calendar.Year}).
+               > Check out http://adventofcode.com/{calendar.Year}.
+               > ```
+               > {calendarLines}
+               > ```
+               > ".StripMargin("> ");
         }
     }
 }
