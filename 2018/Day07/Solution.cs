8577fe1df4753c557626651038a97410571ec35e
David Nemeth Csoka <encse@csokavar.hu>
2018-12-08 07:24:50 +0100

    7,8

---
+++ 2018/Day07/Solution.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2018.Day07 {
+
+    class Solution : Solver {
+
+        public string GetName() => "The Sum of Its Parts";
+
+        public IEnumerable<object> Solve(string input) {
+            yield return PartOne(input);
+            yield return PartTwo(input);
+        }
+
+        string PartOne(string input) {
+
+            var sb = new StringBuilder();
+            var graph = Parse(input);
+            while (graph.Any()) {
+                char minKey = char.MaxValue;
+                foreach (var key in graph.Keys) {
+                    if (graph[key].Count == 0) {
+                        if (key < minKey) {
+                            minKey = key;
+                        }
+                    }
+                }
+                sb.Append(minKey);
+                graph.Remove(minKey);
+                foreach (var key in graph.Keys) {
+                    graph[key].Remove(minKey);
+                }
+            }
+            return sb.ToString();
+        }
+
+        int PartTwo(string input) {
+            var time = 0;
+            var graph = Parse(input);
+
+            var works = new int[5];
+            var items = new char[works.Length];
+
+            while (graph.Any() || works.Any(work => work > 0)) {
+                for (var i = 0; i < works.Length && graph.Any(); i++) {
+                    // start working
+                    if (works[i] == 0) {
+                        char minKey = char.MaxValue;
+                        foreach (var key in graph.Keys) {
+                            if (graph[key].Count == 0) {
+                                if (key < minKey) {
+                                    minKey = key;
+                                }
+                            }
+                        }
+                        if (minKey != char.MaxValue) {
+                            works[i] = 60 + minKey - 'A' + 1;
+                            items[i] = minKey;
+                            graph.Remove(items[i]);
+                        }
+                    }
+                }
+
+                time++;
+
+                for (var i = 0; i < works.Length; i++) {
+                    if (works[i] == 0) {
+                        // wait
+                        continue;
+                    } else if (works[i] == 1) {
+                        // finish
+                        works[i]--;
+                        foreach (var key in graph.Keys) {
+                            graph[key].Remove(items[i]);
+                        }
+
+                    } else if (works[i] > 0) {
+                        // step
+                        works[i]--;
+                    }
+                }
+            }
+
+            return time;
+        }
+
+        Dictionary<char, List<char>> Parse(string input) {
+            var dict = (
+                from line in input.Split("\n")
+                let parts = line.Split(" ")
+                let part = parts[7][0]
+                let partDependsOn = parts[1][0]
+                group partDependsOn by part into g
+                select g
+            ).ToDictionary(g => g.Key, g => g.ToList());
+
+            foreach (var key in new List<char>(dict.Keys)) {
+                foreach (var d in dict[key]) {
+                    if (!dict.ContainsKey(d)) {
+                        dict.Add(d, new List<char>());
+                    }
+                }
+            }
+            return dict;
+        }
+    }
+}
---
+++ 2018/Day08/Solution.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2018.Day08 {
+
+    class Solution : Solver {
+
+        public string GetName() => "Memory Maneuver";
+
+        public IEnumerable<object> Solve(string input) {
+            yield return PartOne(input);
+            yield return PartTwo(input);
+        }
+
+        int PartOne(string input) =>
+            Parse(input).fold(0, (cur, node) => cur + node.metadata.Sum());
+        
+
+        int PartTwo(string input) {
+            return Parse(input).value();
+        }
+
+        Node Parse(string input) {
+            var nums = input.Split(" ").Select(int.Parse).GetEnumerator();
+            Func<int> next = () => {
+                nums.MoveNext();
+                return nums.Current;
+            };
+
+            Func<Node> read = null;
+            read = () => {
+                var node = new Node() {
+                    children = new Node[next()],
+                    metadata = new int[next()]
+                };
+                for (var i = 0; i < node.children.Length; i++) {
+                    node.children[i] = read();
+                }
+                for (var i = 0; i < node.metadata.Length; i++) {
+                    node.metadata[i] = next();
+                }
+                return node;
+            };
+            return read();
+        }
+
+
+    }
+
+    class Node {
+        public Node[] children;
+        public int[] metadata;
+        public T fold<T>(T seed, Func<T, Node, T> aggregate) {
+            return children.Aggregate(aggregate(seed, this), (cur, child) => child.fold(cur, aggregate));
+        }
+
+        public int value() {
+            if(children.Length == 0){
+                return metadata.Sum();
+            }
+
+            var res = 0;
+            foreach(var i in metadata){
+                if(i >= 1 && i <= children.Length){
+                    res += children[i-1].value();
+                }
+            }
+            return res;
+        }
+    }
+}
