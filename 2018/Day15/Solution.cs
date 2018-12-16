ebd317ebc097fa997126ea759a56a241d4c3f47d
David Nemeth Csoka <encse@csokavar.hu>
2018-12-16 08:54:22 +0100

    16

--- 2018/Day15/Solution.cs
+++ 2018/Day15/Solution.cs
@@ -22,59 +22,125 @@ namespace AdventOfCode.Y2018.Day15 {
 
         int PartTwo(string input) {
             var elfAp = 3;
-            while(true){
+            while (true) {
                 var outcome = Outcome(input, 3, elfAp);
-                if(outcome.noElfDied){
+                if (outcome.noElfDied) {
                     return outcome.score;
                 }
                 elfAp++;
             }
         }
-        
-        (bool noElfDied, int score) Outcome(string input, int goblinAp, int elfAp){
-            var state = Parse(input, goblinAp, elfAp);
-            var elfCount = state.players.Count(player => player.elf);
-            
+
+        (bool noElfDied, int score) Outcome(string input, int goblinAp, int elfAp) {
+            var game = Parse(input, goblinAp, elfAp);
+            var elfCount = game.players.Count(player => player.elf);
+
             var rounds = 0;
-            while (Step(state)) {
+            while (game.Step()) {
                 rounds++;
             }
-            
-            return (state.players.Count(p => p.elf) == elfCount, (rounds -1) * state.players.Select(player => player.hp).Sum());
+
+            return (game.players.Count(p => p.elf) == elfCount, (rounds - 1) * game.players.Select(player => player.hp).Sum());
         }
 
-        bool Step(State state) {
-            var moved = false;
-            foreach (var player in state.players.OrderBy(a => a.pos)) {
-                if (player.hp > 0) {
-                    if(Attack(state, player)){
-                        moved = true;
-                    } else {
-                        moved |= Move(state, player);
-                        moved |= Attack(state, player);
+     
+        Game Parse(string input, int goblinAp, int elfAp) {
+            var players = new List<Player>();
+            var lines = input.Split("\n");
+            var mtx = new Block[lines.Length, lines[0].Length];
+
+            var game = new Game{ mtx = mtx, players = players };
+
+            for (var irow = 0; irow < lines.Length; irow++) {
+                for (var icol = 0; icol < lines[0].Length; icol++) {
+                    switch (lines[irow][icol]) {
+                        case '#':
+                            mtx[irow, icol] = Wall.Block;
+                            break;
+                        case '.':
+                            mtx[irow, icol] = Empty.Block;
+                            break;
+                        case var ch when ch == 'G' || ch == 'E':
+                            var player = new Player {
+                                elf = ch == 'E',
+                                ap = ch == 'E' ? elfAp : goblinAp,
+                                pos = (irow, icol),
+                                game = game
+                            };
+                            players.Add(player);
+                            mtx[irow, icol] = player;
+                            break;
                     }
                 }
             }
-            return moved;
+            return game;
+        }
+    }
+    
+    class Game {
+        public Block[,] mtx;
+        public List<Player> players;
+
+        private bool ValidPos((int irow, int icol) pos) =>
+            pos.irow >= 0 && pos.irow < this.mtx.GetLength(0) && pos.icol >= 0 && pos.icol < this.mtx.GetLength(1);
+
+        public Block GetBlock((int irow, int icol) pos) =>
+            ValidPos(pos) ? mtx[pos.irow, pos.icol] : Wall.Block;
+
+        public bool Step() =>
+             players
+                .OrderBy(a => a.pos)
+                .Aggregate(false, (res, player) => res | player.Step());
+    }
+
+    abstract class Block { }
+
+    class Empty : Block { 
+        public static readonly Empty Block = new Empty(); 
+        private Empty(){} 
+    }
+
+    class Wall : Block { 
+        public static readonly Wall Block = new Wall(); 
+        private Wall(){} 
+    }
+
+    class Player : Block {
+        public (int irow, int icol) pos;
+        public bool elf;
+        public int ap = 3;
+        public int hp = 200;
+        public Game game;
+
+        public bool Step() {
+            if (hp <= 0) {
+                return false;
+            } else if (Attack()) {
+                return true;
+            } else if (Move()) {
+                Attack();
+                return true;
+            } else {
+                return false;
+            }
         }
 
-        bool Move(State state, Player player) {
-            var opponents = ClosestOpponents(state, player);
+        private bool Move() {
+            var opponents = ClosestOpponents();
             if (!opponents.Any()) {
                 return false;
             }
             var opponent = opponents.OrderBy(a => a.player.pos).First();
             var nextPos = opponents.Where(a => a.player == opponent.player).Select(a => a.firstStep).OrderBy(_ => _).First();
-            (state.mtx[nextPos.irow, nextPos.icol], state.mtx[player.pos.irow, player.pos.icol]) =
-                (state.mtx[player.pos.irow, player.pos.icol], state.mtx[nextPos.irow, nextPos.icol]);
-            player.pos = nextPos;
+            (game.mtx[nextPos.irow, nextPos.icol], game.mtx[pos.irow, pos.icol]) =
+                (game.mtx[pos.irow, pos.icol], game.mtx[nextPos.irow, nextPos.icol]);
+            pos = nextPos;
             return true;
         }
 
-
-        IEnumerable<(Player player, (int irow, int icol) firstStep)> ClosestOpponents(State state, Player player) {
+        private IEnumerable<(Player player, (int irow, int icol) firstStep)> ClosestOpponents() {
             var minDist = int.MaxValue;
-            foreach (var (otherPlayer, firstStep, dist) in OpponentsByDistance(state, player)) {
+            foreach (var (otherPlayer, firstStep, dist) in OpponentsByDistance()) {
                 if (dist > minDist) {
                     break;
                 } else {
@@ -84,20 +150,20 @@ namespace AdventOfCode.Y2018.Day15 {
             }
         }
 
-        IEnumerable<(Player player, (int irow, int icol) firstStep, int dist)> OpponentsByDistance(State state, Player player) {
+        private  IEnumerable<(Player player, (int irow, int icol) firstStep, int dist)> OpponentsByDistance() {
             var seen = new HashSet<(int irow, int icol)>();
-            seen.Add(player.pos);
+            seen.Add(pos);
             var q = new Queue<((int irow, int icol) pos, (int drow, int dcol) origDir, int dist)>();
 
             foreach (var (drow, dcol) in new[] { (-1, 0), (0, -1), (0, 1), (1, 0) }) {
-                var posT = (player.pos.irow + drow, player.pos.icol + dcol);
+                var posT = (pos.irow + drow, pos.icol + dcol);
                 q.Enqueue((posT, posT, 1));
             }
 
             while (q.Any()) {
                 var (pos, firstStep, dist) = q.Dequeue();
-                switch (GetBlock(state, pos)) {
-                    case Player otherPlayer when player != otherPlayer && otherPlayer.elf != player.elf:
+                switch (game.GetBlock(pos)) {
+                    case Player otherPlayer when this != otherPlayer && otherPlayer.elf != elf:
                         yield return (otherPlayer, firstStep, dist);
                         break;
 
@@ -107,7 +173,7 @@ namespace AdventOfCode.Y2018.Day15 {
                     case Empty _:
                         foreach (var (drow, dcol) in new[] { (-1, 0), (0, -1), (0, 1), (1, 0) }) {
                             var posT = (pos.irow + drow, pos.icol + dcol);
-                            if (!seen.Contains(posT)){
+                            if (!seen.Contains(posT)) {
                                 seen.Add(posT);
                                 q.Enqueue((posT, firstStep, dist + 1));
                             }
@@ -117,78 +183,31 @@ namespace AdventOfCode.Y2018.Day15 {
             }
         }
 
-        bool Attack(State state, Player player) {
+        private bool Attack() {
             var opponents = new List<Player>();
 
             foreach (var (drow, dcol) in new[] { (-1, 0), (0, -1), (0, 1), (1, 0) }) {
-                var posT = (player.pos.irow + drow, player.pos.icol + dcol);
-                var block = GetBlock(state, posT);
+                var posT = (this.pos.irow + drow, this.pos.icol + dcol);
+                var block = game.GetBlock(posT);
                 switch (block) {
-                    case Player otherPlayer when otherPlayer.elf != player.elf:
+                    case Player otherPlayer when otherPlayer.elf != this.elf:
                         opponents.Add(otherPlayer);
                         break;
                 }
             }
 
-            if(!opponents.Any()){
+            if (!opponents.Any()) {
                 return false;
             }
             var minHp = opponents.Select(a => a.hp).Min();
             var opponent = opponents.First(a => a.hp == minHp);
-            opponent.hp -= player.ap;
+            opponent.hp -= this.ap;
             if (opponent.hp <= 0) {
-                state.players.Remove(opponent);
-                state.mtx[opponent.pos.irow, opponent.pos.icol] = new Empty();
+                game.players.Remove(opponent);
+                game.mtx[opponent.pos.irow, opponent.pos.icol] = Empty.Block;
             }
             return true;
         }
 
-     
-        bool ValidPos(State state, (int irow, int icol) pos) {
-            return !(pos.irow < 0 || pos.irow >= state.mtx.GetLength(0) || pos.icol < 0 || pos.icol >= state.mtx.GetLength(1));
-        }
-        Block GetBlock(State state, (int irow, int icol) pos) {
-            return ValidPos(state, pos) ? state.mtx[pos.irow, pos.icol] : new Wall();
-        }
-
-        State Parse(string input, int goblinAp, int elfAp) {
-            var players = new List<Player>();
-            var lines = input.Split("\n");
-            var mtx = new Block[lines.Length, lines[0].Length];
-            for (var irow = 0; irow < lines.Length; irow++) {
-                for (var icol = 0; icol < lines[0].Length; icol++) {
-                    switch (lines[irow][icol]) {
-                        case '#':
-                            mtx[irow, icol] = new Wall();
-                            break;
-                        case '.':
-                            mtx[irow, icol] = new Empty();
-                            break;
-                        case var ch when ch == 'G' || ch == 'E':
-                            var player = new Player { 
-                                elf = ch == 'E', 
-                                ap = ch == 'E' ? elfAp : goblinAp,
-                                pos = (irow, icol) };
-                            players.Add(player);
-                            mtx[irow, icol] = player;
-                            break;
-                    }
-                }
-            }
-            return new State { mtx = mtx, players = players };
-        }
-    }
-    class State {
-        public Block[,] mtx;
-        public List<Player> players;
-    }
-    abstract class Block { }
-    class Empty : Block { }
-    class Wall : Block { }
-    class Player : Block {
-        public (int irow, int icol) pos;
-        public bool elf;
-        public int ap = 3;
-        public int hp = 200;
     }
 }
