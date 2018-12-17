68131c6aa68d7ab39e7a460cf3008d7434ca5a54
David Nemeth Csoka <encse@csokavar.hu>
2018-12-17 08:54:12 +0100

    17 cleanup

--- 2018/Day17/Solution.cs
+++ 2018/Day17/Solution.cs
@@ -16,17 +16,18 @@ namespace AdventOfCode.Y2018.Day17 {
             yield return PartTwo(input);
         }
 
-        int PartOne(string input) {
-            var mtx = new char[2000, 2000];
-            var width = mtx.GetLength(0);
-            var height = mtx.GetLength(1);
+        int PartOne(string input) => Regex.Matches(Fill(input), "[~|]").Count;
+        int PartTwo(string input) => Regex.Matches(Fill(input), "[~]").Count;
 
-            for(var y = 0;y<height; y++){
-                for(var x=0;x<width; x++){
-                    mtx[x,y] = '.';
+        string Fill(string input) {
+            var (width, height) = (2000, 2000);
+            var mtx = new char[width, height];
+
+            for (var y = 0; y < height; y++) {
+                for (var x = 0; x < width; x++) {
+                    mtx[x, y] = '.';
                 }
             }
-
 
             foreach (var line in input.Split("\n")) {
                 var nums = Regex.Matches(line, @"\d+").Select(g => int.Parse(g.Value)).ToArray();
@@ -38,149 +39,67 @@ namespace AdventOfCode.Y2018.Day17 {
                     }
                 }
             }
-         
+            FillRecursive(mtx, 500, 0);
 
-            var minX = int.MaxValue;
-            var maxX = int.MinValue;
-            var minY = int.MaxValue;
-            var maxY = int.MinValue;
-            for(var y = 0;y<height; y++){
-                for(var x=0;x<width; x++){
-                    if(mtx[x,y] == '#'){
+            var (minY, maxY) = (int.MaxValue, int.MinValue);
+            for (var y = 0; y < height; y++) {
+                for (var x = 0; x < width; x++) {
+                    if (mtx[x, y] == '#') {
                         minY = Math.Min(minY, y);
                         maxY = Math.Max(maxY, y);
-
-                        minX = Math.Min(minX, x);
-                        maxX = Math.Max(maxX, x);
                     }
                 }
             }
-            Fill(mtx, 500, 0);
-            var res = 0;
-            for(var y = minY;y<=maxY; y++){
-                for(var x=0;x<width; x++){
-                    if(mtx[x,y] == '|' || mtx[x,y] == '~'){
-                        res++;
-                    }
+            var sb = new StringBuilder();
+            for (var y = minY; y <= maxY; y++) {
+                for (var x = 0; x < width; x++) {
+                    sb.Append(mtx[x, y]);
                 }
+                sb.AppendLine();
             }
-
-            return res;
+            return sb.ToString();
         }
-        int Fill(char[,] mtx, int x, int y) {
+
+        void FillRecursive(char[,] mtx, int x, int y) {
             var width = mtx.GetLength(0);
             var height = mtx.GetLength(1);
-            switch (mtx[x, y]) {
-                case '#':
-                    return 0;
-                case '|':
-                    return 0;
-                case '~':
-                    return 0;
-                default:
-                    mtx[x, y] = '|';
-                    var res = 1;
-                    if (y < height - 1) {
-                        res += Fill(mtx, x, y + 1);
-                        
-                        if (mtx[x, y + 1] == '#' || mtx[x, y + 1] == '~') {
-                            if (x > 0) {
-                                res += Fill(mtx, x - 1, y);
-                            }
-                            if (x < width - 1) {
-                                res += Fill(mtx, x + 1, y);
-                            }
-                        }
+            if (mtx[x, y] != '.') {
+                return;
+            }
+            mtx[x, y] = '|';
+            if (y == height - 1) {
+                return ;
+            }
+            FillRecursive(mtx, x, y + 1);
 
-                        var still = true;
-                        for (var xT = x - 1; still && xT >= 0; xT--) {
-                            if (mtx[xT, y] == '.') {
-                                still = false;
-                            }
-                            if(mtx[xT, y+1] == '|'){
-                                still = false;
-                            }
-                            if (mtx[xT, y] == '#') {
-                                break;
-                            }
-                        }
+            if (mtx[x, y + 1] == '#' || mtx[x, y + 1] == '~') {
+                if (x > 0) {
+                    FillRecursive(mtx, x - 1, y);
+                }
+                if (x < width - 1) {
+                    FillRecursive(mtx, x + 1, y);
+                }
+            }
 
-                        for (var xT = x + 1; still && xT < width; xT++) {
-                            if (mtx[xT, y] == '.') {
-                                still = false;
-                            }
-                            if(mtx[xT, y+1] == '|'){
-                                still = false;
-                            }
-                            if (mtx[xT, y] == '#') {
-                                break;
-                            }
-                        }
-
-                        if (still) {
-                            mtx[x, y] = '~';
-                            for (var xT = x - 1; xT >= 0 && mtx[xT, y] == '|'; xT--) {
-                                mtx[xT, y] = '~';
-                            }
-
-                            for (var xT = x + 1; xT < width && mtx[xT, y] == '|'; xT++) {
-                                mtx[xT, y] = '~';
-                            }
-                        }
+            if (IsStill(mtx, x, y)) {
+                foreach (var dx in new[] { -1, 1 }) {
+                    for (var xT = x; xT >= 0 && xT < width && mtx[xT, y] == '|'; xT += dx) {
+                        mtx[xT, y] = '~';
                     }
-                    return res;
+                }
             }
         }
-        int PartTwo(string input) {
-            var mtx = new char[2000, 2000];
+
+        bool IsStill(char[,] mtx, int x, int y) {
             var width = mtx.GetLength(0);
-            var height = mtx.GetLength(1);
-
-            for(var y = 0;y<height; y++){
-                for(var x=0;x<width; x++){
-                    mtx[x,y] = '.';
-                }
-            }
-
-
-            foreach (var line in input.Split("\n")) {
-                var nums = Regex.Matches(line, @"\d+").Select(g => int.Parse(g.Value)).ToArray();
-                for (var i = nums[1]; i <= nums[2]; i++) {
-                    if (line.StartsWith("x")) {
-                        mtx[nums[0], i] = '#';
-                    } else {
-                        mtx[i, nums[0]] = '#';
+            foreach (var dx in new[] { -1, 1 }) {
+                for (var xT = x; xT >= 0 && xT < width && mtx[xT, y] != '#'; xT += dx) {
+                    if (mtx[xT, y] == '.' || mtx[xT, y + 1] == '|') {
+                        return false;
                     }
                 }
             }
-         
-
-            var minX = int.MaxValue;
-            var maxX = int.MinValue;
-            var minY = int.MaxValue;
-            var maxY = int.MinValue;
-            for(var y = 0;y<height; y++){
-                for(var x=0;x<width; x++){
-                    if(mtx[x,y] == '#'){
-                        minY = Math.Min(minY, y);
-                        maxY = Math.Max(maxY, y);
-
-                        minX = Math.Min(minX, x);
-                        maxX = Math.Max(maxX, x);
-                    }
-                }
-            }
-            Fill(mtx, 500, 0);
-            var res = 0;
-            for(var y = minY;y<=maxY; y++){
-                for(var x=0;x<width; x++){
-                    if(mtx[x,y] == '~'){
-                        res++;
-                    }
-                }
-            }
-
-            return res;
+            return true;
         }
     }
 }
