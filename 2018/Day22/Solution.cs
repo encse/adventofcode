78ab9a700087c960a3189a7c186608f04aa3571a
David Nemeth Csoka <encse@csokavar.hu>
2018-12-22 16:48:40 +0100

    22

--- 2018/Day22/Solution.cs
+++ 2018/Day22/Solution.cs
@@ -17,68 +17,152 @@ namespace AdventOfCode.Y2018.Day22 {
         }
 
         int PartOne(string input) {
-            var lines = input.Split("\n");
-            var depth = Regex.Matches(lines[0], @"\d+").Select(x => int.Parse(x.Value)).Single();
-            var target = Regex.Matches(lines[1], @"\d+").Select(x => int.Parse(x.Value)).ToArray();
-            var (targetX, targetY) = (target[0], target[1]);
-            var m = 20183;
-
-            var erosionLevel = new int[targetX + 1, targetY + 1];
-            for (var x = 0; x <= targetX; x++) {
-                if (x * 16807 < 0) {
-                    throw new Exception();
-                }
-                erosionLevel[x, 0] = ((x * 16807) + depth) % m;
-            }
-
-            for (var y = 0; y <= targetY; y++) {
-                if (y * 48271 < 0) {
-                    throw new Exception();
-                }
-                erosionLevel[0, y] = ((y * 48271) + depth) % m;
-            }
-
-            for (var y = 1; y <= targetY; y++) {
-                for (var x = 1; x <= targetX; x++) {
-
-                    if (erosionLevel[x, y - 1] * erosionLevel[x - 1, y] < 0) {
-                        throw new Exception();
-                    }
-                    erosionLevel[x, y] = ((erosionLevel[x, y - 1] * erosionLevel[x - 1, y]) + depth) % m;
-                }
-            }
-
-            erosionLevel[targetX, targetY] = depth;
-
-            var regionType = new int[targetX + 1, targetY + 1];
-            for (var y = 0; y <= targetY; y++) {
-                for (var x = 0; x <= targetX; x++) {
-                    regionType[x, y] = erosionLevel[x, y] % 3;
-                }
-            }
-
+            var (targetX, targetY, regionType) = Parse(input);
             var riskLevel = 0;
-
             for (var y = 0; y <= targetY; y++) {
                 for (var x = 0; x <= targetX; x++) {
-                    riskLevel += regionType[x, y];
+                    riskLevel += (int)regionType(x, y);
                 }
             }
             return riskLevel;
         }
 
-        string Tsto(int[,] m) {
-            var sb = new StringBuilder();
-            foreach (var irow in Enumerable.Range(0, m.GetLength(1))) {
-                foreach (var icol in Enumerable.Range(0, m.GetLength(0))) {
-                    sb.Append(".=|"[m[icol, irow]]);
-                }
-                sb.AppendLine();
-            }
-            return sb.ToString();
-        }
         int PartTwo(string input) {
-            return 0;
+            var (targetX, targetY, regionType) = Parse(input);
+            var q = new PQueue<((int x, int y) pos, Tool tool)>();
+            var seen = new HashSet<((int x, int y), Tool tool)>();
+
+            q.Enqueue(0, ((0, 0), Tool.Torch));
+
+            while (q.Any()) {
+                var (t, state) = q.Dequeue();
+                var (pos, tool) = state;
+                if (pos.x == targetX && pos.y == targetY && tool == Tool.Torch) {
+                    return t;
+                }
+
+                if (seen.Contains(state)) {
+                    continue;
+                }
+
+                seen.Add(state);
+
+                switch (regionType(pos.x, pos.y)) {
+                    case RegionType.Rocky:
+                        q.Enqueue(t + 7, (pos, tool == Tool.ClimbingGear ? Tool.Torch : Tool.ClimbingGear));
+                        break;
+                    case RegionType.Narrow:
+                        q.Enqueue(t + 7, (pos, tool == Tool.Torch ? Tool.Nothing : Tool.Torch));
+                        break;
+                    case RegionType.Wet:
+                        q.Enqueue(t + 7, (pos, tool == Tool.ClimbingGear ? Tool.Nothing : Tool.ClimbingGear));
+                        break;
+                }
+
+                foreach (var dx in new[] { -1, 0, 1 }) {
+                    foreach (var dy in new[] { -1, 0, 1 }) {
+                        if (Math.Abs(dx) + Math.Abs(dy) != 1) {
+                            continue;
+                        }
+
+                        var posNew = (x: pos.x + dx, y: pos.y + dy);
+                        if (posNew.x < 0 || posNew.y < 0) {
+                            continue;
+                        }
+
+                        switch (regionType(posNew.x, posNew.y)) {
+                            case RegionType.Rocky:
+                                if (tool == Tool.ClimbingGear || tool == Tool.Torch) {
+                                    q.Enqueue(t + 1, (posNew, tool));
+                                }
+                                break;
+                            case RegionType.Narrow:
+                                if (tool == Tool.Torch || tool == Tool.Nothing) {
+                                    q.Enqueue(t + 1, (posNew, tool));
+                                }
+                                break;
+                            case RegionType.Wet:
+
+                                if (tool == Tool.ClimbingGear || tool == Tool.Nothing) {
+                                    q.Enqueue(t + 1, (posNew, tool));
+                                }
+
+                                break;
+                        }
+                    }
+                }
+            }
+
+            throw new Exception();
+        }
+
+        (int targetX, int targetY, Func<int, int, RegionType> regionType) Parse(string input) {
+            var lines = input.Split("\n");
+            var depth = Regex.Matches(lines[0], @"\d+").Select(x => int.Parse(x.Value)).Single();
+            var target = Regex.Matches(lines[1], @"\d+").Select(x => int.Parse(x.Value)).ToArray();
+            var (targetX, targetY) = (target[0], target[1]);
+
+            var m = 20183;
+
+            var erosionLevelCache = new Dictionary<(int, int), int>();
+            int erosionLevel(int x, int y) {
+                var key = (x, y);
+                if (!erosionLevelCache.ContainsKey(key)) {
+                    if (x == targetX && y == targetY) {
+                        erosionLevelCache[key] = depth;
+                    } else if (x == 0 && y == 0) {
+                        erosionLevelCache[key] = depth;
+                    } else if (x == 0) {
+                        erosionLevelCache[key] = ((y * 48271) + depth) % m;
+                    } else if (y == 0) {
+                        erosionLevelCache[key] = ((x * 16807) + depth) % m;
+                    } else {
+                        erosionLevelCache[key] = ((erosionLevel(x, y - 1) * erosionLevel(x - 1, y)) + depth) % m;
+                    }
+                }
+                return erosionLevelCache[key];
+            }
+
+            RegionType regionType(int x, int y) {
+                return (RegionType)(erosionLevel(x, y) % 3);
+            }
+
+            return (targetX, targetY, regionType);
+        }
+    }
+    enum RegionType {
+        Rocky = 0,
+        Wet = 1,
+        Narrow = 2
+    }
+
+    enum Tool {
+        Nothing,
+        Torch,
+        ClimbingGear
+    }
+
+    class PQueue<T> {
+        SortedDictionary<int, Queue<T>> d = new SortedDictionary<int, Queue<T>>();
+        public bool Any() {
+            return d.Any();
+        }
+
+        public void Enqueue(int p, T t) {
+            if (!d.ContainsKey(p)) {
+                d[p] = new Queue<T>();
+            }
+            d[p].Enqueue(t);
+        }
+
+        public (int p, T t) Dequeue() {
+            var p = d.Keys.First();
+            var items = d[p];
+            var t = items.Dequeue();
+            if (!items.Any()) {
+                d.Remove(p);
+            }
+            return (p, t);
         }
     }
 }
