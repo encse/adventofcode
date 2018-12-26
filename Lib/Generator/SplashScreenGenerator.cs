6e62daaabbd39e56b61fea74f3f39eb98bbfef76
David Nemeth Csoka <encse@csokavar.hu>
2018-12-26 07:05:28 +0100

    colors

--- Lib/Generator/SplashScreenGenerator.cs
+++ Lib/Generator/SplashScreenGenerator.cs
@@ -25,63 +25,68 @@ namespace AdventOfCode.Generator {
                 |            Console.WriteLine();
                 |        }}
                 |
-                |       private static void Write(ConsoleColor color, string text){{
-                |           Console.ForegroundColor = color;
-                |           Console.Write(text);
+                |       private static void Write(int rgb, string text){{
+                |           Console.Write($""\u001b[38;2;{{(rgb>>16)&255}};{{(rgb>>8)&255}};{{rgb&255}}m{{text}}"");
                 |       }}
                 |    }}
                 |}}".StripMargin();
         }
 
         private string CalendarPrinter(Calendar calendar) {
-            var theme = new Dictionary<string[], string>() {
-                [new[] { "calendar-edge" }] = "ConsoleColor.Gray",
-                [new[] { "calendar-star" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-complete", "calendar-mark-complete" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-verycomplete", "calendar-mark-complete" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-verycomplete", "calendar-mark-verycomplete" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-ornament0" }] = "ConsoleColor.White",
-                [new[] { "calendar-ornament1" }] = "ConsoleColor.Green",
-                [new[] { "calendar-ornament2" }] = "ConsoleColor.Red",
-                [new[] { "calendar-ornament3" }] = "ConsoleColor.Blue",
-                [new[] { "calendar-ornament4" }] = "ConsoleColor.Magenta",
-                [new[] { "calendar-ornament5" }] = "ConsoleColor.Cyan",
-                [new[] { "calendar-ornament3" }] = "ConsoleColor.DarkCyan",
+            var theme = new Dictionary<string[], int>() {
+                [new[] { "title" }] = 0xffff66,
+                [new[] { "calendar-star" }] = 0xffff66,
+                [new[] { "calendar-complete", "calendar-mark-complete" }] = 0xffff66,
+                [new[] { "calendar-verycomplete", "calendar-mark-complete" }] = 0xfff66,
+                [new[] { "calendar-verycomplete", "calendar-mark-verycomplete" }] = 0xffff66,
+
+                [new[] { "calendar-edge" }] = 0xcccccc,
+                [new[] { "calendar-print-edge" }] = 0x999999,
+                [new[] { "calendar-print-text" }] = 0xcccccc,
+
+                [new[] { "calendar-ornament0" }] = 0x0066ff,
+                [new[] { "calendar-ornament1" }] = 0xff9900,
+                [new[] { "calendar-ornament2" }] = 0xff0000,
+                [new[] { "calendar-ornament3" }] = 0xffff66,
+
+                [new[] { "calendar-tree-star" }] = 0xffff66,
+                [new[] { "calendar-antenna-star" }] = 0xffff66,
+                [new[] { "calendar-antenna-signal0" }] = 0xffff66,
+                [new[] { "calendar-antenna-signal1" }] = 0xffff66,
+                [new[] { "calendar-antenna-signal2" }] = 0xffff66,
+                [new[] { "calendar-antenna-signal3" }] = 0xffff66,
+                [new[] { "calendar-antenna-signal4" }] = 0xffff66,
+                [new[] { "calendar-antenna-signal5" }] = 0xffff66,
+                [new[] { "calendar-tree-ornament0" }] = 0x0066ff,
+                [new[] { "calendar-tree-ornament1" }] = 0xff9900,
+                [new[] { "calendar-tree-ornament2" }] = 0xff0000,
+                [new[] { "calendar-tree-branches" }] = 0x009900,
+                [new[] { "calendar-tree-trunk" }] = 0xaaaaaa,
+                [new[] { "calendar-trunk" }] = 0xcccccc,
+                [new[] { "calendar-streets" }] = 0x666666,
+                [new[] { "calendar-window-dark" }] = 0x333333,
+                [new[] { "calendar-window-red" }] = 0xff0000,
+                [new[] { "calendar-window-green" }] = 0x009900,
+                [new[] { "calendar-window-blue" }] = 0x0066ff,
+                [new[] { "calendar-window-yellow" }] = 0xffff66,
+                [new[] { "calendar-window-brown" }] = 0x553322,
+
+                [new[] { "calendar-lightbeam" }] = 0xffff66,
+
+                [new[] { "calendar-color-s" }] = 0x999999,
+                [new[] { "calendar-color-b" }] = 0x0066ff,
+                [new[] { "calendar-color-e" }] = 0xcccccc,
+                [new[] { "calendar-color-r" }] = 0xff0000,
+                [new[] { "calendar-color-d" }] = 0x880000,
+                [new[] { "calendar-color-n" }] = 0x886655,
+                [new[] { "calendar-color-k" }] = 0xcccccc,
+                [new[] { "calendar-color-g" }] = 0x009900,
+                [new[] { "calendar-color-w" }] = 0xcccccc,
+                [new[] { "calendar-color-t" }] = 0xcccccc,
+                [new[] { "calendar-color-i" }] = 0xff0000,
+                [new[] { "calendar-color-y" }] = 0xffff66, 
 
 
-                [new[] { "calendar-tree-star" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-antenna-star" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-antenna-signal0" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-antenna-signal1" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-antenna-signal2" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-antenna-signal3" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-antenna-signal4" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-antenna-signal5" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-tree-ornament0" }] = "ConsoleColor.Red",
-                [new[] { "calendar-tree-ornament1" }] = "ConsoleColor.Green",
-                [new[] { "calendar-tree-ornament2" }] = "ConsoleColor.Blue",
-                [new[] { "calendar-tree-branches" }] = "ConsoleColor.DarkGreen",
-                [new[] { "calendar-tree-trunk" }] = "ConsoleColor.Gray",
-                [new[] { "calendar-streets" }] = "ConsoleColor.Gray",
-                [new[] { "calendar-window-dark" }] = "ConsoleColor.DarkGray",
-                [new[] { "calendar-window-red" }] = "ConsoleColor.Red",
-                [new[] { "calendar-window-green" }] = "ConsoleColor.Green",
-                [new[] { "calendar-window-blue" }] = "ConsoleColor.Blue",
-                [new[] { "calendar-window-yellow" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-window-brown" }] = "ConsoleColor.DarkGreen",
-
-                [new[] { "calendar-lightbeam" }] = "ConsoleColor.Yellow",
-
-
-                [new[] { "calendar-color-d" }] = "ConsoleColor.DarkRed",
-                [new[] { "calendar-color-y" }] = "ConsoleColor.Yellow",
-                [new[] { "calendar-color-w" }] = "ConsoleColor.White",
-                [new[] { "calendar-color-r" }] = "ConsoleColor.Red",
-                [new[] { "calendar-color-b" }] = "ConsoleColor.Blue",
-                [new[] { "calendar-color-s" }] = "ConsoleColor.Gray",
-                [new[] { "calendar-color-g" }] = "ConsoleColor.Green",
-
-                [new[] { "title" }] = "ConsoleColor.Yellow",
             };
 
             var lines = calendar.Lines.Select(line =>
@@ -100,7 +105,7 @@ namespace AdventOfCode.Generator {
             var bw = new BufferWriter();
             foreach (var line in lines) {
                 foreach (var token in line) {
-                    var consoleColor = "ConsoleColor.DarkGray";
+                    var consoleColor = 0x888888;
                     foreach (var kvp in theme) {
                         if (kvp.Key.All(s => token.Styles.Contains(s))) {
                             consoleColor = kvp.Value;
@@ -110,17 +115,17 @@ namespace AdventOfCode.Generator {
                     bw.Write(consoleColor, token.Text);
                 }
 
-                bw.Write(null, "\n");
+                bw.Write(-1, "\n");
             }
             return bw.GetContent();
         }
 
         class BufferWriter {
             StringBuilder sb = new StringBuilder();
-            string bufferColor = null;
+            int bufferColor = -1;
             string buffer = "";
 
-            public void Write(string color, string text) {
+            public void Write(int color, string text) {
                 if (!string.IsNullOrWhiteSpace(text)) {
                     if (color != bufferColor && !string.IsNullOrWhiteSpace(buffer)) {
                         Flush();
@@ -135,7 +140,7 @@ namespace AdventOfCode.Generator {
                     var block = buffer.Substring(0, Math.Min(100, buffer.Length));
                     buffer = buffer.Substring(block.Length);
                     block = block.Replace("\\", "\\\\").Replace("\"", "\\\"").Replace("\n", "\\n");
-                    sb.AppendLine($@"Write({bufferColor}, ""{block}"");");
+                    sb.AppendLine($@"Write(0x{bufferColor.ToString("x")}, ""{block}"");");
                 }
                 buffer = "";
             }
