e5577fbf4ab67672ee2a12df78790ec8fa204215
David Nemeth Csoka <encse@csokavar.hu>
2019-12-03 08:16:34 +0100

    3

--- 2019/Day02/Solution.cs
+++ 2019/Day02/Solution.cs
@@ -23,13 +23,9 @@ namespace AdventOfCode.Y2019.Day02 {
             for (var sum = 0; ; sum++) {
                 for (var verb = 0; verb <= sum; verb++) {
                     var noun = sum - verb;
-                    try {
-                        var res = ExecIntcode(mem, noun, verb);
-                        if (res == 19690720) {
-                            return 100 * noun + verb;
-                        }
-                    } catch {
-
+                    var res = ExecIntcode(mem, noun, verb);
+                    if (res == 19690720) {
+                        return 100 * noun + verb;
                     }
                 }
             }
