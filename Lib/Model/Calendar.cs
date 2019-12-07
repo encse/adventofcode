0ba0291dfb00dba202ddd593ddbced621c7ef3d6
David Nemeth Csoka <encse@csokavar.hu>
2019-12-07 10:00:24 +0100

    excss for color parsing

--- Lib/Model/Calendar.cs
+++ Lib/Model/Calendar.cs
@@ -3,22 +3,42 @@ using System.Linq;
 using System.Collections.Generic;
 using HtmlAgilityPack;
 using System.Text.RegularExpressions;
+using ExCSS;
 
 namespace AdventOfCode.Model {
 
     public class CalendarToken {
         public string[] Styles = new string[0];
         public string Text { get; set; }
+        public int? ConsoleColor { get; set; }
     }
 
     public class Calendar {
         public int Year;
 
+        public Dictionary<string[], int> Theme = new Dictionary<string[], int>();
         public IReadOnlyList<IReadOnlyList<CalendarToken>> Lines { get; private set; }
 
         public static Calendar Parse(int year, string html) {
             var document = new HtmlAgilityPack.HtmlDocument();
             document.LoadHtml(html);
+
+            var theme = new Dictionary<string[], int>();
+
+            var styleNodes = document.DocumentNode.SelectNodes("//style");
+            var styleSheetParser = new StylesheetParser();
+
+            foreach (var style in styleNodes) {
+
+                var stylesheet = styleSheetParser.Parse(style.InnerText);
+                foreach (ExCSS.StyleRule rule in stylesheet.StyleRules) {
+
+                    var color = ParseRgbColor(rule.Style.Color);
+                    if(color != null){
+                        theme.Add(rule.Selector.Text.Split(" "), color.Value);
+                    }
+                }
+            }
 
             var calendar = document.DocumentNode.SelectSingleNode("//*[contains(@class,'calendar')]");
 
@@ -34,20 +54,26 @@ namespace AdventOfCode.Model {
 
             foreach (var textNode in calendar.SelectNodes(".//text()")) {
                 var styles = new List<string>();
+                int? consoleColor = null;
                 foreach (var node in textNode.Ancestors()) {
                     if (node.Attributes["class"] != null) {
-                        styles.AddRange(node.Attributes["class"].Value.Split(' '));
+                        styles.AddRange(node.Attributes["class"].Value.Split(' ').Select(x => "." + x));
                     }
+
                     if (node.Attributes["style"] != null) {
-                        styles.Add(node.Attributes["style"].Value);
+                        var miniStyleSheet = "x { " + node.Attributes["style"].Value + " } ";
+                        var stylesheet = styleSheetParser.Parse(miniStyleSheet);
+                        if (consoleColor == null) {
+                            consoleColor = ParseRgbColor(stylesheet.StyleRules.Cast<StyleRule>().Single().Style.Color);
+                        }
                     }
                 }
 
                 var text = textNode.InnerText;
                 var widthSpec = styles.FirstOrDefault(style => style.StartsWith("width:"));
-                if(widthSpec != null){
+                if (widthSpec != null) {
                     var m = Regex.Match(widthSpec, "[.0-9]+");
-                    if(m.Success){
+                    if (m.Success) {
                         var width = double.Parse(m.Value) * 1.7;
                         var c = (int)Math.Round(width - text.Length, MidpointRounding.AwayFromZero);
                         if (c > 0) {
@@ -55,7 +81,7 @@ namespace AdventOfCode.Model {
                         }
                     }
                 }
-                
+
                 var i = 0;
                 while (i < text.Length) {
                     var iNext = text.IndexOf("\n", i);
@@ -65,7 +91,8 @@ namespace AdventOfCode.Model {
 
                     line.Add(new CalendarToken {
                         Styles = styles.ToArray(),
-                        Text = HtmlEntity.DeEntitize(text.Substring(i, iNext - i))
+                        Text = HtmlEntity.DeEntitize(text.Substring(i, iNext - i)),
+                        ConsoleColor = consoleColor
                     });
 
                     if (iNext < text.Length) {
@@ -77,7 +104,21 @@ namespace AdventOfCode.Model {
             }
 
 
-            return new Calendar { Year = year, Lines = lines };
+            return new Calendar { Year = year, Theme = theme, Lines = lines };
+        }
+
+        private static int? ParseRgbColor(string st) {
+            Regex regex = new Regex(@"rgb\((?<r>\d{1,3}), (?<g>\d{1,3}), (?<b>\d{1,3})\)");
+            Match match = regex.Match(st);
+            if (match.Success) {
+                int r = int.Parse(match.Groups["r"].Value);
+                int g = int.Parse(match.Groups["g"].Value);
+                int b = int.Parse(match.Groups["b"].Value);
+
+                return (r << 16) + (g << 8) + b;
+            }
+            return null;
         }
     }
+
 }
