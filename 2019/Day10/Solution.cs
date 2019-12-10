a2176b396f5302ddc6a4a7b24797ee2cd2f89c14
David Nemeth Csoka <encse@csokavar.hu>
2019-12-10 19:13:32 +0100

    10

--- 2019/Day10/Solution.cs
+++ 2019/Day10/Solution.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
+
+using AsteroidsByDir = System.Collections.Generic.Dictionary<
+    (int drow, int dcol),
+    System.Collections.Generic.List<(int irow, int icol)>
+>;
 
 namespace AdventOfCode.Y2019.Day10 {
 
@@ -16,106 +18,80 @@ namespace AdventOfCode.Y2019.Day10 {
             yield return PartTwo(input);
         }
 
-        int PartOne(string input) {
-            var map = input.Split("\n");
-            var (crow, ccol) = (map.Length, map[0].Length);
-            var max = 0;
-            for (var irow = 0; irow < crow; irow++) {
-                for (var icol = 0; icol < ccol; icol++) {
-                    if (map[irow][icol] != '#') {
-                        continue;
-                    }
-
-                    var visible = new HashSet<(int, int)>();
-
-                    for (var irowT = 0; irowT < crow; irowT++) {
-                        for (var icolT = 0; icolT < ccol; icolT++) {
-
-                            if (map[irowT][icolT] != '#') {
-                                continue;
-                            }
-
-                            if (irowT != irow || icolT != icol) {
-                                var (dcol, drow) = (icolT - icol, irowT - irow);
-                                var gcd = Math.Abs(Gcd(drow, dcol));
-                                visible.Add((drow / gcd, dcol / gcd));
-                            }
-                        }
-                    }
-
-                    max = Math.Max(max, visible.Count);
-
-                }
-            }
-            return max;
-        }
+        int PartOne(string input) => SelectStationPosition(input).asteroidsByDir.Count;
 
         int PartTwo(string input) {
+            var asteroid = DestroyAll(input).ElementAt(199);
+            return (asteroid.icol * 100 + asteroid.irow);
+        }
+
+        IEnumerable<(int irow, int icol)> DestroyAll(string input) {
+            var (station, asteroidsByDir) = SelectStationPosition(input);
+
+            foreach (var dir in asteroidsByDir.Keys.ToArray()) {
+                asteroidsByDir[dir] = asteroidsByDir[dir]
+                    .OrderBy(a => Math.Abs(a.irow - station.irow) + Math.Abs(a.icol - station.icol))
+                    .ToList();
+            }
+
+            foreach (var dir in Rotate(asteroidsByDir.Keys)) {
+                if (asteroidsByDir.ContainsKey(dir)) {
+                    var asteroid = asteroidsByDir[dir].First();
+                    asteroidsByDir[dir].RemoveAt(0);
+
+                    yield return asteroid;
+
+                    if (!asteroidsByDir[dir].Any()) {
+                        asteroidsByDir.Remove(dir);
+                    }
+                }
+            }
+        }
+
+        IEnumerable<(int drow, int dcol)> Rotate(IEnumerable<(int drow, int dcol)> dirs) {
+            var ordered = dirs.OrderBy(dir => -Math.Atan2(dir.dcol, dir.drow)).ToList();
+            for (var i = 0; ; i++) {
+                yield return ordered[i % ordered.Count];
+            }
+        }
+
+        ((int irow, int icol) station, AsteroidsByDir asteroidsByDir) SelectStationPosition(string input) {
+
+            var res = ((0, 0), asteroidsByDir: new AsteroidsByDir());
+            var asteroids = Asteroids(input);
+
+            foreach (var station in asteroids) {
+                var asteroidsByDir = new AsteroidsByDir();
+                foreach (var asteroid in asteroids) {
+                    if (station != asteroid) {
+                        var (rowDir, colDir) = (asteroid.irow - station.irow, asteroid.icol - station.icol);
+                        var gcd = Math.Abs(Gcd(rowDir, colDir));
+                        var dir = (rowDir / gcd, colDir / gcd);
+
+                        if (!asteroidsByDir.ContainsKey(dir)) {
+                            asteroidsByDir[dir] = new List<(int irow, int icol)>();
+                        }
+                        asteroidsByDir[dir].Add(asteroid);
+                    }
+                }
+                if (asteroidsByDir.Count > res.asteroidsByDir.Count) {
+                    res = (station, asteroidsByDir);
+                }
+            }
+
+            return res;
+        }
+
+        List<(int irow, int icol)> Asteroids(string input) {
             var map = input.Split("\n");
             var (crow, ccol) = (map.Length, map[0].Length);
-            var d = new Dictionary<(int drow, int dcol), List<(int irow, int icol)>>();
-            int irowS = 0;
-            int icolS = 0;
-            for (var irow = 0; irow < crow; irow++) {
-                for (var icol = 0; icol < ccol; icol++) {
-                    if (map[irow][icol] != '#') {
-                        continue;
-                    }
 
-                    var visible = new Dictionary<(int x, int y), List<(int irow, int icol)>>();
-
-                    for (var irowT = 0; irowT < crow; irowT++) {
-                        for (var icolT = 0; icolT < ccol; icolT++) {
-
-                            if (map[irowT][icolT] != '#') {
-                                continue;
-                            }
-
-                            if (irowT != irow || icolT != icol) {
-                                var (dcol, drow) = (icolT - icol, irowT - irow);
-                                var gcd = Math.Abs(Gcd(drow, dcol));
-
-                                var key = (drow / gcd, dcol / gcd);
-
-                                if (!visible.ContainsKey(key)) {
-                                    visible[key] = new List<(int irow, int icol)>();
-                                }
-                                visible[key].Add((irowT, icolT));
-                            }
-                        }
-                    }
-
-                    if (visible.Count > d.Count) {
-                        d = visible;
-                        (irowS, icolS) = (irow, icol);
-                    }
-
-                }
-            }
-
-            var keys = d.Keys.OrderBy(key => Math.Atan2(key.drow, key.dcol)).ToArray();
-            var s = 0;
-            while (keys[s].dcol != 0 || keys[s].drow > 0) {
-                s++;
-            }
-            var b = 0;
-            while (true) {
-                var key = keys[s % keys.Length];
-                if (d[key].Any()) {
-                    var (irow, icol) = d[key].OrderBy(a => Math.Abs(a.irow - irowS) + Math.Abs(a.icol - icolS)).First();
-
-                    d[key].Remove((irow, icol));
-
-                    b++;
-
-                    if (b == 200) {
-                        return (icol * 100 + irow);
-                    }
-                }
-                s++;
-
-            }
-            throw new Exception();
+            return (
+                from irow in Enumerable.Range(0, crow)
+                from icol in Enumerable.Range(0, ccol)
+                where map[irow][icol] == '#'
+                select (irow, icol)
+            ).ToList();
         }
 
         int Gcd(int a, int b) {
