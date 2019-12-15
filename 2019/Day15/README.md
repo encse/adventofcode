01c0debc121fa5d996304cd56da6341143d84b16
David Nemeth Csoka <encse@csokavar.hu>
2019-12-15 16:17:57 +0100

    15/2

--- 2019/Day15/README.md
+++ 2019/Day15/README.md
@@ -96,3 +96,68 @@ Now, because of the reply of `2`, you know you've found the *oxygen system*! In 
 *What is the fewest number of movement commands* required to move the repair droid from its starting position to the location of the oxygen system?
 
 
+## --- Part Two ---
+You quickly repair the oxygen system; oxygen gradually fills the area.
+
+Oxygen starts in the location containing the repaired oxygen system. It takes *one minute* for oxygen to spread to all open locations that are adjacent to a location that already contains oxygen. Diagonal locations are *not* adjacent.
+
+In the example above, suppose you've used the droid to explore the area fully and have the following map (where locations that currently contain oxygen are marked `O`):
+
+```
+` ##   
+#..## 
+#.#..#
+#.O.# 
+ ###  
+`
+```
+
+Initially, the only location which contains oxygen is the location of the repaired oxygen system.  However, after one minute, the oxygen spreads to all open (`.`) locations that are adjacent to a location containing oxygen:
+
+```
+` ##   
+#..## 
+#.#..#
+#OOO# 
+ ###  
+`
+```
+
+After a total of two minutes, the map looks like this:
+
+```
+` ##   
+#..## 
+#O#O.#
+#OOO# 
+ ###  
+`
+```
+
+After a total of three minutes:
+
+```
+` ##   
+#O.## 
+#O#OO#
+#OOO# 
+ ###  
+`
+```
+
+And finally, the whole region is full of oxygen after a total of four minutes:
+
+```
+` ##   
+#OO## 
+#O#OO#
+#OOO# 
+ ###  
+`
+```
+
+So, in this example, all locations contain oxygen after `*4*` minutes.
+
+Use the repair droid to get a complete map of the area. *How many minutes will it take to fill with oxygen?*
+
+
--- 2019/Day15/Solution.cs
+++ 2019/Day15/Solution.cs
@@ -18,6 +18,62 @@ namespace AdventOfCode.Y2019.Day15 {
 
         int PartOne(string input) {
             var icm = new IntCodeMachine(input);
+            return PathToO2(icm).Count;
+        }
+
+        int PartTwo(string input) {
+           var icm = new IntCodeMachine(input);
+           var o2 = PathToO2(icm);
+           
+           
+
+            var q = new Queue<(int x, int y, ImmutableList<int> path)>();
+            var seen = new HashSet<(int x, int y)>();
+
+            (int dx, int dy)[] dirs = new[] { (0, -1), (0, 1), (-1, 0), (1, 0) };
+
+            var e = new []{-1, 2,1,4,3};
+            
+            q.Enqueue((0, 0, o2));
+            seen.Add((0, 0));
+            var longestPath = 0;
+            while (q.Any()) {
+                var (x, y, path) = q.Dequeue();
+                longestPath = Math.Max(longestPath, path.Count);
+
+                for (var i = 0; i < dirs.Length; i++) {
+                    var (xT, yT) = (x + dirs[i].dx, y + dirs[i].dy);
+
+                    if (!seen.Contains((xT, yT))) {
+                        seen.Add((xT, yT));
+
+                        var pathT = path.Add(i + 1);
+
+                        var res = walk(icm, pathT);
+                        switch (res) {
+                            case 0: break;
+                            case 1: 
+                            case 2: q.Enqueue((xT, yT, pathT)); break;
+                            default: throw new Exception();
+                        }
+                    }
+                }
+            }
+
+            return longestPath - o2.Count;
+
+        }
+
+        int walk(IntCodeMachine icm, ImmutableList<int> path){
+            icm.Reset();
+            var res = 0;
+            foreach(var step in path){
+                res= (int)icm.Run(step).Single();
+            }
+            return res;
+        }
+
+        ImmutableList<int> PathToO2(IntCodeMachine icm){
             var q = new Queue<(int x, int y, ImmutableList<int> path)>();
             var seen = new HashSet<(int x, int y)>();
 
@@ -25,15 +81,7 @@ namespace AdventOfCode.Y2019.Day15 {
 
             var e = new []{-1, 2,1,4,3};
            
-            int walk(ImmutableList<int> path){
-                icm.Reset();
-                var res = 0;
-                foreach(var step in path){
-                    res= (int)icm.Run(step).Single();
-                }
-                return res;
-            }
-
+            
             q.Enqueue((0, 0, ImmutableList<int>.Empty));
             seen.Add((0, 0));
             while (true) {
@@ -47,21 +95,17 @@ namespace AdventOfCode.Y2019.Day15 {
 
                         var pathT = path.Add(i + 1);
 
-                        var res = walk(pathT);
+                        var res = walk(icm, pathT);
                         switch (res) {
                             case 0: break;
                             case 1: q.Enqueue((xT, yT, pathT)); break;
-                            case 2: return pathT.Count;
+                            case 2: return pathT;
                             default: throw new Exception();
                         }
                     }
                 }
 
             }
-        }
-
-        int PartTwo(string input) {
-            return 0;
         }
     }
 }
