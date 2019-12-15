fcb34289584fc98dbb80e64a778d2adbd4445215
David Nemeth Csoka <encse@csokavar.hu>
2019-12-15 16:26:07 +0100

    x

--- 2019/Day15/Solution.cs
+++ 2019/Day15/Solution.cs
@@ -18,74 +18,39 @@ namespace AdventOfCode.Y2019.Day15 {
 
         int PartOne(string input) {
             var icm = new IntCodeMachine(input);
-            return PathToO2(icm).Count;
+            return Traverse(icm, ImmutableList<int>.Empty).Count;
         }
 
         int PartTwo(string input) {
-           var icm = new IntCodeMachine(input);
-           var o2 = PathToO2(icm);
-           
-           
-
-            var q = new Queue<(int x, int y, ImmutableList<int> path)>();
-            var seen = new HashSet<(int x, int y)>();
-
-            (int dx, int dy)[] dirs = new[] { (0, -1), (0, 1), (-1, 0), (1, 0) };
-
-            var e = new []{-1, 2,1,4,3};
-            
-            q.Enqueue((0, 0, o2));
-            seen.Add((0, 0));
-            var longestPath = 0;
-            while (q.Any()) {
-                var (x, y, path) = q.Dequeue();
-                longestPath = Math.Max(longestPath, path.Count);
-
-                for (var i = 0; i < dirs.Length; i++) {
-                    var (xT, yT) = (x + dirs[i].dx, y + dirs[i].dy);
-
-                    if (!seen.Contains((xT, yT))) {
-                        seen.Add((xT, yT));
-
-                        var pathT = path.Add(i + 1);
-
-                        var res = walk(icm, pathT);
-                        switch (res) {
-                            case 0: break;
-                            case 1: 
-                            case 2: q.Enqueue((xT, yT, pathT)); break;
-                            default: throw new Exception();
-                        }
-                    }
-                }
-            }
-
-            return longestPath - o2.Count;
-
+            var icm = new IntCodeMachine(input);
+            var o2 = Traverse(icm, ImmutableList<int>.Empty);
+            return Traverse(icm, o2).Count - o2.Count;
         }
 
-        int walk(IntCodeMachine icm, ImmutableList<int> path){
+        int walk(IntCodeMachine icm, ImmutableList<int> path) {
             icm.Reset();
             var res = 0;
-            foreach(var step in path){
-                res= (int)icm.Run(step).Single();
+            foreach (var step in path) {
+                res = (int)icm.Run(step).Single();
             }
             return res;
         }
 
-        ImmutableList<int> PathToO2(IntCodeMachine icm){
+        ImmutableList<int> Traverse(IntCodeMachine icm,  ImmutableList<int> startPath) {
             var q = new Queue<(int x, int y, ImmutableList<int> path)>();
             var seen = new HashSet<(int x, int y)>();
 
             (int dx, int dy)[] dirs = new[] { (0, -1), (0, 1), (-1, 0), (1, 0) };
 
-            var e = new []{-1, 2,1,4,3};
-           
-            
-            q.Enqueue((0, 0, ImmutableList<int>.Empty));
+            var e = new[] { -1, 2, 1, 4, 3 };
+
+            q.Enqueue((0, 0, startPath));
             seen.Add((0, 0));
-            while (true) {
+            ImmutableList<int> lastPath = null;
+            var stop = false;
+            while (!stop && q.Any()) {
                 var (x, y, path) = q.Dequeue();
+                lastPath = path;
 
                 for (var i = 0; i < dirs.Length; i++) {
                     var (xT, yT) = (x + dirs[i].dx, y + dirs[i].dy);
@@ -94,18 +59,18 @@ namespace AdventOfCode.Y2019.Day15 {
                         seen.Add((xT, yT));
 
                         var pathT = path.Add(i + 1);
-
                         var res = walk(icm, pathT);
                         switch (res) {
                             case 0: break;
                             case 1: q.Enqueue((xT, yT, pathT)); break;
-                            case 2: return pathT;
+                            case 2: lastPath = pathT; stop = true; break;
                             default: throw new Exception();
                         }
                     }
                 }
 
             }
+            return lastPath;
         }
     }
 }
