aa2283a44efcb75451d6fd7ae929a065196b5c1a
David Nemeth Csoka <encse@csokavar.hu>
2019-12-22 14:31:08 +0100

    facepalm

--- 2019/Day22/Solution.cs
+++ 2019/Day22/Solution.cs
@@ -15,56 +15,50 @@ namespace AdventOfCode.Y2019.Day22 {
         }
 
         BigInteger PartOne(string input) {
-            var p = 10007;
+            var m = 10007;
             var iter = 1;
-            var (a, b) = Parse(input, p, iter);
-            return Mod(a * 2019 + b, p);
+            var (a, b) = Parse(input, m, iter);
+            return Mod(a * 2019 + b, m);
         }
 
         BigInteger PartTwo(string input) {
-            var p = 119315717514047;
+            var m = 119315717514047;
             var iter = 101741582076661;
-            var (a, b) = Parse(input, p, iter);
+            var (a, b) = Parse(input, m, iter);
 
-            var a_i = BigInteger.ModPow(a, p - 2, p);
-
-            // modular inverse a'la little Fermat: a_i * a ≡ 1 (m) 
-            return Mod(a_i * (2020 - b) , p); 
+            return Mod(ModInv(a, m) * (2020 - b), m);
         }
 
         BigInteger Mod(BigInteger a, BigInteger m) => ((a % m) + m) % m;
+        BigInteger ModInv(BigInteger a, BigInteger m) => BigInteger.ModPow(a, m - 2, m);
 
-        (BigInteger a, BigInteger big) Parse(string input, long m, long iter) {
+        (BigInteger a, BigInteger big) Parse(string input, long m, long n) {
             var a = new BigInteger(1);
             var b = new BigInteger(0);
 
             foreach (var line in input.Split('\n')) {
                 if (line.Contains("into new stack")) {
-                    a *= -1;
+                    a = -a;
                     b = m - b - 1;
                 } else if (line.Contains("cut")) {
-                    var n = long.Parse(Regex.Match(line, @"-?\d+").Value);
-                    b = b - n + m;
+                    var i = long.Parse(Regex.Match(line, @"-?\d+").Value);
+                    b = m + b - i;
                 } else if (line.Contains("increment")) {
-                    var n = long.Parse(Regex.Match(line, @"-?\d+").Value);
-                    a *= n;
-                    b *= n;
+                    var i = long.Parse(Regex.Match(line, @"-?\d+").Value);
+                    a *= i;
+                    b *= i;
                 } else {
                     throw new Exception();
                 }
             }
-           
+
             var resA = BigInteger.One;
             var resB = BigInteger.Zero;
-         
-            while (iter > 0) {
-                if (iter % 2 == 1) {
-                    (resA, resB) = ((a * resA) % m, (a * resB + b) % m);
-                }
 
-                (a, b) = ((a * a) % m, (a + 1) * b % m);
-                iter >>= 1;
-            }
+            // resA = a^n
+            resA = BigInteger.ModPow(a, n, m);
+            // resB = b * (1 + a + a^2 + ... a^n) = b * (a^n - 1) / (a - 1);
+            resB = b * (BigInteger.ModPow(a, n, m) - 1) * ModInv(a - 1, m) % m;
 
             return (resA, resB);
         }
