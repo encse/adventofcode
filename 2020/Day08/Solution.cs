07f89e2c7a8b52b37305465a2260bbb942ba3e83
David Nemeth Csoka <encse@csokavar.hu>
2020-12-08 13:11:16 +0100

    08

--- 2020/Day08/Solution.cs
+++ 2020/Day08/Solution.cs
@@ -5,68 +5,58 @@ using System.Linq;
 namespace AdventOfCode.Y2020.Day08 {
 
     record Stm(string op, int arg);
-    record State(int ip, int acc, bool terminated);
 
     [ProblemName("Handheld Halting")]
     class Solution : Solver {
-     
+
         public IEnumerable<object> Solve(string input) {
             yield return PartOne(input);
             yield return PartTwo(input);
         }
 
-        int PartOne(string input) {
-            var program = Parse(input);
+        int PartOne(string input) => Run(Parse(input)).acc;
 
-            var state = new State(0, 0, false);
-            for (var seen = new HashSet<int>(); !seen.Contains(state.ip);) {
-                seen.Add(state.ip);
-                state = Step(state, program);
-            }
+        int PartTwo(string input) =>
+            Patches(Parse(input))
+                .Select(Run)
+                .First(res => res.terminated).acc;
 
-            return state.acc;
-        }
-
-        int PartTwo(string input) {
-            foreach (var program in Patches(Parse(input))) {
-
-                var state = new State(0, 0, false);
-                for (var t = 0; t < 1000 /* big enough for this input */; t++) {
-                    state = Step(state, program);
-                }
-
-                if (state.terminated) {
-                    return state.acc;
-                }
-            }
-            throw new Exception();
-        }
+        Stm[] Parse(string input) =>
+            input.Split("\n")
+                .Select(line => line.Split(" "))
+                .Select(parts => new Stm(parts[0], int.Parse(parts[1])))
+                .ToArray();
 
         IEnumerable<Stm[]> Patches(Stm[] program) =>
-            from lineToPatch in Enumerable.Range(0, program.Length) 
-            where program[lineToPatch].op != "acc"
-            select 
-                program.Select((stm, line) => 
-                    line != lineToPatch ? stm :
-                    stm.op == "jmp"     ? stm with {op = "nop" }:
-                    stm.op == "nop"     ? stm with {op = "jmp" } :
-                    throw new Exception()
-                ).ToArray();
+            Enumerable.Range(0, program.Length)
+                .Where(line => program[line].op != "acc")
+                .Select(lineToPatch =>
+                    program.Select((stm, line) =>
+                        line != lineToPatch ? stm :
+                        stm.op == "jmp" ? stm with { op = "nop" } :
+                        stm.op == "nop" ? stm with { op = "jmp" } :
+                        throw new Exception()
+                    ).ToArray()
+                );
 
-        State Step(State state, Stm[] program) =>
-            state.terminated           ? state :
-            state.ip >= program.Length ? state with { terminated = true } :
-            program[state.ip] switch {
-                ("nop", _      ) => state with { ip = state.ip + 1 },
-                ("acc", var arg) => state with { ip = state.ip + 1, acc = state.acc + arg },
-                ("jmp", var arg) => state with { ip = state.ip + arg },
-                _ => throw new Exception()
-            };
+        (int acc, bool terminated) Run(Stm[] program) {
+            var (ip, acc, seen) = (0, 0, new HashSet<int>());
 
-        Stm[] Parse(string input) => (
-                from line in input.Split("\n")
-                let parts = line.Split(" ")
-                select new Stm(parts[0], int.Parse(parts[1]))
-            ).ToArray();
+            while (true) {
+                if (ip >= program.Length) {
+                    return (acc, true);
+                } else if (seen.Contains(ip)) {
+                    return (acc, false);
+                } else {
+                    seen.Add(ip);
+                    var stm = program[ip];
+                    switch (stm.op) {
+                        case "nop": ip++; break;
+                        case "acc": ip++; acc += stm.arg; break;
+                        case "jmp": ip += stm.arg; break;
+                    };
+                }
+            }
+        }
     }
 }
