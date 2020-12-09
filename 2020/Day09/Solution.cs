1fe8cec3f671cea348ee414e0ca0bb6336c84080
David Nemeth Csoka <encse@csokavar.hu>
2020-12-09 07:56:53 +0100

    09

--- 2020/Day09/Solution.cs
+++ 2020/Day09/Solution.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
 
 namespace AdventOfCode.Y2020.Day09 {
 
-    [ProblemName("Encoding Error")]      
+    [ProblemName("Encoding Error")]
     class Solution : Solver {
 
         public IEnumerable<object> Solve(string input) {
@@ -15,42 +12,33 @@ namespace AdventOfCode.Y2020.Day09 {
             yield return PartTwo(input);
         }
 
+        IEnumerable<int> Range(int min, int lim) => Enumerable.Range(min, lim - min);
+
         long PartOne(string input) {
-            var lines = input.Split("\n").Select(long.Parse).ToList();
-            var numbers = new Queue<long>(lines.Take(25));
-            for(var i=25;i<lines.Count;i++){
-                var num = lines[i];
-                var found = false;
-                for(var j =0;j<numbers.Count;j++){
-                    for(var k =j+1;k<numbers.Count;k++){
-                        if(numbers.ElementAt(j) + numbers.ElementAt(k) == num){
-                            found = true;
-                        }
-                    }
-                }
-                if(!found){
-                    return num;
-                }
-                numbers.Dequeue();
-                numbers.Enqueue(num);
-            }
-            throw new Exception();
+            var numbers = input.Split("\n").Select(long.Parse).ToArray();
+
+            bool Mismatch(int i) => (
+                from j in Range(i - 25, i)
+                from k in Range(j + 1, i)
+                select numbers[j] + numbers[k]
+            ).All(sum => sum != numbers[i]);
+
+            return numbers[Range(25, input.Length).First(Mismatch)];
         }
 
         long PartTwo(string input) {
             var d = PartOne(input);
-
             var lines = input.Split("\n").Select(long.Parse).ToList();
 
-            for(var j =0;j<lines.Count;j++){
+            foreach (var j in Range(0, lines.Count)) {
                 var s = lines[j];
-                for(var k =j+1;k<lines.Count;k++){
-                    s+= lines[k];
-
-                    if(s == d){
-                        return lines.Skip(j-1).Take(k-j+1).Min() + lines.Skip(j-1).Take(k-j+1).Max();
-                    } if(s>d) {
+                foreach (var k in Range(j + 1, lines.Count)) {
+                    s += lines[k];
+                    if (s > d) {
                         break;
+                    } else if (s == d) {
+                        var range = lines.GetRange(j, k - j + 1);
+                        return range.Min() + range.Max();
                     }
                 }
             }
