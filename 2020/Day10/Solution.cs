be1498d781744732575394b67e446cfb5c522d32
David Nemeth Csoka <encse@csokavar.hu>
2020-12-10 08:15:03 +0100

    10

--- 2020/Day10/Solution.cs
+++ 2020/Day10/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
 namespace AdventOfCode.Y2020.Day10 {
@@ -13,32 +14,37 @@ namespace AdventOfCode.Y2020.Day10 {
         }
 
         int PartOne(string input) {
-            var num = input.Split("\n").Select(int.Parse).OrderBy(x => x).ToList();
-            num.Insert(0, 0);
-            num.Add(num[^1] + 3);
-            var a = Enumerable.Range(1, num.Count - 1).Aggregate(0, (t, i) => t + (num[i] - num[i - 1] == 1 ? 1 : 0));
-            var b = Enumerable.Range(1, num.Count - 1).Aggregate(0, (t, i) => t + (num[i] - num[i - 1] == 3 ? 1 : 0));
-            return a * b;
+            var jolts = Parse(input);
+            var window = jolts.Skip(1).Zip(jolts).Select(p => (current: p.First, prev: p.Second));
+
+            return
+                 window.Count(pair => pair.current - pair.prev == 1) *
+                 window.Count(pair => pair.current - pair.prev == 3);
         }
 
         long PartTwo(string input) {
-            var num = input.Split("\n").Select(int.Parse).OrderBy(x => x).ToList();
-            num.Insert(0, 0);
-            num.Add(num[^1] + 3);
-
+            var jolts = Parse(input);
             var cache = new Dictionary<int, long>();
             long Rec(int prevI, int i) =>
-                i >= num.Count          ? 0 :
-                num[i] - num[prevI] > 3 ? 0 :
-                i == num.Count - 1      ? 1 :
-                /* otherwise */         cache.GetOrCompute(i, () => Rec(i, i + 1) + Rec(i, i + 2) + Rec(i, i + 3));
+                i >= jolts.Count            ? 0 :
+                jolts[i] - jolts[prevI] > 3 ? 0 :
+                i == jolts.Count - 1        ? 1 :
+                /* otherwise */             cache.GetOrCompute(i, () => Rec(i, i + 1) + Rec(i, i + 2) + Rec(i, i + 3));
 
-            return Rec(0,0);
+            return Rec(0, 0);
+        }
+
+        ImmutableList<int> Parse(string input) {
+            var num = input.Split("\n").Select(int.Parse).OrderBy(x => x);
+            return ImmutableList
+                .Create(0)
+                .AddRange(num)
+                .Add(num.Last() + 3);
         }
     }
 
     static class Cache {
-        public static B GetOrCompute<A,B>(this Dictionary<A,B> dict, A a, Func<B> compute){
+        public static B GetOrCompute<A, B>(this Dictionary<A, B> dict, A a, Func<B> compute) {
             if (!dict.ContainsKey(a)) {
                 dict[a] = compute();
             }
