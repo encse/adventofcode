66760cfff05ffc65f8f19b5a5d971aaf2eab41e9
David Nemeth Csoka <encse@csokavar.hu>
2020-12-12 10:28:11 +0100

    12 42 lines

--- 2020/Day12/Solution.cs
+++ 2020/Day12/Solution.cs
@@ -12,24 +12,25 @@ namespace AdventOfCode.Y2020.Day12 {
             yield return PartTwo(input);
         }
 
-        int PartOne(string input) => MoveShip(input, (1, 0), false);
-        int PartTwo(string input) => MoveShip(input, (10, 1), true);
+        int PartOne(string input) => MoveShip(input, true);
+        int PartTwo(string input) => MoveShip(input, false);
 
-        int MoveShip(string input, (int x, int y) dir, bool part2) =>
-            input.Split("\n")
-                .Select(line => ((ch: line[0], arg: int.Parse(line.Substring(1)))))
+        int MoveShip(string input, bool part1) =>
+            input
+                .Split("\n")
+                .Select(line => (line[0], int.Parse(line.Substring(1))))
                 .Aggregate(
-                    (pos: (x: 0, y: 0), dir: dir), 
+                    (pos: (x: 0, y: 0), dir: part1 ? (x: 1, y: 0) : (x: 10, y: 1)), 
                     (state, line) => 
                         line switch {
-                            ('N', var arg) when part2 => (state.pos, (state.dir.x, state.dir.y + arg)),
-                            ('N', var arg)            => ((state.pos.x,  state.pos.y + arg), state.dir),
-                            ('S', var arg) when part2 => (state.pos, (state.dir.x, state.dir.y - arg)),
-                            ('S', var arg)            => ((state.pos.x, state.pos.y - arg), state.dir),
-                            ('E', var arg) when part2 => (state.pos, (state.dir.x + arg, state.dir.y)),
-                            ('E', var arg)            => ((state.pos.x + arg,  state.pos.y), state.dir),
-                            ('W', var arg) when part2 => (state.pos, (state.dir.x -line.arg, state.dir.y)), 
-                            ('W', var arg)            => ((state.pos.x - arg, state.pos.y), state.dir),
+                            ('N', var arg) when part1 => ((state.pos.x,  state.pos.y + arg), state.dir),
+                            ('N', var arg)            => (state.pos, (state.dir.x, state.dir.y + arg)),
+                            ('S', var arg) when part1 => ((state.pos.x, state.pos.y - arg), state.dir),
+                            ('S', var arg)            => (state.pos, (state.dir.x, state.dir.y - arg)),
+                            ('E', var arg) when part1 => ((state.pos.x + arg,  state.pos.y), state.dir),
+                            ('E', var arg)            => (state.pos, (state.dir.x + arg, state.dir.y)),
+                            ('W', var arg) when part1 => ((state.pos.x - arg, state.pos.y), state.dir),
+                            ('W', var arg)            => (state.pos, (state.dir.x - arg, state.dir.y)), 
                             ('F', var arg)            => ((state.pos.x + arg * state.dir.x, state.pos.y + arg * state.dir.y), state.dir),
                             ('L', 90)  or ('R', 270)  => (state.pos, (-state.dir.y,  state.dir.x)),
                             ('L', 270) or ('R', 90)   => (state.pos, ( state.dir.y, -state.dir.x)),
