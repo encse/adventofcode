d6f0b779a66b7f465b6a4ca891201d250633a182
David Nemeth Csoka <encse@csokavar.hu>
2020-12-12 07:40:00 +0100

    12

--- 2020/Day12/Solution.cs
+++ 2020/Day12/Solution.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 namespace AdventOfCode.Y2020.Day12 {
 
     record Point(int x, int y);
+    record State(Point p, Point wp);
 
     [ProblemName("Rain Risk")]
     class Solution : Solver {
@@ -13,40 +14,41 @@ namespace AdventOfCode.Y2020.Day12 {
             yield return PartTwo(input);
         }
 
-        int PartOne(string input) => Ship(input, new Point(1, 0), false);
-        int PartTwo(string input) => Ship(input, new Point(10, 1), true);
+        int PartOne(string input) => MoveShip(input, new Point(1, 0), false);
+        int PartTwo(string input) => MoveShip(input, new Point(10, 1), true);
 
-        int Ship(string input, Point wp, bool part2) {
-            var p = new Point(0, 0);
+        int MoveShip(string input, Point wp, bool part2) {
+            var state = new State(new Point(0, 0), wp);
+
             foreach (var line in input.Split("\n")) {
                 var (ch, arg) = (line[0], int.Parse(line.Substring(1)));
 
-                Point NSEW(Point p) =>
-                    ch == 'N' ? new Point(p.x, p.y + arg) :
-                    ch == 'S' ? new Point(p.x, p.y - arg) :
-                    ch == 'E' ? new Point(p.x + arg, p.y) :
-                    ch == 'W' ? new Point(p.x - arg, p.y) :
-                    p;
-
-                if (part2) {
-                    wp = NSEW(wp);
-                } else {
-                    p = NSEW(p);
-                }
-
-                p = ch == 'F' ? new Point(p.x + wp.x * arg, p.y + wp.y * arg) : p;
-
-                wp = (ch, arg) switch {
-                    (_, 180) => new Point(-wp.x, -wp.y),
-                    ('R', 90) => new Point(wp.y, -wp.x),
-                    ('L', 270) => new Point(wp.y, -wp.x),
-                    ('L', 90) => new Point(-wp.y, wp.x),
-                    ('R', 270) => new Point(-wp.y, wp.x),
-                    _ => wp
+                Point NSEW(Point p) => ch switch {
+                    'N' => new Point(p.x, p.y + arg),
+                    'S' => new Point(p.x, p.y - arg),
+                    'E' => new Point(p.x + arg, p.y),
+                    'W' => new Point(p.x - arg, p.y),
+                    _ => p
                 };
 
+                if (part2) {
+                    state = state with {wp = NSEW(state.wp)};
+                } else {
+                    state = state with {p = NSEW(state.p)};
+                }
+                
+                state = (ch, arg) switch {
+                    ('F',   _) => state with { p = new Point( state.p.x + state.wp.x * arg, state.p.y + state.wp.y * arg)},
+                    ('R',  90) => state with {wp = new Point( state.wp.y, -state.wp.x)},
+                    ('R', 180) => state with {wp = new Point(-state.wp.x, -state.wp.y)},
+                    ('R', 270) => state with {wp = new Point(-state.wp.y, state.wp.x)},
+                    ('L',  90) => state with {wp = new Point(-state.wp.y, state.wp.x)},
+                    ('L', 180) => state with {wp = new Point(-state.wp.x, -state.wp.y)},
+                    ('L', 270) => state with {wp = new Point( state.wp.y, -state.wp.x)},
+                    _ => state
+                };
             }
-            return Math.Abs(p.x) + Math.Abs(p.y);
+            return Math.Abs(state.p.x) + Math.Abs(state.p.y);
         }
     }
 }
