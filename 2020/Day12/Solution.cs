4542af19c1f2c6824626136a630e5bf8b927e8b7
David Nemeth Csoka <encse@csokavar.hu>
2020-12-12 09:35:57 +0100

    12

--- 2020/Day12/Solution.cs
+++ 2020/Day12/Solution.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace AdventOfCode.Y2020.Day12 {
@@ -11,45 +12,43 @@ namespace AdventOfCode.Y2020.Day12 {
             yield return PartTwo(input);
         }
 
-        int PartOne(string input) => MoveShip(input, new State(new Vec2(0, 0), new Vec2(1, 0)), false);
-        int PartTwo(string input) => MoveShip(input, new State(new Vec2(0, 0), new Vec2(10, 1)), true);
+        int PartOne(string input) => MoveShip(input, (1, 0), false);
+        int PartTwo(string input) => MoveShip(input, (10, 1), true);
 
-        int MoveShip(string input, State state, bool part2) {
-            foreach (var line in input.Split("\n")) {
-                var (ch, arg) = (line[0], int.Parse(line.Substring(1)));
-
-                state = ch switch {
-                    'N' when part2 => state with { dir = state.dir + new Vec2(0, arg) },
-                    'N'            => state with { pos = state.pos + new Vec2(0, arg) },
-                    'S' when part2 => state with { dir = state.dir + new Vec2(0, -arg) },
-                    'S'            => state with { pos = state.pos + new Vec2(0, -arg) },
-                    'E' when part2 => state with { dir = state.dir + new Vec2(arg, 0) }, 
-                    'E'            => state with { pos = state.pos + new Vec2(arg, 0) },
-                    'W' when part2 => state with { dir = state.dir + new Vec2(-arg, 0) }, 
-                    'W'            => state with { pos = state.pos + new Vec2(-arg, 0) },
-                    'F'            => state with { pos = state.pos + arg * state.dir },
-                    'L'            => state with { dir = state.dir.Rotate(arg) },
-                    'R'            => state with { dir = state.dir.Rotate(360 - arg) },
-                    _ => throw new Exception()
-                };
-            }
-            return Math.Abs(state.pos.x) + Math.Abs(state.pos.y);
-        }
+        int MoveShip(string input, Vec2 dir, bool part2) =>
+            input.Split("\n")
+                .Select(line => ((ch: line[0], arg: int.Parse(line.Substring(1)))))
+                .Aggregate(
+                    (pos: new Vec2(0,0), dir: dir), 
+                    (state, line) => 
+                        line.ch switch {
+                            'N' when part2 => (state.pos, state.dir + (0,  line.arg)),
+                            'N'            => (state.pos + (0,  line.arg), state.dir),
+                            'S' when part2 => (state.pos, state.dir + (0, -line.arg)),
+                            'S'            => (state.pos + (0, -line.arg), state.dir),
+                            'E' when part2 => (state.pos, state.dir + ( line.arg, 0)), 
+                            'E'            => (state.pos + ( line.arg, 0), state.dir),
+                            'W' when part2 => (state.pos, state.dir + (-line.arg, 0)), 
+                            'W'            => (state.pos + (-line.arg, 0), state.dir),
+                            'F'            => (state.pos + line.arg * state.dir, state.dir),
+                            'L'            => (state.pos, state.dir.Rotate(line.arg)),
+                            'R'            => (state.pos, state.dir.Rotate(360 - line.arg)),
+                            _ => throw new Exception()
+                        }, 
+                    state => Math.Abs(state.pos.x) + Math.Abs(state.pos.y));
     }
-    
+
     record Vec2(int x, int y) {
 
-        public static Vec2 operator +(Vec2 a, Vec2 b) => new Vec2(a.x + b.x, a.y + b.y);
-
-        public static Vec2 operator *(int m, Vec2 v) => new Vec2(m * v.x, m * v.y);
+        public static implicit operator Vec2((int x, int y) p) => new Vec2(p.x, p.y);
+        public static Vec2 operator +(Vec2 a, Vec2 b) => (a.x + b.x, a.y + b.y);
+        public static Vec2 operator *(int m, Vec2 v) => (m * v.x, m * v.y);
 
         public Vec2 Rotate(int arg) => arg switch {
-            90 => new Vec2(-y, x),
-            180 => new Vec2(-x, -y),
-            270 => new Vec2(y, -x),
+            90 => (-y, x),
+            180 => (-x, -y),
+            270 => (y, -x),
             _ => throw new Exception()
         };
     }
-
-    record State(Vec2 pos, Vec2 dir);
 }
