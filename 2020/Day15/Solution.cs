3843e33d7d88ef42f7ca44d01c9c2feddd4fa287
David Nemeth Csoka <encse@csokavar.hu>
2020-12-15 12:14:36 +0100

    15

--- 2020/Day15/Solution.cs
+++ 2020/Day15/Solution.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Y2020.Day15 {
@@ -10,19 +9,13 @@ namespace AdventOfCode.Y2020.Day15 {
         public object PartTwo(string input) => NumberAt(input, 30000000);
 
         public int NumberAt(string input, int count) {
-            var nums = input.Split(",").Select(int.Parse).ToArray();
-            var spoken = new Dictionary<int, int>();
-            var prev = -1;
-            for (var i = 0; i < count; i++) {
-                var cur = 
-                    i < nums.Length          ? nums[i] :
-                    spoken.ContainsKey(prev) ? i - spoken[prev] :
-                    /*otherwise*/            0;
-
-                spoken[prev] = i; 
-                prev = cur;
+            var numbers = input.Split(",").Select(int.Parse).ToArray();
+            var lastSeen = numbers.Select((num, i) => (num, i)).ToDictionary(kvp => kvp.num, kvp => kvp.i + 1);
+            var number = numbers.Last();
+            for (var round = lastSeen.Count; round < count; round++) {
+                (lastSeen[number], number) = (round, lastSeen.ContainsKey(number) ? round - lastSeen[number] : 0);
             }
-            return prev;
-        } 
+            return number;
+        }
     }
 }
