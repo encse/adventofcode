7b2120d49a115cbac5bbd5b4a51be8aaedae10b9
David Nemeth Csoka <encse@csokavar.hu>
2020-12-17 07:58:21 +0100

    17 more speed up

--- 2020/Day17/Solution.cs
+++ 2020/Day17/Solution.cs
@@ -6,17 +6,16 @@ namespace AdventOfCode.Y2020.Day17 {
 
     [ProblemName("Conway Cubes")]
     class Solution : Solver {
-
         public object PartOne(string input) {
             var ds = (from dx in new[] { -1, 0, 1 }
                       from dy in new[] { -1, 0, 1 }
                       from dz in new[] { -1, 0, 1 }
-                      where dx != 0 || dy != 0 || dz != 0 
+                      where dx != 0 || dy != 0 || dz != 0
                       select (dx, dy, dz)).ToArray();
             return Solve(
                 input,
                 (x, y) => (x: x, y: y, z: 0),
-                (p) => from d in ds select (p.x + d.dx, p.y + d.dy, p.z + d.dz));
+                (p) => ds.Select(d => (p.x + d.dx, p.y + d.dy, p.z + d.dz)));
         }
 
         public object PartTwo(string input) {
@@ -24,13 +23,13 @@ namespace AdventOfCode.Y2020.Day17 {
                       from dy in new[] { -1, 0, 1 }
                       from dz in new[] { -1, 0, 1 }
                       from dw in new[] { -1, 0, 1 }
-                      where dx != 0 || dy != 0 || dz != 0 || dw != 0 
+                      where dx != 0 || dy != 0 || dz != 0 || dw != 0
                       select (dx, dy, dz, dw)).ToArray();
 
             return Solve(
                 input,
                 (x, y) => (x: x, y: y, z: 0, w: 0),
-                (p) => from d in ds select (p.x + d.dx, p.y + d.dy, p.z + d.dz, p.w + d.dw));
+                (p) => ds.Select(d => (p.x + d.dx, p.y + d.dy, p.z + d.dz, p.w + d.dw)));
         }
 
         private int Solve<T>(string input, Func<int, int, T> create, Func<T, IEnumerable<T>> neighbours) {
@@ -46,20 +45,26 @@ namespace AdventOfCode.Y2020.Day17 {
             }
             for (var i = 0; i < 6; i++) {
                 var newActivePoints = new HashSet<T>();
-                var seenPoints = new HashSet<T>();
-                foreach (var p in activePoints) {
-                    var activeNeighbours = neighbours(p).Count(n => activePoints.Contains(n));
-                    if (activeNeighbours == 2 || activeNeighbours == 3) {
-                        newActivePoints.Add(p);
-                    }
-                    foreach (var pT in neighbours(p)) {
-                        if (!activePoints.Contains(pT) && !seenPoints.Contains(pT)) {
-                            seenPoints.Add(pT);
-                            activeNeighbours = neighbours(pT).Count(n => activePoints.Contains(n));
-                            if (activeNeighbours == 3) {
-                                newActivePoints.Add(pT);
-                            }
+                var inactivePoints = new Dictionary<T, int>();
+
+                foreach (var point in activePoints) {
+                    var activeNeighbours = 0;
+                    foreach (var neighbour in neighbours(point)) {
+                        if (activePoints.Contains(neighbour)) {
+                            activeNeighbours++;
+                        } else {
+                            inactivePoints[neighbour] = inactivePoints.GetValueOrDefault(neighbour) + 1;
                         }
+                    }
+
+                    if (activeNeighbours == 2 || activeNeighbours == 3) {
+                        newActivePoints.Add(point);
+                    }
+                }
+
+                foreach (var (point, activeNeighbours) in inactivePoints) {
+                    if (activeNeighbours == 3) {
+                        newActivePoints.Add(point);
                     }
                 }
                 activePoints = newActivePoints;
