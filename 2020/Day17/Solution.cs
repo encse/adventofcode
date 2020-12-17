998f1b4223bee5393ce38870fea6805f00ef63d3
David Nemeth Csoka <encse@csokavar.hu>
2020-12-17 07:38:02 +0100

    17

--- 2020/Day17/Solution.cs
+++ 2020/Day17/Solution.cs
@@ -1,102 +1,67 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
 
 namespace AdventOfCode.Y2020.Day17 {
 
     [ProblemName("Conway Cubes")]
     class Solution : Solver {
 
-        private IEnumerable<(int, int, int)> Neighbours((int x, int y, int z) p) {
-            foreach (var dx in new[] { -1, 0, 1 }) {
-                foreach (var dy in new[] { -1, 0, 1 }) {
-                    foreach (var dz in new[] { -1, 0, 1 }) {
-                        if (dx != 0 || dy != 0 || dz != 0) {
-                            yield return (p.x + dx, p.y + dy, p.z + dz);
-                        }
-                    }
-                }
-            }
-        }
+        public object PartOne(string input) =>
+            Solve<(int x, int y, int z)>(
+                input, 
+                (x, y) => (x, y, 0), 
+                (p) =>
+                   from dx in new[] { -1, 0, 1 }
+                   from dy in new[] { -1, 0, 1 }
+                   from dz in new[] { -1, 0, 1 }
+                   where dx != 0 || dy != 0 || dz != 0
+                   select (p.x + dx, p.y + dy, p.z + dz));
 
-        private IEnumerable<(int, int, int, int)> Neighbours2((int x, int y, int z, int w) p) {
-            foreach (var dx in new[] { -1, 0, 1 }) {
-                foreach (var dy in new[] { -1, 0, 1 }) {
-                    foreach (var dz in new[] { -1, 0, 1 }) {
-                        foreach (var dw in new[] { -1, 0, 1 }) {
-                            if (dx != 0 || dy != 0 || dz != 0 || dw != 0) {
-                                yield return (p.x + dx, p.y + dy, p.z + dz, p.w + dw);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        public object PartOne(string input) {
-            var m = new HashSet<(int x, int y, int z)>();
+        public object PartTwo(string input) =>
+            Solve<(int x, int y, int z, int w)>(
+                input, 
+                (x, y) => (x, y, 0, 0), 
+                (p) =>
+                   from dx in new[] { -1, 0, 1 }
+                   from dy in new[] { -1, 0, 1 }
+                   from dz in new[] { -1, 0, 1 }
+                   from dw in new[] { -1, 0, 1 }
+                   where dx != 0 || dy != 0 || dz != 0 || dw != 0
+                   select (p.x + dx, p.y + dy, p.z + dz, p.w + dw));
+
+        private int Solve<T>(string input, Func<int, int, T> create, Func<T, IEnumerable<T>> neighbours) {
+            var activePoints = new HashSet<T>();
             var lines = input.Split("\n");
             var (height, width) = (lines.Length, lines[0].Length);
             for (var y = 0; y < height; y++) {
                 for (var x = 0; x < width; x++) {
                     if (lines[y][x] == '#') {
-                        m.Add((x, y, 0));
+                        activePoints.Add(create(x, y));
                     }
                 }
             }
             for (var i = 0; i < 6; i++) {
-                var newM = new HashSet<(int x, int y, int z)>();
-                foreach (var p in m) {
-                    var activeNeighbours = Neighbours(p).Count(n => m.Contains(n));
+                var newActivePoints = new HashSet<T>();
+                var seenPoints = new HashSet<T>();
+                foreach (var p in activePoints) {
+                    var activeNeighbours = neighbours(p).Count(n => activePoints.Contains(n));
                     if (activeNeighbours == 2 || activeNeighbours == 3) {
-                        newM.Add(p);
+                        newActivePoints.Add(p);
                     }
-                    foreach (var pT in Neighbours(p)) {
-                        if (!m.Contains(pT)) {
-                            activeNeighbours = Neighbours(pT).Count(n => m.Contains(n));
+                    foreach (var pT in neighbours(p)) {
+                        if (!activePoints.Contains(pT) && !seenPoints.Contains(pT)) {
+                            seenPoints.Add(pT);
+                            activeNeighbours = neighbours(pT).Count(n => activePoints.Contains(n));
                             if (activeNeighbours == 3) {
-                                newM.Add(pT);
+                                newActivePoints.Add(pT);
                             }
                         }
                     }
                 }
-                m = newM;
+                activePoints = newActivePoints;
             }
-            return m.Count();
-        }
-
-        public object PartTwo(string input) {
-             var m = new HashSet<(int x, int y, int z, int w)>();
-            var lines = input.Split("\n");
-            var (height, width) = (lines.Length, lines[0].Length);
-            for (var y = 0; y < height; y++) {
-                for (var x = 0; x < width; x++) {
-                    if (lines[y][x] == '#') {
-                        m.Add((x, y, 0, 0));
-                    }
-                }
-            }
-            for (var i = 0; i < 6; i++) {
-                var newM = new HashSet<(int x, int y, int z, int w)>();
-                foreach (var p in m) {
-                    var activeNeighbours = Neighbours2(p).Count(n => m.Contains(n));
-                    if (activeNeighbours == 2 || activeNeighbours == 3) {
-                        newM.Add(p);
-                    }
-                    foreach (var pT in Neighbours2(p)) {
-                        if (!m.Contains(pT)) {
-                            activeNeighbours = Neighbours2(pT).Count(n => m.Contains(n));
-                            if (activeNeighbours == 3) {
-                                newM.Add(pT);
-                            }
-                        }
-                    }
-                }
-                m = newM;
-            }
-            return m.Count();
+            return activePoints.Count();
         }
     }
 }
