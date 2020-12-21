8834369e54a08b5e634ae1c86820cefe5eeace0f
David Nemeth Csoka <encse@csokavar.hu>
2020-12-21 09:34:20 +0100

    21

--- 2020/Day21/Solution.cs
+++ 2020/Day21/Solution.cs
@@ -1,97 +1,70 @@
-using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
 
 namespace AdventOfCode.Y2020.Day21 {
+
+    record Problem(
+        HashSet<string> allergens, 
+        HashSet<string> ingredients, 
+        (HashSet<string> ingredients, HashSet<string> allergens)[] mapping);
 
     [ProblemName("Allergen Assessment")]
     class Solution : Solver {
 
         public object PartOne(string input) {
-            var allIngredients = new HashSet<string>();
-            var allAllergens = new HashSet<string>();
-
-            foreach (var line in input.Split("\n")) {
-                var parts = line.Trim(')').Split(" (contains ");
-                allIngredients.UnionWith(parts[0].Split(" "));
-                allAllergens.UnionWith(parts[1].Split(", "));
-            }
-
-            var map = new Dictionary<string, HashSet<string>>();
-
-            foreach (var allergen in allAllergens) {
-                var candidates = allIngredients.ToHashSet();
-                foreach (var line in input.Split("\n")) {
-                    var parts = line.Trim(')').Split(" (contains ");
-                    var ingerients = parts[0].Split(" ").ToHashSet();
-                    var allergens = parts[1].Split(", ").ToHashSet();
-
-                    if (allergens.Contains(allergen)) {
-                        candidates.IntersectWith(ingerients);
-                    }
-                }
-                map[allergen] = candidates;
-            }
-
-            var suspicious = new HashSet<string>();
-            foreach (var ingredients in map.Values) {
-                suspicious.UnionWith(ingredients);
-            }
-
-            var c = 0;
-            foreach (var line in input.Split("\n")) {
-                var parts = line.Trim(')').Split(" (contains ");
-                var ingerients = parts[0].Split(" ").ToHashSet();
-                c += ingerients.Count(ingredient => !suspicious.Contains(ingredient));
-            }
-
-            return c;
+            var problem = Parse(input);
+            var suspiciousIngredients = UnionAll(GetIngredientsByAllergene(problem).Values);
+            return problem.mapping.Select(entry => entry.ingredients.Except(suspiciousIngredients).Count()).Sum();
         }
 
         public object PartTwo(string input) {
-            var allIngredients = new HashSet<string>();
-            var allAllergens = new HashSet<string>();
-
-            foreach (var line in input.Split("\n")) {
-                var parts = line.Trim(')').Split(" (contains ");
-                allIngredients.UnionWith(parts[0].Split(" "));
-                allAllergens.UnionWith(parts[1].Split(", "));
-            }
-
-            var map = new Dictionary<string, HashSet<string>>();
-
-            foreach (var allergen in allAllergens) {
-                map[allergen] = allIngredients.ToHashSet();
-            }
-
-            while (map.Values.Any(v => v.Count > 1)) {
-                foreach (var allergen in allAllergens) {
-                    var candidates = map[allergen];
+            var problem = Parse(input);
+            var ingredientsByAllergene = GetIngredientsByAllergene(problem);
+            
+            // The problem is set up in a way that we identify the allergene - ingredient pairs one by one. 
+            while (ingredientsByAllergene.Values.Any(ingredients => ingredients.Count > 1)) {
+                foreach (var allergen in problem.allergens) {
+                    var candidates = ingredientsByAllergene[allergen];
                     if (candidates.Count == 1) {
-                        foreach(var allergenT in allAllergens){
-                            if(allergen != allergenT){
-                                map[allergenT].Remove(candidates.Single());
-                            }
-                        }
-                    } else {
-                        foreach (var line in input.Split("\n")) {
-                            var parts = line.Trim(')').Split(" (contains ");
-                            var ingerients = parts[0].Split(" ").ToHashSet();
-                            var allergens = parts[1].Split(", ").ToHashSet();
-
-                            if (allergens.Contains(allergen)) {
-                                candidates.IntersectWith(ingerients);
+                        foreach (var allergenT in problem.allergens) {
+                            if (allergen != allergenT) {
+                                ingredientsByAllergene[allergenT].Remove(candidates.Single());
                             }
                         }
                     }
                 }
-
             }
 
-            return string.Join(",", allAllergens.OrderBy(a => a).Select(a => map[a].Single()));
+            return string.Join(",", problem.allergens.OrderBy(a => a).Select(a => ingredientsByAllergene[a].Single()));
         }
+
+        private Problem Parse(string input) {
+            var mapping = (
+                from line in input.Split("\n")
+                    let parts = line.Trim(')').Split(" (contains ")
+                    let ingredients = parts[0].Split(" ").ToHashSet()
+                    let allergens = parts[1].Split(", ").ToHashSet()
+                select (ingredients, allergens)
+            ).ToArray();
+
+            return new Problem(
+                UnionAll(from line in mapping select line.allergens),
+                UnionAll(from line in mapping select line.ingredients),
+                mapping
+            );
+        }
+
+        private Dictionary<string, HashSet<string>> GetIngredientsByAllergene(Problem problem) =>
+            problem.allergens.ToDictionary(
+                allergene => allergene, 
+                allergene => problem.mapping
+                    .Where(entry => entry.allergens.Contains(allergene))
+                    .Aggregate(
+                        problem.ingredients as IEnumerable<string>,
+                        (res, entry) => res.Intersect(entry.ingredients))
+                    .ToHashSet());
+
+        private HashSet<string> UnionAll(IEnumerable<HashSet<string>> sets) =>
+            sets.Aggregate<IEnumerable<string>>((result, set) => result.Union(set)).ToHashSet();
     }
 }
