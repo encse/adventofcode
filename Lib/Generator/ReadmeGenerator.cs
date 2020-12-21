87f9518df6fb139283481a4220406503f86803e4
David Nemeth Csoka <encse@csokavar.hu>
2020-12-21 20:43:07 +0100

    readme

--- Lib/Generator/ReadmeGenerator.cs
+++ Lib/Generator/ReadmeGenerator.cs
@@ -11,6 +11,30 @@ namespace AdventOfCode.Generator {
                > C# solutions to the Advent of Code problems.
                > Check out https://adventofcode.com.
                > ![](demo.gif)
+               > 
+               > I worked as a .Net developer for more than 10 years before switching to TypeScript. This repository is to
+               > keep my C# knowledge fresh and to follow the latest changes of the language.
+               > 
+               > No rules are written in stone but I definitely don't use any external dependencies only what .Net provides.
+               > No fancy libraries to make things short or predefined algorithms to parameterize.
+               > 
+               > Everything is self contained. Each problem is solved by plain C# classes without any 'base' to derive from.
+               > The solvers have distinct entry points for part 1 and 2. There is no local state, part 2 starts from scratch, 
+               > but code sharing between part 1 and 2 is important to me. (Unless it makes things hard to read.)
+               > 
+               > I prefer to use functional style, local or anonymous functions, immutability and linq over the state manipulation 
+               > style of oop, but I'm not very strict about this. Whatever I see fit for the problem.
+               > 
+               > One thing that you dont see much in C# projects is K-R style parentheses alignment. Sorry about that...
+               > 
+               > My programming style should be pretty consistent during an event but I'm sure there are changes between 
+               > the years as I find something new or forget about stuff I learned last year.
+               > 
+               > I try to keep things tight and golf the solution to a certain level, but don't want to overgolf it. (Sometimes I fail.)
+               > 
+               > I don't use many comments, but if I find that the solution is not straightforward, the algorithm has a name, or it is 
+               > using some special property of the input I might explain it in a line or two. 
+               > 
                > ## Dependencies
 
                > - This project is based on `.NET 5`. It should work on Windows, Linux and OS X.
