1de603e983138fe56d141fffe458ff8945fd3cf8
David Nemeth Csoka <encse@csokavar.hu>
2020-12-25 07:39:27 +0100

    25

--- 2020/Day25/Solution.cs
+++ 2020/Day25/Solution.cs
@@ -1,34 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
 
 namespace AdventOfCode.Y2020.Day25 {
 
-    [ProblemName("Combo Breaker")]      
+    [ProblemName("Combo Breaker")]
     class Solution : Solver {
 
         public object PartOne(string input) {
             var numbers = input.Split("\n").Select(int.Parse).ToArray();
-            
+            var mod = 20201227;
             var loop = 0;
-            var subj  = 7L;
-            var card = subj;
-            var door = subj;
-            while(card != numbers[0] && door != numbers[1]){
-                card = (card * subj) % 20201227;
-                door = (door * subj) % 20201227;
+            var subj = 7L;
+            var num = subj;
+            while (num != numbers[0] && num != numbers[1]) {
+                num = (num * subj) % mod;
                 loop++;
             }
-            subj = card == numbers[0] ? numbers[1] : numbers[0];
-            var d = subj;
-            while(loop > 0){
-                d = (d * subj) % 20201227;
-                loop --;
+
+            subj = num == numbers[0] ? numbers[1] : numbers[0];
+            num = subj;
+            while (loop > 0) {
+                num = (num * subj) % mod;
+                loop--;
             }
-            return d;
+            return num;
         }
 
     }
