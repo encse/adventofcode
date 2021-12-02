30d3df76b62c9db03c8287c63bcdb4180471df82
David Nemeth Cs <encse@csokavar.hu>
2021-12-02 07:17:31 +0100

    day1 cleanup

--- 2021/Day01/Solution.cs
+++ 2021/Day01/Solution.cs
@@ -6,15 +6,18 @@ namespace AdventOfCode.Y2021.Day01;
 [ProblemName("Sonar Sweep")]
 class Solution : Solver {
 
-    public object PartOne(string input) => CountIncreases(Numbers(input));
+    public object PartOne(string input) => DepthIncreaseCount(Numbers(input));
 
-    public object PartTwo(string input) => CountIncreases(Blocks(Numbers(input)));
+    public object PartTwo(string input) => DepthIncreaseCount(ThreeMeasurements(Numbers(input)));
 
-    int[] Blocks(int[] ns) => (from r in Window(3, ns) select r.Sum()).ToArray();
+    int DepthIncreaseCount(int[] ns) => Window(2, ns).Count(p => p[0] < p[1]);
 
-    int CountIncreases(int[] ns) => Window(2, ns).Count(p => p[0] < p[1]);
+    // the sum of elements in a sliding window of 3
+    int[] ThreeMeasurements(int[] ns) => (from r in Window(3, ns) select r.Sum()).ToArray();
 
-    IEnumerable<int[]> Window(int w, int[] ns) => Enumerable.Range(0, ns.Length - w + 1).Select(k => ns[k..(k + w)]);
-
+    // parse input to array of numbers
     int[] Numbers(string input) => (from n in input.Split('\n') select int.Parse(n)).ToArray();
+
+    // create a w wide sliding window from the elements of the array
+    IEnumerable<int[]> Window(int w, int[] ns) => Enumerable.Range(0, ns.Length - w + 1).Select(k => ns[k..(k + w)]);
 }
