cf3205c63d8704e22373ca6bb9614dfee7a5dd56
David Nemeth Csoka <encse@csokavar.hu>
2021-12-04 15:53:44 +0100

    day 04 cleanup

--- 2021/Day04/Solution.cs
+++ 2021/Day04/Solution.cs
@@ -1,31 +1,32 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Y2021.Day04;
 
-[ProblemName("Giant Squid")]
+[ProblemName("Giant Squid")] 
 class Solution : Solver {
 
-    public object PartOne(string input) => BoardsInWinningOrder(input).First().point;
-    public object PartTwo(string input) => BoardsInWinningOrder(input).Last().point;
+    public object PartOne(string input) => BoardsInOrderOfCompletion(input).First().point;
+    public object PartTwo(string input) => BoardsInOrderOfCompletion(input).Last().point;
 
-    IEnumerable<Board> BoardsInWinningOrder(string input) {
+    IEnumerable<BingoBoard> BoardsInOrderOfCompletion(string input) {
 
         var lines = input.Split("\n");
 
-        // first line contains the numbers
+        // first line contains the numbers to be drawn
         var numbers = lines[0].Split(",").Select(int.Parse);
 
-        // followed by an empty line and blocks of 6 lines, each describing a board:
-        var boards = new List<Board>();
-        for (var i = 2; i < lines.Length; i += 6) {
-            boards.Add(new Board(lines[i..(i + 5)]));
-        }
+        // followed by an empty line and blocks of 6 lines, each describing a bingo board:
+        var boards = (
+            from st in lines.Skip(2).Chunk(6)
+            select new BingoBoard(st)
+        ).ToHashSet();
 
         // let's play the game
-        foreach (var num in numbers) {
+        foreach (var number in numbers) {
             foreach (var board in boards.ToArray()) {
-                board.AddNumber(num);
+                board.AddNumber(number);
                 if (board.point > 0) {
                     yield return board;
                     boards.Remove(board);
@@ -37,10 +38,10 @@ class Solution : Solver {
 
 record Cell(int number, bool marked = false);
 
-// Let's be ho-ho-ooop this time.
-class Board {
+// Let's be ho-ho-hoOOP this time.
+class BingoBoard {
 
-    public int point { get; private set;}
+    public int point { get; private set; }
     private List<Cell> cells;
 
     IEnumerable<Cell> CellsInRow(int irow) =>
@@ -49,12 +50,11 @@ class Board {
     IEnumerable<Cell> CellsInCol(int icol) =>
         from irow in Enumerable.Range(0, 5) select cells[irow * 5 + icol];
 
-    public Board(string[] lines) {
+    public BingoBoard(string[] lines) {
 
         // split the input into words & read the numbers into cells
         cells = (
-            from word in string.Join(" ", lines).Split(" ")
-            where word != ""
+            from word in string.Join(" ", lines).Split(" ", StringSplitOptions.RemoveEmptyEntries)
             select new Cell(int.Parse(word))
         ).ToList();
     }
