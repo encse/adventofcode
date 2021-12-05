ae11f90ffd1837f297ea3fd947fd504b96785189
David Nemeth Csoka <encse@csokavar.hu>
2021-12-05 07:51:15 +0100

    day 04 cleanup

--- 2021/Day04/Solution.cs
+++ 2021/Day04/Solution.cs
@@ -7,27 +7,22 @@ namespace AdventOfCode.Y2021.Day04;
 [ProblemName("Giant Squid")] 
 class Solution : Solver {
 
-    public object PartOne(string input) => BoardsInOrderOfCompletion(input).First().point;
-    public object PartTwo(string input) => BoardsInOrderOfCompletion(input).Last().point;
+    public object PartOne(string input) => BoardsInOrderOfCompletion(input).First().score;
+    public object PartTwo(string input) => BoardsInOrderOfCompletion(input).Last().score;
 
     IEnumerable<BingoBoard> BoardsInOrderOfCompletion(string input) {
 
-        var lines = input.Split("\n");
+        var blocks = input.Split("\n\n");
 
-        // first line contains the numbers to be drawn
-        var numbers = lines[0].Split(",").Select(int.Parse);
-
-        // followed by an empty line and blocks of 6 lines, each describing a bingo board:
-        var boards = (
-            from st in lines.Skip(2).Chunk(6) // .Net 6 introduced Chunk()
-            select new BingoBoard(st)
-        ).ToHashSet();
+        // first block contains the numbers to be drawn, rest describe bingo boards:
+        var numbers = blocks[0].Split(",");
+        var boards = (from block in blocks.Skip(1) select new BingoBoard(block)).ToHashSet();
 
         // let's play the game
         foreach (var number in numbers) {
             foreach (var board in boards.ToArray()) {
                 board.AddNumber(number);
-                if (board.point > 0) {
+                if (board.score > 0) {
                     yield return board;
                     boards.Remove(board);
                 }
@@ -36,12 +31,12 @@ class Solution : Solver {
     }
 }
 
-record Cell(int number, bool marked = false);
+record Cell(string number, bool marked = false);
 
 // Let's be ho-ho-hoOOP this time.
 class BingoBoard {
 
-    public int point { get; private set; }
+    public int score { get; private set; }
     private List<Cell> cells;
 
     IEnumerable<Cell> CellsInRow(int irow) =>
@@ -50,16 +45,16 @@ class BingoBoard {
     IEnumerable<Cell> CellsInCol(int icol) =>
         from irow in Enumerable.Range(0, 5) select cells[irow * 5 + icol];
 
-    public BingoBoard(string[] lines) {
+    public BingoBoard(string st) {
 
-        // split the input into words & read the numbers into cells
+        // split the input into words & read them numbers into cells
         cells = (
-            from word in string.Join(" ", lines).Split(" ", StringSplitOptions.RemoveEmptyEntries)
-            select new Cell(int.Parse(word))
+            from word in st.Split(" \n".ToArray(), StringSplitOptions.RemoveEmptyEntries)
+            select new Cell(word)
         ).ToList();
     }
 
-    public void AddNumber(int number) {
+    public void AddNumber(string number) {
 
         var icell = cells.FindIndex(cell => cell.number == number);
 
@@ -68,7 +63,7 @@ class BingoBoard {
             // mark the cell
             cells[icell] = cells[icell] with { marked = true };
 
-            // if the board is completed, compute point
+            // if the board is completed, compute score
             for (var i = 0; i < 5; i++) {
                 if (
                     CellsInRow(i).All(cell => cell.marked) ||
@@ -76,9 +71,9 @@ class BingoBoard {
                 ) {
 
                     var unmarkedNumbers =
-                        from cell in cells where !cell.marked select cell.number;
+                        from cell in cells where !cell.marked select int.Parse(cell.number);
 
-                    point = number * unmarkedNumbers.Sum();
+                    score = int.Parse(number) * unmarkedNumbers.Sum();
                 }
             }
         }
