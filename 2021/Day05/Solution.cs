d590662f4a4b3bd5d95c2baabef6307feec3ddf0
David Nemeth Csoka <encse@csokavar.hu>
2021-12-05 08:10:19 +0100

    day 05 cleanup

--- 2021/Day05/Solution.cs
+++ 2021/Day05/Solution.cs
@@ -8,38 +8,39 @@ namespace AdventOfCode.Y2021.Day05;
 [ProblemName("Hydrothermal Venture")]
 class Solution : Solver {
 
-    public object PartOne(string input) => GetIntersectionCount(input, true);
-    public object PartTwo(string input) => GetIntersectionCount(input, false);
+    public object PartOne(string input) => GetIntersectionCount(ParseLines(input, skipDiagonal: true));
+    public object PartTwo(string input) => GetIntersectionCount(ParseLines(input, skipDiagonal: false));
 
-    int GetIntersectionCount(string input, bool skipDiagonal) =>
-        ParseLines(input).Aggregate(
+    int GetIntersectionCount(IEnumerable<Line> lines) =>
+        lines.Aggregate(
             new Dictionary<Vec2, int>(),
-            (grid, line) => DrawLine(grid, line, skipDiagonal),
+            DrawLine,
             grid => grid.Count(kvp => kvp.Value > 1)
         );
 
-    Dictionary<Vec2, int> DrawLine(Dictionary<Vec2, int> grid, Line line, bool skipDiagonal)  {
+    Dictionary<Vec2, int> DrawLine(Dictionary<Vec2, int> grid, Line line) {
         var dx = Math.Sign(line.to.x - line.from.x);
         var dy = Math.Sign(line.to.y - line.from.y);
+        var pt = line.from; 
 
-        if (!skipDiagonal || dx == 0 || dy == 0) {
-            var pt = line.from;
+        while (true) {
             grid[pt] = grid.GetValueOrDefault(pt, 0) + 1;
-            while (pt != line.to) {
-                pt = new Vec2(pt.x + dx, pt.y + dy);
-                grid[pt] = grid.GetValueOrDefault(pt, 0) + 1;
+            if (pt == line.to) {
+                break;
             }
+            pt = pt with {x = pt.x + dx, y = pt.y + dy};
         }
         return grid;
     }
 
-    IEnumerable<Line> ParseLines(string input) => 
-        from line in input.Split("\n") 
+    IEnumerable<Line> ParseLines(string input, bool skipDiagonal) =>
+        from line in input.Split("\n")
         let num = ParseNumbers(line).ToArray()
+        where !skipDiagonal || num[0] == num[2] || num[1] == num[3]
         select new Line(new Vec2(num[0], num[1]), new Vec2(num[2], num[3]));
 
     IEnumerable<int> ParseNumbers(string stLine) =>
-        from st in stLine.Split(", ->".ToArray(), StringSplitOptions.RemoveEmptyEntries) 
+        from st in stLine.Split(", ->".ToArray(), StringSplitOptions.RemoveEmptyEntries)
         select int.Parse(st);
 }
 
