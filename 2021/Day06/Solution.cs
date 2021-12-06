912409a643e33d21424143bdd2db06af84ea9d61
David Nemeth Csoka <encse@csokavar.hu>
2021-12-06 07:20:47 +0100

    day 06

---
+++ 2021/Day06/Solution.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+
+namespace AdventOfCode.Y2021.Day06;
+
+[ProblemName("Lanternfish")]
+class Solution : Solver {
+
+    public object PartOne(string input) => FishCountAfterNDays(input, 80);
+    public object PartTwo(string input) => FishCountAfterNDays(input, 256);
+
+    long FishCountAfterNDays(string input, int days) {
+
+        // group the fish by their timer, no need to deal with them one by one:
+        var fishCountByInternalTimer = new long[9];
+        foreach (var ch in input.Split(',')) {
+            fishCountByInternalTimer[int.Parse(ch)]++;
+        }
+        
+        // we will model a circular shift register, with an additional feedback:
+        //       0123456           78 
+        //   ┌──[       ]─<─(+)───[  ]──┐
+        //   └──────>────────┴─────>────┘
+
+        for (var t = 0; t < days; t++) {
+            fishCountByInternalTimer[(t + 7) % 9] += fishCountByInternalTimer[t % 9];
+        }
+
+        return fishCountByInternalTimer.Sum();
+    }
+}
