1afc8e9abc1321c8f6da7bed5af1906e71bfa38a
David Nemeth Cs <encse@csokavar.hu>
2021-12-07 16:11:02 +0100

    day 07 cleanup

--- 2021/Day07/Solution.cs
+++ 2021/Day07/Solution.cs
@@ -7,27 +7,21 @@ namespace AdventOfCode.Y2021.Day07;
 class Solution : Solver {
 
     public object PartOne(string input) => 
-        MinimalTotalFuel(input, fuelFromDistance: distance => distance);
+        FuelMin(input, fuelConsumption: distance => distance);
 
     public object PartTwo(string input) => 
-        MinimalTotalFuel(input, fuelFromDistance: distance => (1 + distance) * distance / 2);
+        FuelMin(input, fuelConsumption: distance => (1 + distance) * distance / 2);
         
-    int MinimalTotalFuel(string input, Func<int, int> fuelFromDistance) {
+    int FuelMin(string input, Func<int, int> fuelConsumption) {
         var positions = input.Split(",").Select(int.Parse).ToArray();
 
-        // Compute the total fuel consumption for each possible target position.
+        var totalFuelToReachTarget = (int target) => 
+            positions.Select(position => fuelConsumption(Math.Abs(target - position))).Sum();
+
+        // Minimize the total fuel consumption checking each possible target position.
         // We have just about 1000 of these, so an O(n^2) algorithm will suffice.
         var minPosition = positions.Min();
         var maxPosition = positions.Max();
-
-        var totalFuelByTarget =
-            from target in Enumerable.Range(minPosition, maxPosition - minPosition + 1)
-            let fuelByPosition = 
-                from position in positions select fuelFromDistance(Math.Abs(target - position))
-            select 
-                fuelByPosition.Sum();
-
-        // Get minimum:
-        return totalFuelByTarget.Min();
+        return Enumerable.Range(minPosition, maxPosition - minPosition + 1).Select(totalFuelToReachTarget).Min();
     }
 }
