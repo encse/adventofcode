8cf70cfeb68be282fbc39b187b6b40031af10693
David Nemeth Cs <encse@csokavar.hu>
2021-12-08 20:02:28 +0100

    day 08 alternative

--- 2021/Day08/Solution.cs
+++ 2021/Day08/Solution.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -19,7 +18,7 @@ class Solution : Solver {
     */
 
     public object PartOne(string input) {
-        
+
         // we can identify digits 1, 7, 4 and 8 by their active segments count:
         var segmentCounts = new[] { "cd", "acf", "bcdf", "abcdefg" }.Select(x => x.Length).ToHashSet();
 
@@ -33,44 +32,41 @@ class Solution : Solver {
     }
 
     public object PartTwo(string input) {
-        var ns =
-            from line in input.Split("\n")
-            let parts = line.Split(" | ")
+        var res = 0;
+        foreach(var line in input.Split("\n")) {
+            var parts = line.Split(" | ");
+            var patterns = parts[0].Split(" ").Select(x => x.ToHashSet()).ToArray();
 
-            // first we need a reader for the shuffled segment pattern:
-            let reader = GetDigitReader(parts[0])
+            // let's figure out what segments belong to each digit
+            var digits = new HashSet<char>[10];
+
+            // we can do these by length:
+            digits[1] = patterns.Single(pattern => pattern.Count() == "cf".Length);
+            digits[4] = patterns.Single(pattern => pattern.Count() == "bcdf".Length);
+
+            // it turns out that the following tripplet uniquely identifies the rest:
+            var lookup = (int segmentCount, int commonWithOne, int commonWithFour) =>
+                patterns.Single(pattern => 
+                    pattern.Count() == segmentCount && 
+                    pattern.Intersect(digits[1]).Count() == commonWithOne && 
+                    pattern.Intersect(digits[4]).Count() == commonWithFour
+                );
             
+            digits[0] = lookup(6, 2, 3);  
+            digits[2] = lookup(5, 1, 2);
+            digits[3] = lookup(5, 2, 3);
+            digits[5] = lookup(5, 1, 3);
+            digits[6] = lookup(6, 1, 3);
+            digits[7] = lookup(3, 2, 2);
+            digits[8] = lookup(7, 2, 4);
+            digits[9] = lookup(6, 2, 4);
+                    
+            var reader = (string v) => 
+                Enumerable.Range(0, 10).Single(i => digits[i].SetEquals(v));
+
             // then decode the digits one by one:
-            let digits = parts[1].Split(" ")
-            select digits.Aggregate(0, (n, digit) => n * 10 + reader(digit));
-
-        return ns.Sum();
-    }
-
-    Func<string, int> GetDigitReader(string input) {
-        var patterns = input.Split(" ").Select(x => x.ToHashSet()).ToArray();
-        var digits = new HashSet<char>[10];
-   
-        // it turns out that the following tripplet uniquely identifies each digit:
-        var lookup = (int segmentCount, int commonWithOne, int commonWithFour) =>
-            patterns.Single(pattern => 
-                pattern.Count() == segmentCount && 
-                pattern.Intersect(digits[1]).Count() == commonWithOne && 
-                pattern.Intersect(digits[4]).Count() == commonWithFour
-            );
-        
-        digits[1] = patterns.Single(pattern => pattern.Count() == 2);
-        digits[4] = patterns.Single(pattern => pattern.Count() == 4);
-
-        digits[0] = lookup(6, 2, 3);  
-        digits[2] = lookup(5, 1, 2);
-        digits[3] = lookup(5, 2, 3);
-        digits[5] = lookup(5, 1, 3);
-        digits[6] = lookup(6, 1, 3);
-        digits[7] = lookup(3, 2, 2);
-        digits[8] = lookup(7, 2, 4);
-        digits[9] = lookup(6, 2, 4);
-
-        return (string v) => Enumerable.Range(0, 10).Single(i => digits[i].SetEquals(v));
+            res += parts[1].Split(" ").Aggregate(0, (n, digit) => n * 10 + reader(digit));
+        }
+        return res;
     }
 }
