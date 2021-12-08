12e96130d45b424f93de8314b2a1190dc42a5eb3
David Nemeth Cs <encse@csokavar.hu>
2021-12-08 19:48:18 +0100

    day 08 alternative

--- 2021/Day08/Solution.cs
+++ 2021/Day08/Solution.cs
@@ -8,35 +8,26 @@ namespace AdventOfCode.Y2021.Day08;
 class Solution : Solver {
 
    /*
-              0:      1:      2:      3:      4:
-             aaaa    ....    aaaa    aaaa    ....
-            b    c  .    c  .    c  .    c  b    c
-            b    c  .    c  .    c  .    c  b    c
-             ....    ....    dddd    dddd    dddd
-            e    f  .    f  e    .  .    f  .    f
-            e    f  .    f  e    .  .    f  .    f
-             gggg    ....    gggg    gggg    ....
-
-              5:      6:      7:      8:      9:
-             aaaa    aaaa    aaaa    aaaa    aaaa
-            b    .  b    .  .    c  b    c  b    c
-            b    .  b    .  .    c  b    c  b    c
-             dddd    dddd    ....    dddd    dddd
-            .    f  e    f  .    f  e    f  .    f
-            .    f  e    f  .    f  e    f  .    f
-             gggg    gggg    ....    gggg    gggg
+              0:      1:      2:      3:      4:      5:      6:      7:      8:      9:
+             aaaa    ....    aaaa    aaaa    ....    aaaa    aaaa    aaaa    aaaa    aaaa
+            b    c  .    c  .    c  .    c  b    c  b    .  b    .  .    c  b    c  b    c
+            b    c  .    c  .    c  .    c  b    c  b    .  b    .  .    c  b    c  b    c
+             ....    ....    dddd    dddd    dddd    dddd    dddd    ....    dddd    dddd
+            e    f  .    f  e    .  .    f  .    f  .    f  e    f  .    f  e    f  .    f
+            e    f  .    f  e    .  .    f  .    f  .    f  e    f  .    f  e    f  .    f
+             gggg    ....    gggg    gggg    ....    gggg    gggg    ....    gggg    gggg
     */
 
     public object PartOne(string input) {
-        // we can identify digits 1, 7, 4 and 8 by their active segments count
-        // digit 1 -> two active segments, digit 7 -> three, etc
-        var activeSegmentCounts = new[] { 2, 3, 4, 7 };
+        
+        // we can identify digits 1, 7, 4 and 8 by their active segments count:
+        var segmentCounts = new[] { "cd", "acf", "bcdf", "abcdefg" }.Select(x => x.Length).ToHashSet();
 
         return (
             from line in input.Split("\n")
             let parts = line.Split(" | ")
             from segment in parts[1].Split(" ")
-            where activeSegmentCounts.Contains(segment.Length)
+            where segmentCounts.Contains(segment.Length)
             select 1
         ).Count();
     }
@@ -46,10 +37,10 @@ class Solution : Solver {
             from line in input.Split("\n")
             let parts = line.Split(" | ")
 
-            // first we need to find a function that decodes the digit from a shuffled segment pattern:
+            // first we need a reader for the shuffled segment pattern:
             let reader = GetDigitReader(parts[0])
             
-            // then decode the digits one by one
+            // then decode the digits one by one:
             let digits = parts[1].Split(" ")
             select digits.Aggregate(0, (n, digit) => n * 10 + reader(digit));
 
@@ -57,30 +48,29 @@ class Solution : Solver {
     }
 
     Func<string, int> GetDigitReader(string input) {
-     
-        var digits = new[]{
-            "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"
-        }.Select(x => x.ToHashSet()).ToArray();
-
-        var segmentCount = (HashSet<char> a) => a.Count();
-        var commonSegmentCount = (HashSet<char> a, HashSet<char> b) => a.Intersect(b).Count();
-
-        var left = input.Split(" ").Select(x => x.ToHashSet()).ToArray();
-        
-        // It so happens that we can find the digits with the below A) and B) properties in this order:
-        var shuffledDigits = new Dictionary<int, HashSet<char>>();
-        foreach (var i in new[] { 1, 4, 7, 8, 3, 5, 6, 9, 0, 2 }) {
-            shuffledDigits[i] = left.Single(candidate =>
-                // A) should have the proper active segment count
-                segmentCount(candidate) == segmentCount(digits[i]) &&
-
-                // B) should have the expected number of common segments with the already found digits
-                shuffledDigits.Keys.All(j =>
-                    commonSegmentCount(candidate, shuffledDigits[j]) ==
-                    commonSegmentCount(digits[i], digits[j]))
+        var patterns = input.Split(" ").Select(x => x.ToHashSet()).ToArray();
+        var digits = new HashSet<char>[10];
+   
+        // it turns out that the following tripplet uniquely identifies each digit:
+        var lookup = (int segmentCount, int commonWithOne, int commonWithFour) =>
+            patterns.Single(pattern => 
+                pattern.Count() == segmentCount && 
+                pattern.Intersect(digits[1]).Count() == commonWithOne && 
+                pattern.Intersect(digits[4]).Count() == commonWithFour
             );
-        }
+        
+        digits[1] = patterns.Single(pattern => pattern.Count() == 2);
+        digits[4] = patterns.Single(pattern => pattern.Count() == 4);
 
-        return (string v) => shuffledDigits.Single(kvp => kvp.Value.SetEquals(v)).Key;
+        digits[0] = lookup(6, 2, 3);  
+        digits[2] = lookup(5, 1, 2);
+        digits[3] = lookup(5, 2, 3);
+        digits[5] = lookup(5, 1, 3);
+        digits[6] = lookup(6, 1, 3);
+        digits[7] = lookup(3, 2, 2);
+        digits[8] = lookup(7, 2, 4);
+        digits[9] = lookup(6, 2, 4);
+
+        return (string v) => Enumerable.Range(0, 10).Single(i => digits[i].SetEquals(v));
     }
 }
