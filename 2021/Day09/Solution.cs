d436d178c1e03a157fea8b10dda6be88668c37d1
David Nemeth Cs <encse@csokavar.hu>
2021-12-10 10:10:54 +0100

    day 09 cleanup

--- 2021/Day09/Solution.cs
+++ 2021/Day09/Solution.cs
@@ -10,66 +10,61 @@ class Solution : Solver {
     public object PartOne(string input) {
         var map = GetMap(input);
 
-        // get the risk values of the low positions:
-        var riskValue = (Pos pos) => 1 + map[pos];
-        return GetLowPositions(map).Select(riskValue).Sum();
+        // find the 'low points' and return a hash computed from their heights:
+        return GetLowPoints(map).Select(point => 1 + map[point]).Sum();
     }
 
     public object PartTwo(string input) {
         var map = GetMap(input);
 
         // find the 3 biggest basins and return a hash computed from their size:
-        return GetLowPositions(map)
+        return GetLowPoints(map)
             .Select(p => BasinSize(map, p))
             .OrderByDescending(basinSize => basinSize)
             .Take(3)
             .Aggregate(1, (m, basinSize) => m * basinSize);
     }
 
-    // store the positions in a dictionary so that we can iterate over them and 
-    // to easily deal with positions outside the area (with GetValueOrDefault)
-    ImmutableDictionary<Pos, int> GetMap(string input) {
-        var lines = input.Split("\n");
+    // store the points in a dictionary so that we can iterate over them and 
+    // to easily deal with points outside the area using GetValueOrDefault
+    ImmutableDictionary<Point, int> GetMap(string input) {
+        var map = input.Split("\n");
         return (
-            from irow in Enumerable.Range(0, lines[0].Length)
-            from icol in Enumerable.Range(0, lines.Length)
-            select new KeyValuePair<Pos, int>(new Pos(irow, icol), lines[irow][icol] - '0')
+            from y in Enumerable.Range(0, map[0].Length)
+            from x in Enumerable.Range(0, map.Length)
+            select new KeyValuePair<Point, int>(new Point(x, y), map[y][x] - '0')
         ).ToImmutableDictionary();
     }
 
-    IEnumerable<Pos> Neighbours(Pos pos) => 
+    IEnumerable<Point> Neighbours(Point point) => 
         new [] {
-           pos with {irow = pos.irow + 1},
-           pos with {irow = pos.irow - 1},
-           pos with {icol = pos.icol + 1},
-           pos with {icol = pos.icol - 1},
+           point with {y = point.y + 1},
+           point with {y = point.y - 1},
+           point with {x = point.x + 1},
+           point with {x = point.x - 1},
         };
 
-    // position is low if each of its neighbours is higher:
-    public IEnumerable<Pos> GetLowPositions(ImmutableDictionary<Pos, int>  map) =>
-        from pos in map.Keys 
-        let height = map[pos]
-        where Neighbours(pos).All(posN => height < map.GetValueOrDefault(posN, 9))
-        select pos;
+    public IEnumerable<Point> GetLowPoints(ImmutableDictionary<Point, int>  map) =>
+        from point in map.Keys 
+        // point is low if each of its neighbours is higher:
+        where Neighbours(point).All(neighbour => map[point] < map.GetValueOrDefault(neighbour, 9))
+        select point;
 
-    public int BasinSize(ImmutableDictionary<Pos, int> map, Pos pos) {
+    public int BasinSize(ImmutableDictionary<Point, int> map, Point point) {
         // flood fill algorithm
-        var filled = new HashSet<Pos>{pos};
-        var queue = new Queue<Pos>(filled);
+        var filled = new HashSet<Point>{point};
+        var queue = new Queue<Point>(filled);
 
         while (queue.Any()) {
-            pos = queue.Dequeue();
-            filled.Add(pos);
-            foreach (var posN in Neighbours(pos)) {
-                if (!filled.Contains(posN) && map.GetValueOrDefault(posN, 9) != 9) {
-                    queue.Enqueue(posN);
-                    filled.Add(posN);
+            foreach (var neighbour in Neighbours(queue.Dequeue()).Except(filled)) {
+                if (map.GetValueOrDefault(neighbour, 9) != 9) {
+                    queue.Enqueue(neighbour);
+                    filled.Add(neighbour);
                 }
             }
         }
-
         return filled.Count;
     }
 }
 
-record Pos(int irow, int icol);
+record Point(int x, int y);
