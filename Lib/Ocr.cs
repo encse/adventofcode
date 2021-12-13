ff2ad1ed098b3d29c2a225d76486dbf14fead64d
David Nemeth Csoka <encse@csokavar.hu>
2021-12-13 15:44:42 +0100

    more ocr

--- Lib/Ocr.cs
+++ Lib/Ocr.cs
@@ -4,19 +4,19 @@ using System;
 namespace AdventOfCode;
 
 static class OcrExtension {
-    public static OcrString Ocr(this string st,  int charWidth = 5, int charHeight = 5){
+    public static OcrString Ocr(this string st,  int charWidth = 5, int charHeight = 6){
         return new OcrString(st, charWidth, charHeight);
     }
 }
 
-record OcrString (string st, int charWidth = 5, int charHeight = 5) {
+record OcrString (string st, int charWidth, int charHeight) {
     public override string ToString() {
         var lines = st.Split("\n");
         var width = lines[0].Length;
         var height = lines.Length;
 
         var dict = new Dictionary<long, string>{
-            // 5 x 5
+            // 5 x 6
             {0x19297A52, "A"},
             {0x3252F4A4, "A"},
             {0x725C94B8, "B"},
@@ -40,11 +40,11 @@ record OcrString (string st, int charWidth = 5, int charHeight = 5) {
             {0x7844443C, "Z"},
 
             // 8x10
-            {0x09F109090909F000, "B"},
-            {0x010101010108F000, "C"},
-            {0x010139090918E800, "G"},
-            {0x101010111110E000, "J"},
-            {0x102040810101F800, "Z"},
+            {0x909F109090909F0, "B"},
+            {0x1010101010108F0, "C"},
+            {0x1010139090918E8, "G"},
+            {0x10101010111110E0, "J"},
+            {0x8102040810101F8, "Z"},
 
             {0, ""},
         };
@@ -54,7 +54,7 @@ record OcrString (string st, int charWidth = 5, int charHeight = 5) {
         for (var ch = 0; ch < Math.Ceiling(width / (double)charWidth); ch++) {
             var hash = 0L;
             var stChar = "";
-            for (var irow = 0; irow <= charHeight; irow++) {
+            for (var irow = 0; irow < charHeight; irow++) {
                 for (var i = 0; i < charWidth; i++) {
                     var icol = (ch * charWidth) + i;
                     var point = irow < height && icol < lines[irow].Length ? lines[irow][icol] : ' ';
