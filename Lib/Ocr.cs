6fa9dad09e5c0ac47e36916bb5510dd2e22a7b27
David Nemeth Csoka <encse@csokavar.hu>
2021-12-13 15:02:12 +0100

    add some more letters

--- Lib/Ocr.cs
+++ Lib/Ocr.cs
@@ -18,6 +18,7 @@ record OcrString (string st, int charWidth = 5, int charHeight = 5) {
         var dict = new Dictionary<long, string>{
             // 5 x 5
             {0x19297A52, "A"},
+            {0x3252F4A4, "A"},
             {0x725C94B8, "B"},
             {0x32508498, "C"},
             {0x7A1C843C, "E"},
@@ -25,13 +26,18 @@ record OcrString (string st, int charWidth = 5, int charHeight = 5) {
             {0x3D0E4210, "F"},
             {0x3250B49C, "G"},
             {0x252F4A52, "H"},
+            {0x4A5E94A4, "H"},
             {0x0C210A4C, "J"},
             {0x18421498, "J"},
+            {0x4A98A524, "K"},
             {0x2108421E, "L"},
+            {0x4210843C, "L"},
             {0x7252E420, "P"},
             {0x7252E524, "R"},
+            {0x4A529498, "U"},
             {0x462A2108, "Y"},
             {0x3C22221E, "Z"},
+            {0x7844443C, "Z"},
 
             // 8x10
             {0x09F109090909F000, "B"},
