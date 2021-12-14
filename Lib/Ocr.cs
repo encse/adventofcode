5fbbe44f53ceba15edba85e2f076629d1c9fd0f0
David Nemeth Csoka <encse@csokavar.hu>
2021-12-14 14:41:48 +0100

    more ocr

--- Lib/Ocr.cs
+++ Lib/Ocr.cs
@@ -1,75 +1,107 @@
-using System.Collections.Generic;
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode;
 
 static class OcrExtension {
-    public static OcrString Ocr(this string st,  int charWidth = 5, int charHeight = 6){
-        return new OcrString(st, charWidth, charHeight);
+    public static OcrString Ocr(this string st) {
+        return new OcrString(st);
     }
 }
 
-record OcrString (string st, int charWidth, int charHeight) {
+record OcrString(string st) {
     public override string ToString() {
-        var lines = st.Split("\n");
+        var lines = st.Split("\n")
+            .SkipWhile(x => string.IsNullOrWhiteSpace(x))
+            .TakeWhile(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+
+        while (lines.All(line => line.StartsWith(" "))) {
+            lines = GetRect(lines, 1, 0, lines[0].Length - 1, lines.Length).Split("\n");
+        }
+
+        while (lines.All(line => line.EndsWith(" "))) {
+            lines = GetRect(lines, 0, 0, lines[0].Length - 1, lines.Length).Split("\n");
+        }
+
         var width = lines[0].Length;
         var height = lines.Length;
 
-        var dict = new Dictionary<long, string>{
-            // 5 x 6
-            {0x19297A52, "A"},
-            {0x3252F4A4, "A"},
-            {0x725C94B8, "B"},
-            {0x32508498, "C"},
-            {0x7A1C843C, "E"},
-            {0x7A1C8420, "F"},
-            {0x3D0E4210, "F"},
-            {0x3250B49C, "G"},
-            {0x252F4A52, "H"},
-            {0x4A5E94A4, "H"},
-            {0x0C210A4C, "J"},
-            {0x18421498, "J"},
-            {0x4A98A524, "K"},
-            {0x2108421E, "L"},
-            {0x4210843C, "L"},
-            {0x7252E420, "P"},
-            {0x7252E524, "R"},
-            {0x4A529498, "U"},
-            {0x462A2108, "Y"},
-            {0x3C22221E, "Z"},
-            {0x7844443C, "Z"},
+        var smallAlphabet = StripMargin(@"
+        | A    B    C    E    F    G    H    I    J    K    L    O    P    R    S    U    Y    Z    
+        |  ##  ###   ##  #### ####  ##  #  #  ###   ## #  # #     ##  ###  ###   ### #  # #   ##### 
+        | #  # #  # #  # #    #    #  # #  #   #     # # #  #    #  # #  # #  # #    #  # #   #   # 
+        | #  # ###  #    ###  ###  #    ####   #     # ##   #    #  # #  # #  # #    #  #  # #   #  
+        | #### #  # #    #    #    # ## #  #   #     # # #  #    #  # ###  ###   ##  #  #   #   #   
+        | #  # #  # #  # #    #    #  # #  #   #  #  # # #  #    #  # #    # #     # #  #   #  #    
+        | #  # ###   ##  #### #     ### #  #  ###  ##  #  # ####  ##  #    #  # ###   ##    #  #### 
+        ");
 
-            // 8x10
-            {0x909F109090909F0, "B"},
-            {0x1010101010108F0, "C"},
-            {0x1010139090918E8, "G"},
-            {0x10101010111110E0, "J"},
-            {0x8102040810101F8, "Z"},
+        var largeAlphabet = StripMargin(@"
+        | A       B       C       E       F       G       H       J       K       L       N       P       R       X       Z
+        |   ##    #####    ####   ######  ######   ####   #    #     ###  #    #  #       #    #  #####   #####   #    #  ######  
+        |  #  #   #    #  #    #  #       #       #    #  #    #      #   #   #   #       ##   #  #    #  #    #  #    #       #  
+        | #    #  #    #  #       #       #       #       #    #      #   #  #    #       ##   #  #    #  #    #   #  #        #  
+        | #    #  #    #  #       #       #       #       #    #      #   # #     #       # #  #  #    #  #    #   #  #       #   
+        | #    #  #####   #       #####   #####   #       ######      #   ##      #       # #  #  #####   #####     ##       #    
+        | ######  #    #  #       #       #       #  ###  #    #      #   ##      #       #  # #  #       #  #      ##      #     
+        | #    #  #    #  #       #       #       #    #  #    #      #   # #     #       #  # #  #       #   #    #  #    #      
+        | #    #  #    #  #       #       #       #    #  #    #  #   #   #  #    #       #   ##  #       #   #    #  #   #       
+        | #    #  #    #  #    #  #       #       #   ##  #    #  #   #   #   #   #       #   ##  #       #    #  #    #  #       
+        | #    #  #####    ####   ######  #        ### #  #    #   ###    #    #  ######  #    #  #       #    #  #    #  ######  
+        ");
 
-            {0, ""},
-        };
+        var charMap =
+            lines.Length == smallAlphabet.Length - 1 ? smallAlphabet :
+            lines.Length == largeAlphabet.Length - 1 ? largeAlphabet :
+            throw new Exception("Could not find alphabet");
 
+        var charWidth = charMap == smallAlphabet ? 5 : 8;
+        var charHeight = charMap == smallAlphabet ? 6 : 10;
         var res = "";
+        for (var i = 0; i < width; i += charWidth) {
+            res += Detect(lines, i, charWidth, charHeight, charMap);
+        }
+        return res;
+    }
 
-        for (var ch = 0; ch < Math.Ceiling(width / (double)charWidth); ch++) {
-            var hash = 0L;
-            var stChar = "";
-            for (var irow = 0; irow < charHeight; irow++) {
-                for (var i = 0; i < charWidth; i++) {
-                    var icol = (ch * charWidth) + i;
-                    var point = irow < height && icol < lines[irow].Length ? lines[irow][icol] : ' ';
-                    stChar += point;
-                    if (point != ' ' && point != '.') {
-                        hash += 1;
-                    } 
-                    hash <<= 1;
-                }
-                stChar += "\n";
+    string[] StripMargin(string st) => (
+        from line in Regex.Split(st, "\r?\n")
+        where Regex.IsMatch(line, @"^\s*\| ")
+        select Regex.Replace(line, @"^\s* \| ", "")
+    ).ToArray();
+
+    public string Detect(string[] text, int icolLetter, int charWidth, int charHeight, string[] charMap) {
+        var textRect = GetRect(text, icolLetter, 0, charWidth, charHeight);
+
+        for (var icol = 0; icol < charMap[0].Length; icol += charWidth) {
+            var ch = charMap[0][icol].ToString();
+            var charPattern = GetRect(charMap, icol, 1, charWidth, charHeight);
+            var found = Enumerable.Range(0, charPattern.Length).All(i => {
+                var textWhiteSpace = " .".Contains(textRect[i]);
+                var charWhiteSpace = " .".Contains(charPattern[i]);
+                return textWhiteSpace == charWhiteSpace;
+            });
+
+            if (found) {
+                return ch;
             }
-            if (!dict.ContainsKey(hash)) {
-                throw new Exception($"Unrecognized letter with hash: 0x{hash.ToString("X")}\n{stChar}");
+        }
+
+        throw new Exception($"Unrecognized letter: \n{textRect}\n");
+    }
+
+    string GetRect(string[] st, int icol0, int irow0, int ccol, int crow) {
+        var res = "";
+        for (var irow = irow0; irow < irow0 + crow; irow++) {
+            for (var icol = icol0; icol < icol0 + ccol; icol++) {
+                var ch = irow < st.Length && icol < st[irow].Length ? st[irow][icol] : ' ';
+                res += ch;
             }
-            res += dict[hash];
+            if (irow + 1 != irow0 + crow) {
+                res += "\n";
+            }
         }
         return res;
     }
