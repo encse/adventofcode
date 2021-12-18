da3cd5faca465344b8205fdb1d9362d869be2ada
David Nemeth Csoka <encse@csokavar.hu>
2021-12-18 07:31:14 +0100

    day18 cleanup. WHAT THE HECK WAS THIS?

--- 2021/Day18/Solution.cs
+++ 2021/Day18/Solution.cs
@@ -1,197 +1,181 @@
-using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
 
 namespace AdventOfCode.Y2021.Day18;
 
 [ProblemName("Snailfish")]
 class Solution : Solver {
 
+    // WARNING: Total nonsense follows below.
+    //
+    //     .-""-.
+    //    /,..___\
+    //   () {_____}
+    //     (/-@-@-\)
+    //     {`-=^=-'}
+    //     {  `-'  } Max
+    //      {     }
+    //       `---'
+
     public object PartOne(string input) {
-        List<Token> tokens = null;
-        foreach (var line in input.Split("\n")) {
-            var tokensB = Tokenize(line);
-            tokens = tokens == null ? tokensB : Sum(tokens, tokensB);
-            // Console.WriteLine(ToString(tokens));
-        }
-
-        return Magnitude(tokens);
-    }
-
-    string ToString(List<Token> tokens) {
-        var res = "";
-        foreach (var token in tokens) {
-            if (token.kind == Kind.L) {
-                res += "[";
-            } else if (token.kind == Kind.R) {
-                res += "]";
-            } else if (token.kind == Kind.Num) {
-                res += token.value + " ";
-            }
-        }
-        return res;
+        // sum up all the 'numbers' in the input
+        return input.Split("\n").Select(ParseNumber).Aggregate(
+            new Number(), 
+            (acc, number) => !acc.Any() ? number : Sum(acc, number), 
+            Magnitude
+        );
     }
 
     public object PartTwo(string input) {
-        List<Token>[] tokens = input.Split("\n").Select(Tokenize).ToArray();
-        
+        // get the highest magnitude resulted from adding any two 'numbers' in the input:
+        var numbers = input.Split("\n").Select(ParseNumber).ToArray();
         return (
-            from i in Enumerable.Range(0, tokens.Length)
-            from j in Enumerable.Range(0, tokens.Length)
+            from i in Enumerable.Range(0, numbers.Length)
+            from j in Enumerable.Range(0, numbers.Length)
             where i != j
-            select Magnitude(Sum(tokens[i], tokens[j]))
+            select Magnitude(Sum(numbers[i], numbers[j]))
         ).Max();
     }
 
+    long Magnitude(Number number) {
+        var itoken = 0; // we will process the number tokenwise
 
-    long Magnitude(List<Token> tokens) {
-
-        var i = 0;
-        Token getToken() {
-            return tokens[i++];
-        }
-
-        Node parseNode() {
-            var token = getToken();
-            if (token.kind == Kind.Num) {
-                return new Node(token, null, null);
+        long computeRecursive() {
+            var token = number[itoken++];
+            if (token.kind == TokenKind.Digit) {
+                // just the number
+                return token.value;
             } else {
-                var left = parseNode();
-                var right = parseNode();
-                getToken();
-                return new Node(token, left, right);
+                // take left and right side of the pair
+                var left = computeRecursive();
+                var right = computeRecursive();
+                itoken++; // don't forget to eat the closing parenthesis
+                return  3 * left + 2 * right;
             }
         }
-        var node = parseNode();
 
-        long magni(Node node) {
-            if (node.left == null) {
-                return node.token.value;
-            } else {
-                return 3 * magni(node.left) + 2 * magni(node.right);
-            }
+        return computeRecursive();
+    }
+
+
+    Number Sum(Number numberA, Number numberB) {
+        // just wrap A and B in a new 'number' and reduce:
+
+        var numbers = new Number();
+        numbers.Add(new Token(TokenKind.LeftParenthesis));
+        numbers.AddRange(numberA);
+        numbers.AddRange(numberB);
+        numbers.Add(new Token(TokenKind.RightParenthesis));
+
+        return Reduce(numbers);
+    }
+
+    Number Reduce(Number number) {
+        while (Explode(number) || Split(number)) {
+            ; // repeat until we cannot explod or split anymore
         }
-        return magni(node);
+        return number;
     }
 
-    List<Token> Sum(List<Token> tokensA, List<Token> tokensB) {
-        // Console.WriteLine("sum     " + ToString(tokensA) + " + " + ToString(tokensB));
-        var tokens = new List<Token>();
-        tokens.Add(new Token(Kind.L, 0));
-        tokens.AddRange(tokensA);
-        tokens.AddRange(tokensB);
-        tokens.Add(new Token(Kind.R, 0));
-        return Reduce(tokens);
-    }
-
-    List<Token> Reduce(List<Token> tokens) {
-        // Console.WriteLine("reduce  "+ ToString(tokens));
-        while (true) {
-            bool cont;
-            (tokens, cont) = Explode(tokens);
-            if (cont) {
-                // Console.WriteLine("explode " + ToString(tokens));
-                continue;
-            }
-            (tokens, cont) = Split(tokens);
-            if (cont) {
-                // Console.WriteLine("split   " + ToString(tokens));
-                continue;
-            }
-            break;
-        }
-        // Console.WriteLine("reduced " + ToString(tokens));
-        return tokens;
-    }
-
-    void Assert(bool cond) {
-        if (!cond) { throw new Exception(""); };
-    }
-
-    (List<Token>, bool) Explode(List<Token> tokens) {
+    bool Explode(Number number) {
+        // exploding means we need to find the first pair in the number 
+        // that is embedded in 4 other pairs and get rid of it:
         var depth = 0;
-        for (var i = 0; i < tokens.Count; i++) {
-            if (tokens[i].kind == Kind.L) {
+        for (var i = 0; i < number.Count; i++) {
+            if (number[i].kind == TokenKind.LeftParenthesis) {
                 depth++;
                 if (depth == 5) {
-                    // akkor redukalni kell
-                    Assert(tokens[i + 1].kind == Kind.Num);
-                    Assert(tokens[i + 2].kind == Kind.Num);
+                    // we are deep enough, let's to the reduce part
 
+                    // find the digit to the left (if any) and increase:
                     for (var j = i - 1; j >= 0; j--) {
-                        if (tokens[j].kind == Kind.Num) {
-                            tokens[j] = tokens[j] with { value = tokens[j].value + tokens[i + 1].value };
-                            break;
-                        }
-                    }
-                    for (var j = i + 3; j < tokens.Count; j++) {
-                        if (tokens[j].kind == Kind.Num) {
-                            tokens[j] = tokens[j] with { value = tokens[j].value + tokens[i + 2].value };
+                        if (number[j].kind == TokenKind.Digit) {
+                            number[j] = number[j] with { value = number[j].value + number[i + 1].value };
                             break;
                         }
                     }
 
-                    tokens.RemoveRange(i, 4);
-                    tokens.Insert(i, new Token(Kind.Num, 0));
-                    return (tokens, true);
+                    // find the digit to the right (if any) and increase:
+                    for (var j = i + 3; j < number.Count; j++) {
+                        if (number[j].kind == TokenKind.Digit) {
+                            number[j] = number[j] with { value = number[j].value + number[i + 2].value };
+                            break;
+                        }
+                    }
+
+                    // replace [a b] with 0:
+                    number.RemoveRange(i, 4);
+                    number.Insert(i, new Token(TokenKind.Digit, 0));
+
+                    // successful reduce:
+                    return true;
                 }
-            } else if (tokens[i].kind == Kind.R) {
+            } else if (number[i].kind == TokenKind.RightParenthesis) {
                 depth--;
             }
         }
-        return (tokens, false);
+
+        // couldn't reduce:
+        return false;
     }
 
-    (List<Token>, bool) Split(List<Token> tokens) {
-        for (var i = 0; i < tokens.Count; i++) {
-            if (tokens[i].value >= 10) {
-                var v = tokens[i].value;
-                tokens.RemoveRange(i, 1);
-                tokens.InsertRange(i, new[]{
-                     new Token(Kind.L, 0),
-                     new Token(Kind.Num, v/2),
-                     new Token(Kind.Num, v-v/2),
-                     new Token(Kind.R, 0)
+    bool Split(Number number) {
+
+        // spliting means we neeed to find a token with a high value and make a pair out of it:
+        for (var i = 0; i < number.Count; i++) {
+            if (number[i].value >= 10) {
+
+                var v = number[i].value;
+                number.RemoveRange(i, 1);
+                number.InsertRange(i, new[]{
+                     new Token(TokenKind.LeftParenthesis),
+                     new Token(TokenKind.Digit, v/2),
+                     new Token(TokenKind.Digit, v-v/2),
+                     new Token(TokenKind.RightParenthesis)
                 });
-                return (tokens, true);
+
+                // successful split:
+                return true;
             }
         }
-        return (tokens, false);
+         // couldn't split:
+        return false;
     }
 
-    List<Token> Tokenize(string st) {
-        var res = new List<Token>();
+    // tokenize the input to a list of '[' ']' and digit tokens
+    Number ParseNumber(string st) {
+        var res = new Number();
         var n = "";
         foreach (var ch in st) {
             if (ch >= '0' && ch <= '9') {
                 n += ch;
             } else {
                 if (n != "") {
-                    res.Add(new Token(Kind.Num, int.Parse(n)));
+                    res.Add(new Token(TokenKind.Digit, int.Parse(n)));
                     n = "";
                 }
                 if (ch == '[') {
-                    res.Add(new Token(Kind.L, 0));
+                    res.Add(new Token(TokenKind.LeftParenthesis));
                 } else if (ch == ']') {
-                    res.Add(new Token(Kind.R, 0));
+                    res.Add(new Token(TokenKind.RightParenthesis));
                 }
             }
         }
         if (n != "") {
-            res.Add(new Token(Kind.Num, int.Parse(n)));
+            res.Add(new Token(TokenKind.Digit, int.Parse(n)));
             n = "";
         }
         return res;
     }
 
 }
-enum Kind {
-    L,
-    R,
-    Num
+
+// we will work with a list of tokens directly
+enum TokenKind {
+    LeftParenthesis,
+    RightParenthesis,
+    Digit
 }
-record Token(Kind kind, int value);
-record Node(Token token, Node left, Node right);
+record Token(TokenKind kind, int value = 0);
+
+class Number : List<Token>{};
