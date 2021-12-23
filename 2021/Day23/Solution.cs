58ceefa9c28d54ec2f3f46f0b131f5eac0a50979
David Nemeth Csoka <encse@csokavar.hu>
2021-12-23 21:35:55 +0100

    day 23

--- 2021/Day23/Solution.cs
+++ 2021/Day23/Solution.cs
@@ -8,37 +8,18 @@ namespace AdventOfCode.Y2021.Day23;
 [ProblemName("Amphipod")]
 class Solution : Solver {
 
-    public object PartOne(string input) {
-        return 0;
+    public object PartOne(string input)  => Solve(input).Min();
+    public object PartTwo(string input)  => Solve(Upscale(input)).Min();
+
+    string Upscale(string input){
+        var lines = input.Split("\n").ToList();
+        lines.Insert(3, "  #D#C#B#A#");
+        lines.Insert(4, "  #D#B#A#C#");
+        return string.Join("\n", lines);
     }
 
-    public object PartTwo(string input) {
-        return QQQ(input).Min();
-    }
-
-    public object PartThree(string input) {
-        var mazes = input.Split("\n\n").Select(GetMaze).ToArray();
-        var cost = 0;
-        for (var i = 0; i < mazes.Length - 1; i++) {
-            var found = false;
-            foreach (var n in Neighbours(mazes[i])) {
-                if (n.Tsto() == mazes[i + 1].Tsto()) {
-                    cost += n.cost;
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                Console.WriteLine("xxx");
-            }
-        }
-        return cost;
-        //    return QQQ(input).Min();
-    }
-
-    IEnumerable<int> QQQ(string input) {
-        var mazes = input.Split("\n\n").Select(GetMaze).ToArray();
-        var maze = mazes[0];
+    IEnumerable<int> Solve(string input) {
+        var maze = GetMaze(input);
 
         var q = new PriorityQueue<Maze, int>();
         var seen = new Dictionary<string, Maze>();
@@ -60,41 +41,14 @@ class Solution : Solver {
         while (q.Count > 0) {
             maze = q.Dequeue();
 
-            // Maze nreq = null;
-            // int ireq = -1;
-            // for (var i = 0; i < mazes.Length - 1; i++) {
-            //     if (mazes[i].Tsto() == maze.Tsto()) {
-            //         nreq = mazes[i + 1];
-            //         ireq = i;
-
-            //         break;
-            //     }
-            // }
-
-            // var found = false;
             foreach (var mazeT in Neighbours(maze)) {
-                // if (!found && nreq != null) {
-                //     if (mazeT.Tsto() == nreq.Tsto()) {
-                //         Console.WriteLine(mazeT.Tsto());
-                //         Console.WriteLine(mazeT.cost - maze.cost);
-                //         Console.WriteLine(ireq);
-                //         found = true;
-                //     }
-                // }
-
                 if (finished(mazeT)) {
-                    // Console.WriteLine("xxx\n" + mazeT.Tsto());
-                    Console.WriteLine(mazeT.cost);
                     yield return mazeT.cost;
                 } else if (!seen.ContainsKey(mazeT.Tsto()) || seen[mazeT.Tsto()].cost > mazeT.cost) {
                     seen[mazeT.Tsto()] = mazeT;
                     q.Enqueue(mazeT with { prev = maze }, mazeT.cost);
                 }
             }
-            // if (nreq != null && !found) {
-            //     Console.WriteLine("x");
-            // }
-            // Console.WriteLine("--");
         }
     }
 
