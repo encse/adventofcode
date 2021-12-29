c7bb9b8fc4a0466f864ae9b9b83defa590e69bf3
David Nemeth Csoka <encse@csokavar.hu>
2021-12-29 14:07:57 +0100

    day 24

--- 2021/Day24/Solution.cs
+++ 2021/Day24/Solution.cs
@@ -1,58 +1,51 @@
-using System.Numerics;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Y2021.Day24;
 
 [ProblemName("Arithmetic Logic Unit")]
 class Solution : Solver {
 
-    public object PartOne(string input) {
-        return "96979989692495";
-    }
+    public object PartOne(string input) => Solve(input).max;
+    public object PartTwo(string input) => Solve(input).min;
 
-    public object PartTwo(string input) {
-       return "51316214181141";
-    }
+    (string min, string max) Solve(string input) {
 
-    BigInteger Run2(string input, BigInteger z, string[] lines) {
-        int from = 0;
-        var ich = 0;
-        BigInteger step(int iblock, BigInteger z, BigInteger S, BigInteger T, BigInteger U) {
-            if (z < 0) {
-                return -1;
+        var max = Enumerable.Repeat(int.MinValue, 14).ToArray();
+        var min = Enumerable.Repeat(int.MaxValue, 14).ToArray();
+        var d = new int[14];
+        var digits = Enumerable.Range(1, 9).ToArray();
+        var stack = new Stack<int>();
+        var blocks = input.Split("inp w\n").Skip(1).ToArray();
+
+        for (var i = 0; i < 14; i++) {
+            var block = blocks[i];
+            var lines = block.Split('\n');
+
+            if (block.Contains("div z 1")) {
+                d[i] = int.Parse(lines[^4].Split(' ').Last());
+
+                stack.Push(i);
+            } else {
+                d[i] = int.Parse(lines[4].Split(' ').Last());
+
+                var pair = stack.Pop();
+                foreach (var digit in digits) {
+                    var digitPair = digit - d[pair] - d[i];
+                    if (digits.Contains(digitPair)) {
+                        if (digitPair > max[pair]) {
+                            (max[pair], max[i]) = (digitPair, digit);
+                        }
+                        if (digitPair < min[pair]) {
+                            (min[pair], min[i]) = (digitPair, digit);
+                        }
+                    }
+                }
             }
-            if (iblock < from || ich >= input.Length) {
-                return z;
-            }
-            var w = input[ich++] - '0';
-
-            var x = (z % 26 + T) != w ? 1 : 0;
-            z = z / S;
-            return z * (25 * x + 1) + (w + U) * x;
         }
 
-        var zOrig = z;
-        z = step(0, z, 1, 12, 1);           // 9    ------------\  5
-        z = step(1, z, 1, 13, 9);           // 6    -------\    |  1
-                                            //             |    |
-        z = step(2, z, 1, 12, 11);          // 9           |    |  3
-        z = step(3, z, 26, -13, 6);         // 7           |    |  1
-                                            //             |    |
-        z = step(4, z, 1, 11, 6);           // 9   ----\   |    |  6
-                                            //         |   |    |
-        z = step(5, z, 1, 15, 13);          // 9       |   |    |  2
-        z = step(6, z, 26, -14, 13);        // 8       |   |    |  1
-                                            //         |   |    |
-        z = step(7, z, 1, 12, 5);           // 9       |   |    |  4
-        z = step(8, z, 26, -8, 7);          // 6       |   |    |  1
-                                            //         |   |    |
-        z = step(9, z, 1, 14, 2);           // 9       |   |    |  8
-        z = step(10, z, 26, -9, 10);        // 2       |   |    |  1
-                                            //         |   |    |
-        z = step(11, z, 26, -11, 14);       // 4  <----/   |    |  1
-        z = step(12, z, 26, -6, 7);         // 9  <--------/    |  4
-        z = step(13, z, 26, -5, 1);         // 5  <-------------/  1
-
-        return z;
+        return (string.Join("", min), string.Join("", max));
     }
 
 }
