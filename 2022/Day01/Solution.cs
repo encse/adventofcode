eb5db63b551fc524c02fb7380dcb85423cd0192c
David Nemeth Csoka <encse@csokavar.hu>
2022-12-01 07:10:53 +0100

    2022/01

---
+++ 2022/Day01/Solution.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Y2022.Day01;
+
+[ProblemName("Calorie Counting")]      
+class Solution : Solver {
+
+    public object PartOne(string input) => 
+        GetCaloriesPerElf(input).First();
+
+    public object PartTwo(string input) =>
+        GetCaloriesPerElf(input).Take(3).Sum();
+
+    private IEnumerable<int> GetCaloriesPerElf(string input) =>
+        from elf in input.Split("\n\n")
+        let calories = elf.Split('\n').Select(int.Parse).Sum()
+        orderby calories descending
+        select calories;
+}
