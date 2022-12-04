87ebc207d560ad5cc36d810850a60bb478def74b
David Nemeth Csoka <encse@csokavar.hu>
2022-12-04 08:25:23 +0100

    2022/04

--- 2022/Day04/Solution.cs
+++ 2022/Day04/Solution.cs
@@ -5,22 +5,22 @@ namespace AdventOfCode.Y2022.Day04;
 
 [ProblemName("Camp Cleanup")]
 class Solution : Solver {
-    public object PartOne(string input) =>  DuplicateWorkCount(input, Contains);
-    public object PartTwo(string input) =>  DuplicateWorkCount(input, Overlaps);
+    public object PartOne(string input) => DuplicateWorkCount(input, Contains);
+    public object PartTwo(string input) => DuplicateWorkCount(input, Overlaps);
 
     record struct Range(int from, int to);
     bool Contains(Range r1, Range r2) => r1.from <= r2.from && r2.to <= r1.to; 
     bool Overlaps(Range r1, Range r2) => r1.to >= r2.from && r1.from <= r2.to; 
 
     private int DuplicateWorkCount(string input, Func<Range, Range, bool> rangeCheck) {
-        var parseRange = (string input) => 
-            new Range(int.Parse(input.Split('-')[0]), int.Parse(input.Split('-')[1]));
-        var parseWorkOrder = (string input) => input.Split(',').Select(parseRange);
-        var parseWorkOrders = (string input) => input.Split("\n").Select(parseWorkOrder);
+        var parseRanges = (string line) => 
+            from range in line.Split(',') 
+            let fromTo = range.Split('-').Select(int.Parse)
+            select new Range(fromTo.First(), fromTo.Last());
 
-        return parseWorkOrders(input).Count(workOrder => 
-            rangeCheck(workOrder.ElementAt(0), workOrder.ElementAt(1)) || 
-            rangeCheck(workOrder.ElementAt(1), workOrder.ElementAt(0))
+        return input.Split("\n").Select(parseRanges).Count(ranges => 
+            rangeCheck(ranges.First(), ranges.Last()) || 
+            rangeCheck(ranges.Last(), ranges.First())
         );
     }
 }
