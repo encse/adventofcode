b4621b9a336954b24795c87a914ebf8d7d6933e6
David Nemeth Csoka <encse@csokavar.hu>
2022-12-09 09:04:54 +0100

    2022/09

---
+++ 2022/Day09/Solution.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Y2022.Day09;
+
+[ProblemName("Rope Bridge")]
+class Solution : Solver {
+
+    public object PartOne(string input) => Tails(input, 2).ToHashSet().Count;
+    public object PartTwo(string input) => Tails(input, 10).ToHashSet().Count;
+
+    private IEnumerable<Knot> Tails(string input, int ropeLength) {
+        var rope = Enumerable.Repeat(new Knot(0, 0), ropeLength).ToArray();
+        yield return rope.Last();
+
+        foreach (var line in input.Split("\n")) {
+            var parts = line.Split(' ');
+            var dir = parts[0];
+            var dist = int.Parse(parts[1]);
+
+            for (var i = 0; i < dist; i++) {
+                MoveHead(rope, dir);
+                yield return rope.Last();
+            }
+        }
+    }
+
+    record Knot(int irow, int icol);
+
+    // moving the head in the given direction, inplace update of the knots following to the rules
+    void MoveHead(Knot[] rope, string dir) {
+        rope[0] = dir switch {
+            "U" => rope[0] with { irow = rope[0].irow - 1 },
+            "D" => rope[0] with { irow = rope[0].irow + 1 },
+            "L" => rope[0] with { icol = rope[0].icol - 1 },
+            "R" => rope[0] with { icol = rope[0].icol + 1 },
+            _ => throw new ArgumentException(dir)
+        };
+
+        for (var i = 1; i < rope.Length; i++) {
+            var prevKnot = rope[i - 1];
+            var thisKnot = rope[i];
+            var adjacent = Math.Abs(prevKnot.irow - thisKnot.irow) < 2 && Math.Abs(prevKnot.icol - thisKnot.icol) < 2;
+            if (!adjacent) {
+                rope[i] = new Knot(
+                    thisKnot.irow + Math.Sign(prevKnot.irow - thisKnot.irow),
+                    thisKnot.icol + Math.Sign(prevKnot.icol - thisKnot.icol)
+                );
+            }
+        }
+    }
+}
