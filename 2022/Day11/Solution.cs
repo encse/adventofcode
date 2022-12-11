c11fe95ed457c6aec0df0b3a6fd6b2604677a0f2
David Nemeth Csoka <encse@csokavar.hu>
2022-12-11 09:09:52 +0100

    2022/11 cleanup

--- 2022/Day11/Solution.cs
+++ 2022/Day11/Solution.cs
@@ -9,86 +9,71 @@ namespace AdventOfCode.Y2022.Day11;
 class Solution : Solver {
 
     public object PartOne(string input) {
-        var monkeys = ParseMonkeys(input).ToList();
-        return Run(20, monkeys, w => w / 3);
+        var monkeys = ParseMonkeys(input);
+        Run(20, monkeys, w => w / 3);
+        return GetMonkeyBusinessLevel(monkeys);
     }
 
     public object PartTwo(string input) {
-        var monkeys = ParseMonkeys(input).ToList();
+        var monkeys = ParseMonkeys(input);
         var mod = monkeys.Aggregate(1, (mod, monkey) => mod * monkey.mod);
-        return Run(10_000, monkeys, w => w % mod);
+        Run(10_000, monkeys, w => w % mod);
+        return GetMonkeyBusinessLevel(monkeys);
     }
 
-    IEnumerable<Monkey> ParseMonkeys(string input) => 
-        from block in input.Split("\n\n") select ParseMonkey(block);
+    Monkey[] ParseMonkeys(string input) =>
+        input.Split("\n\n").Select(ParseMonkey).ToArray();
 
     Monkey ParseMonkey(string input) {
         var monkey = new Monkey();
 
-        foreach (var match in input.Split("\n").Select(LineParser)) {
-            match("Starting items: (.*)", (arg) => {
+        foreach (var line in input.Split("\n")) {
+            var tryParse = LineParser(line);
+            if (tryParse(@"Monkey (\d+)", out var arg)) {
+                // pass
+            } else if (tryParse("Starting items: (.*)", out arg)) {
                 monkey.items = new Queue<long>(arg.Split(", ").Select(long.Parse));
-            });
-            match(@"Operation: new = old \* old", _ => {
+            } else if (tryParse(@"Operation: new = old \* old", out _)) {
                 monkey.operation = old => old * old;
-            });
-            match(@"Operation: new = old \* (\d+)", arg => {
+            } else if (tryParse(@"Operation: new = old \* (\d+)", out arg)) {
                 monkey.operation = old => old * int.Parse(arg);
-            });
-            match(@"Operation: new = old \+ (\d+)", arg => {
+            } else if (tryParse(@"Operation: new = old \+ (\d+)", out arg)) {
                 monkey.operation = old => old + int.Parse(arg);
-            });
-            match(@"Test: divisible by (\d+)", arg => {
+            } else if (tryParse(@"Test: divisible by (\d+)", out arg)) {
                 monkey.mod = int.Parse(arg);
-            });
-            match(@"If true: throw to monkey (\d+)", arg => {
+            } else if (tryParse(@"If true: throw to monkey (\d+)", out arg)) {
                 monkey.passToMonkeyIfDivides = int.Parse(arg);
-            });
-            match(@"If false: throw to monkey (\d+)", arg => {
+            } else if (tryParse(@"If false: throw to monkey (\d+)", out arg)) {
                 monkey.passToMonkeyOtherwise = int.Parse(arg);
-            });
+            } else {
+                throw new ArgumentException(line);
+            }
         }
         return monkey;
     }
 
-    Action<string, Action<string>> LineParser(string line) {
-        // if the line matches pattern, takes the value of the last match group and calls the given action with it.
-        void match(string pattern, Action<string> action) {
-            var m = Regex.Match(line, pattern);
-            if (m.Success) {
-                action(m.Groups[m.Groups.Count - 1].Value);
-            }
-        }
-        return match;
-    }
+    long GetMonkeyBusinessLevel(IEnumerable<Monkey> monkeys) => 
+        monkeys
+            .OrderByDescending(monkey => monkey.inspectedItems)
+            .Take(2)
+            .Aggregate(1L, (res, monkey) => res * monkey.inspectedItems);
 
-    long Run(int rounds, List<Monkey> monkeys, Func<long, long> updateWorryLevel) {
+    void Run(int rounds, Monkey[] monkeys, Func<long, long> updateWorryLevel) {
         for (var i = 0; i < rounds; i++) {
-            Round(monkeys, updateWorryLevel);
-        }
-        var activeMonkeys = (
-            from monkey in monkeys 
-            orderby monkey.inspectedItems descending 
-            select monkey
-        ).ToArray();
+            foreach (var monkey in monkeys) {
+                while (monkey.items.Any()) {
+                    monkey.inspectedItems++;
 
-        return (long)activeMonkeys[0].inspectedItems * activeMonkeys[1].inspectedItems;
-    }
+                    var item = monkey.items.Dequeue();
+                    item = monkey.operation(item);
+                    item = updateWorryLevel(item);
 
-    void Round(List<Monkey> monkeys, Func<long, long> updateWorryLevel) {
-        foreach (var monkey in monkeys) {
-            while (monkey.items.Any()) {
-                monkey.inspectedItems++;
+                    var targetMonkey = item % monkey.mod == 0 ?
+                        monkey.passToMonkeyIfDivides :
+                        monkey.passToMonkeyOtherwise;
 
-                var item = monkey.items.Dequeue();
-                item = monkey.operation(item);
-                item = updateWorryLevel(item);
-
-                int targetMonkey = item % monkey.mod == 0 ? 
-                    monkey.passToMonkeyIfDivides : 
-                    monkey.passToMonkeyOtherwise;
-
-                monkeys[targetMonkey].items.Enqueue(item);
+                    monkeys[targetMonkey].items.Enqueue(item);
+                }
             }
         }
     }
@@ -96,9 +81,24 @@ class Solution : Solver {
     class Monkey {
         public Queue<long> items;
         public Func<long, long> operation;
-        public int mod;
-        public int passToMonkeyIfDivides;
-        public int passToMonkeyOtherwise;
         public int inspectedItems;
+        public int mod;
+        public int passToMonkeyIfDivides, passToMonkeyOtherwise;
     }
+
+    // converts a line into a tryParse-style parser function
+    TryParse LineParser(string line) {
+        bool match(string pattern, out string arg) {
+            var m = Regex.Match(line, pattern);
+            if (m.Success) {
+                arg = m.Groups[m.Groups.Count - 1].Value;
+                return true;
+            } else {
+                arg = "";
+                return false;
+            }
+        }
+        return match;
+    }
+    delegate bool TryParse(string pattern, out string arg);
 }
