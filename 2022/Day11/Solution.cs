f8d7bdcb0138e5a8b52650e1790cf38fb15d5731
David Nemeth Csoka <encse@csokavar.hu>
2022-12-11 08:03:03 +0100

    2022/11 cleanup

--- 2022/Day11/Solution.cs
+++ 2022/Day11/Solution.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Text;
-using System.Numerics;
 
 namespace AdventOfCode.Y2022.Day11;
 
@@ -13,80 +10,94 @@ class Solution : Solver {
 
     public object PartOne(string input) {
         var monkeys = Parse(input);
-        for(var i =0;i<20;i++) {
-            Round(monkeys, worryLevel => worryLevel /3);
-        }
-        var res = monkeys.OrderByDescending(m => m.inspected).Take(2).ToArray();
-        return res[0].inspected * res[1].inspected;
+        return Run(20, monkeys, w => w / 3);
     }
 
     public object PartTwo(string input) {
         var monkeys = Parse(input);
 
         var mod = 1;
-        foreach(var m in monkeys){
-            mod *= m.test;
+        foreach (var m in monkeys) {
+            mod *= m.mod;
         }
 
-        for(var i =0;i<10000;i++) {
-            Round(monkeys, worryLevel => worryLevel % mod);
-        }
-        var res = monkeys.OrderByDescending(m => m.inspected).Take(2).ToArray();
-        return (long)res[0].inspected * (long)res[1].inspected;
+        return Run(10_000, monkeys, w => w % mod);
     }
-
 
     List<Monkey> Parse(string input) {
         var monkeys = new List<Monkey>();
+
         foreach (var block in input.Split("\n\n")) {
             var lines = block.Split("\n");
-            var items = new Queue<long>(lines[1].Split("Starting items: ")[1].Split(", ").Select(long.Parse));
-            Func<long,long> operation;
-            if (lines[2].Contains("Operation: new = old * old")) {
-                operation = (long old) => old * old;
-            } else if (lines[2].Contains("Operation: new = old * ")) {
-                var arg = int.Parse(lines[2].Split("Operation: new = old * ")[1]);
-                operation = (long old) => old * arg;
-            } else {
-                var arg = int.Parse(lines[2].Split("Operation: new = old + ")[1]);
-                operation = (long old) => old + arg;
-            }
 
-            var test = int.Parse(lines[3].Split("Test: divisible by ")[1]);
-            var ifTrue = int.Parse(lines[4].Split("If true: throw to monkey ")[1]);
-            var ifFalse =int.Parse(lines[5].Split("If false: throw to monkey ")[1]);
-            var monkey = new Monkey(
-                worryLevels:items,
-                operation: operation,
-                test: test,
-                ifTrue: ifTrue,
-                ifFalse: ifFalse
-            );
+            var monkey = new Monkey();
+            foreach (var line in lines) {
+                Match(line, "Starting items: (.*)", (arg) => {
+                    monkey.items = new Queue<long>(arg.Split(", ").Select(long.Parse));
+                });
+                Match(line, @"Operation: new = old \* old", _ => {
+                    monkey.operation = old => old * old;
+                });
+                Match(line, @"Operation: new = old \* (\d+)", arg => {
+                    monkey.operation = old => old * int.Parse(arg);
+                });
+                Match(line, @"Operation: new = old \+ (\d+)", arg => {
+                    monkey.operation = old => old + int.Parse(arg);
+                });
+                Match(line, @"Test: divisible by (\d+)", arg => {
+                    monkey.mod = int.Parse(arg);
+                });
+                Match(line, @"If true: throw to monkey (\d+)", arg => {
+                    monkey.passToMonkeyIfTrue = int.Parse(arg);
+                });
+                Match(line, @"If false: throw to monkey (\d+)", arg => {
+                    monkey.passToMonkeyIfFalse = int.Parse(arg);
+                });
+            }
             monkeys.Add(monkey);
         }
         return monkeys;
     }
+
+    void Match(string line, string pattern, Action<string> action) {
+        var m = Regex.Match(line, pattern);
+        if (m.Success) {
+            action(m.Groups[m.Groups.Count - 1].Value);
+        }
+    }
+
+    long Run(int rounds, List<Monkey> monkeys, Func<long, long> updateWorryLevel) {
+        for (var i = 0; i < rounds; i++) {
+            Round(monkeys, updateWorryLevel);
+        }
+        var activeMonkeys = monkeys.OrderByDescending(m => m.inspectedItems).Take(2).ToArray();
+        return (long)activeMonkeys[0].inspectedItems * activeMonkeys[1].inspectedItems;
+    }
+
     void Round(List<Monkey> monkeys, Func<long, long> updateWorryLevel) {
-       
-
         foreach (var monkey in monkeys) {
-            while (monkey.worryLevels.Any()) {
-                var worryLevel = monkey.worryLevels.Dequeue();
-                worryLevel = monkey.operation(worryLevel);
-                monkey.inspected++;
+            while (monkey.items.Any()) {
+                monkey.inspectedItems++;
 
-                worryLevel = updateWorryLevel(worryLevel);
+                var item = monkey.items.Dequeue();
+                item = monkey.operation(item);
+                item = updateWorryLevel(item);
 
-                if (worryLevel % monkey.test == 0) {
-                    monkeys[monkey.ifTrue].worryLevels.Enqueue(worryLevel);
-                } else {
-                    monkeys[monkey.ifFalse].worryLevels.Enqueue(worryLevel);
-                }
+                int targetMonkey = item % monkey.mod == 0 ? 
+                    monkey.passToMonkeyIfTrue : 
+                    monkey.passToMonkeyIfFalse;
+
+                monkeys[targetMonkey].items.Enqueue(item);
             }
         }
     }
 
-    record Monkey(Queue<long> worryLevels, Func<long, long> operation, int test, int ifTrue, int ifFalse) {
-        public int inspected { get; set; }
+    class Monkey {
+        public Queue<long> items;
+        public Func<long, long> operation;
+        public int mod;
+        public int passToMonkeyIfTrue;
+        public int passToMonkeyIfFalse;
+        public int inspectedItems;
     }
 }
