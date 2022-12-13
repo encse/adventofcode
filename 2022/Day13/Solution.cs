4aaf7a9dac0901ed298eee264e217dd7d7944606
David Nemeth Csoka <encse@csokavar.hu>
2022-12-13 08:36:42 +0100

    2022/13 with JsonNode.Parse

--- 2022/Day13/Solution.cs
+++ 2022/Day13/Solution.cs
@@ -14,26 +14,25 @@ class Solution : Solver {
             .Sum();
 
     public object PartTwo(string input) {
-        var p1 = JsonNode.Parse("[[2]]");
-        var p2 = JsonNode.Parse("[[6]]");
-        var packets = GetPackets(input).Append(p1).Append(p2).ToList();
+        var dividerPackets = new []{JsonNode.Parse("[[2]]"), JsonNode.Parse("[[6]]")}; 
+        var packets = dividerPackets.Concat(GetPackets(input)).ToList();
         packets.Sort(Compare);
-        return (packets.IndexOf(p1) + 1) * (packets.IndexOf(p2) + 1);
+        return (packets.IndexOf(dividerPackets[0]) + 1) * (packets.IndexOf(dividerPackets[1]) + 1);
     }
 
     IEnumerable<JsonNode> GetPackets(string input) =>
-         input
-            .Split("\n")
-            .Where(x => !string.IsNullOrEmpty(x))
-            .Select(line => JsonNode.Parse(line));
+        from line in input.Split("\n") 
+        where !string.IsNullOrEmpty(line) 
+        select JsonNode.Parse(line);
 
     int Compare(JsonNode left, JsonNode right) {
         if (left is JsonValue leftVal && right is JsonValue rightVal){
             return left.GetValue<int>() - right.GetValue<int>();
         }
 
-        var leftArray = left switch { JsonArray a => a, _ => new JsonArray(left.GetValue<int>())};
+        var leftArray  = left  switch { JsonArray a => a, _ => new JsonArray(left.GetValue<int>())};
         var rightArray = right switch { JsonArray a => a, _ => new JsonArray(right.GetValue<int>())};
+
         foreach (var (leftItem, rightItem) in Enumerable.Zip(leftArray, rightArray)) {
             var c = Compare(leftItem, rightItem);
             if (c != 0) {
