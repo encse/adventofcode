48a3a6e3cd7f9cbbd26e6f2830806c8968881c29
David Nemeth Csoka <encse@csokavar.hu>
2022-12-20 13:06:04 +0100

    2022/20 commented

--- 2022/Day20/Solution.cs
+++ 2022/Day20/Solution.cs
@@ -14,20 +14,21 @@ class Solution : Solver {
             .Split("\n")
             .Select(line => multiplier * long.Parse(line))
             .ToArray();
-
-        // permuted array position -> numbers array position
-        // e.g. perm[5] means: 
-        //    element 5 of the perm array can be found at perm[5] in the nums array
+        
+        // invariant: 
+        // num[perm[0]] the first element of the permuted array
+        // num[perm[1]] the second element of the permuted array
+        // ....
         var perm = Enumerable.Range(0, nums.Length).ToArray();
-
+      
         for (var iter = 0; iter < iterations; iter++) {
+          
             for (var inum = 0; inum < nums.Length; inum++) {
-
-                // ith num should be moved to the right by 'num' steps with wrap around:
-                var iperm = Array.IndexOf(perm, inum);
+                // move to the right by 'num' steps with wrap around:
+                var iperm = Array.IndexOf(perm, inum); 
                 var steps = nums[inum] % (nums.Length - 1);
                 var dir = Math.Sign(steps);
-                
+
                 for (var i = 0; i != steps; i += dir) {
                     var ipermNext = (iperm + dir + nums.Length) % nums.Length;
                     (perm[ipermNext], perm[iperm]) = (perm[iperm], perm[ipermNext]);
