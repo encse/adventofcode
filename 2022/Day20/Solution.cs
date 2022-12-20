a3d557db5c0c54ffefe262c42cd2758dabbd7261
David Nemeth Csoka <encse@csokavar.hu>
2022-12-20 12:58:19 +0100

    2022/20 commented

--- 2022/Day20/Solution.cs
+++ 2022/Day20/Solution.cs
@@ -20,26 +20,16 @@ class Solution : Solver {
         //    element 5 of the perm array can be found at perm[5] in the nums array
         var perm = Enumerable.Range(0, nums.Length).ToArray();
 
-        for (var i = 0; i < iterations; i++) {
+        for (var iter = 0; iter < iterations; iter++) {
             for (var inum = 0; inum < nums.Length; inum++) {
-                var num = nums[inum];
 
-                if (num < 0) {
-                    // negative numbers can be thought of as positive numbers
-                    // we don't want to implement moving in both directions
-                    // so let's convert it to a positive number here 
-                    num = -num % (nums.Length - 1);
-                    num = (nums.Length - 1) - num;
-                } else {
-                    num = num % (nums.Length - 1);
-                }
-
-                // position of inum in the permuted array:
+                // ith num should be moved to the right by 'num' steps with wrap around:
                 var iperm = Array.IndexOf(perm, inum);
-
-                // should be moved to the right by 'num' steps with wrap around:
-                for (var j = 0; j < num; j++) {
-                    var ipermNext = (iperm + 1) % nums.Length;
+                var steps = nums[inum] % (nums.Length - 1);
+                var dir = Math.Sign(steps);
+                
+                for (var i = 0; i != steps; i += dir) {
+                    var ipermNext = (iperm + dir + nums.Length) % nums.Length;
                     (perm[ipermNext], perm[iperm]) = (perm[iperm], perm[ipermNext]);
                     iperm = ipermNext;
                 }
@@ -50,8 +40,8 @@ class Solution : Solver {
         var idx0 = Array.IndexOf(nums, 0);
         return (
             nums[(idx0 + 1000) % nums.Length] +
-            nums[(idx0 + 2000) % nums.Length] + 
-            nums[(idx0 + 3000) % nums.Length] 
+            nums[(idx0 + 2000) % nums.Length] +
+            nums[(idx0 + 3000) % nums.Length]
         );
     }
 }
