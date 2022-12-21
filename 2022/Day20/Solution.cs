ce210cfc36c7292e3d0fff70c63fe77bfce18aa6
David Nemeth Csoka <encse@csokavar.hu>
2022-12-21 10:28:41 +0100

    2022/20 cleanup

--- 2022/Day20/Solution.cs
+++ 2022/Day20/Solution.cs
@@ -1,48 +1,53 @@
-using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Y2022.Day20;
 
 [ProblemName("Grove Positioning System")]
 class Solution : Solver {
 
-    public object PartOne(string input) => GetGrooveCoordinates(1, 1, input);
-    public object PartTwo(string input) => GetGrooveCoordinates(10, 811589153L, input);
+    record Data(int idx, long num);
 
-    long GetGrooveCoordinates(int iterations, long multiplier, string input) {
-        var nums = input
-            .Split("\n")
-            .Select(line => multiplier * long.Parse(line))
-            .ToArray();
-        
-        // invariant: 
-        // num[perm[0]] the first element of the permuted array
-        // num[perm[1]] the second element of the permuted array
-        // ....
-        var perm = Enumerable.Range(0, nums.Length).ToArray();
-      
-        for (var iter = 0; iter < iterations; iter++) {
-          
-            for (var inum = 0; inum < nums.Length; inum++) {
-                // move to the right by 'num' steps with wrap around:
-                var iperm = Array.IndexOf(perm, inum); 
-                var steps = nums[inum] % (nums.Length - 1);
-                var dir = Math.Sign(steps);
+    public object PartOne(string input) =>
+         GetGrooveCoordinates(Mix(Parse(input, 1)));
 
-                for (var i = 0; i != steps; i += dir) {
-                    var ipermNext = (iperm + dir + nums.Length) % nums.Length;
-                    (perm[ipermNext], perm[iperm]) = (perm[iperm], perm[ipermNext]);
-                    iperm = ipermNext;
-                }
-            }
+    public object PartTwo(string input) {
+        var data = Parse(input, 811589153L);
+        for (var i = 0; i < 10; i++) {
+            data = Mix(data);
         }
+        return GetGrooveCoordinates(data);
+    }
 
-        nums = perm.Select(inum => nums[inum]).ToArray();
-        var idx0 = Array.IndexOf(nums, 0);
+    List<Data> Parse(string input, long m) =>
+        input
+            .Split("\n")
+            .Select((line, idx) => new Data(idx, long.Parse(line) * m))
+            .ToList();
+
+    List<Data> Mix(List<Data> numsWithIdx) {
+        var mod = numsWithIdx.Count - 1;
+        for (var idx = 0; idx < numsWithIdx.Count; idx++) {
+            var srcIdx = numsWithIdx.FindIndex(x => x.idx == idx);
+            var num = numsWithIdx[srcIdx];
+
+            var dstIdx = (srcIdx + num.num) % mod;
+            if (dstIdx < 0) {
+                dstIdx += mod;
+            }
+
+            numsWithIdx.RemoveAt(srcIdx);
+            numsWithIdx.Insert((int)dstIdx, num);
+        }
+        return numsWithIdx;
+    }
+
+    long GetGrooveCoordinates(List<Data> numsWithIdx) {
+        var idx = numsWithIdx.FindIndex(x => x.num == 0);
         return (
-            nums[(idx0 + 1000) % nums.Length] +
-            nums[(idx0 + 2000) % nums.Length] +
-            nums[(idx0 + 3000) % nums.Length]
+            numsWithIdx[(idx + 1000) % numsWithIdx.Count].num +
+            numsWithIdx[(idx + 2000) % numsWithIdx.Count].num +
+            numsWithIdx[(idx + 3000) % numsWithIdx.Count].num
         );
     }
 }
