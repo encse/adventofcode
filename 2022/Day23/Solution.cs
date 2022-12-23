b281dfb753e679d8ee4df4b28ab1c026384d7917
David Nemeth Csoka <encse@csokavar.hu>
2022-12-23 08:21:25 +0100

    2022/23

--- 2022/Day23/Solution.cs
+++ 2022/Day23/Solution.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace AdventOfCode.Y2022.Day23;
 
@@ -12,36 +13,38 @@ class Solution : Solver {
         for (var i = 0; i < 10; i++) {
             state = Step(state);
         }
-        var w = state.elves.MaxBy(p => p.icol).icol - state.elves.MinBy(p => p.icol).icol + 1;
-        var h = state.elves.MaxBy(p => p.irow).irow - state.elves.MinBy(p => p.irow).irow + 1;
+
+        // smallest enclosing rectangle
+        var w = state.elves.MaxBy(p => p.Real).Real - state.elves.MinBy(p => p.Real).Real + 1;
+        var h = state.elves.MaxBy(p => p.Imaginary).Imaginary - state.elves.MinBy(p => p.Imaginary).Imaginary + 1;
+
+        // return area - elf count
         return w * h - state.elves.Count;
     }
 
     public object PartTwo(string input) {
-        var state = Parse(input);
+        // interate until fixpoint is reached
         var steps = 0;
+
+        var state = Parse(input);
         while (!state.fixpoint) {
             state = Step(state);
             steps++;
         }
+
         return steps;
     }
 
-
     State Step(State state) {
 
-        bool occupied(Pos pos) => state.elves.Contains(pos);
-
-        bool lonely(Pos elf) => directions.All(dir => !occupied(elf + dir));
+        bool occupied(Complex pos) => state.elves.Contains(pos);
+        bool lonely(Complex pos) => directions.All(dir => !occupied(pos + dir));
 
         // elf proposes a postion if nobody is nearby in that direction
-        bool proposes(Pos elf, string dir) => extendDir(dir).All(d => !occupied(elf + d));
-
-        // adds intercardinal positions to an ordinal postion, e.g for N it returns NW, N, NE; 
-        string[] extendDir(string dir) => directions.Where(d => d.Contains(dir)).ToArray();
+        bool proposes(Complex elf, Complex dir) => ExtendDir(dir).All(d => !occupied(elf + d));
 
         // for each position (key) it has a list of the elves who wants to step there
-        var proposals = new Dictionary<Pos, List<Pos>>();
+        var proposals = new Dictionary<Complex, List<Complex>>();
 
         foreach (var elf in state.elves) {
             if (lonely(elf)) {
@@ -52,7 +55,7 @@ class Solution : Solver {
                 if (proposes(elf, dir)) {
                     var pos = elf + dir;
                     if (!proposals.ContainsKey(pos)) {
-                        proposals[pos] = new List<Pos>();
+                        proposals[pos] = new List<Complex>();
                     }
                     proposals[pos].Add(elf);
                     break;
@@ -60,6 +63,7 @@ class Solution : Solver {
             }
         }
 
+        // move elves, compute fixpoint flag
         var fixpoint = true;
         foreach (var p in proposals) {
             var (to, from) = p;
@@ -69,44 +73,50 @@ class Solution : Solver {
                 fixpoint = false;
             }
         }
-        
+
         return new State(
-            fixpoint,
             state.elves,
-            state.directions.Skip(1).Concat(state.directions.Take(1)).ToList()
+            state.directions.Skip(1).Concat(state.directions.Take(1)).ToList(),
+            fixpoint
         );
     }
 
     State Parse(string input) {
         var lines = input.Split("\n");
+
         var elves = (
             from irow in Enumerable.Range(0, lines.Length)
             from icol in Enumerable.Range(0, lines[0].Length)
             where lines[irow][icol] == '#'
-            select new Pos(irow, icol)
+            select new Complex(icol, irow)
         ).ToHashSet();
-        var directions = new List<string>() { "N", "S", "W", "E" };
 
-        return new State(fixpoint:false, elves, directions: directions);
+        var directions = new List<Complex>() { N, S, W, E };
+
+        return new State(elves, directions, fixpoint: false);
     }
 
-    string[] directions = "NW N NE E SE S SW W".Split(" ");
+    ///  -------
 
-    record Pos(int irow, int icol) {
-        public static Pos operator +(Pos pos, string dir) =>
-            dir switch {
-                "N" => pos with { irow = pos.irow - 1 },
-                "S" => pos with { irow = pos.irow + 1 },
-                "W" => pos with { icol = pos.icol - 1 },
-                "E" => pos with { icol = pos.icol + 1 },
-                "NW" => pos + "N" + "W",
-                "NE" => pos + "N" + "E",
-                "SW" => pos + "S" + "W",
-                "SE" => pos + "S" + "E",
-                _ => throw new Exception()
-            };
-    }
+    record State(HashSet<Complex> elves, List<Complex> directions, bool fixpoint);
 
-    record State(bool fixpoint, HashSet<Pos> elves, List<string> directions);
+    static Complex N = new Complex(0, -1);
+    static Complex E = new Complex(1, 0);
+    static Complex S = new Complex(0, 1);
+    static Complex W = new Complex(-1, 0);
+    static Complex NW = N + W;
+    static Complex NE = N + E;
+    static Complex SE = S + E;
+    static Complex SW = S + W;
+
+    static Complex[] directions = new[] { NW, N, NE, E, SE, S, SW, W };
+
+    // Extends an ordinal position with its intercardinal neighbours
+    Complex[] ExtendDir(Complex dir) =>
+        dir == N ? new[] { NW, N, NE } :
+        dir == E ? new[] { NE, E, SE } :
+        dir == S ? new[] { SW, S, SE } :
+        dir == W ? new[] { NW, W, SW } :
+                   throw new Exception();
 
 }
