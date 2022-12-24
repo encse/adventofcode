a7bbc3465e48cd46d9e05c420bdb24879ae0d8ec
David Nemeth Csoka <encse@csokavar.hu>
2022-12-24 21:25:00 +0100

    2022/24

--- 2022/Day24/Solution.cs
+++ 2022/Day24/Solution.cs
@@ -1,18 +1,14 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace AdventOfCode.Y2022.Day24;
 
 [ProblemName("Blizzard Basin")]
 class Solution : Solver {
-    
+
     // We do a standard A* algorithm, the only trick is that
-    // the 'map' always changes as blizzards move. Luckily blizzards get 
-    // into a loop pretty soon, so we have to deal with only a few 
-    // different maps. These are precomputed in the Parse() phase and
-    // stored in a Maps structure. Later it's very cheap to check if
-    // a position is free or not at a given time.
+    // the 'map' always changes as blizzards move, I used a Maps
+    // class that can be queried with time + irow + icol.
 
     public object PartOne(string input) {
         var (entry, exit, maps) = Parse(input);
@@ -34,15 +30,15 @@ class Solution : Solver {
 
         var q = new PriorityQueue<SearchState, int>();
 
-        void enqueue(SearchState state) {
+        int f(SearchState state) {
             // estimate the remaining step count with Manhattan distance
             var dist =
                 Math.Abs(goal.irow - state.pos.irow) +
                 Math.Abs(goal.icol - state.pos.icol);
-            q.Enqueue(state, state.time + dist);
+            return state.time + dist;
         }
 
-        enqueue(intialState);
+        q.Enqueue(intialState, f(intialState));
         HashSet<SearchState> seen = new HashSet<SearchState>();
 
         while (q.Count > 0) {
@@ -54,7 +50,7 @@ class Solution : Solver {
             foreach (var stateNeighbour in NextStates(state, maps)) {
                 if (!seen.Contains(stateNeighbour)) {
                     seen.Add(stateNeighbour);
-                    enqueue(stateNeighbour);
+                    q.Enqueue(stateNeighbour, f(stateNeighbour));
                 }
             }
         }
@@ -80,112 +76,55 @@ class Solution : Solver {
         }
     }
 
-    // Create a list of maps from the input containing all blizzard positions as
-    // time goes. It will loop pretty quickly.
     (Pos entry, Pos exit, Maps maps) Parse(string input) {
-        var mapList = new List<string[]>();
-        var blizzards = ParseBlizzards(input);
-        var map = MarkBlizzards(input, blizzards);
-        while (true) {
-            mapList.Add(map);
-            blizzards = blizzards.Select(b => b.Step()).ToList();
-            map = MarkBlizzards(input, blizzards);
-            if (string.Join("\n", map) == string.Join("\n", mapList[0])) {
-                break;
-            }
-        }
-
-        var maps = new Maps(mapList.ToArray());
+        var maps = new Maps(input);
         var entry = new Pos(0, 1);
         var exit = new Pos(maps.crow - 1, maps.ccol - 2);
         return (entry, exit, maps);
     }
 
-    List<Blizzard> ParseBlizzards(string input) {
-        var lines = input.Split("\n");
-        var crow = lines.Length;
-        var ccol = lines[0].Length;
-
-        var result = new List<Blizzard>();
-        for (var irow = 0; irow < crow; irow++) {
-            for (var icol = 0; icol < ccol; icol++) {
-                var ch = lines[irow][icol];
-                var dir =
-                    ch == '>' ? new Pos(0, 1) :
-                    ch == '<' ? new Pos(0, -1) :
-                    ch == '^' ? new Pos(-1, 0) :
-                    ch == 'v' ? new Pos(1, 0) :
-                                new Pos(0, 0);
-                if (dir != new Pos(0, 0)) {
-                    result.Add(
-                        new Blizzard(new Pos(irow, icol), dir, crow, ccol));
-                }
-            }
-        }
-        return result;
-    }
-
-    // Updates the input map with different blizzard postions. The positions
-    // are just are marked with a 'B', their moving direction is not kept.
-    string[] MarkBlizzards(string input, IEnumerable<Blizzard> blizzards) {
-        var map = input
-            .Replace(">", ".")
-            .Replace("<", ".")
-            .Replace("^", ".")
-            .Replace("v", ".")
-            .Split("\n")
-            .Select(line => line.ToCharArray())
-            .ToArray();
-
-        foreach (var b in blizzards) {
-            map[b.pos.irow][b.pos.icol] = 'B';
-        }
-
-        return map.Select(line => string.Join("", line)).ToArray();
-    }
-
     record SearchState(int time, Pos pos);
-
     record Pos(int irow, int icol);
 
-    // Represents one blizzard that can move in a certain direction 
-    // looping around the map as per task description.
-    record Blizzard(Pos pos, Pos dir, int crow, int ccol) {
-        public Blizzard Step() {
-            var irow = pos.irow + dir.irow;
-            if (irow == 0) {
-                irow = crow - 2;
-            } else if (irow == crow - 1) {
-                irow = 1;
-            }
-
-            var icol = pos.icol + dir.icol;
-            if (icol == 0) {
-                icol = ccol - 2;
-            } else if (icol == ccol - 1) {
-                icol = 1;
-            }
-
-            return this with { pos = new Pos(irow, icol) };
-        }
-    }
-
     // Time indexable list of maps
-    record Maps(string[][] maps) {
-        public int crow => maps[0].Length;
-        public int ccol => maps[0][0].Length;
+    class Maps {
+        private string[] lines;
+        public readonly int crow;
+        public readonly int ccol;
+
+        public Maps(string input) {
+            lines = input.Split("\n");
+            this.crow = lines.Length;
+            this.ccol = lines[0].Length;
+        }
 
         public char Get(int time, Pos pos) {
-            var map = maps[time % maps.Length];
+            if (pos.irow == 0 && pos.icol == 1) {
+                return '.';
+            }
+            if (pos.irow == crow - 1 && pos.icol == ccol - 2) {
+                return '.';
+            }
 
-            if (pos.irow >= 0 && pos.irow < crow &&
-                pos.icol >= 0 && pos.icol < ccol
-            ) {
-                return map[pos.irow][pos.icol];
-            } else {
+            if (pos.irow <= 0 || pos.irow >= crow - 1 || pos.icol <= 0 || pos.icol >= ccol - 1) {
                 return '#';
+            }
+
+            var icolLeft =  (pos.icol - 1 - time + 1000 * (ccol - 2)) % (ccol - 2) + 1;
+            var icolRight = (pos.icol - 1 + time + 1000 * (ccol - 2)) % (ccol - 2) + 1;
+            var irowUp =    (pos.irow - 1 - time + 1000 * (crow - 2)) % (crow - 2) + 1;
+            var irowDown =  (pos.irow - 1 + time + 1000 * (crow - 2)) % (crow - 2) + 1;
+
+            if (
+                lines[pos.irow][icolLeft] == '>' ||
+                lines[pos.irow][icolRight] == '<' ||
+                lines[irowUp][pos.icol] == 'v' ||
+                lines[irowDown][pos.icol] == '^'
+            ) {
+                return '#';
+            } else {
+                return '.';
             }
         }
     }
-
 }
