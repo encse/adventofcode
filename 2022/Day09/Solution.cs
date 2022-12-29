c537d2ee7f3284f30ad737d3d5d38f117a721387
David Nemeth Cs <encse@csokavar.hu>
2022-12-29 18:21:25 +0100

    Update Solution.cs

--- 2022/Day09/Solution.cs
+++ 2022/Day09/Solution.cs
@@ -10,8 +10,9 @@ class Solution : Solver {
     public object PartOne(string input) => Tails(input, 2).ToHashSet().Count;
     public object PartTwo(string input) => Tails(input, 10).ToHashSet().Count;
 
-    // Moves a rope with the given length according to the input and
-    // returns the position of its tail in each step.
+    // simulates a rope with the given length as its head moves
+    // according to the input and returns the position of its 
+    // tail knot in each step.
     private IEnumerable<Knot> Tails(string input, int ropeLength) {
         var rope = Enumerable.Repeat(new Knot(0, 0), ropeLength).ToArray();
         yield return rope.Last();
@@ -30,7 +31,8 @@ class Solution : Solver {
 
     record struct Knot(int irow, int icol);
 
-    // moves the head in the given direction, inplace update of the rope
+    // moves the head in the given direction, inplace update 
+    // of the rope
     void MoveHead(Knot[] rope, string dir) {
         rope[0] = dir switch {
             "U" => rope[0] with { irow = rope[0].irow - 1 },
@@ -40,7 +42,8 @@ class Solution : Solver {
             _ => throw new ArgumentException(dir)
         };
 
-        // move knots which are not adjacent to their previous sibling in the rope:
+        // knots move when become disconnected from the previous 
+        // sibling in the rope:
         for (var i = 1; i < rope.Length; i++) {
             var drow = rope[i - 1].irow - rope[i].irow; 
             var dcol = rope[i - 1].icol - rope[i].icol;
