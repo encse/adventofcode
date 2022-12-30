5e2806bddfa17df93e6cd4847d39c8f03782c3e6
David Nemeth Csoka <encse@csokavar.hu>
2022-12-30 09:01:03 +0100

    2022/04 comment

--- 2022/Day04/Solution.cs
+++ 2022/Day04/Solution.cs
@@ -5,22 +5,37 @@ namespace AdventOfCode.Y2022.Day04;
 
 [ProblemName("Camp Cleanup")]
 class Solution : Solver {
-    public object PartOne(string input) => DuplicateWorkCount(input, Contains);
-    public object PartTwo(string input) => DuplicateWorkCount(input, Overlaps);
+    public object PartOne(string input) => DuplicatedWorkCount(input, Contains);
+    public object PartTwo(string input) => DuplicatedWorkCount(input, Overlaps);
 
     record struct Range(int from, int to);
+
+    // True if r1 contains r2 [ { } ] 
     bool Contains(Range r1, Range r2) => r1.from <= r2.from && r2.to <= r1.to; 
+    
+    // True if r1 overlaps r2 { [ } ], the other direction is not checked.
     bool Overlaps(Range r1, Range r2) => r1.to >= r2.from && r1.from <= r2.to; 
 
-    private int DuplicateWorkCount(string input, Func<Range, Range, bool> rangeCheck) {
+    // DuplicatedWorkCount goes over the lines in the input, converts them to 
+    // ranges A and B, and counts how many times rangeCheck(A,B) or 
+    // rangeCheck(B, A) is true. The check is applied in both ways so that the 
+    // Contains and Overlaps functions don't have to check each directions.
+    private int DuplicatedWorkCount(
+        string input, 
+        Func<Range, Range, bool> rangeCheck
+    ) {
+        // '36-41,35-40' becomes [Range(36, 41), Range(35, 40)]
         var parseRanges = (string line) => 
             from range in line.Split(',') 
             let fromTo = range.Split('-').Select(int.Parse)
             select new Range(fromTo.First(), fromTo.Last());
 
-        return input.Split("\n").Select(parseRanges).Count(ranges => 
-            rangeCheck(ranges.First(), ranges.Last()) || 
-            rangeCheck(ranges.Last(), ranges.First())
-        );
+        return input
+            .Split("\n")
+            .Select(parseRanges)
+            .Count(ranges => 
+                rangeCheck(ranges.First(), ranges.Last()) || 
+                rangeCheck(ranges.Last(), ranges.First())
+            );
     }
 }
