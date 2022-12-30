2c292795929cb557c5e7ecc4f35fc5c740e35ae0
David Nemeth Csoka <encse@csokavar.hu>
2022-12-30 09:01:21 +0100

    2022/06 comment

--- 2022/Day06/Solution.cs
+++ 2022/Day06/Solution.cs
@@ -8,7 +8,10 @@ class Solution : Solver {
 
     public object PartOne(string input) => StartOfBlock(input, 4);
     public object PartTwo(string input) => StartOfBlock(input, 14);
+
+    // Slides a window of length l over the input and finds the first position
+    // where each character is different. Returns the right of the window.
     int StartOfBlock(string input, int l) =>
          Enumerable.Range(l, input.Length)
-            .First(i => input.Substring(i-l, l).ToHashSet().Count == l);
+            .First(i => input.Substring(i - l, l).ToHashSet().Count == l);
 }
