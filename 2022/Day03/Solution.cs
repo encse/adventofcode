de703ecc0a0495d575d8df2b37154df0ce16cbd1
David Nemeth Csoka <encse@csokavar.hu>
2022-12-30 09:00:56 +0100

    2022/03 comment

--- 2022/Day03/Solution.cs
+++ 2022/Day03/Solution.cs
@@ -8,12 +8,20 @@ namespace AdventOfCode.Y2022.Day03;
 class Solution : Solver {
 
     public object PartOne(string input) =>
+        // A line can be divided to two 'compartments' of equal length. We need 
+        // to find the common item (letter) in them, and convert it to a number 
+        // called 'priority'. Do this for each line and sum the priorities. 
+        // We use 'chunk' to split a line in half.
         input.Split("\n")
             .Select(line => line.Chunk(line.Length/2)) // 🥩 
             .Select(GetCommonItemPriority)
             .Sum();
 
     public object PartTwo(string input) =>
+        // Here we need to find the common item in three consecutive lines, 
+        // convert it to priority as before, and sum it up along the whole 
+        // input. 
+        // This is again conveniently done using the chunk function.
         input.Split("\n")
             .Chunk(3)
             .Select(GetCommonItemPriority)
