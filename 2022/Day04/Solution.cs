672d35cb04749c14e65a1852e5b6f64ad307afa3
David Nemeth Csoka <encse@csokavar.hu>
2022-12-31 21:11:21 +0100

    2022/04 comments

--- 2022/Day04/Solution.cs
+++ 2022/Day04/Solution.cs
@@ -5,10 +5,14 @@ namespace AdventOfCode.Y2022.Day04;
 
 [ProblemName("Camp Cleanup")]
 class Solution : Solver {
+    
+    // Each line of the input represents two ranges - job done by two elves.
+    // We need to find those lines where the elves did some work twice.
+    // Part 1 and 2 differs in how we define 'duplicated work'.
+    record struct Range(int from, int to);
+
     public object PartOne(string input) => DuplicatedWorkCount(input, Contains);
     public object PartTwo(string input) => DuplicatedWorkCount(input, Overlaps);
-
-    record struct Range(int from, int to);
 
     // True if r1 contains r2 [ { } ] 
     bool Contains(Range r1, Range r2) => r1.from <= r2.from && r2.to <= r1.to; 
@@ -16,15 +20,15 @@ class Solution : Solver {
     // True if r1 overlaps r2 { [ } ], the other direction is not checked.
     bool Overlaps(Range r1, Range r2) => r1.to >= r2.from && r1.from <= r2.to; 
 
-    // DuplicatedWorkCount goes over the lines in the input, converts them to 
-    // ranges A and B, and counts how many times rangeCheck(A,B) or 
-    // rangeCheck(B, A) is true. The check is applied in both ways so that the 
-    // Contains and Overlaps functions don't have to check each directions.
+    // DuplicatedWorkCount parses each input line into ranges and applies 
+    // rangeCheck on them to find duplicated work. RangeCheck doesnt have to be 
+    // symmetrical in its arguments, but DuplicatedWorkCount makes it so calling
+    // it twice with the arguments swapped.
     private int DuplicatedWorkCount(
         string input, 
         Func<Range, Range, bool> rangeCheck
     ) {
-        // '36-41,35-40' becomes [Range(36, 41), Range(35, 40)]
+        // E.g. '36-41,35-40' becomes [Range(36, 41), Range(35, 40)]
         var parseRanges = (string line) => 
             from range in line.Split(',') 
             let fromTo = range.Split('-').Select(int.Parse)
