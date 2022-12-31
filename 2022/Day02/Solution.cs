8ec7758b60875ec8012d56768135aa6f9d9c7273
David Nemeth Csoka <encse@csokavar.hu>
2022-12-31 14:59:35 +0100

    2022/02 in a more readable way

--- 2022/Day02/Solution.cs
+++ 2022/Day02/Solution.cs
@@ -6,34 +6,57 @@ namespace AdventOfCode.Y2022.Day02;
 [ProblemName("Rock Paper Scissors")]
 class Solution : Solver {
 
-     public object PartOne(string input) {
-        return (
-            from match in input.Split('\n')
-            select Score(match, 'A', 'B', 'C')
-        ).Sum();
+    // There are many obscure ways of solving this challenge, this is a 
+    // rather explicit one. We parse the input lines into a pair of
+    // Rock/Paper/Scissors signs represented as 1,2,3 (the values from the
+    // problem description). We calculate the score for each pair and sum up
+    // the result.
+
+    enum Sign {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3,
     }
 
-    public object PartTwo(string input) {
-        return (
+    public object PartOne(string input) => (
             from match in input.Split('\n')
-            select Score(match, Prev(match[0]), match[0], Next(match[0]))
+                let elf = 
+                    match[0] == 'A' ? Sign.Rock :
+                    match[0] == 'B' ? Sign.Paper :
+                    match[0] == 'C' ? Sign.Scissors :
+                         throw new ArgumentException(match)
+                let you = 
+                    match[2] == 'X' ? Sign.Rock :
+                    match[2] == 'Y' ? Sign.Paper :
+                    match[2] == 'Z' ? Sign.Scissors :
+                                      throw new ArgumentException(match)
+            select Score(elf, you)
         ).Sum();
-    }
+
+    public object PartTwo(string input) => (
+            from match in input.Split('\n')
+                let elf = 
+                    match[0] == 'A' ? Sign.Rock :
+                    match[0] == 'B' ? Sign.Paper :
+                    match[0] == 'C' ? Sign.Scissors :
+                                      throw new ArgumentException(match)
+                let you = 
+                    match[2] == 'X' ? Next(Next(elf)): // elf wins
+                    match[2] == 'Y' ? elf :            // draw
+                    match[2] == 'Z' ? Next(elf) :      // you win
+                                      throw new ArgumentException(match)
+            select Score(elf, you)
+        ).Sum();
     
-    int Score(string match, char xMeaning, char yMeaning, char zMeaning) {
-        var you = 
-            match[2] == 'X' ? xMeaning :
-            match[2] == 'Y' ? yMeaning :
-            match[2] == 'Z' ? zMeaning :
-            throw new Exception();
+    int Score(Sign elfSign, Sign yourSign) =>
+        yourSign == Next(elfSign)       ? 6 + (int)yourSign : // you win
+        yourSign == elfSign             ? 3 + (int)yourSign : // draw
+        yourSign == Next(Next(elfSign)) ? 0 + (int)yourSign : // elf wins
+        throw new ArgumentException();
 
-        return 
-            you == Prev(match[0]) ? 0 + you - 'A' + 1:
-            you == match[0]       ? 3 + you - 'A' + 1:
-            you == Next(match[0]) ? 6 + you - 'A' + 1:
-            throw new Exception();
-    }
-
-    char Next(char ch) => "ABCAB"[ch - 'A' + 1];
-    char Prev(char ch) => "ABCAB"[ch - 'A' + 2];
+    Sign Next(Sign sign) => 
+        sign == Sign.Rock     ? Sign.Paper : 
+        sign == Sign.Paper    ? Sign.Scissors : 
+        sign == Sign.Scissors ? Sign.Rock : 
+        throw new ArgumentException();
 }
