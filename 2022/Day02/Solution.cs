e1ae78397226bffcb1b8f3e1cd54af327e26db29
David Nemeth Csoka <encse@csokavar.hu>
2023-01-01 09:29:36 +0100

    2022/02 factor out some logic

--- 2022/Day02/Solution.cs
+++ 2022/Day02/Solution.cs
@@ -7,8 +7,8 @@ namespace AdventOfCode.Y2022.Day02;
 class Solution : Solver {
 
     // There are many obscure ways of solving this challenge. You can use 
-    // mod 3 arithmetic or play with ASCII encoding. This approach is a more 
-    // explicit one. I think it is as simple as it gets.
+    // mod 3 arithmetic or play with ASCII encoding. This approach is more 
+    // explicit. I think it is as simple as it gets.
 
     // We parse the input lines into a pair of Rock/Paper/Scissors signs 
     // represented by 1,2,3 (the values from the problem description), 
@@ -22,40 +22,40 @@ class Solution : Solver {
         Scissors = 3,
     }
 
-    public object PartOne(string input) => (
-        from line in input.Split('\n')
-            let elf = 
-                line[0] == 'A' ? Sign.Rock :
-                line[0] == 'B' ? Sign.Paper :
-                line[0] == 'C' ? Sign.Scissors :
-                                 throw new ArgumentException(line)
-            let you = 
-                line[2] == 'X' ? Sign.Rock :
-                line[2] == 'Y' ? Sign.Paper :
-                line[2] == 'Z' ? Sign.Scissors :
-                                 throw new ArgumentException(line)
-        select Score(elf, you)
-    ).Sum();
-
-    public object PartTwo(string input) => (
-        from line in input.Split('\n')
-            let elf = 
-                line[0] == 'A' ? Sign.Rock :
-                line[0] == 'B' ? Sign.Paper :
-                line[0] == 'C' ? Sign.Scissors :
-                                 throw new ArgumentException(line)
-            let you = 
-                line[2] == 'X' ? Next(Next(elf)): // elf wins
-                line[2] == 'Y' ? elf :            // draw
-                line[2] == 'Z' ? Next(elf) :      // you win
-                                 throw new ArgumentException(line)
-        select Score(elf, you)
-    ).Sum();
+    public object PartOne(string input) => 
+        Total(input, ElfParser, HumanParser1);
     
-    int Score(Sign elfSign, Sign yourSign) =>
-        yourSign == Next(elfSign)       ? 6 + (int)yourSign : // you win
-        yourSign == elfSign             ? 3 + (int)yourSign : // draw
-        yourSign == Next(Next(elfSign)) ? 0 + (int)yourSign : // elf wins
+    public object PartTwo(string input) => 
+        Total(input, ElfParser, HumanParser2);
+
+    Sign ElfParser(string line) =>
+        line[0] == 'A' ? Sign.Rock :
+        line[0] == 'B' ? Sign.Paper :
+        line[0] == 'C' ? Sign.Scissors :
+                         throw new ArgumentException(line);
+
+    Sign HumanParser1(string line) =>   
+        line[2] == 'X' ? Sign.Rock :
+        line[2] == 'Y' ? Sign.Paper :
+        line[2] == 'Z' ? Sign.Scissors :
+                         throw new ArgumentException(line);
+
+    Sign HumanParser2(string line) =>   
+        line[2] == 'X' ? Next(Next(ElfParser(line))): // elf wins
+        line[2] == 'Y' ? ElfParser(line) :            // draw
+        line[2] == 'Z' ? Next(ElfParser(line)) :      // you win
+                         throw new ArgumentException(line);
+          
+    int Total(string input, Func<string, Sign> elf, Func<string, Sign> human) =>
+        input
+            .Split("\n")
+            .Select(line => Score(elf(line), human(line)))
+            .Sum();
+
+    int Score(Sign elfSign, Sign humanSign) =>
+        humanSign == Next(elfSign)       ? 6 + (int)humanSign : // human wins
+        humanSign == elfSign             ? 3 + (int)humanSign : // draw
+        humanSign == Next(Next(elfSign)) ? 0 + (int)humanSign : // elf wins
                                           throw new ArgumentException();
 
     Sign Next(Sign sign) => 
