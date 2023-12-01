d156c75a18f62700f0b72660593e2eec3d50f05c
David Nemeth Csoka <encse@csokavar.hu>
2023-12-01 09:13:16 +0100

    2023/01 with regex

--- 2023/Day01/Solution.cs
+++ 2023/Day01/Solution.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Y2023.Day01;
 
@@ -7,46 +7,31 @@ namespace AdventOfCode.Y2023.Day01;
 class Solution : Solver {
 
     public object PartOne(string input) => 
-        Solve(input, Digit);
+        Solve(input, @"\d");
 
     public object PartTwo(string input) => 
-        Solve(input, st => Digit(st) ?? SpelledOutNumber(st));
+        Solve(input, @"\d|one|two|three|four|five|six|seven|eight|nine");
 
-    int? Digit(string st) => char.IsDigit(st[0]) ? st[0] - '0' : null;
+    int Solve(string input, string rx) =>
+        input.Split("\n").Select(line => GetNumber(line, rx)).Sum();
+  
+    int GetNumber(string line, string rx) {
+        var first = Regex.Match(line, rx);
+        var last = Regex.Match(line, rx, RegexOptions.RightToLeft);
 
-    int? SpelledOutNumber(string st) =>
-        st.StartsWith("one") ? 1 :
-        st.StartsWith("two") ? 2 :
-        st.StartsWith("three") ? 3 :
-        st.StartsWith("four") ? 4 :
-        st.StartsWith("five") ? 5 :
-        st.StartsWith("six") ? 6 :
-        st.StartsWith("seven") ? 7 :
-        st.StartsWith("eight") ? 8 :
-        st.StartsWith("nine") ? 9 :
-        null;
-
-    int Solve(string input, Func<string, int?> parser) =>
-        input.Split("\n").Select(line => GetNumber(line, parser)).Sum();
-
-    // Go over the line from both ends using the parser and find the first numbers 
-    // in both directions, these will be the "first" and "last" digits.
-    // Returns 0 if no numbers found.
-    int GetNumber(string line, Func<string, int?> parse) {
-        int? first = null;
-        int? last = null;
-
-        for (var i =0; i < line.Length; i++) {
-            var prefix = line[i..]; 
-            first = first ?? parse(prefix);
-
-            var suffix = line[(line.Length - i - 1)..];
-            last = last ?? parse(suffix);
-            
-            if (first.HasValue && last.HasValue) {
-                return first.Value * 10 + last.Value;
-            }
-        }
-        return 0;
+        return ParseMatch(first) * 10 + ParseMatch(last);
     }
+
+    int ParseMatch(Match m) => 
+        !m.Success ? 0 :
+        m.Groups[0].Value == "one" ? 1 :
+        m.Groups[0].Value == "two" ? 2 :
+        m.Groups[0].Value == "three" ? 3 :
+        m.Groups[0].Value == "four" ? 4 :
+        m.Groups[0].Value == "five" ? 5 :
+        m.Groups[0].Value == "six" ? 6 :
+        m.Groups[0].Value == "seven" ? 7 :
+        m.Groups[0].Value == "eight" ? 8 :
+        m.Groups[0].Value == "nine" ? 9 :
+        int.Parse(m.Groups[0].Value);
 }
