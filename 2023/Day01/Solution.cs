ede74f49af685d883566c861a6bfee3279db70a9
David Nemeth Csoka <encse@csokavar.hu>
2023-12-01 08:54:57 +0100

    2023/01 cleanup

--- 2023/Day01/Solution.cs
+++ 2023/Day01/Solution.cs
@@ -7,63 +7,46 @@ namespace AdventOfCode.Y2023.Day01;
 class Solution : Solver {
 
     public object PartOne(string input) => 
-        input.Split("\n").Select(GetNumber).Sum();
+        Solve(input, Digit);
 
-    public object PartTwo(string input)=>
-        input.Split("\n").Select(PreProcess).Select(GetNumber).Sum();
+    public object PartTwo(string input) => 
+        Solve(input, st => Digit(st) ?? SpelledOutNumber(st));
 
-    int GetNumber(string line) {
-        var digits = line.Where(char.IsDigit).Select(ch => ch - '0').ToArray();
-        var (first, last) = (digits.First(), digits.Last());
-        return 10 * first + last;
-    } 
+    int? Digit(string st) => char.IsDigit(st[0]) ? st[0] - '0' : null;
 
-    string PreProcess(string line) {
-        var st = "";
-        for (var i =0;st == "" && i < line.Length;i++) {
-            if (line[i..].StartsWith("0")) st += "0";
-            if (line[i..].StartsWith("1")) st += "1";
-            if (line[i..].StartsWith("2")) st += "2";
-            if (line[i..].StartsWith("3")) st += "3";
-            if (line[i..].StartsWith("4")) st += "4";
-            if (line[i..].StartsWith("5")) st += "5";
-            if (line[i..].StartsWith("6")) st += "6";
-            if (line[i..].StartsWith("7")) st += "7";
-            if (line[i..].StartsWith("8")) st += "8";
-            if (line[i..].StartsWith("9")) st += "9";
-            if (line[i..].StartsWith("one")) st += "1";
-            if (line[i..].StartsWith("two")) st += "2";
-            if (line[i..].StartsWith("three")) st += "3";
-            if (line[i..].StartsWith("four")) st += "4";
-            if (line[i..].StartsWith("five")) st += "5";
-            if (line[i..].StartsWith("six")) st += "6";
-            if (line[i..].StartsWith("seven")) st += "7";
-            if (line[i..].StartsWith("eight")) st += "8";
-            if (line[i..].StartsWith("nine")) st += "9";
+    int? SpelledOutNumber(string st) =>
+        st.StartsWith("one") ? 1 :
+        st.StartsWith("two") ? 2 :
+        st.StartsWith("three") ? 3 :
+        st.StartsWith("four") ? 4 :
+        st.StartsWith("five") ? 5 :
+        st.StartsWith("six") ? 6 :
+        st.StartsWith("seven") ? 7 :
+        st.StartsWith("eight") ? 8 :
+        st.StartsWith("nine") ? 9 :
+        null;
+
+    int Solve(string input, Func<string, int?> parser) =>
+        input.Split("\n").Select(line => GetNumber(line, parser)).Sum();
+
+    // Go over the line from both ends using the parser and find the first numbers 
+    // in both directions, these will be the "first" and "last" digits.
+    // Returns 0 if no numbers found.
+    int GetNumber(string line, Func<string, int?> parse) {
+        int? first = null;
+        int? last = null;
+
+        for (var i =0; i < line.Length; i++) {
+            var prefix = line[i..]; 
+            first = first ?? parse(prefix);
+
+            var suffix = line[(line.Length - i - 1)..];
+            last = last ?? parse(suffix);
+            
+            if (first.HasValue && last.HasValue) {
+                return first.Value * 10 + last.Value;
+            }
         }
-
-        for (var i =line.Length-1;st.Length == 1;i--) {
-            if (line[i..].StartsWith("0")) st += "0";
-            if (line[i..].StartsWith("1")) st += "1";
-            if (line[i..].StartsWith("2")) st += "2";
-            if (line[i..].StartsWith("3")) st += "3";
-            if (line[i..].StartsWith("4")) st += "4";
-            if (line[i..].StartsWith("5")) st += "5";
-            if (line[i..].StartsWith("6")) st += "6";
-            if (line[i..].StartsWith("7")) st += "7";
-            if (line[i..].StartsWith("8")) st += "8";
-            if (line[i..].StartsWith("9")) st += "9";
-            if (line[i..].StartsWith("one")) st += "1";
-            if (line[i..].StartsWith("two")) st += "2";
-            if (line[i..].StartsWith("three")) st += "3";
-            if (line[i..].StartsWith("four")) st += "4";
-            if (line[i..].StartsWith("five")) st += "5";
-            if (line[i..].StartsWith("six")) st += "6";
-            if (line[i..].StartsWith("seven")) st += "7";
-            if (line[i..].StartsWith("eight")) st += "8";
-            if (line[i..].StartsWith("nine")) st += "9";
-        }
-
-        return st;
+        return 0;
     }
 }
