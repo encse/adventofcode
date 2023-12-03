2b331a52abef51fd8dc92eb0e9e066e44fed340a
David Nemeth Csoka <encse@csokavar.hu>
2023-12-03 10:34:00 +0100

    renames

--- 2023/Day03/Solution.cs
+++ 2023/Day03/Solution.cs
@@ -14,23 +14,23 @@ class Solution : Solver
     public object PartOne(string input) {
         var rows = input.Split("\n");
         var symbols = Parse(rows, new Regex(@"[^.0-9]"));
-        var numbers = Parse(rows, new Regex(@"\d+"));
+        var nums = Parse(rows, new Regex(@"\d+"));
 
         return (
-            from n in numbers
+            from n in nums
             where symbols.Any(s => NextTo(s, n))
-            select n.Number
+            select n.Int
         ).Sum();
     }
 
     public object PartTwo(string input) {
         var rows = input.Split("\n");
         var gears = Parse(rows, new Regex(@"\*"));
-        var nums = Parse(rows, new Regex(@"\d+"));
+        var numbers = Parse(rows, new Regex(@"\d+"));
 
         return (
             from g in gears
-            let neighbours = from n in nums where NextTo(n, g) select n.Number
+            let neighbours = from n in numbers where NextTo(n, g) select n.Int
             where neighbours.Count() == 2
             select neighbours.First() * neighbours.Last()
         ).Sum();
@@ -39,9 +39,9 @@ class Solution : Solver
     // checks that the parts are touching each other, i.e. rows are within 1 
     // step also the columns (using https://stackoverflow.com/a/3269471).
     bool NextTo(Part p1, Part p2) => 
-        Math.Abs(p2.irow - p1.irow) <= 1 &&
-        p1.icol <= p2.icol + p2.value.ToString().Length &&
-        p2.icol <= p1.icol + p1.value.ToString().Length;
+        Math.Abs(p2.Irow - p1.Irow) <= 1 &&
+        p1.Icol <= p2.Icol + p2.Text.Length &&
+        p2.Icol <= p1.Icol + p1.Text.Length;
 
     // returns the matches of rx with its coordinates
     Part[] Parse(string[] rows, Regex rx) => (
@@ -51,6 +51,6 @@ class Solution : Solver
     ).ToArray();
 }
 
-record Part(string value, int irow, int icol) {
-    public int Number => int.Parse(value);
+record Part(string Text, int Irow, int Icol) {
+    public int Int => int.Parse(Text);
 }
