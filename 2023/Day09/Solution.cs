b45f447f808515e309031f4e999e6d591725d6de
David Nemeth Csoka <encse@csokavar.hu>
2023-12-09 09:08:21 +0100

    2023/09

--- 2023/Day09/Solution.cs
+++ 2023/Day09/Solution.cs
@@ -6,8 +6,8 @@ namespace AdventOfCode.Y2023.Day09;
 [ProblemName("Mirage Maintenance")]
 class Solution : Solver {
 
-    public object PartOne(string input) => Solve(input, Right);
-    public object PartTwo(string input) => Solve(input, Left);
+    public object PartOne(string input) => Solve(input, ExtrapolateRight);
+    public object PartTwo(string input) => Solve(input, ExtrapolateLeft);
 
     long Solve(string input, Func<long[], long> extrapolate) =>
         input.Split("\n").Select(ParseNumbers).Select(extrapolate).Sum();
@@ -21,9 +21,9 @@ class Solution : Solver {
 
     // I went a bit further and recurse until there are no numbers left. It's
     // more compact this way and doesn't affect the runtime much.
-    long Right(long[] numbers) =>
-        numbers.Length == 0 ? 0 : Right(Diff(numbers)) + numbers.Last();
+    long ExtrapolateRight(long[] numbers) =>
+        numbers.Length == 0 ? 0 : ExtrapolateRight(Diff(numbers)) + numbers.Last();
 
-    long Left(long[] numbers) =>
-       numbers.Length == 0 ? 0 : numbers.First() - Left(Diff(numbers));
+    long ExtrapolateLeft(long[] numbers) =>
+       numbers.Length == 0 ? 0 : numbers.First() - ExtrapolateLeft(Diff(numbers));
 }
