db4b1934855dd93f57f251177c480c4cd198f319
David Nemeth Csoka <encse@csokavar.hu>
2023-12-09 08:50:10 +0100

    2023/08

--- 2023/Day08/Solution.cs
+++ 2023/Day08/Solution.cs
@@ -1,49 +1,47 @@
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+
+using Map = System.Collections.Generic.Dictionary<string, (string Left, string Right)>;
 
 namespace AdventOfCode.Y2023.Day08;
 
 [ProblemName("Haunted Wasteland")]
 class Solution : Solver {
 
-    // WIP
-
     public object PartOne(string input) => Solve(input, "AAA", "ZZZ");
-    public object PartTwo(string input) => Solve(input, "A..", "..Z");
+    public object PartTwo(string input) => Solve(input, "A", "Z");
 
-    long Solve(string input, string start, string end) {
-        var parts = input.Split("\n\n");
-        var dirs = parts[0];
-        var map = new Dictionary<string, (string, string)>();
-        foreach (var line in parts[1].Split("\n")) {
-            var m = Regex.Matches(line, "[A-Z]+").ToArray();
-            map[m[0].Value] = (m[1].Value, m[2].Value);
-        }
+    long Solve(string input, string aMarker, string zMarker) {
+        var blocks = input.Split("\n\n");
+        var dirs = blocks[0];
+        var map = ParseMap(blocks[1]);
 
-        var res = 1L;
-        foreach(var st in map.Keys) {
-            if (Regex.IsMatch(st, start)) {
-                res = Lcm(res, Steps(st, end, dirs, map));
-            }
-        }
-        return res;
+        // From each start node calculate the steps to the first Z node, then 
+        // suppose that if we continue wandering around in the desert the 
+        /// distance between the Z nodes is always the same.
+        // The input was set up this way, which justifies the use of LCM in 
+        // computing the final result.
+        return map.Keys
+            .Where(w => w.EndsWith(aMarker))
+            .Select(w => StepsToZ(w, zMarker, dirs, map))
+            .Aggregate(1L, Lcm);
     }
 
     long Lcm(long a, long b) => a * b / Gcd(a, b);
     long Gcd(long a, long b) => b == 0 ? a : Gcd(b, a % b);
 
-    long Steps(string st, string end, string dirs, Dictionary<string, (string, string)> map) {
+    long StepsToZ(string current, string zMarker, string dirs, Map map) {
         var i = 0;
-        while (!Regex.IsMatch(st, end)) {
+        while (!current.EndsWith(zMarker)) {
             var dir = dirs[i % dirs.Length];
-            if (dir == 'L') {
-                st = map[st].Item1;
-            } else {
-                st = map[st].Item2;
-            }
+            current =  dir == 'L' ? map[current].Left : map[current].Right;
             i++;
         }
         return i;
     }
+
+    Map ParseMap(string input) =>
+        input.Split("\n")
+            .Select(line => Regex.Matches(line, "[A-Z]+"))
+            .ToDictionary(m => m[0].Value, m => (m[1].Value, m[2].Value));
 }
--- 2023/SplashScreen.cs
+++ 2023/SplashScreen.cs
@@ -9,12 +9,13 @@ class SplashScreenImpl : SplashScreen {
 
         var color = Console.ForegroundColor;
         Write(0xcc00, false, "           ▄█▄ ▄▄█ ▄ ▄ ▄▄▄ ▄▄ ▄█▄  ▄▄▄ ▄█  ▄▄ ▄▄▄ ▄▄█ ▄▄▄\n           █▄█ █ █ █ █ █▄█ █ █ █   █ █ █▄ ");
-            Write(0xcc00, false, " █  █ █ █ █ █▄█\n           █ █ █▄█ ▀▄▀ █▄▄ █ █ █▄  █▄█ █   █▄ █▄█ █▄█ █▄▄  /* 2023 */\n            \n ");
-            Write(0xcc00, false, "                                                           \n                                        ");
-            Write(0xcc00, false, "                    \n                                                            \n                  ");
-            Write(0xcc00, false, "                                          \n                                                         ");
-            Write(0xcc00, false, "   \n                                                            \n                                   ");
-            Write(0xcc00, false, "                         \n                                                            \n           ");
+            Write(0xcc00, false, " █  █ █ █ █ █▄█\n           █ █ █▄█ ▀▄▀ █▄▄ █ █ █▄  █▄█ █   █▄ █▄█ █▄█ █▄▄  int y = 2023;\n           ");
+            Write(0xcc00, false, " \n                                                            \n                                     ");
+            Write(0xcc00, false, "                       \n                                                            \n               ");
+            Write(0xcc00, false, "                                             \n                                                      ");
+            Write(0xcc00, false, "      \n                                                            \n                                ");
+            Write(0xcc00, false, "                            \n                                                            \n          ");
+            Write(0xcc00, false, " ");
             Write(0x333333, false, "            *        ");
             Write(0xe3b585, false, "' ''...                       ");
             Write(0x666666, false, "10\n                     ");
