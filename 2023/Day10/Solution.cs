de3e8616c21504ace978ae793ebc4d5973a469ea
David Nemeth Csoka <encse@csokavar.hu>
2023-12-10 08:14:27 +0100

    2023/10 commented

--- 2023/Day10/Solution.cs
+++ 2023/Day10/Solution.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Numerics;
-using AngleSharp.Common;
 
 namespace AdventOfCode.Y2023.Day10;
 
@@ -18,38 +17,35 @@ class Solution : Solver {
     static readonly Complex[] Dirs = [Up, Right, Down, Left];
 
     public object PartOne(string input) {
-        return GetLoopPositions(ParseMap(input)).Count / 2;
-    }
-
-    public object PartTwo(string input) {
-
         var map = ParseMap(input);
-        var positions = GetLoopPositions(map);
-        var ccol = map.Keys.Select(k => k.Real).Max() + 1;
-        var crow = map.Keys.Select(k => k.Imaginary).Max();
-
-        map = map.Keys.ToDictionary(k => k, k => positions.Contains(k) ? map[k] : '.');
-
-        var newMap = new Map();
-        for (var irow = 0; irow < crow; irow++) {
-            for (var icol = 0; icol < ccol; icol++) {
-                var ch = map[new Complex(icol, irow)];
-                var m = Magnify(ch);
-                for (var v = 0; v < 9; v++) {
-                    newMap[new Complex(3 * icol + (v % 3), 3 * irow + v / 3)] = m[v];
-                }
-            }
-        }
-        ccol = 3 * ccol + 2;
-        crow = 3 * crow + 2;
-        Fill(newMap);
-        return newMap.Values.Count(v => v == '.');
+        var loop = LoopPositions(map);
+        return loop.Count / 2;
     }
 
-    HashSet<Complex> GetLoopPositions(Map map) {
+    // We pretty much want to flood fill from a corner that is outside the map.
+    // But we need to deal with loops not part of the map and the narrow
+    // tunnels where 'paint' should flood in but not represented as an individual 
+    // cell in the input, such as two vertical pipes next to each other ||
+    // 
+    // The trick is to find the loop, remove everything that is not part
+    // of the loop, then scale up the map so that these narrow tunnels become
+    // real cells. Run the the flood fill algoithm in the scaled up map and finally 
+    // calculate the cells that were not 'painted', i.e. inside the original loop.
+    public object PartTwo(string input) {
+        var map = ParseMap(input);
+        var loop = LoopPositions(map);
+        map = Filter(map, loop);
+        map = Replace(map, '.', 'I');
+        map = ScaleUp(map);
+        map = Fill(map, Complex.Zero, ".I", 'O');
+        return map.Values.Count(v => v == 'I');
+    }
+
+    // Finds 'S' in the map and returns the coordinates that make up the loop
+    HashSet<Complex> LoopPositions(Map map) {
         var curr = map.Keys.Single(k => map[k] == 'S');
         var positions = new HashSet<Complex>() { };
-        var dir = Dirs.First(dir => CanStep(curr, dir, map));
+        var dir = Dirs.First(dir => DirsIn(map[curr + dir]).Contains(dir));
 
         for (; !positions.Contains(curr);) {
             positions.Add(curr);
@@ -63,46 +59,27 @@ class Solution : Solver {
         return positions;
     }
 
-    void Fill(Map map) {
-        var ccol = map.Keys.Select(k => k.Real).Max();
-        var crow = map.Keys.Select(k => k.Imaginary).Max();
-
+    // Fills the map using flood fill replacing chars of charsToFill with the
+    // given fillChar. Every other character of the map is considered as a wall.
+    Map Fill(Map map, Complex start, string charsToFill, char fillChar) {
         var q = new Queue<Complex>();
-        q.Enqueue(new Complex(0, 0));
+        q.Enqueue(start);
         while (q.Any()) {
             var p = q.Dequeue();
-            if (map[p] != ',' && map[p] != '.') {
+            if (!charsToFill.Contains(map[p])) {
                 continue;
             }
-            map[p] = 'O';
+            map[p] = fillChar;
             foreach (var d in Dirs) {
                 if (map.ContainsKey(p + d)) {
                     q.Enqueue(p + d);
                 }
             }
         }
-    }
-    Complex Rotate(Complex d) => d * Complex.ImaginaryOne;
-
-    bool CanStep(Complex p, Complex d, Map map) {
-        var curr = map[p];
-        var next = map.GetValueOrDefault(p + d, '.');
-        return DirsOut(curr).Contains(d) && DirsIn(next).Contains(d);
+        return map;
     }
 
-    string Magnify(char ch) => ch switch {
-        '7' => ",,,-7,,|,",
-        'F' => ",,,,F-,|,",
-        'L' => ",|,,L-,,,",
-        'J' => ",|,-J,,,,",
-        '|' => ",|,,|,,|,",
-        '-' => ",,,---,,,",
-        'S' => ",|,-S-,|,",
-        '.' => ",,,,.,,,,",
-        _ => throw new ArgumentException()
-    };
-
-
+    // In which directions can a cell left
     Complex[] DirsOut(char ch) => ch switch {
         '7' => [Left, Down],
         'F' => [Right, Down],
@@ -114,7 +91,9 @@ class Solution : Solver {
         _ => []
     };
 
-    Complex[] DirsIn(char ch) => DirsOut(ch).Select(ch => -ch).ToArray();
+    // In which directions can a cell entered
+    Complex[] DirsIn(char ch) => 
+        DirsOut(ch).Select(ch => -ch).ToArray();
 
     Map ParseMap(string input) {
         var rows = input.Split("\n");
@@ -123,18 +102,62 @@ class Solution : Solver {
         var res = new Map();
         for (var irow = 0; irow < crow; irow++) {
             for (var icol = 0; icol < ccol; icol++) {
-                res[new Complex(icol + 1, irow + 1)] = rows[irow][icol];
+                res[new Complex(icol, irow)] = rows[irow][icol];
             }
-        }
-        for (var irow = -1; irow <= crow; irow++) {
-            res[new Complex(0, irow)] = '.';
-            res[new Complex(ccol + 1, irow)] = '.';
-        }
-        for (var icol = -1; icol <= ccol; icol++) {
-            res[new Complex(icol, 0)] = '.';
-            res[new Complex(icol, crow + 1)] = '.';
         }
         return res;
     }
 
+    Map Filter(Map map, HashSet<Complex> keep) => 
+        map.Keys.ToDictionary(k => k, k => keep.Contains(k) ? map[k] : '.');
+
+    Map Replace(Map map, char chSrc, char chDst) => 
+        map.Keys.ToDictionary(k => k, k => map[k] == chSrc ? chDst : map[k]);
+
+
+    // Adds a 1 width border of '.' around the map
+    Map Pad(Map map) {
+        var ccol = map.Keys.Select(k => k.Real).Max();
+        var crow = map.Keys.Select(k => k.Imaginary).Max();
+
+        for (var irow = -1; irow <= crow; irow++) {
+            map[new Complex(0, irow)] = '.';
+            map[new Complex(ccol + 1, irow)] = '.';
+        }
+        for (var icol = -1; icol <= ccol; icol++) {
+            map[new Complex(icol, 0)] = '.';
+            map[new Complex(icol, crow + 1)] = '.';
+        }
+
+        return map.Keys.ToDictionary(k => k + new Complex(1, 1), k => map[k]);
+    }
+
+    // Creates a 3x scaled up map applying the patterns of MagnifyCh
+    Map ScaleUp(Map map) {
+        var ccol = map.Keys.Select(k => k.Real).Max();
+        var crow = map.Keys.Select(k => k.Imaginary).Max();
+        var newMap = new Map();
+        for (var irow = 0; irow < crow; irow++) {
+            for (var icol = 0; icol < ccol; icol++) {
+                var ch = map[new Complex(icol, irow)];
+                var m = MagnifyCh(ch);
+                for (var v = 0; v < 9; v++) {
+                    newMap[new Complex(3 * icol + (v % 3), 3 * irow + v / 3)] = m[v];
+                }
+            }
+        }
+        return newMap;
+    }
+
+    // Defines a 3x3 magnifiying pattern for a char
+    string MagnifyCh(char ch) => ch switch {
+        '7' => "...-7..|.",
+        'F' => "....F-.|.",
+        'L' => ".|..L-...",
+        'J' => ".|.-J....",
+        '|' => ".|..|..|.",
+        '-' => "...---...",
+        'S' => ".|.-S-.|.",
+        _ => "...." + ch + "....", // just leave it in the middle
+    };
 }
