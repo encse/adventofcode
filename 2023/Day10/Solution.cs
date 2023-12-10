b0bd65afe9771176b0d7ca299787b7183736df43
David Nemeth Csoka <encse@csokavar.hu>
2023-12-10 13:17:57 +0100

    2023/10 with raytrace

--- 2023/Day10/Solution.cs
+++ 2023/Day10/Solution.cs
@@ -6,7 +6,6 @@ using System.Numerics;
 
 namespace AdventOfCode.Y2023.Day10;
 
-using Cell = char;
 using Map = Dictionary<Complex, char>;
 
 [ProblemName("Pipe Maze")]
@@ -17,31 +16,35 @@ class Solution : Solver {
     static readonly Complex Right = Complex.One;
     static readonly Complex[] Dirs = [Up, Right, Down, Left];
 
+    static readonly Dictionary<char, Complex[]>  OutputDirs = new Dictionary<char, Complex[]>{
+        {'7', [Left, Down] },
+        {'F', [Right, Down]},
+        {'L', [Up, Right]},
+        {'J', [Up, Left]},
+        {'|', [Up, Down]},
+        {'-', [Left, Right]},
+        {'S', [Up, Down, Left, Right]},
+    };
+
     public object PartOne(string input) {
         var map = ParseMap(input);
         var loop = LoopPositions(map);
         return loop.Count / 2;
     }
 
-    // We pretty much want to flood fill from a corner that is outside the loop.
-    // Unfortunately we need to deal with loops not part of the relevant loop and 
-    // the narrow tunnels where 'paint' should flow in but not represented as 
-    // an individual cell in the input, such as two vertical pipes next to 
-    // each other like ||.
-    // 
-    // The trick is to find the loop first, then remove everything that is not part
-    // of it. Now scale up the map so that these narrow tunnels become real cells. 
-    // I map each cell to a 3x3 pattern. 
-    // 
-    // Then run the the flood fill algoithm in the scaled up map and finally 
-    // calculate the cells that were not 'painted', i.e. inside the original loop.
     public object PartTwo(string input) {
         var map = ParseMap(input);
-        map = Filter(map, LoopPositions(map));
-        map = ScaleUp(map);
-        map = Fill(map, 0, ".", ' '); // top left corner is always '.'
-        map = ScaleDown(map);
-        return map.Values.Count(v => v == '.');
+        var loop = LoopPositions(map);
+
+        // remove pipes not in the loop:
+        map = (
+            from kvp in map
+            let position = kvp.Key
+            let cell = loop.Contains(position) ? kvp.Value : '.'
+            select (position, cell)
+        ).ToDictionary();
+
+        return map.Keys.Count(position => Inside(map, position));
     }
 
     // Returns the positions that make up the loop starting at 'S'
@@ -50,7 +53,7 @@ class Solution : Solver {
         var positions = new HashSet<Complex>();
 
         // pick one direction that leads out from S and connected to the neighbour
-        var dir = Dirs.First(dir => OutputDirs(map[position + dir]).Contains(-dir));
+        var dir = Dirs.First(dir => OutputDirs[map[position + dir]].Contains(-dir));
 
         for (; ; ) {
             positions.Add(position);
@@ -58,29 +61,30 @@ class Solution : Solver {
             if (map[position] == 'S') {
                 break;
             }
-            dir = OutputDirs(map[position]).Single(dirOut => dirOut != -dir);
+            dir = OutputDirs[map[position]].Single(dirOut => dirOut != -dir);
         }
         return positions;
     }
 
-    // Fills the map using flood fill. Replaces cells of cellsToFill with the
-    // given filler. Every other cell of the map is considered as wall.
-    Map Fill(Map map, Complex start, string cellsToFill, Cell filler) {
-        var q = new Queue<Complex>();
-        q.Enqueue(start);
-        while (q.Any()) {
-            var p = q.Dequeue();
-            if (!cellsToFill.Contains(map[p])) {
-                continue;
-            }
-            map[p] = filler;
-            foreach (var d in Dirs) {
-                if (map.ContainsKey(p + d)) {
-                    q.Enqueue(p + d);
-                }
-            }
+    // Check if position is inside the loop using ray casting algorithm
+    bool Inside(Map map, Complex position) {
+        var cell = map[position];
+        if (cell != '.') {
+            return false;
         }
-        return map;
+
+        // Imagine a small elf starting from the top right segment of a cell 
+        // and moving to the left jumping over the pipes it encounters.
+        // Every jump flips the "inside" variable.
+        var inside = false;
+        position--;
+        while (map.ContainsKey(position)) {
+            if ("SJL|".Contains(map[position])) {
+                inside = !inside;
+            }
+            position--;
+        }
+        return inside;
     }
 
     Map ParseMap(string input) {
@@ -95,55 +99,4 @@ class Solution : Solver {
         }
         return res;
     }
-
-    // Clears cells not in the provided hashset.
-    Map Filter(Map map, HashSet<Complex> keep) =>
-        map.Keys.ToDictionary(k => k, k => keep.Contains(k) ? map[k] : '.');
-
-    // Creates a 3x scaled up map applying the patterns of ScaleUpCell
-    Map ScaleUp(Map map) {
-        var newMap = new Map();
-        foreach (var pos in map.Keys) {
-            var cell = map[pos];
-            var pattern = ScaleUpCell(cell);
-            for (var v = 0; v < 9; v++) {
-                var d = v % 3 + v / 3 * Complex.ImaginaryOne;
-                newMap[3 * pos + d] = pattern[v];
-            }
-        }
-        return newMap;
-    }
-
-    // Invert ScaleUp, keeping the center of each 3x3 cell
-    Map ScaleDown(Map map) =>
-        map.Keys
-            .Where(key => key.Imaginary % 3 == 1 && key.Real % 3 == 1)
-            .ToDictionary(
-                k => (k - new Complex(1, 1)) / 3,
-                k => map[k]
-            );
-
-    // Directions leading out from a cell
-    Complex[] OutputDirs(Cell cell) => cell switch {
-        '7' => [Left, Down],
-        'F' => [Right, Down],
-        'L' => [Up, Right],
-        'J' => [Up, Left],
-        '|' => [Up, Down],
-        '-' => [Left, Right],
-        'S' => [Up, Down, Left, Right],
-        _ => []
-    };
-
-    //  Returns a 3x3 pattern for a cell
-    string ScaleUpCell(Cell cell) => cell switch {
-        '7' => "...-7..|.",
-        'F' => "....F-.|.",
-        'L' => ".|..L-...",
-        'J' => ".|.-J....",
-        '|' => ".|..|..|.",
-        '-' => "...---...",
-        'S' => ".|.-S-.|.",
-        _ => "...." + cell + "....", // just leave it in the middle
-    };
 }
