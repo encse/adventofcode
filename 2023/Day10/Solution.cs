48d62ac64109f53f3c98ce6dd9c097f9cc09e5df
David Nemeth Csoka <encse@csokavar.hu>
2023-12-10 12:14:52 +0100

    2023/10 commented

--- 2023/Day10/Solution.cs
+++ 2023/Day10/Solution.cs
@@ -6,6 +6,7 @@ using System.Numerics;
 
 namespace AdventOfCode.Y2023.Day10;
 
+using Cell = char;
 using Map = Dictionary<Complex, char>;
 
 [ProblemName("Pipe Maze")]
@@ -36,44 +37,43 @@ class Solution : Solver {
     // calculate the cells that were not 'painted', i.e. inside the original loop.
     public object PartTwo(string input) {
         var map = ParseMap(input);
-        var loop = LoopPositions(map);
-        map = Filter(map, loop);
-        map = Replace(map, '.', 'I');
+        map = Filter(map, LoopPositions(map));
         map = ScaleUp(map);
-        // start from the top left corner, works for the input
-        map = Fill(map, Complex.Zero, ".I", 'O'); 
-        return map.Values.Count(v => v == 'I');
+        map = Fill(map, 0, ".", ' '); // top left corner is always '.'
+        map = ScaleDown(map);
+        return map.Values.Count(v => v == '.');
     }
 
-    // Finds 'S' in the map and returns the coordinates that make up the loop
+    // Returns the positions that make up the loop starting at 'S'
     HashSet<Complex> LoopPositions(Map map) {
-        var curr = map.Keys.Single(k => map[k] == 'S');
-        var positions = new HashSet<Complex>() { };
-        var dir = Dirs.First(dir => DirsIn(map[curr + dir]).Contains(dir));
+        var position = map.Keys.Single(k => map[k] == 'S');
+        var positions = new HashSet<Complex>();
 
-        for (; !positions.Contains(curr);) {
-            positions.Add(curr);
-            curr += dir;
-            if (map[curr] == 'S') {
+        // pick one direction that leads out from S and connected to the neighbour
+        var dir = Dirs.First(dir => OutputDirs(map[position + dir]).Contains(-dir));
+
+        for (; ; ) {
+            positions.Add(position);
+            position += dir;
+            if (map[position] == 'S') {
                 break;
             }
-            var outs = DirsOut(map[curr]);
-            dir = DirsOut(map[curr]).Single(dirOut => dirOut != -dir);
+            dir = OutputDirs(map[position]).Single(dirOut => dirOut != -dir);
         }
         return positions;
     }
 
-    // Fills the map using flood fill replacing chars of charsToFill with the
-    // given fillChar. Every other character of the map is considered as a wall.
-    Map Fill(Map map, Complex start, string charsToFill, char fillChar) {
+    // Fills the map using flood fill. Replaces cells of cellsToFill with the
+    // given filler. Every other cell of the map is considered as wall.
+    Map Fill(Map map, Complex start, string cellsToFill, Cell filler) {
         var q = new Queue<Complex>();
         q.Enqueue(start);
         while (q.Any()) {
             var p = q.Dequeue();
-            if (!charsToFill.Contains(map[p])) {
+            if (!cellsToFill.Contains(map[p])) {
                 continue;
             }
-            map[p] = fillChar;
+            map[p] = filler;
             foreach (var d in Dirs) {
                 if (map.ContainsKey(p + d)) {
                     q.Enqueue(p + d);
@@ -82,22 +82,6 @@ class Solution : Solver {
         }
         return map;
     }
-
-    // In which directions can a cell left
-    Complex[] DirsOut(char ch) => ch switch {
-        '7' => [Left, Down],
-        'F' => [Right, Down],
-        'L' => [Up, Right],
-        'J' => [Up, Left],
-        '|' => [Up, Down],
-        '-' => [Left, Right],
-        'S' => [Up, Down, Left, Right],
-        _ => []
-    };
-
-    // In which directions can a cell entered
-    Complex[] DirsIn(char ch) => 
-        DirsOut(ch).Select(ch => -ch).ToArray();
 
     Map ParseMap(string input) {
         var rows = input.Split("\n");
@@ -112,31 +96,47 @@ class Solution : Solver {
         return res;
     }
 
-    Map Filter(Map map, HashSet<Complex> keep) => 
+    // Clears cells not in the provided hashset.
+    Map Filter(Map map, HashSet<Complex> keep) =>
         map.Keys.ToDictionary(k => k, k => keep.Contains(k) ? map[k] : '.');
 
-    Map Replace(Map map, char chSrc, char chDst) => 
-        map.Keys.ToDictionary(k => k, k => map[k] == chSrc ? chDst : map[k]);
-
-    // Creates a 3x scaled up map applying the patterns of MagnifyCh
+    // Creates a 3x scaled up map applying the patterns of ScaleUpCell
     Map ScaleUp(Map map) {
-        var ccol = map.Keys.Select(k => k.Real).Max();
-        var crow = map.Keys.Select(k => k.Imaginary).Max();
         var newMap = new Map();
-        for (var irow = 0; irow < crow; irow++) {
-            for (var icol = 0; icol < ccol; icol++) {
-                var ch = map[new Complex(icol, irow)];
-                var m = MagnifyCh(ch);
-                for (var v = 0; v < 9; v++) {
-                    newMap[new Complex(3 * icol + (v % 3), 3 * irow + v / 3)] = m[v];
-                }
+        foreach (var pos in map.Keys) {
+            var cell = map[pos];
+            var pattern = ScaleUpCell(cell);
+            for (var v = 0; v < 9; v++) {
+                var d = v % 3 + v / 3 * Complex.ImaginaryOne;
+                newMap[3 * pos + d] = pattern[v];
             }
         }
         return newMap;
     }
 
-    // Defines a 3x3 magnifiying pattern for a char
-    string MagnifyCh(char ch) => ch switch {
+    // Invert ScaleUp, keeping the center of each 3x3 cell
+    Map ScaleDown(Map map) =>
+        map.Keys
+            .Where(key => key.Imaginary % 3 == 1 && key.Real % 3 == 1)
+            .ToDictionary(
+                k => (k - new Complex(1, 1)) / 3,
+                k => map[k]
+            );
+
+    // Directions leading out from a cell
+    Complex[] OutputDirs(Cell cell) => cell switch {
+        '7' => [Left, Down],
+        'F' => [Right, Down],
+        'L' => [Up, Right],
+        'J' => [Up, Left],
+        '|' => [Up, Down],
+        '-' => [Left, Right],
+        'S' => [Up, Down, Left, Right],
+        _ => []
+    };
+
+    //  Returns a 3x3 pattern for a cell
+    string ScaleUpCell(Cell cell) => cell switch {
         '7' => "...-7..|.",
         'F' => "....F-.|.",
         'L' => ".|..L-...",
@@ -144,6 +144,6 @@ class Solution : Solver {
         '|' => ".|..|..|.",
         '-' => "...---...",
         'S' => ".|.-S-.|.",
-        _ => "...." + ch + "....", // just leave it in the middle
+        _ => "...." + cell + "....", // just leave it in the middle
     };
 }
