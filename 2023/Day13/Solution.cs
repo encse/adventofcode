57b75985236429e48557a6c5099b7b165411275a
David Nemeth Csoka <encse@csokavar.hu>
2023-12-13 10:51:03 +0100

    2023/13 funprog

--- 2023/Day13/Solution.cs
+++ 2023/Day13/Solution.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.Collections.Immutable;
-
-using Map = System.Collections.Immutable.ImmutableDictionary<System.Numerics.Complex, char>;
+using Map = System.Collections.Generic.Dictionary<System.Numerics.Complex, char>;
 
 namespace AdventOfCode.Y2023.Day13;
 
@@ -13,74 +11,31 @@ class Solution : Solver {
 
     Complex Right = 1;
     Complex Down = Complex.ImaginaryOne;
+    Complex Ortho(Complex dir) => dir == Right ? Down : Right;
 
-    public object PartOne(string input) => Solve(input, Part1Score);
-    public object PartTwo(string input) => Solve(input, Part2Score);
+    public object PartOne(string input) => Solve(input, 0);
+    public object PartTwo(string input) => Solve(input, 1);
 
-    int Solve(string input, Func<Map, int> score) =>
-        input.Split("\n\n").Select(ParseMap).Select(score).Sum();
+    double Solve(string input, int allowedSmudges) => (
+        from block in input.Split("\n\n")
+        let map = ParseMap(block)
+        select GetScore(map, allowedSmudges)
+    ).Sum();
 
-    int Part1Score(Map map) => GetScores(map).Single();
+    double GetScore(Map map, int allowedSmudges) => (
+        from dir in new Complex[] { Right, Down }
+        from mirror in Positions(map, dir, dir)
+        where FindSmudges(map, mirror, dir) == allowedSmudges
+        select mirror.Real + 100 * mirror.Imaginary
+    ).First();
 
-    // try patching the map in all possible ways, and return the first score that differs from part1
-    int Part2Score(Map map) {
-        var part1Score = Part1Score(map);
-        return (
-            from patched in Patches(map)
-            from score in GetScores(patched)
-            where score != part1Score
-            select score
-        ).First();
-    }
-
-    // Find all possible mirrors and return the score associated with them
-    int[] GetScores(Map map) {
-        return [
-            ..GetMirrors(map, Down),
-            ..GetMirrors(map, Right).Select(r => r * 100),
-        ];
-    }
-
-    // Starting from (0,0) go over the map in the given direction and check 
-    // each row/column for possible mirror locations. Return the common 
-    // intersection of these candidates.
-    int[] GetMirrors(Map map, Complex step) {
-        var ortho = step == Right ? Down : Right;
-        var res = Get1DMirrors(map, Complex.Zero, ortho).ToHashSet();
-        for (var pos = step; map.ContainsKey(pos); pos += step) {
-            res.IntersectWith(Get1DMirrors(map, pos, ortho));
-        }
-        return res.ToArray();
-    }
-
-    // Collects the possible mirror locations in a single row/column
-    IEnumerable<int> Get1DMirrors(Map map, Complex pos, Complex dir) {
-        for (var i = 1; map.ContainsKey(pos); i++, pos += dir) {
-            if (Is1DMirror(map, pos, dir)) {
-                yield return i;
-            }
-        }
-    }
-
-    // Validate the mirror property for a single row/column starting at pos.
-    bool Is1DMirror(Map map, Complex pos, Complex dir) {
-        if (!map.ContainsKey(pos + dir)) {
-            return false; // cannot a place a mirror at the edge of the map
-        }
-
-        for (var i = Complex.Zero; ; i += dir) {
-            if (!map.ContainsKey(pos - i) || !map.ContainsKey(pos + i + dir)) {
-                return true; // we reached the end of the map, it's a mirror
-            } else if (map[pos - i] != map[pos + i + dir]) {
-                return false; // not a mirror image
-            }
-        }
-    }
-
-    // All possible versions of a given map when changing a single key
-    IEnumerable<Map> Patches(Map map) =>
-        from key in map.Keys
-        select map.SetItem(key, map[key] == '.' ? '#' : '.');
+    // cast a ray from each postion along the mirror and count the 'smuggles'
+    int FindSmudges(Map map, Complex mirror, Complex rayDir) => (
+        from ray0 in Positions(map, mirror, Ortho(rayDir))
+        let rayA = Positions(map, ray0, rayDir)
+        let rayB = Positions(map, ray0 - rayDir, -rayDir)
+        select Enumerable.Zip(rayA, rayB).Count(p => map[p.First] != map[p.Second])
+    ).Sum();
 
     Map ParseMap(string input) {
         var rows = input.Split("\n");
@@ -90,6 +45,13 @@ class Solution : Solver {
             let pos = new Complex(icol, irow)
             let cell = rows[irow][icol]
             select new KeyValuePair<Complex, char>(pos, cell)
-        ).ToImmutableDictionary();
+        ).ToDictionary();
+    }
+
+    // allowed poistions of the map from 'start' going in 'dir'
+    IEnumerable<Complex> Positions(Map map, Complex start, Complex dir) {
+        for (var pos = start; map.ContainsKey(pos); pos += dir) {
+            yield return pos;
+        }
     }
 }
