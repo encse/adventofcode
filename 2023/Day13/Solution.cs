788722620e56b9528a60ad3a146ef8ad3b5dd91c
David Nemeth Csoka <encse@csokavar.hu>
2023-12-13 09:42:19 +0100

    2023/13 commented

--- 2023/Day13/Solution.cs
+++ 2023/Day13/Solution.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Collections.Immutable;
 
-using Map = System.Collections.Generic.Dictionary<System.Numerics.Complex, char>;
-using AngleSharp.Common;
+using Map = System.Collections.Immutable.ImmutableDictionary<System.Numerics.Complex, char>;
 
 namespace AdventOfCode.Y2023.Day13;
 
@@ -14,72 +14,60 @@ class Solution : Solver {
     Complex Right = 1;
     Complex Down = Complex.ImaginaryOne;
 
-    public object PartOne(string input) => Solve(input, UnpatchedScore);
-    public object PartTwo(string input) => Solve(input, PatchedScore);
+    public object PartOne(string input) => Solve(input, Part1Score);
+    public object PartTwo(string input) => Solve(input, Part2Score);
 
-    int Solve(string input, Func<Map, int> score) =>  (
-        from block in input.Split("\n\n")
-        let map = ParseMap(block)
-        select score(map)
-    ).Sum();
+    int Solve(string input, Func<Map, int> score) =>
+        input.Split("\n\n").Select(ParseMap).Select(score).Sum();
 
-    int UnpatchedScore(Map map) => GetScores(map).Single();
+    int Part1Score(Map map) => GetScores(map).Single();
 
-    int PatchedScore(Map map) {
-        var unpatchedScore = GetScores(map).Single();
+    // try patching the map in all possible ways, and return the first score that differs from part1
+    int Part2Score(Map map) {
+        var part1Score = Part1Score(map);
         return (
-            from patchedMap in Patches(map)
-            from patchedScore in GetScores(patchedMap)
-            where patchedScore != unpatchedScore
-            select patchedScore
+            from patched in Patches(map)
+            from score in GetScores(patched)
+            where score != part1Score
+            select score
         ).First();
     }
 
-    // returns all possible patched versions of a given map 
-    IEnumerable<Map> Patches(Map map) {
-        foreach (var key in map.Keys) {
-            var res = new Map(map);
-            res[key] = map[key] == '.' ? '#' : '.';
-            yield return res;
-        }
-    }
-    
-    // Find all possible mirrors horiontally and vertically and returns the score 
-    // associated with them
+    // Find all possible mirrors and return the score associated with them
     int[] GetScores(Map map) {
         return [
-            ..GetMirrorPositionsByDir(map, Down), 
-            ..GetMirrorPositionsByDir(map, Right).Select(r => r * 100),
+            ..GetMirrors(map, Down),
+            ..GetMirrors(map, Right).Select(r => r * 100),
         ];
     }
 
     // Starting from (0,0) go over the map in the given direction and check 
-    // each row/column for possible mirror locations. The result is the common 
+    // each row/column for possible mirror locations. Return the common 
     // intersection of these candidates.
-    int[] GetMirrorPositionsByDir(Map map, Complex dir) {
-        var ortho = dir == Right ? Down : Right;
-        var res = MirrorCandidates(map, Complex.Zero, ortho).ToHashSet();
-        for (var pos = dir; map.ContainsKey(pos); pos += dir) {
-            res.IntersectWith(MirrorCandidates(map, pos, ortho));
+    int[] GetMirrors(Map map, Complex step) {
+        var ortho = step == Right ? Down : Right;
+        var res = Get1DMirrors(map, Complex.Zero, ortho).ToHashSet();
+        for (var pos = step; map.ContainsKey(pos); pos += step) {
+            res.IntersectWith(Get1DMirrors(map, pos, ortho));
         }
         return res.ToArray();
     }
 
     // Collects the possible mirror locations in a single row/column
-    IEnumerable<int> MirrorCandidates(Map map, Complex pos, Complex dir) {
-        for(var i=1; map.ContainsKey(pos); i++, pos += dir) {
-            if (IsMirrorCandidate(map, pos, dir)) {
+    IEnumerable<int> Get1DMirrors(Map map, Complex pos, Complex dir) {
+        for (var i = 1; map.ContainsKey(pos); i++, pos += dir) {
+            if (Is1DMirror(map, pos, dir)) {
                 yield return i;
             }
         }
     }
 
-    // go in both directions from pos and validate the mirror property
-    bool IsMirrorCandidate(Map map, Complex pos, Complex dir) {
+    // Validate the mirror property for a single row/column starting at pos.
+    bool Is1DMirror(Map map, Complex pos, Complex dir) {
         if (!map.ContainsKey(pos + dir)) {
             return false; // cannot a place a mirror at the edge of the map
         }
-        
+
         for (var i = Complex.Zero; ; i += dir) {
             if (!map.ContainsKey(pos - i) || !map.ContainsKey(pos + i + dir)) {
                 return true; // we reached the end of the map, it's a mirror
@@ -89,6 +77,11 @@ class Solution : Solver {
         }
     }
 
+    // All possible versions of a given map when changing a single key
+    IEnumerable<Map> Patches(Map map) =>
+        from key in map.Keys
+        select map.SetItem(key, map[key] == '.' ? '#' : '.');
+
     Map ParseMap(string input) {
         var rows = input.Split("\n");
         return (
@@ -97,6 +90,6 @@ class Solution : Solver {
             let pos = new Complex(icol, irow)
             let cell = rows[irow][icol]
             select new KeyValuePair<Complex, char>(pos, cell)
-        ).ToDictionary();
+        ).ToImmutableDictionary();
     }
 }
