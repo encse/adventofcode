ef59202e46479e6de603aa5145917da8177ef900
David Nemeth Csoka <encse@csokavar.hu>
2023-12-18 09:18:44 +0100

    2023/18

---
+++ 2023/Day18/Solution.cs
@@ -0,0 +1,57 @@
+namespace AdventOfCode.Y2023.Day18;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+[ProblemName("Lavaduct Lagoon")]
+class Solution : Solver {
+
+    public object PartOne(string input)=> Area(Steps1(input));
+    public object PartTwo(string input) => Area(Steps2(input));
+
+    IEnumerable<Complex> Steps1(string input) =>
+        from line in input.Split('\n')
+        let parts = line.Split(' ')
+        let dir = parts[0] switch {
+            "R" => Complex.One,
+            "U" => -Complex.ImaginaryOne,
+            "L" => -Complex.One,
+            "D" => Complex.ImaginaryOne,
+            _ => throw new Exception()
+        }
+        let dist = int.Parse(parts[1])
+        select dir * dist;
+
+    IEnumerable<Complex> Steps2(string input) =>
+        from line in input.Split('\n')
+        let hex = line.Split(' ')[2]
+        let dir = hex[7] switch {
+            '0' => Complex.One,
+            '1' => -Complex.ImaginaryOne,
+            '2' => -Complex.One,
+            '3' => Complex.ImaginaryOne,
+            _ => throw new Exception()
+        }
+        let dist = Convert.ToInt32(hex[2..7], 16)
+        select dir * dist;
+
+    double Area(IEnumerable<Complex> steps) {
+        var vertices = Vertices(steps).ToList();
+        var circumference = steps.Select(x => x.Magnitude).Sum();
+        var shiftedVertices = vertices.Skip(1).Append(vertices[0]);
+        var shoelaces =
+            from pair in vertices.Zip(shiftedVertices)
+            select pair.First.Real * pair.Second.Imaginary - pair.First.Imaginary * pair.Second.Real;
+        return Math.Abs(shoelaces.Sum()) / 2 + circumference / 2 + 1;
+    }
+
+     IEnumerable<Complex> Vertices(IEnumerable<Complex> steps) {
+        var pos = Complex.Zero;
+        foreach (var step in steps) {
+            pos += step;
+            yield return pos;
+        }
+    }
+}
