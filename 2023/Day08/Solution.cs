fd6444c250d9cb1623b0373ac3c67a988cb1b024
David Nemeth Csoka <encse@csokavar.hu>
2023-12-18 10:57:07 +0100

    2023/08

--- 2023/Day08/Solution.cs
+++ 2023/Day08/Solution.cs
@@ -1,9 +1,8 @@
+namespace AdventOfCode.Y2023.Day08;
+
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
-
-namespace AdventOfCode.Y2023.Day08;
-using Map = Dictionary<string, (string Left, string Right)>;
+using Map = System.Collections.Generic.Dictionary<string, (string Left, string Right)>;
 
 [ProblemName("Haunted Wasteland")]
 class Solution : Solver {
