337f7c6416d8e23847554b7858f4fff0612b12a2
David Nemeth Csoka <encse@csokavar.hu>
2023-12-19 14:23:19 +0100

    2023/19

--- 2023/Day19/Solution.cs
+++ 2023/Day19/Solution.cs
@@ -15,13 +15,14 @@ record Cond(int dim, int num, string jmp);
 [ProblemName("Aplenty")]
 class Solution : Solver {
     
-    // Part 1 can be understood in the context of Part 2. Part 2 asks to compute the
-    // 'accepted' volume of a four dimensional hypercube. It has some elaborate way to
-    // recursively slice up the cube parallel to its edges to smaller and smaller pieces 
-    // and decide if the sub-sub cubes at the bottom are accepted or not. Our Part 2 
-    // algorithm follows these rules and returns the 'accepted' volume we are looking for. 
+    // Part 1 can be understood in the context of Part 2. Part 2 asks to compute 
+    // the accepted volume of a four dimensional hypercube. It has some elaborate 
+    // way to recursively slice up the cube parallel to its edges to smaller and 
+    // smaller pieces and decide if the sub-sub cubes at the bottom are accepted 
+    // or not. Our Part 2 algorithm follows these rules and returns the 'accepted' 
+    // volume we are looking for. 
 
-    // We can use this algorithm to solve Part 1 starting from small 1 x 1 x 1 x 1 cubes
+    // We can use this algorithm to solve Part 1 starting from unit sized cubes
     // and checking if they are fully accepted or not.
 
     public object PartOne(string input) {
@@ -29,7 +30,7 @@ class Solution : Solver {
         var rules = ParseRules(parts[0]);
         return (
             from cube in ParseUnitCube(parts[1])
-            where AcceptedVolume(rules, cube, "in") == 1
+            where Accepted(rules, cube, "in") == 1
             select cube.Select(dim => dim.begin).Sum()
         ).Sum();
     }
@@ -38,13 +39,13 @@ class Solution : Solver {
         var parts = input.Split("\n\n");
         var rules = ParseRules(parts[0]);
         var cube = Enumerable.Repeat(new Range(1, 4000), 4).ToImmutableArray();
-        return AcceptedVolume(rules, cube, "in");
+        return Accepted(rules, cube, "in");
     }
 
     BigInteger Volume(Cube cube) =>
         cube.Aggregate(BigInteger.One, (m, dim) => m * (dim.end - dim.begin + 1));
 
-    BigInteger AcceptedVolume(Rules rules, Cube cube, string state) {
+    BigInteger Accepted(Rules rules, Cube cube, string state) {
         if (cube.Any(coord => coord.end < coord.begin)) {
             return 0; // cube is empty
         } else if (state == "A") {
@@ -57,19 +58,19 @@ class Solution : Solver {
                 // slicing happens here in the presence of < and > symbols.
                 if (TryParseCond(stm, '<', out var cond)) {
                     // here we have a condition, something like 'x < 1000 : foo'
-                    // so the cube is split along the right dimension (x) into two halves:
+                    // the cube is split along the right dimension (x) into two:
                     var (lo, hi) = SplitRange(cube[cond.dim], cond.num);
                     // recurse with the accepted half to 'foo':
-                    res += AcceptedVolume(rules, cube.SetItem(cond.dim, lo), cond.jmp);
+                    res += Accepted(rules, cube.SetItem(cond.dim, lo), cond.jmp);
                     // and continue with the rejected half
                     cube = cube.SetItem(cond.dim, hi);
                 } else if (TryParseCond(stm, '>', out cond)) {
                     // same as the other case with 'x > 1000'
                     var (lo, hi) = SplitRange(cube[cond.dim], cond.num + 1);
-                    res += AcceptedVolume(rules, cube.SetItem(cond.dim, hi), cond.jmp);
+                    res += Accepted(rules, cube.SetItem(cond.dim, hi), cond.jmp);
                     cube = cube.SetItem(cond.dim, lo);
                 } else {
-                    res += AcceptedVolume(rules, cube, stm);
+                    res += Accepted(rules, cube, stm);
                 }
             }
             return res;
@@ -107,6 +108,6 @@ class Solution : Solver {
 
     IEnumerable<Cube> ParseUnitCube(string input) =>
         from line in input.Split('\n')
-        let nums = Regex.Matches(line, @"\d+").Select(m => int.Parse(m.Value)).ToArray()
+        let nums = Regex.Matches(line, @"\d+").Select(m => int.Parse(m.Value))
         select nums.Select(n => new Range(n, n)).ToImmutableArray();
 }
