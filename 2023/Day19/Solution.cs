c48c9aa519f581d36f4ebbb96a4a91852dc68137
David Nemeth Csoka <encse@csokavar.hu>
2023-12-19 15:46:27 +0100

    2023/19

--- 2023/Day19/Solution.cs
+++ 2023/Day19/Solution.cs
@@ -10,17 +10,17 @@ using Rules = System.Collections.Generic.Dictionary<string, string>;
 using Cube = System.Collections.Immutable.ImmutableArray<Range>;
 
 record Range(int begin, int end);
-record Cond(int dim, int num, string jmp);
+record Cond(int dim, int num, string state);
 
 [ProblemName("Aplenty")]
 class Solution : Solver {
-    
+
     // Part 1 can be understood in the context of Part 2. Part 2 asks to compute 
     // the accepted volume of a four dimensional hypercube. It has some elaborate 
-    // way to recursively slice up the cube parallel to its edges to smaller and 
-    // smaller pieces and decide if the sub-sub cubes at the bottom are accepted 
-    // or not. Our Part 2 algorithm follows these rules and returns the 'accepted' 
-    // volume we are looking for. 
+    // way to slice up the cube parallel to its edges to smaller and smaller pieces 
+    // and decide if the final sub-sub cubes are accepted or not. Our Part 2 
+    // algorithm follows these rules and returns the 'accepted'volume we are 
+    // looking for. 
 
     // We can use this algorithm to solve Part 1 starting from unit sized cubes
     // and checking if they are fully accepted or not.
@@ -30,8 +30,8 @@ class Solution : Solver {
         var rules = ParseRules(parts[0]);
         return (
             from cube in ParseUnitCube(parts[1])
-            where Accepted(rules, cube, "in") == 1
-            select cube.Select(dim => dim.begin).Sum()
+            where Accepted(rules, cube) == 1
+            select cube.Select(r => r.begin).Sum()
         ).Sum();
     }
 
@@ -39,66 +39,61 @@ class Solution : Solver {
         var parts = input.Split("\n\n");
         var rules = ParseRules(parts[0]);
         var cube = Enumerable.Repeat(new Range(1, 4000), 4).ToImmutableArray();
-        return Accepted(rules, cube, "in");
+        return Accepted(rules, cube);
     }
 
     BigInteger Volume(Cube cube) =>
-        cube.Aggregate(BigInteger.One, (m, dim) => m * (dim.end - dim.begin + 1));
+        cube.Aggregate(BigInteger.One, (m, r) => m * (r.end - r.begin + 1));
 
-    BigInteger Accepted(Rules rules, Cube cube, string state) {
-        if (cube.Any(coord => coord.end < coord.begin)) {
-            return 0; // cube is empty
-        } else if (state == "A") {
-            return Volume(cube); // cube is accepted as it is
-        } else if (state == "R") {
-            return 0; // cube is rejected
-        } else {
-            var res = BigInteger.Zero;
-            foreach (var stm in rules[state].Split(",")) {
-                // slicing happens here in the presence of < and > symbols.
-                if (TryParseCond(stm, '<', out var cond)) {
-                    // here we have a condition, something like 'x < 1000 : foo'
-                    // the cube is split along the right dimension (x) into two:
-                    var (lo, hi) = SplitRange(cube[cond.dim], cond.num);
-                    // recurse with the accepted half to 'foo':
-                    res += Accepted(rules, cube.SetItem(cond.dim, lo), cond.jmp);
-                    // and continue with the rejected half
-                    cube = cube.SetItem(cond.dim, hi);
-                } else if (TryParseCond(stm, '>', out cond)) {
-                    // same as the other case with 'x > 1000'
-                    var (lo, hi) = SplitRange(cube[cond.dim], cond.num + 1);
-                    res += Accepted(rules, cube.SetItem(cond.dim, hi), cond.jmp);
-                    cube = cube.SetItem(cond.dim, lo);
-                } else {
-                    res += Accepted(rules, cube, stm);
+    BigInteger Accepted(Rules rules, Cube cube) {
+        var q = new Queue<(Cube cube, string state)>();
+        q.Enqueue((cube, "in"));
+        
+        BigInteger res = 0;
+        while (q.Any()) {
+            (cube, var state) = q.Dequeue();
+            if (cube.Any(coord => coord.end < coord.begin)) {
+                continue; // cube is empty
+            } else if (state == "A") {
+                res += Volume(cube); // cube is accepted
+            } else if (state == "R") {
+                continue; // cube is rejected
+            } else {
+                foreach (var stm in rules[state].Split(",")) {
+                    if (TryParseCond(stm, '<', out var cond)) {
+                        var (cube1, cube2) = SplitCube(cube, cond.dim, cond.num);
+                        q.Enqueue((cube1, cond.state));
+                        cube = cube2;
+                    } else if (TryParseCond(stm, '>', out cond)) {
+                        var (cube1, cube2) = SplitCube(cube, cond.dim, cond.num + 1);
+                        q.Enqueue((cube2, cond.state));
+                        cube = cube1;
+                    } else {
+                        q.Enqueue((cube, stm));
+                    }
                 }
             }
-            return res;
         }
+        return res;
     }
+
+    // Cuts a cube at 'num' along the specified dimension, other dimensions are unaffected.
+    (Cube lo, Cube hi) SplitCube(Cube cube, int dim, int num) {
+        var r = cube[dim];
+        return (
+            cube.SetItem(dim, r with { end = Math.Min(num - 1, r.end) }),
+            cube.SetItem(dim, r with { begin = Math.Max(r.begin, num) })
+        );
+    }
+
+    int ParseDim(string st) => st switch { "x" => 0, "m" => 1, "a" => 2, _ => 3 };
 
     bool TryParseCond(string st, char ch, out Cond cond) {
-        if (!st.Contains(ch)) {
-            cond = null;
-            return false;
-        } else {
-            // st has the form of "x<1000:foo"
-            var parts = st.Split(ch, ':');
-            cond = new Cond(
-                dim: parts[0] switch { "x" => 0, "m" => 1, "a" => 2, _ => 3 },
-                num: int.Parse(parts[1]),
-                jmp: parts[2]
-            );
-            return true;
-        }
+        var parts = st.Split(ch, ':');
+        var success = parts.Length == 3;
+        cond = success ? new Cond(ParseDim(parts[0]), int.Parse(parts[1]), parts[2]) : null;
+        return success;
     }
-
-    // returns two ranges containing [r.begin .. num - 1], [num .. r.end], 
-    // returns empty ranges where end < begin if num is outside of r
-    (Range lo, Range hi) SplitRange(Range r, int num) => (
-        r with { begin = r.begin, end = Math.Min(num - 1, r.end) },
-        r with { begin = Math.Max(r.begin, num), end = r.end }
-    );
 
     Rules ParseRules(string input) => (
         from line in input.Split('\n')
