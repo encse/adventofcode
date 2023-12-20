a6183133b9362dd66877d2c9f0eaec4587d9922b
David Nemeth Csoka <encse@csokavar.hu>
2023-12-20 08:52:36 +0100

    lib

--- Lib/Generator/SolutionTemplateGenerator.cs
+++ Lib/Generator/SolutionTemplateGenerator.cs
@@ -5,14 +5,14 @@ namespace AdventOfCode.Generator;
 class SolutionTemplateGenerator {
     public string Generate(Problem problem) {
         return $$"""
+            namespace AdventOfCode.Y{{problem.Year}}.Day{{problem.Day:00}};
+
             using System;
             using System.Collections.Generic;
             using System.Collections.Immutable;
             using System.Linq;
             using System.Text.RegularExpressions;
             using System.Text;
-            
-            namespace AdventOfCode.Y{{problem.Year}}.Day{{problem.Day:00}};
             
             [ProblemName("{{problem.Title}}")]
             class Solution : Solver {
