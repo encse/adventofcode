ead5823adb6c944d1d3adc239b4cc22d17c6b7ea
David Nemeth Csoka <encse@csokavar.hu>
2023-12-22 11:45:35 +0100

    2023/22 wip

--- 2023/Day22/Solution.cs
+++ 2023/Day22/Solution.cs
@@ -7,102 +7,95 @@ using System.Text.RegularExpressions;
 
 record Range(int begin, int end);
 
-record Cube(Range x, Range y, Range z) {
+record Block(Range x, Range y, Range z) {
     public int Bottom => z.begin;
     public int Top => z.end;
 }
 
-record Connections(
-    Dictionary<Cube, HashSet<Cube>> cubesAbove,
-    Dictionary<Cube, HashSet<Cube>> cubesBelow
+record Supports(
+    Dictionary<Block,HashSet<Block>> blocksAbove, 
+    Dictionary<Block, HashSet<Block>> blocksBelow
 );
 
 [ProblemName("Sand Slabs")]
 class Solution : Solver {
 
-    public object PartOne(string input) {
-        var cubes = ParseCubes(input);
-        cubes = Fall(cubes);
-        var conn = Analyze(cubes);
-        return cubes.Count(cube => Boom(cube, conn) == 0);
-    }
+    public object PartOne(string input) => Kaboom(input).Count(x => x == 0);
+    public object PartTwo(string input) => Kaboom(input).Sum();
 
-    public object PartTwo(string input) {
-        var cubes = ParseCubes(input);
-        cubes = Fall(cubes);
-        var conn = Analyze(cubes);
-        return cubes.Select(cube => Boom(cube, conn)).Sum();
-    }
+    // goes over the blocks in the input and desintegrates them one by one.
+    // returns the number of blocks that this would case falling.
+    IEnumerable<int> Kaboom(string input) {
+        var blocks = Fall(ParseBlocks(input));
+        var supports = GetSupports(blocks);
 
-    // returns the number of other bricks that would fall when disintegrating 'cube'
-    int Boom(Cube cube, Connections conn) {
-        var q = new Queue<Cube>();
-        q.Enqueue(cube);
+        foreach (var desintegratedBlock in blocks) {
+            var q = new Queue<Block>();
+            q.Enqueue(desintegratedBlock);
 
-        var fallingCubes = new HashSet<Cube>();
-        while (q.Any()) {
-            cube = q.Dequeue();
-            fallingCubes.Add(cube);
+            var falling = new HashSet<Block>();
+            while (q.TryDequeue(out var block)) {
+                falling.Add(block);
+                var blocksStartFalling =
+                    from blockT in supports.blocksAbove[block]
+                    where supports.blocksBelow[blockT].IsSubsetOf(falling)
+                    select blockT;
 
-            var startsFalling = from cubeT in conn.cubesAbove[cube]
-                                let supports = conn.cubesBelow[cubeT]
-                                where supports.IsSubsetOf(fallingCubes)
-                                select cubeT;
-
-            foreach (var cubeT in startsFalling) {
-                q.Enqueue(cubeT);
+                foreach (var blockT in blocksStartFalling) {
+                    q.Enqueue(blockT);
+                }
             }
+            yield return falling.Count - 1;
         }
-        return fallingCubes.Count - 1;
     }
 
-    Cube[] Fall(Cube[] cubes) {
-        cubes = cubes.OrderBy(cube => cube.Bottom).ToArray();
-
-        for (var i = 0; i < cubes.Length; i++) {
-            var cubeI = cubes[i];
+    // applies 'gravity' to the blocks
+    Block[] Fall(Block[] blocks) {
+        blocks = blocks.OrderBy(block => block.Bottom).ToArray();
+        for (var i = 0; i < blocks.Length; i++) {
             var bottom = 1;
             for (var j = 0; j < i; j++) {
-                var cubeJ = cubes[j];
-                if (IntersectsXY(cubeI, cubeJ)) {
-                    bottom = Math.Max(bottom, cubeJ.Top + 1);
+                if (IntersectsXY(blocks[i], blocks[j])) {
+                    bottom = Math.Max(bottom, blocks[j].Top + 1);
                 }
             }
 
-            var fall = cubeI.Bottom - bottom;
-            cubes[i] = cubeI with { z = new Range(cubeI.Bottom - fall, cubeI.Top - fall) };
+            var fall = blocks[i].Bottom - bottom;
+            blocks[i] = blocks[i] with {
+                z = new Range(blocks[i].Bottom - fall, blocks[i].Top - fall)
+            };
         }
-        return cubes;
+        return blocks;
     }
 
-    Connections Analyze(Cube[] cubes) {
-        var cubesAbove = cubes.ToDictionary(cube => cube, _ => new HashSet<Cube>());
-        var cubesBelow = cubes.ToDictionary(cube => cube, _ => new HashSet<Cube>());
-        for (var i = 0; i < cubes.Length; i++) {
-            var cubeLo = cubes[i];
-            for (var j = i + 1; j < cubes.Length; j++) {
-                var cubeHi = cubes[j];
-                if (cubeLo.Top == cubeHi.Bottom - 1 && IntersectsXY(cubeHi, cubeLo)) {
-                    cubesBelow[cubeHi].Add(cubeLo);
-                    cubesAbove[cubeLo].Add(cubeHi);
+    // returns a pair of dictionaries that tells the upper and lower neighbours for each block
+    Supports GetSupports(Block[] blocks) {
+        var blocksAbove = blocks.ToDictionary(b => b, _ => new HashSet<Block>());
+        var blocksBelow = blocks.ToDictionary(b => b, _ => new HashSet<Block>());
+        for (var i = 0; i < blocks.Length; i++) {
+            var blockLo = blocks[i];
+            for (var j = i + 1; j < blocks.Length; j++) {
+                var blockHi = blocks[j];
+                if (blockLo.Top == blockHi.Bottom - 1 && IntersectsXY(blockHi, blockLo)) {
+                    blocksBelow[blockHi].Add(blockLo);
+                    blocksAbove[blockLo].Add(blockHi);
                 }
             }
         }
-        return new Connections(cubesAbove, cubesBelow);
+        return new Supports(blocksAbove, blocksBelow);
     }
 
-    bool IntersectsXY(Cube cubeA, Cube cubeB) =>
-        Intersects(cubeA.x, cubeB.x) && Intersects(cubeA.y, cubeB.y);
+    bool IntersectsXY(Block blockA, Block blockB) =>
+        Intersects(blockA.x, blockB.x) && Intersects(blockA.y, blockB.y);
 
     // see https://stackoverflow.com/a/3269471
-    bool Intersects(Range r1, Range r2) =>
-        r1.begin <= r2.end && r2.begin <= r1.end;
+    bool Intersects(Range r1, Range r2) => r1.begin <= r2.end && r2.begin <= r1.end;
 
-    Cube[] ParseCubes(string input) => (
+    Block[] ParseBlocks(string input) => (
         from p in Enumerable.Zip(input.Split('\n'), Enumerable.Range(1, 100000))
         let line = p.First
         let id = p.Second
         let v = (from m in Regex.Matches(line, @"\d+") select int.Parse(m.Value)).ToArray()
-        select new Cube(new Range(v[0], v[3]), new Range(v[1], v[4]), new Range(v[2], v[5]))
+        select new Block(new Range(v[0], v[3]), new Range(v[1], v[4]), new Range(v[2], v[5]))
     ).ToArray();
 }
