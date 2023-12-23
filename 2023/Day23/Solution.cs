501fd4ff3fa38adc1a88aa8030446c0135c12f8a
David Nemeth Csoka <encse@csokavar.hu>
2023-12-23 14:10:58 +0100

    2023/23

--- 2023/Day23/Solution.cs
+++ 2023/Day23/Solution.cs
@@ -2,139 +2,132 @@ namespace AdventOfCode.Y2023.Day23;
 
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Numerics;
 using System.Linq;
-using Map = System.Collections.Immutable.IImmutableDictionary<System.Numerics.Complex, char>;
-record Node(int id, Complex pos);
-record Edge(Node from, Node to, int length);
+using Map = System.Collections.Generic.Dictionary<System.Numerics.Complex, char>;
+using Node = long; // will use powers of two to identify nodes.
+record Edge(Node start, Node end, int distance);
 
 [ProblemName("A Long Walk")]
 class Solution : Solver {
 
-    public object PartOne(string input) {
-        return Solve(ParseMap(input));
-    }
+    // Instead of dealing with the 'map' tiles directly, we convert it to a graph.
+    // Nodes: the entry tile, the exit and the crossroad tiles.
+    // Edges: two nodes are connected if there is a direct path between them that 
+    //        doesn't contain crossroads.
+    // This reduces a problem to ~30 nodes and 120 edges for the Part 2 case
+    // which can be solved using a dynamic programming approach.
 
-    public object PartTwo(string input) {
-        input = input.Replace(">", ".").Replace("v", ".");
-        // Console.WriteLine(input);
-        return Solve(ParseMap(input));
-    }
+    static readonly Complex Up = -Complex.ImaginaryOne;
+    static readonly Complex Down = Complex.ImaginaryOne;
+    static readonly Complex Left = -1;
+    static readonly Complex Right = 1;
 
-    int Solve(Map map) {
-        var startPos = 1;
-        var goalPos = map.Keys.MaxBy(pos => pos.Imaginary + pos.Real) - 1;
+    Complex[] Dirs = [Up, Down, Left, Right];
 
-        Console.WriteLine(map[startPos]);
-        Console.WriteLine(map[goalPos]);
-        var nodes = Nodes(map).ToArray();
-        var edges = Edges(map, nodes).ToArray();
+    Dictionary<char, Complex[]> exits = new() {
+        ['<'] = [Left],
+        ['>'] = [Right],
+        ['^'] = [Up],
+        ['v'] = [Down],
+        ['.'] = [Up, Down, Left, Right],
+        ['#'] = []
+    };
 
-        Console.WriteLine(("nodes", nodes.Length));
-        Console.WriteLine(("edges", edges.Length));
+    public object PartOne(string input) => Solve(input);
+    public object PartTwo(string input) => Solve(RemoveSlopes(input));
 
-        var start = nodes.Single(n => n.pos == startPos);
-        var goal = nodes.Single(n => n.pos == goalPos);
+    string RemoveSlopes(string st) =>
+        string.Join("", st.Select(ch => ">v<^".Contains(ch) ? '.' : ch));
 
-        // 6563
-        return Dp(start, goal, ImmutableHashSet<Node>.Empty, edges, new Dictionary<string, int>());
-    }
+    int Solve(string input) {
+        var (nodes, edges) = MakeGraph(input);
+        // nodes are ordered so that:
+        var (start, goal) = (nodes.First(), nodes.Last()); 
 
-    int Dp(Node start, Node end, ImmutableHashSet<Node> visited, Edge[] edges, Dictionary<string, int> cache) {
-        if (visited.Contains(start)) {
-            return -1;
-        }
-        if ( end == start ){
-            return 0;
-        }
-        visited = visited.Add(start);
-        var key = start.id + ";" + string.Join("-", visited.OrderBy(x => x.id).Select(x => x.id));
-        if (!cache.ContainsKey(key)) {
-            var m = -1;
-            foreach (var e in edges.Where(e => e.from == start)) {
-                var g = Dp(e.to, end, visited, edges, cache);
-                if (g >= 0) {
-                    m = Math.Max(m, e.length + g);
-                }
+        // Dynamic programming using a cache, 'visited' is a bitset of 'nodes'.
+        var cache = new Dictionary<(Node, long), int>();
+        int LongestPath(Node node, long visited) {
+            if (node == goal) {
+                return 0;
+            } else if ((visited & node) != 0) {
+                return int.MinValue; // small enough to represent '-infinity'
             }
-            cache[key] = m;
+            var key = (node, visited);
+            if (!cache.ContainsKey(key)) {
+                cache[key] = edges
+                    .Where(e => e.start == node)
+                    .Select(e => e.distance + LongestPath(e.end, visited | node))
+                    .Max();
+            }
+            return cache[key];
         }
-        return cache[key];
+        return LongestPath(start, 0); 
     }
 
-    Complex[] dirs = [1, -1, Complex.ImaginaryOne, -Complex.ImaginaryOne];
+    (Node[], Edge[]) MakeGraph(string input) {
+        var map = ParseMap(input);
 
-    bool IsFree(Map map, Complex pos) {
-        return map.ContainsKey(pos) && map[pos] != '#';
-    }
-    bool IsRoad(Map map, Complex pos) {
-        return IsFree(map, pos) && dirs.Count(dir => IsFree(map, pos + dir)) == 2;
-    }
-
-    Node[] Nodes(Map map) {
-        var nodePositions = map.Keys.Where(pos => IsFree(map, pos) && !IsRoad(map, pos));
-        return Enumerable.Zip(Enumerable.Range(0, int.MaxValue), nodePositions).Select(p =>
-            new Node(p.First, p.Second)
+        // positions are ordered in top -> down, left -> right, so the entry 
+        // node becomes the first (with id==1) and exit is the last.
+        var nodePositions = (
+            from pos in map.Keys
+            orderby pos.Imaginary, pos.Real
+            where IsFree(map, pos) && !IsRoad(map, pos)
+            select pos
         ).ToArray();
-    }
 
-    Edge[] Edges(Map map, Node[] nodes) {
-        return (
-            from nodeA in nodes
-            from nodeB in nodes
-            where nodeA != nodeB
-            let path = GetPath(map, nodeA.pos, nodeB.pos)
-            where path != int.MaxValue
-            select new Edge(nodeA, nodeB, path)
+        // the proper power of 2 from a node index in the array
+        var node = (int idx) => 1L << (idx - 1);
+
+        var nodes = (
+            from i in Enumerable.Range(0, nodePositions.Length)
+            select node(i)
         ).ToArray();
+
+        var edges = (
+            from i in Enumerable.Range(0, nodePositions.Length)
+            from j in Enumerable.Range(0, nodePositions.Length)
+            where i != j
+            let distance = Distance(map, nodePositions[i], nodePositions[j])
+            where distance > 0
+            select new Edge(node(i), node(j), distance)
+        ).ToArray();
+
+        return (nodes, edges);
     }
 
-    int GetPath(Map map, Complex from, Complex to) {
+    // Length of the road between two crossroads; -1 if not neighbours
+    int Distance(Map map, Complex crossroadA, Complex crossroadB) {
         var q = new Queue<(Complex, int)>();
-        q.Enqueue((from, 0));
-        var seen = new HashSet<Complex> { from };
+        q.Enqueue((crossroadA, 0));
+
+        var visited = new HashSet<Complex> { crossroadA };
         while (q.Any()) {
             var (pos, dist) = q.Dequeue();
-            if (pos == to) {
-                return dist;
-            }
-            foreach (var posT in Next(map, pos)) {
-                if (!IsFree(map, posT) || seen.Contains(posT)) {
-                    continue;
-                }
-                if (posT == to || IsRoad(map, posT)) {
-                    seen.Add(posT);
+            foreach (var dir in exits[map[pos]]) {
+                var posT = pos + dir;
+                if (posT == crossroadB) {
+                    return dist + 1;
+                }  else if (IsRoad(map, posT) && !visited.Contains(posT)) {
+                    visited.Add(posT);
                     q.Enqueue((posT, dist + 1));
                 }
             }
         }
-        return int.MaxValue;
+        return -1;
     }
 
-    IEnumerable<Complex> Next(Map map, Complex pos) {
-        if (map[pos] == '.') {
-            foreach (var dir in dirs) {
-                yield return pos + dir;
-            }
-        } else if (map[pos] == '>') {
-            yield return pos + 1;
-        } else if (map[pos] == 'v') {
-            yield return pos + Complex.ImaginaryOne;
-        } else {
-            Console.WriteLine(map[pos]);
-            throw new Exception();
-        }
-    }
+    bool IsFree(Map map, Complex p) => map.ContainsKey(p) && map[p] != '#';
+    bool IsRoad(Map map, Complex p) => Dirs.Count(d => IsFree(map, p + d)) == 2;
 
     Map ParseMap(string input) {
         var lines = input.Split('\n');
         return (
             from irow in Enumerable.Range(0, lines.Length)
             from icol in Enumerable.Range(0, lines[0].Length)
-            let cell = lines[irow][icol]
             let pos = new Complex(icol, irow)
-            select new KeyValuePair<Complex, char>(pos, cell)
-        ).ToImmutableDictionary();
+            select new KeyValuePair<Complex, char>(pos, lines[irow][icol])
+        ).ToDictionary();
     }
 }
--- 2023/SplashScreen.cs
+++ 2023/SplashScreen.cs
@@ -8,8 +8,8 @@ class SplashScreenImpl : SplashScreen {
 
         var color = Console.ForegroundColor;
         Write(0xcc00, false, "           ▄█▄ ▄▄█ ▄ ▄ ▄▄▄ ▄▄ ▄█▄  ▄▄▄ ▄█  ▄▄ ▄▄▄ ▄▄█ ▄▄▄\n           █▄█ █ █ █ █ █▄█ █ █ █   █ █ █▄ ");
-            Write(0xcc00, false, " █  █ █ █ █ █▄█\n           █ █ █▄█ ▀▄▀ █▄▄ █ █ █▄  █▄█ █   █▄ █▄█ █▄█ █▄▄  {:year 2023}\n            ");
-            Write(0xcc00, false, "\n                                ");
+            Write(0xcc00, false, " █  █ █ █ █ █▄█\n           █ █ █▄█ ▀▄▀ █▄▄ █ █ █▄  █▄█ █   █▄ █▄█ █▄█ █▄▄  0x0000 | 2023\n           ");
+            Write(0xcc00, false, " \n                                ");
             Write(0xa25151, false, "...'''''''''...                    \n                             .'' ");
             Write(0xdf2308, true, "~");
             Write(0xa5a8af, false, "/\\");
@@ -176,8 +176,8 @@ class SplashScreenImpl : SplashScreen {
             Write(0xcc00, false, "'..' .'  ");
             Write(0xcccccc, false, "22 ");
             Write(0xffff66, false, "**\n                         ");
-            Write(0xffffff, false, ".''");
-            Write(0x666666, false, "   -   - ");
+            Write(0xffffff, false, ".''   ");
+            Write(0xccccff, false, "-   - ");
             Write(0xcc00, false, "'..  ");
             Write(0x5555bb, false, "~");
             Write(0xcc00, false, "..'");
@@ -187,16 +187,17 @@ class SplashScreenImpl : SplashScreen {
             Write(0xcc00, false, "..'    ");
             Write(0xcccccc, false, " 4 ");
             Write(0xffff66, false, "**\n                       ");
-            Write(0xffffff, false, ".'");
-            Write(0x666666, false, " * -    /\\     ");
+            Write(0xffffff, false, ".' ");
+            Write(0xffff66, true, "* ");
+            Write(0xccccff, false, "-    ");
+            Write(0xffffff, false, "/\\     ");
             Write(0xcc00, false, "'''..");
             Write(0xd4dde4, false, "/");
             Write(0xcc00, false, "......'''       ");
             Write(0xcccccc, false, "23 ");
-            Write(0xffff66, false, "*");
-            Write(0x666666, false, "*\n                       ");
-            Write(0xffffff, false, ":");
-            Write(0x333333, false, "  -          ");
+            Write(0xffff66, false, "**\n                       ");
+            Write(0xffffff, false, ":  ");
+            Write(0xccccff, false, "-          ");
             Write(0xffffff, false, "/\\    ");
             Write(0xccccff, false, "-");
             Write(0xd4dde4, false, "/  ");
