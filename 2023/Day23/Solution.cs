ad353f5ebe5f592bc60dcd3c2bf96551f8fa8eb1
David Nemeth Csoka <encse@csokavar.hu>
2023-12-23 09:55:52 +0100

    wip

--- 2023/Day23/Solution.cs
+++ 2023/Day23/Solution.cs
@@ -5,50 +5,105 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Numerics;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
 using Map = System.Collections.Immutable.IImmutableDictionary<System.Numerics.Complex, char>;
-using System.Collections;
+record Node(int id, Complex pos);
+record Edge(Node from, Node to, int length);
 
 [ProblemName("A Long Walk")]
 class Solution : Solver {
 
     public object PartOne(string input) {
-        return Solve(ParseMap(input), 1, false).Max();
+        return Solve(ParseMap(input));
     }
 
     public object PartTwo(string input) {
-       return Solve(ParseMap(input), 1, true).Max();
+        input = input.Replace(">", ".").Replace("v", ".");
+        return Solve(ParseMap(input));
     }
 
-    IEnumerable<int> Solve(Map map, Complex pos, bool part2) {
-        var goal = map.Keys.MaxBy(pos => pos.Imaginary + pos.Real) - 1;
-        Console.WriteLine(map[pos]);
-        Console.WriteLine(map[goal]);
-        var q = new Queue<(Complex pos, ImmutableHashSet<Complex> seen)>();
-        q.Enqueue((pos, ImmutableHashSet<Complex>.Empty.Add(pos)));
+    int Solve(Map map) {
+        var startPos = 1;
+        var goalPos = map.Keys.MaxBy(pos => pos.Imaginary + pos.Real) - 1;
+
+        var nodes = Nodes(map).ToArray();
+        var edges = Edges(map, nodes).ToArray();
+
+        Console.WriteLine(("nodes", nodes.Length));
+        Console.WriteLine(("edges", edges.Length));
+
+        var start = nodes.Single(n => n.pos == startPos);
+        var goal = nodes.Single(n => n.pos == goalPos);
+        return Dp(start, goal, ImmutableHashSet<Node>.Empty.Add(start), edges, new Dictionary<string, int>());
+    }
+
+    int Dp(Node start, Node end, ImmutableHashSet<Node> visited, Edge[] edges, Dictionary<string, int> cache) {
+        var key = start.id + "-" + string.Join(",", visited.OrderBy(x => x.id).Select(x => x.id));
+        if (!cache.ContainsKey(key)) {
+            var m = 0;
+            foreach (var e in edges.Where(e => e.from == start)) {
+                if (!visited.Contains(e.to)) {
+                    m = Math.Max(m, e.length + Dp(e.to, end, visited.Add(e.to), edges, cache));
+                }
+            }
+            cache[key] = m;
+        }
+        return cache[key];
+    }
+
+    Complex[] dirs = [1, -1, Complex.ImaginaryOne, -Complex.ImaginaryOne];
+
+    bool IsFree(Map map, Complex pos) {
+        return map.ContainsKey(pos) && map[pos] != '#';
+    }
+    bool IsRoad(Map map, Complex pos) {
+        return IsFree(map, pos) && dirs.Count(dir => IsFree(map, pos + dir)) == 2;
+    }
+
+    Node[] Nodes(Map map) {
+        var nodePositions = map.Keys.Where(pos => IsFree(map, pos) && !IsRoad(map, pos));
+        return Enumerable.Zip(Enumerable.Range(0, int.MaxValue), nodePositions).Select(p =>
+            new Node(p.First, p.Second)
+        ).ToArray();
+    }
+
+    Edge[] Edges(Map map, Node[] nodes) {
+        return (
+            from nodeA in nodes
+            from nodeB in nodes
+            where nodeA != nodeB
+            let path = GetPath(map, nodeA.pos, nodeB.pos)
+            where path != int.MaxValue
+            select new Edge(nodeA, nodeB, path)
+        ).ToArray();
+    }
+
+    int GetPath(Map map, Complex from, Complex to) {
+        var q = new Queue<(Complex, int)>();
+        q.Enqueue((from, 0));
+        var seen = new HashSet<Complex>();
+
         while (q.Any()) {
-            (pos, var seen) = q.Dequeue();
-            if (pos == goal) {
-                Console.WriteLine((seen.Count, q.Count));
-                yield return seen.Count-1;
-            } else {
-                foreach (var posT in Next(map, pos, part2)) {
-                    if (!seen.Contains(posT)) {
-                        q.Enqueue((posT, seen.Add(posT)));
-                    }
+            var (pos, dist) = q.Dequeue();
+            if (pos == to) {
+                return dist;
+            }
+            foreach (var posT in Next(map, pos)) {
+                if (!IsFree(map, posT) || seen.Contains(posT)) {
+                    continue;
+                }
+                if (posT == to || IsRoad(map, posT)) {
+                    seen.Add(posT);
+                    q.Enqueue((posT, dist + 1));
                 }
             }
         }
+        return int.MaxValue;
     }
 
-    IEnumerable<Complex> Next(Map map, Complex pos, bool part2) {
-        if (map[pos] == '.' || part2) {
-            foreach (var dir in new[] { 1, -1, Complex.ImaginaryOne, -Complex.ImaginaryOne }) {
-                var posT = pos + dir;
-                if (map.ContainsKey(posT) && map[posT] != '#') {
-                    yield return posT;
-                }
+    IEnumerable<Complex> Next(Map map, Complex pos) {
+        if (map[pos] == '.') {
+            foreach (var dir in dirs) {
+                yield return pos + dir;
             }
         } else if (map[pos] == '>') {
             yield return pos + 1;
