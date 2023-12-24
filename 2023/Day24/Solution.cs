ce8ffdc69c1b3f6d78905d0b9371d84b4abb530f
David Nemeth Csoka <encse@csokavar.hu>
2023-12-24 23:01:40 +0100

    2023/23 wip

--- 2023/Day24/Solution.cs
+++ 2023/Day24/Solution.cs
@@ -2,11 +2,10 @@ namespace AdventOfCode.Y2023.Day24;
 
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Text;
 using System.Numerics;
+using System.Data;
 
 record Range(BigInteger begin, BigInteger end);
 record Particle(Vec2 pos, Vec2 vel);
@@ -56,7 +55,7 @@ class Solution : Solver {
             return true;
         }
 
-        return (v.x - p.pos.x) / p.vel.x < 0;
+        return (decimal)(v.x - p.pos.x) / (decimal)p.vel.x < 0;
     }
 
     Vec2 MeetPoint(Particle p1, Particle p2) {
@@ -78,120 +77,54 @@ class Solution : Solver {
         return m1.Inv() * v;
     }
 
-
-    // BigInteger FlipTime(Particle3 p1, Particle3 p2) {
-    //     if (p1.pos.x == p2.pos.x) {
-    //         if (p1.vel.x == p2.vel.x) {
-    //             return int.MaxValue;
-    //         }
-    //         return 1;
-    //     }
-
-    //     // p1.x + v1.x*t = p2.x + v2.x*t
-    //     if (p1.vel.x == p2.vel.x) {
-    //         return int.MaxValue;
-    //     }
-
-    //     var t = (p1.pos.x - p2.pos.x) / (p2.vel.x - p1.vel.x);
-    //     return Math.Floor(t) + 1;
-    // }
-
-    BigInteger Min(BigInteger a, BigInteger b) {
-        return a < b ? a : b;
-    }
-    public object PartTwo(string input) {
-        var particles = ParseParticles3(input);
-
-        Dictionary<Particle3, BigInteger> flip = new Dictionary<Particle3, BigInteger>();
-        // p1x + v1x*t = p0x + v0x*t
-        // p1x - p0x = t * (v0x - v1x)
-        // p1x  =  p0x + t * (v0x - v1x)
-
-        for (var v0x = -100000; v0x < 100000; v0x++) {
-            // (BigInteger mod, BigInteger a)
-            var selector = particles.Where(p1 => IsPrime((v0x - p1.vel.x)));
-            var items0 = selector.Select(p1 => (mod: (v0x - p1.vel.x), p1)).ToArray();
-            var items = new List<(BigInteger mod, Particle3 p)>();
-            foreach(var item in items0) {
-                if (items.All(i => i.mod != item.mod)){
-                    items.Add(item);
+    BigInteger Solve(Func<Vec3, BigInteger> dim, Particle3[] particles) {
+        for (var v0 = -10000; v0 < 10000; v0++) {
+            var items = new List<(BigInteger dv, BigInteger x)>();
+            foreach (var p in particles) {
+                var dv = v0 - dim(p.vel);
+                if (IsPrime(dv) && items.All(i => i.dv != dv)) {
+                    items.Add((dv: dv, x: dim(p.pos)));
                 }
             }
             if (items.Count > 1) {
-                var p0X = ChineseRemainderTheorem(items.Select(i => (i.mod, i.p.pos.x)).ToArray());
-
-                if (p0X != 0) {
-                    // Console.WriteLine((items.Length, p0X, v0x));
-                    var ok = true;
-                    // foreach (var p1 in items.Select(i => i.p)) {
-                    foreach (var p1 in particles) {
-                        if (v0x == p1.vel.x) {
-                            Console.Write("x");
-                            continue;
+                var p0 = ChineseRemainderTheorem(items.ToArray());
+                var ok = true;
+                foreach (var p in particles) {
+                    var dv = v0 - dim(p.vel);
+                    var dx = dim(p.pos) > p0 ? dim(p.pos) - p0 : p0 - dim(p.pos);
+                    if (dv == 0) {
+                        if (dx != 0) {
+                            ok = false;
                         }
-                        var dv = (v0x - p1.vel.x);
-                        var p = IsPrime(dv);
-                        var dx = p1.pos.x > p0X ? p1.pos.x - p0X : p0X - p1.pos.x;
-                        var m =dx % dv;
-                        if (m != 0) {
+                    } else {
+                        if (dx % dv != 0) {
                             ok = false;
                         }
                     }
-                    if (ok) {
-                        Console.WriteLine(("ok", items.Count, p0X, v0x));
-                    }
-                    // break;
+                }
+                if (ok) {
+                    return p0;
                 }
             }
-
         }
-        return 0;
+        throw new Exception();
+
+    }
+
+    public object PartTwo(string input) {
+        var particles = ParseParticles3(input);
+        return Solve(v => v.x, particles) + Solve(v => v.y, particles) + Solve(v => v.z, particles);
     }
 
     public static bool IsPrime(BigInteger number) {
         if (number <= 2) return false;
         if (number % 2 == 0) return false;
 
-        for (int i = 3; i*i <= number; i += 2)
+        for (int i = 3; i * i <= number; i += 2)
             if (number % i == 0)
                 return false;
 
         return true;
-    }
-
-    // Vec3 Intersection(Line p1, Line p2) {
-    //     var particle1xy = new Particle(new Vec2(p1.p.x, p1.p.y), new Vec2(p1.v.x, p1.v.y));
-    //     var particle2xy = new Particle(new Vec2(p2.p.x, p2.p.y), new Vec2(p2.v.x, p2.v.y));
-
-    //     var particle1xz = new Particle(new Vec2(p1.p.x, p1.p.z), new Vec2(p1.v.x, p1.v.z));
-    //     var particle2xz = new Particle(new Vec2(p2.p.x, p2.p.z), new Vec2(p2.v.x, p2.v.z));
-
-    //     var m_xy = MeetPoint(particle1xy, particle2xy);
-    //     var m_xz = MeetPoint(particle1xz, particle2xz);
-
-    //     if (m_xy == null || m_xz == null){
-    //         return null;
-    //     }
-
-    //     if (!AlmostEq(m_xy.x,  m_xz.x)) {
-    //         return null;
-    //     }
-
-    //     return new Vec3(m_xy.x, m_xy.y, m_xz.y);
-    // }
-
-    // bool AlmostEq(BigInteger d1, BigInteger d2) {
-    //     return Math.Abs(d1 - d2) < (BigInteger)0.001;
-    // }
-
-    static BigInteger SquereRoot(BigInteger square) {
-        if (square < 0) return 0;
-
-        BigInteger root = square / 3;
-        int i;
-        for (i = 0; i < 32; i++)
-            root = (root + square / root) / 2;
-        return root;
     }
 
     Particle[] ParseParticles(string input) => (
@@ -204,48 +137,8 @@ class Solution : Solver {
     Particle3[] ParseParticles3(string input) => (
         from line in input.Split('\n')
         let v = Regex.Matches(line, @"-?\d+").Select(m => BigInteger.Parse(m.Value)).ToArray()
-        select new Particle3(new Vec3(v[2], v[1], v[2]), new Vec3(v[5], v[4], v[5]))
+        select new Particle3(new Vec3(v[0], v[1], v[2]), new Vec3(v[3], v[4], v[5]))
     ).ToArray();
-
-    Line MakeLine(Particle3 p) {
-        return new Line(p.vel, p.pos);
-    }
-
-    // Plane MakePlane(Line l1, Line l2) {
-    //     if (Intersection(l1, l2) == null) {
-    //         return null;
-    //     }
-    //     var n = l1.v.CrossProduct(l2.v);
-    //     if (n.Norm2() == 0) {
-    //         return null;
-    //     }
-    //     var d = l1.p.Dot(n);
-    //     return new Plane(n, d);
-    // }
-
-    // bool Middle(Particle3[] particles, Particle3 p) {
-    //     var left = particles.Any(pT => pT.pos.x < p.pos.x && pT.vel.x < 0);
-    //     var right = particles.Any(pT => pT.pos.x > p.pos.x && pT.vel.x > 0);
-    //     return left && right;
-    // }
-
-    // Line CommonLine(Plane p1, Plane p2) {
-    //     // find the common point of the planes at z = 0;
-    //     var m = new Mat2(
-    //         p1.n.x, p1.n.y,
-    //         p2.n.x, p2.n.y
-    //     );
-
-    //     if (m.Det == 0) {
-    //         return null;
-    //     }
-
-    //     var pxy = m.Inv() * new Vec2(p1.d, p2.d);
-    //     var p = new Vec3(pxy.x, pxy.y, 0);
-
-    //     var v = p1.n.CrossProduct(p2.n);
-    //     return new Line(v, p);
-    // }
 
     // https://rosettacode.org/wiki/Chinese_remainder_theorem#C.23
     BigInteger ChineseRemainderTheorem((BigInteger mod, BigInteger a)[] items) {
@@ -273,9 +166,6 @@ record Vec2(BigInteger x, BigInteger y) {
     }
 }
 
-record Plane(Vec3 n, BigInteger d);
-record Line(Vec3 v, Vec3 p);
-
 record Vec3(BigInteger x, BigInteger y, BigInteger z) {
     public static Vec3 operator +(Vec3 v1, Vec3 v2) {
         return new Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
@@ -285,19 +175,6 @@ record Vec3(BigInteger x, BigInteger y, BigInteger z) {
     }
     public static Vec3 operator *(BigInteger d, Vec3 v1) {
         return new Vec3(d * v1.x, d * v1.y, d * v1.z);
-    }
-    public BigInteger Dot(Vec3 b) {
-        return x * b.x + y * b.y + z * b.z;
-    }
-    public Vec3 CrossProduct(Vec3 b) {
-        return new Vec3(
-            y * b.z - z * b.y,
-            z * b.x - x * b.z,
-            x * b.y - y * b.x
-        );
-    }
-    public BigInteger Norm2() {
-        return Dot(this);
     }
 }
 
