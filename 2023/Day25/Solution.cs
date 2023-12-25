0b146adbf9cf10b514523de8c7aed7873b23d3d0
David Nemeth Csoka <encse@csokavar.hu>
2023-12-25 21:43:41 +0100

    2023/25

--- 2023/Day25/Solution.cs
+++ 2023/Day25/Solution.cs
@@ -2,230 +2,86 @@ namespace AdventOfCode.Y2023.Day25;
 
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
-using System.Runtime.InteropServices;
-using System.Diagnostics.Contracts;
-using AngleSharp.Common;
 
 [ProblemName("Snowverload")]
-class Solution : Solver
-{
+class Solution : Solver {
 
-    (string, string) rev((string, string) p) => (p.Item2, p.Item1);
-    public object PartOne(string input)
-    {
-
-        var g = Parse(input);
-        var m = FindMinCut(g);
-        Console.WriteLine(m);
-        while (m != 3)
-        {
-            g = Parse(input);
-            m = FindMinCut(g);
-            Console.WriteLine(m);
+    public object PartOne(string input) {
+        Random r = new Random();
+        
+        // call Stoer–Wagner algorithm until it finds a cut with 3 edges:
+        var (size, c1, c2) = FindCut(input, r);
+        while (size != 3) {
+            (size, c1, c2) = FindCut(input, r);
         }
-        return m;
+        return c1 * c2;
     }
 
-    Random r = new Random();
+    // https://en.wikipedia.org/wiki/Stoer%E2%80%93Wagner_algorithm
+    // The Stoer–Wagner algorithm returns the size of one 'cut' of the graph. 
+    // It's a randomized algorithm that is 'likely' to find the minimal cut 
+    // in a reasonable time. The algorithm is extended to also return the sizes 
+    // of the two components separated by the cut.
+    (int size, int c1, int c2) FindCut(string input,  Random r) {
 
+        var graph = Parse(input);
+        var componentSize = graph.Keys.ToDictionary(k => k, _ => 1);
 
-    int FindMinCut(Dictionary<string, List<string>> graph)
-    {
-        Check(graph);
-        Dictionary<string, int> componentSize = graph.Keys.ToDictionary(k => k, _ => 1);
-        while (graph.Count > 2)
-        {
-            // Choose a random edge
-            int randomEdgeIndex = r.Next(graph.Count);
-            var u = graph.Keys.ElementAt(randomEdgeIndex);
+        while (graph.Count > 2) {
+            // Choose a random edge u-v
+            var u = graph.Keys.ElementAt(r.Next(graph.Count));
             var v = graph[u][r.Next(graph[u].Count)];
 
-            if (u == v)
-            {
-                Console.WriteLine("????");
+            // Contract the edge, merge nodes u and v by removing 'v'
+            // from the graph and rebinding the edges of 'v' so that it
+            // starts from 'u'. 
+            // There are no multiple edges between two nodes in the original
+            // graph, but this algorithm will introduce those.
+            foreach (var neighbour in graph[v].ToArray()) {
+                graph[neighbour].Remove(v);
+                graph[neighbour].Add(u);
             }
 
-            if (!graph.ContainsKey(v))
-            {
-                Console.WriteLine("????");
-            }
-            // Contract the edge (merge vertices u and v)
-            var neighbours = graph[v].ToArray();
-            foreach (var neighbor in neighbours)
-            {
-                if(neighbor == u){
-                    continue;
-                }
-                if(neighbor == v){
-                    Console.WriteLine("????");
-                }
-                if (!graph.ContainsKey(neighbor))
-                {
-                    Console.WriteLine("????");
-                }
-
-                graph[neighbor].Remove(v);
-                graph[neighbor].Add(u);
-            }
-
-            componentSize[u] = componentSize[u] + componentSize[v];
-            graph[u] = graph[u].Concat(graph[v]).Where(x => x != u && x != v).ToList();
+            // add edges starting from 'v'
+            graph[u] = [..graph[u].Concat(graph[v]).Where(n => n != u && n != v)];
             graph.Remove(v);
+
+            // update component size
+            componentSize[u] = componentSize[u] + componentSize[v];
+            componentSize.Remove(v);
         }
 
-        // The remaining graph has only two vertices
-        var firstVertex = graph.Keys.First();
-        if (graph[firstVertex].Count == 3){
-            var c1 = componentSize[graph.Keys.First()];
-            var c2 = componentSize[graph.Keys.Last()];
-            Console.WriteLine(c1 * c2);
-        }
-        return graph[firstVertex].Count;
+        // At the end we have just two node with some edges between them.
+        // The number of those edges equals to the size of the cut.
+        var nodeA = graph.Keys.First();
+        var nodeB = graph.Keys.Last();
+        var cutSize = graph[nodeA].Count;
+        return (cutSize, componentSize[nodeA], componentSize[nodeB]);
     }
 
-    private bool Check(Dictionary<string, List<string>> graph)
-    {
-        foreach (var v in graph.Keys)
-        {
-            foreach (var u in graph[v])
-            {
-                if (!graph.ContainsKey(u))
-                {
-                    return false;
-                }
-                if (!graph[u].Contains(v))
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    // Return adjacency list representation of input, but with the edges 
+    // recorded both ways, unlike in the input which contains them in one
+    // direction only.
+    Dictionary<string, List<string>> Parse(string input) {
+        Dictionary<string, List<string>> res = new();
 
-
-    Dictionary<string, List<string>> Parse(string input)
-    {
-        var res = new Dictionary<string, List<string>>();
-        var add = (string a, string b) =>
-        {
-            if (!res.ContainsKey(a))
-            {
-                res[a] = new List<string>();
+        var connect = (string u, string v) => {
+            if (!res.ContainsKey(u)) {
+                res[u] = new();
             }
-            if (res[a].Contains(b))
-            {
-                Console.WriteLine("???");
-            }
-            res[a].Add(b);
+            res[u].Add(v);
         };
 
-        foreach (var line in input.Split('\n'))
-        {
+        foreach (var line in input.Split('\n')) {
             var parts = line.Split(": ");
-            var left = parts[0];
-            var rights = parts[1].Split(' ');
-            foreach (var right in rights)
-            {
-                add(left, right);
-                add(right, left);
+            var u = parts[0];
+            var nodes = parts[1].Split(' ');
+            foreach (var v in nodes) {
+                connect(u, v);
+                connect(v, u);
             }
         }
-
         return res;
     }
-
-
 }
-
-
-// int FindMinCut(Dictionary<string, List<string>> graph)
-// {
-//     Check(graph);
-//     var inode = 0;
-//     while (graph.Count > 2)
-//     {
-//         // Choose a random edge
-//         int randomEdgeIndex = r.Next(graph.Count);
-//         var u = graph.Keys.ElementAt(randomEdgeIndex);
-//         var v = graph[u][r.Next(graph[u].Count)];
-
-//         if (u == v)
-//         {
-//             Console.WriteLine("????");
-//         }
-
-
-//         // Contract the edge (merge vertices u and v)
-//         var newNode = "" + inode;
-//         inode++;
-//         if (graph.ContainsKey(newNode))
-//         {
-//             Console.WriteLine("????");
-//         }
-
-//         foreach (var q in new[] { u, v })
-//         {
-//             if (!graph.ContainsKey(q))
-//             {
-//                 continue;
-//             }
-//             var neighbours = graph[q];
-//             foreach (var neighbor in neighbours.ToArray())
-//             {
-//                 if (neighbor == u || neighbor == v)
-//                 {
-//                     continue;
-//                 }
-//                 if (!graph.ContainsKey(neighbor))
-//                 {
-//                     continue;
-
-//                 }
-
-//                 graph[neighbor].Remove(q);
-//                 if (!graph[neighbor].Contains(newNode))
-//                 {
-//                     graph[neighbor].Add(newNode);
-//                     if (neighbor == newNode)
-//                     {
-//                         Console.WriteLine("xxx");
-//                     }
-//                 }
-//             }
-//         }
-
-//         graph[newNode] = new ();
-//         if (graph.ContainsKey(u))
-//         {
-//             graph[newNode].AddRange(graph[u]);
-//         }
-//         if (graph.ContainsKey(v))
-//         {
-//             graph[newNode].AddRange(graph[v]);
-//         }
-//         graph[newNode] = graph[newNode].Distinct().ToList();
-//         graph[newNode].Remove(u);
-//         graph[newNode].Remove(v);
-
-//         if (graph[newNode].Count == 0){
-//             graph.Remove(newNode);
-//         }
-
-//         graph.Remove(u);
-//         graph.Remove(v);
-//     }
-
-//     // The remaining graph has only two vertices
-//     var firstVertex = graph.Keys.First();
-//     var lastVertex = graph.Keys.Last();
-//     var res = graph[firstVertex].Count + graph[lastVertex].Count;
-//     if(res == 3){
-//         Console.WriteLine(string.Join(", ", graph[firstVertex]));
-//         Console.WriteLine(string.Join(", ", graph[lastVertex]));
-//     }
-//     return res;
-// }
