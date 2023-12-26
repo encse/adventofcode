17041f2e33d3449196cddaa528edc3f51471f51f
David Nemeth Csoka <encse@csokavar.hu>
2023-12-26 19:46:28 +0100

    2023/25

--- 2023/Day25/Solution.cs
+++ 2023/Day25/Solution.cs
@@ -3,13 +3,15 @@ namespace AdventOfCode.Y2023.Day25;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using AngleSharp.Common;
 
 [ProblemName("Snowverload")]
 class Solution : Solver {
 
     public object PartOne(string input) {
-        Random r = new Random();
-        // call Karger's algorithm in a loop until it finds a cut with 3 edges:
+        Random r = new Random(25);
+
+        // run Karger's algorithm until it finds a cut with 3 edges
         var (cutSize, c1, c2) = FindCut(input, r);
         while (cutSize != 3) {
             (cutSize, c1, c2) = FindCut(input, r);
@@ -18,57 +20,57 @@ class Solution : Solver {
     }
 
     // https://en.wikipedia.org/wiki/Karger%27s_algorithm
-    // The Karger's algorithm returns the size of one 'cut' of the graph. 
-    // It's a randomized algorithm that is 'likely' to find the minimal cut 
-    // in a reasonable time. The algorithm is extended to return the sizes 
-    // of the two components separated by the cut as well.
-    // This is for education purposes only, very inefficient.
+    // Karger's algorithm finds a cut of a graph and returns its size. 
+    // It's not necessarily the minimal cut, because it's a randomized algorithm 
+    // but it's 'likely' to find the minimal cut in reasonable time. 
+    // The algorithm is extended to return the sizes of the two components 
+    // separated by the cut as well.
     (int size, int c1, int c2) FindCut(string input, Random r) {
-
         var graph = Parse(input);
         var componentSize = graph.Keys.ToDictionary(k => k, _ => 1);
 
-        var rebind = (string oldNode, string newNode, string ignoreNode) => {
-            foreach (var neighbour in graph[oldNode].ToArray()) {
-                if (neighbour != ignoreNode) {
-                    graph[neighbour].Remove(oldNode);
-                    graph[neighbour].Add(newNode);
+        // updates backreferences of oldNode to point to newNode
+        var rebind = (string oldNode, string newNode) => {
+            foreach (var n in graph[oldNode]) {
+                while (graph[n].Remove(oldNode)) {
+                    graph[n].Add(newNode);
                 }
             }
         };
 
         for (var id = 0; graph.Count > 2; id++) {
-            // We decrease the the number of nodes and edges by one. First 
-            // choose a random edge u-v. Introduce a new node that inherits 
-            // every edges of the nodes u and v (apart from u-v itself) then 
-            // remove u and v from the graph. It's component size is the sum
-            // of the component sizes of u and v.
+            // Decrease the the number of nodes one. First select two nodes u 
+            // and v connected with an edge. Introduce a new node that inherits 
+            // every edges goint out from tehse (excluding the edges between them). 
+            // Set the new nodes' component size to the sum of the component 
+            // sizes of u and v. Remove u and v from the graph.
             var u = graph.Keys.ElementAt(r.Next(graph.Count));
             var v = graph[u][r.Next(graph[u].Count)];
+            string[] edge = [u, v];
 
-            var merged = $"merge-${id}";
+            var merged = "merge-" + id;
             graph[merged] = [
-                .. from n in graph[u] where n != v select n,
-                .. from n in graph[v] where n != u select n
+                ..from n in graph[u] where n != v select n,
+                ..from n in graph[v] where n != u select n
             ];
-            rebind(u, merged, v);
-            rebind(v, merged, u);
+            rebind(u, merged);
+            rebind(v, merged);
+
+            componentSize[merged] = componentSize[u] + componentSize[v];
 
             graph.Remove(u);
             graph.Remove(v);
-
-            componentSize[merged] = componentSize[u] + componentSize[v];
         }
 
-        // at the end we have just two nodes with some edges between them,
-        // the number of those edges equals to the size of the cut. Component size
-        // tells the number of nodes in the two sides created by the cut.
+        // two nodes remains with some edges between them, the number of those 
+        // edges equals to the size of the cut. Component size tells the number 
+        // of nodes in the two sides created by the cut.
         var nodeA = graph.Keys.First();
         var nodeB = graph.Keys.Last();
         return (graph[nodeA].Count(), componentSize[nodeA], componentSize[nodeB]);
     }
 
-    // Returns an adjacency list representation of the input. Edges are recorded 
+    // returns an adjacency list representation of the input. Edges are recorded 
     // both ways, unlike in the input which contains them in one direction only.
     Dictionary<string, List<string>> Parse(string input) {
         var graph = new Dictionary<string, List<string>>();
