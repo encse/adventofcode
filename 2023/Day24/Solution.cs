c557501bb4fd66f62f80952749da2d66744a2c9c
David Nemeth Csoka <encse@csokavar.hu>
2023-12-28 09:58:52 +0100

    wip

--- 2023/Day24/Solution.cs
+++ 2023/Day24/Solution.cs
@@ -1,7 +1,6 @@
 namespace AdventOfCode.Y2023.Day24;
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Numerics;
@@ -14,7 +13,7 @@ record Mat2(decimal a, decimal b, decimal c, decimal d);
 record Particle(Vec2 pos, Vec2 vel);
 
 // part 2
-record Vec3(BigInteger x, BigInteger y, BigInteger z);
+record Vec3(decimal x, decimal y, decimal z);
 record Particle3(Vec3 pos, Vec3 vel);
 
 [ProblemName("Never Tell Me The Odds")]
@@ -28,7 +27,7 @@ class Solution : Solver {
         return (
             from i in Enumerable.Range(0, particles.Length)
             from j in Enumerable.Range(0, particles.Length)
-            where i< j
+            where i < j
             let intersection = Meet(particles[i], particles[j])
             where (
                 intersection.pos != null &&
@@ -37,12 +36,55 @@ class Solution : Solver {
                 intersection.t1 >= 0 && intersection.t2 >= 0
             )
             select 1
-        ).Sum(); 
+        ).Sum();
     }
 
     public object PartTwo(string input) {
         var particles = ParseParticles3(input);
-        return Solve(v => v.x, particles) + Solve(v => v.y, particles) + Solve(v => v.z, particles);
+        var xy = Solve2D(particles, p => p.x, p => p.y);
+        var xz = Solve2D(particles, p => p.x, p => p.z);
+        return Math.Round(xy.p.x + xy.p.y + xz.p.y);
+    }
+
+    (Vec2 p, Vec2 v) Solve2D(
+        Particle3[] particles,
+        Func<Vec3, decimal> dim1,
+        Func<Vec3, decimal> dim2
+    ) {
+        var s = 500;
+        for (var v1 = -s; v1 < s; v1++) {
+            for (var v2 = -s; v2 < s; v2++) {
+                var p1 = new Particle(
+                    new Vec2(dim1(particles[0].pos), dim2(particles[0].pos)),
+                    new Vec2(dim1(particles[0].vel) - v1, dim2(particles[0].vel) - v2)
+                );
+                var p2 = new Particle(
+                    new Vec2(dim1(particles[1].pos), dim2(particles[1].pos)),
+                    new Vec2(dim1(particles[1].vel) - v1, dim2(particles[1].vel) - v2)
+                );
+                var (mp, _, __) = Meet(p1, p2);
+                if (mp == null) {
+                    continue;
+                }
+                var ok = true;
+                for (var i = 0; i < particles.Length && ok; i++) {
+                    var p = new Vec2(dim1(particles[i].pos), dim2(particles[i].pos));
+                    var v = new Vec2(dim1(particles[i].vel) - v1, dim2(particles[i].vel) - v2);
+
+                    if (v.x != 0 && v.y != 0) {
+                        var tx = (mp.x - p.x) / v.x;
+                        var ty = (mp.y - p.y) / v.y;
+                        if (Math.Abs(tx - ty) > (decimal)0.00001) {
+                            ok = false;
+                        }
+                    }
+                }
+                if (ok) {
+                    return (mp, new Vec2(v1, v2));
+                }
+            }
+        }
+        throw new Exception();
     }
 
     // the position where the path of the particles cross and the time(s) when 
@@ -50,7 +92,7 @@ class Solution : Solver {
     (Vec2 pos, decimal t1, decimal t2) Meet(Particle p1, Particle p2) {
         // Solving m * x = b provides the meeting point
         Mat2 m = new Mat2(
-            p1.vel.y, -p1.vel.x, 
+            p1.vel.y, -p1.vel.x,
             p2.vel.y, -p2.vel.x
         );
         Vec2 b = new Vec2(
@@ -62,12 +104,12 @@ class Solution : Solver {
         // to compute the determinant, the inverse of m, and one matrix 
         // multiplication, so I'll inline it here. I can't make it better.
         var determinant = m.a * m.d - m.b * m.c;
-        if (determinant == 0) {
+        if (determinant == 0 || p1.vel.x == 0 || p2.vel.x == 0) {
             return (null, -1, -1); //particles don't meet
         }
 
         var inverse = new Mat2(
-            m.d / determinant, -m.b / determinant, 
+            m.d / determinant, -m.b / determinant,
             -m.c / determinant, m.a / determinant
         );
 
@@ -82,79 +124,16 @@ class Solution : Solver {
         return (pos, t1, t2);
     }
 
-    BigInteger Solve(Func<Vec3, BigInteger> dim, Particle3[] particles) {
-        for (var v0 = -10000; v0 < 10000; v0++) {
-            var items = new List<(BigInteger dv, BigInteger x)>();
-            foreach (var p in particles) {
-                var dv = v0 - dim(p.vel);
-                if (IsPrime(dv) && items.All(i => i.dv != dv)) {
-                    items.Add((dv: dv, x: dim(p.pos)));
-                }
-            }
-            if (items.Count > 1) {
-                var p0 = ChineseRemainderTheorem(items.ToArray());
-                var ok = true;
-                foreach (var p in particles) {
-                    var dv = v0 - dim(p.vel);
-                    var dx = dim(p.pos) > p0 ? dim(p.pos) - p0 : p0 - dim(p.pos);
-                    if (dv == 0) {
-                        if (dx != 0) {
-                            ok = false;
-                        }
-                    } else {
-                        if (dx % dv != 0) {
-                            ok = false;
-                        }
-                    }
-                }
-                if (ok) {
-                    return p0;
-                }
-            }
-        }
-        throw new Exception();
-
-    }
-
-    public static bool IsPrime(BigInteger number) {
-        if (number <= 2) return false;
-        if (number % 2 == 0) return false;
-
-        for (int i = 3; i * i <= number; i += 2)
-            if (number % i == 0)
-                return false;
-
-        return true;
-    }
-
     Particle[] ParseParticles(string input) => (
         from line in input.Split('\n')
         let v = Regex.Matches(line, @"-?\d+").Select(m => decimal.Parse(m.Value)).ToArray()
         select new Particle(new Vec2(v[0], v[1]), new Vec2(v[3], v[4]))
     ).ToArray();
 
-
     Particle3[] ParseParticles3(string input) => (
         from line in input.Split('\n')
-        let v = Regex.Matches(line, @"-?\d+").Select(m => BigInteger.Parse(m.Value)).ToArray()
+        let v = Regex.Matches(line, @"-?\d+").Select(m => decimal.Parse(m.Value)).ToArray()
         select new Particle3(new Vec3(v[0], v[1], v[2]), new Vec3(v[3], v[4], v[5]))
     ).ToArray();
-
-    // https://rosettacode.org/wiki/Chinese_remainder_theorem#C.23
-    BigInteger ChineseRemainderTheorem((BigInteger mod, BigInteger a)[] items) {
-        var prod = items.Aggregate(BigInteger.One, (acc, item) => acc * item.mod);
-        var sum = items.Select((item, i) => {
-            var p = prod / item.mod;
-            return item.a * ModInv(p, item.mod) * p;
-        });
-
-        var s = BigInteger.Zero;
-        foreach (var item in sum) {
-            s += item;
-        }
-
-        return s % prod;
-    }
-    BigInteger ModInv(BigInteger a, BigInteger m) => BigInteger.ModPow(a, m - 2, m);
 }
 
