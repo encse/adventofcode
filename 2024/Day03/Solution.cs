4c568ae00111cb48104006e48c1e72b15a3a7312
encse <encse@csokavar.hu>
2024-12-03 19:15:12 +0100

    day 2024/03

--- 2024/Day03/Solution.cs
+++ 2024/Day03/Solution.cs
@@ -6,9 +6,9 @@ using System.Text.RegularExpressions;
 [ProblemName("Mull It Over")]
 class Solution : Solver {
 
-    public object PartOne(string input) => Solve(input, @"mul\((\d+),(\d+)\)");
+    public object PartOne(string input) => Solve(input, @"mul\((\d{1,3}),(\d{1,3})\)");
 
-    public object PartTwo(string input) => Solve(input, @"mul\((\d+),(\d+)\)|don't\(\)|do\(\)");
+    public object PartTwo(string input) => Solve(input, @"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)");
 
     long Solve(string input, string rx) {
         // overly functionaly approach...
@@ -17,9 +17,9 @@ class Solution : Solver {
             (enabled: true, res: 0L), 
             (acc, m) => 
                 (m.Value, acc.res, acc.enabled) switch {
-                    ("don't()", _, _)      => (false, acc.res),
-                    ("do()", _, _)         => (true, acc.res),
-                    (var v, var res, true) => 
+                    ("don't()", _, _)  => (false, acc.res),
+                    ("do()", _, _)     => (true, acc.res),
+                    (_, var res, true) => 
                         (true, res + int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value)),
                     _ => acc
                 },
