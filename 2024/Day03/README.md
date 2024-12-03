194b9e115b42b3d0e26bc0beef1c0775ee4efce0
encse <encse@csokavar.hu>
2024-12-03 07:19:12 +0100

    day 2024/03

---
+++ 2024/Day03/README.md
@@ -0,0 +1,8 @@
+## --- Day 3: Mull It Over ---
+"Our computers are having issues, so I have no idea if we have any Chief Historians in stock! You're welcome to check the warehouse, though," says the mildly flustered shopkeeper at the [North Pole Toboggan Rental Shop](/2020/day/2). The Historians head out to take a look.
+
+The shopkeeper turns to you. "Any chance you can see why our computers are having issues again?"
+
+The computer appears to be trying to run a program, but its memory (your puzzle input) is <em>corrupted</em>. All of the instructions have been jumbled up!
+
+Read the [full puzzle](https://adventofcode.com/2024/day/3).
---
+++ 2024/Day03/Solution.cs
@@ -0,0 +1,28 @@
+namespace AdventOfCode.Y2024.Day03;
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+[ProblemName("Mull It Over")]
+class Solution : Solver {
+
+    public object PartOne(string input) => Solve(input, @"mul\((\d+),(\d+)\)");
+
+    public object PartTwo(string input) => Solve(input, @"mul\((\d+),(\d+)\)|don't\(\)|do\(\)");
+
+    long Solve(string input, string rx) {
+        // overly functionaly approach...
+        var matches = Regex.Matches(input, rx, RegexOptions.Multiline);
+        return matches.Aggregate(
+            (enabled: true, res: 0L), 
+            (acc, m) => 
+                (m.Value, acc.res, acc.enabled) switch {
+                    ("don't()", _, _)      => (false, acc.res),
+                    ("do()", _, _)         => (true, acc.res),
+                    (var v, var res, true) => (true, res + int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value)),
+                    _ => acc
+                },
+            acc => acc.res
+        );
+    }
+}
