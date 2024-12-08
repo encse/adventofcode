66d6b690972cb5a03fb5d9b6e1e4e76be06576b0
encse <encse@csokavar.hu>
2024-12-08 07:29:34 +0100

    2024/08

---
+++ 2024/Day08/README.md
@@ -0,0 +1,8 @@
+## --- Day 8: Resonant Collinearity ---
+You find yourselves on the [roof](/2016/day/25) of a top-secret Easter Bunny installation.
+
+While The Historians do their thing, you take a look at the familiar <em>huge antenna</em>. Much to your surprise, it seems to have been reconfigured to emit a signal that makes people 0.1% more likely to buy Easter Bunny brand Imitation Mediocre Chocolate as a Christmas gift! Unthinkable!
+
+Scanning across the city, you find that there are actually many such antennas. Each antenna is tuned to a specific <em>frequency</em> indicated by a single lowercase letter, uppercase letter, or digit. You create a map (your puzzle input) of these antennas. For example:
+
+Read the [full puzzle](https://adventofcode.com/2024/day/8).
---
+++ 2024/Day08/Solution.cs
@@ -0,0 +1,66 @@
+namespace AdventOfCode.Y2024.Day08;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Numerics;
+
+using Map = System.Collections.Immutable.ImmutableDictionary<System.Numerics.Complex, char>;
+
+[ProblemName("Resonant Collinearity")]
+class Solution : Solver {
+    public object PartOne(string input) => GetUniquePositions(input, GetAntinodes1).Count();
+    public object PartTwo(string input) => GetUniquePositions(input, GetAntinodes2).Count();
+
+    HashSet<Complex> GetUniquePositions(string input, GetAntinodes antinodePostions) {
+        var map = GetMap(input);
+
+        var antennaLocations = (
+            from pos in map.Keys 
+            where char.IsAsciiLetterOrDigit(map[pos])
+            select pos
+        ).ToArray();
+
+        return (
+             from srcAntenna in antennaLocations
+             from dstAntenna in antennaLocations
+             where srcAntenna != dstAntenna && map[srcAntenna] == map[dstAntenna]
+             from antinode in antinodePostions(srcAntenna, dstAntenna, map)
+             select antinode
+         ).ToHashSet();
+    }
+
+    // returns the antinode positions on the srcAntenna side induced by dstAntenna
+    delegate IEnumerable<Complex> GetAntinodes(Complex srcAntenna, Complex dstAntenna, Map map);
+
+    // in part 1 we just look at the immediate neighbour
+    IEnumerable<Complex> GetAntinodes1(Complex srcAntenna, Complex dstAntenna, Map map) {
+        var dir = dstAntenna - srcAntenna;
+        var antinote = srcAntenna - dir;
+        if (map.Keys.Contains(antinote)) {
+            yield return antinote;
+        }
+    }
+
+    // in part 2 this becomes a cycle, plus srcAntenna is also a valid position now
+    IEnumerable<Complex> GetAntinodes2(Complex srcAntenna, Complex dstAntenna, Map map) {
+        var dir = dstAntenna - srcAntenna;
+        var antinote = srcAntenna;
+        while (map.Keys.Contains(antinote)) {
+            yield return antinote;
+            antinote -= dir;
+        }
+    }
+
+    // store the points in a dictionary so that we can iterate over them and 
+    // to easily deal with points outside the area using GetValueOrDefault
+    Map GetMap(string input) {
+        var map = input.Split("\n");
+        return (
+            from y in Enumerable.Range(0, map.Length)
+            from x in Enumerable.Range(0, map[0].Length)
+            select new KeyValuePair<Complex, char>(x - y * Complex.ImaginaryOne, map[y][x])
+        ).ToImmutableDictionary();
+    }
+}
