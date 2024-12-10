a3a99f5178f0b6944df0d02d7da35768d7c88176
Dávid Németh Cs <encse@csokavar.hu>
2024-12-10 08:38:54 +0100

    2024/09

--- 2024/Day09/Solution.cs
+++ 2024/Day09/Solution.cs
@@ -1,149 +1,77 @@
 namespace AdventOfCode.Y2024.Day09;
 
-using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
-using System.Security.AccessControl;
 
 [ProblemName("Disk Fragmenter")]
 class Solution : Solver {
 
-    // WIP
+    public object PartOne(string input) => Checksum(CompactFs(Parse(input), fragmentsEnabled: true));
 
-    public object PartOne(string input) {
-        return Defragment1(Parse(input));
-    }
-    // public object PartOne(string input) {
-    //     var extracted = Extract(input);
-    //     var i = 0;
-    //     var j = extracted.Length - 1;
-    //     while (i < j) {
-    //         if (extracted[j] == -1) {
-    //             j--;
-    //         } else if (extracted[i] != -1) {
-    //             i++;
-    //         } else {
-    //             (extracted[i], extracted[j]) = (extracted[j], extracted[i]);
-    //             j--;
-    //             i++;
-    //         }
-    //     }
-    //     var res = 0L;
-    //     for (i = 0; i < extracted.Length; i++) {
-    //         if (extracted[i] != -1) {
-    //             res += i * extracted[i];
-    //         }
-    //     }
-    //     return res;
-    // }
+    public object PartTwo(string input) => Checksum(CompactFs(Parse(input), fragmentsEnabled: false));
 
-    public object PartTwo(string input) {
-        return Defragment2(Parse(input));
-    }
-
-    public long Defragment1(List<Part> fs) {
-        var j = fs.Count - 1;
-        while (j > 0) {
-            if (fs[j].inode == -1) {
-                j--;
+    // moves used blocks of the filesystem towards the beginning of the disk using RelocateBlock
+    LinkedList<Block> CompactFs(LinkedList<Block> fs, bool fragmentsEnabled) {
+        var (i, j) = (fs.First, fs.Last);
+        while (i != j) {
+            if (i.Value.fileId != -1) {
+                i = i.Next;
+            } else if (j.Value.fileId == -1) {
+                j = j.Previous;
             } else {
-                for (var i = 0; i <= j; i++) {
-                    if (i == j) {
-                        j--;
-                        break;
-                    } else if (fs[i].inode == -1 && fs[i].length == fs[j].length) {
-                        (fs[i], fs[j]) = (fs[j], fs[i]);
-                        j--;
-                        break;
-                    } else if (fs[i].inode == -1 && fs[i].length > fs[j].length) {
-                        fs.Insert(i+1, new Part(-1, fs[i].length - fs[j].length));
-                        j++;
-                        (fs[i], fs[j]) = (fs[j], new Part(-1, fs[j].length));
-                        j--;
-                        break;
-                    } else if (fs[i].inode == -1 && fs[i].length < fs[j].length) {
-                        fs[i] =  new Part(fs[j].inode, fs[i].length);
-                        fs[j] =  new Part(fs[j].inode, fs[j].length - fs[i].length);
-                        break;
-                    }
-
-                }
+                RelocateBlock(fs, i, j, fragmentsEnabled);
+                j = j.Previous;
             }
         }
+        return fs;
+    }
 
-        var q = Flatten(fs).ToArray();
+    // Relocates the contents of block `j` to a free space starting after the given node `start`. 
+    // - Searches for the first suitable free block after `start`.
+    // - If a block of equal size is found, `j` is moved entirely to that block.
+    // - If a larger block is found, part of it is used for `j`, and the remainder is split into 
+    //   a new free block.
+    // - If a smaller block is found and fragmentation is enabled, a portion of `j` is moved to fit, 
+    //   leaving the remainder in place.
+    void RelocateBlock(LinkedList<Block> fs, LinkedListNode<Block> start, LinkedListNode<Block> j, bool fragmentsEnabled) {
+        for (var i = start; i != j; i = i.Next) {
+            if (i.Value.fileId != -1) {
+                // noop
+            } else if (i.Value.length == j.Value.length) {
+                (i.Value, j.Value) = (j.Value, i.Value);
+                return;
+            } else if (i.Value.length > j.Value.length) {
+                var d = i.Value.length - j.Value.length;
+                i.Value = j.Value;
+                j.Value = j.Value with { fileId = -1 };
+                fs.AddAfter(i, new Block(-1, d));
+                return;
+            } else if (i.Value.length < j.Value.length && fragmentsEnabled) {
+                var d = j.Value.length - i.Value.length;
+                i.Value = i.Value with { fileId = j.Value.fileId };
+                j.Value = j.Value with { length = d };
+                fs.AddAfter(j, new Block(-1, i.Value.length));
+            }
+        }
+    }
+
+    long Checksum(LinkedList<Block> fs) {
         var res = 0L;
-        for (var i = 0; i < q.Length; i++) {
-            if (q[i].inode != -1) {
-                res += i * q[i].inode;
+        var l = 0;
+        for (var i = fs.First; i != null; i = i.Next) {
+            for (var k = 0; k < i.Value.length; k++) {
+                if (i.Value.fileId != -1) {
+                    res += l * i.Value.fileId;
+                }
+                l++;
             }
         }
         return res;
     }
 
-      public long Defragment2(List<Part> fs) {
-        var j = fs.Count - 1;
-        while (j > 0) {
-            if (fs[j].inode == -1) {
-                j--;
-            } else {
-                for (var i = 0; i <= j; i++) {
-                    if (i == j){
-                        j--;
-                        break;
-                    } else if (fs[i].inode == -1 && fs[i].length == fs[j].length) {
-                        (fs[i], fs[j]) = (fs[j], fs[i]);
-                        j--;
-                        break;
-                    } else if (fs[i].inode == -1 && fs[i].length > fs[j].length) {
-                        fs.Insert(i+1, new Part(-1, fs[i].length - fs[j].length));
-                        j++;
-                        (fs[i], fs[j]) = (fs[j], new Part(-1, fs[j].length));
-                        j--;
-                        break;
-                    }
-                }
-            }
-        }
-
-        var q = Flatten(fs).ToArray();
-        var res = 0L;
-        for (var i = 0; i < q.Length; i++) {
-            if (q[i].inode != -1) {
-                res += i * q[i].inode;
-            }
-        }
-        return res;
-    }
-
-    IEnumerable<Part> Flatten(List<Part> parts) {
-        for(var i = 0;i<parts.Count;i++){
-            for(var j=0;j<parts[i].length;j++){
-                yield return new Part(parts[i].inode, 1);
-            }
-        }
-    }
-
-    IEnumerable<int> GetDescriptors() {
-        for (var i = 0; ; i++) {
-            yield return i;
-            yield return -1;
-        }
-    }
-
-    int[] Extract(string input) =>
-        Enumerable.Zip(GetDescriptors(), input)
-            .SelectMany(p => Enumerable.Repeat(p.First, p.Second - '0'))
-            .ToArray();
-
-    List<Part> Parse(string input) {
-        return input.Select((ch, i) => new Part(i % 2 == 1 ? -1 : i / 2, ch - '0')).ToList();
+    LinkedList<Block> Parse(string input) {
+        return new LinkedList<Block>(input.Select((ch, i) => new Block(i % 2 == 1 ? -1 : i / 2, ch - '0')));
     }
 }
 
-record struct Part(int inode, int length) {
-
-}
+record struct Block(int fileId, int length) { }
