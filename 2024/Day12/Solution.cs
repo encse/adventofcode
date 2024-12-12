9acc234e4db631e1de595cc42b12406257b91e81
Dávid Németh Cs <encse@csokavar.hu>
2024-12-12 10:49:50 +0100

    2024/12

--- 2024/Day12/Solution.cs
+++ 2024/Day12/Solution.cs
@@ -1,13 +1,12 @@
 namespace AdventOfCode.Y2024.Day12;
 
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Numerics;
 
-using Map = System.Collections.Immutable.ImmutableDictionary<System.Numerics.Complex, char>;
-using Component = System.Collections.Generic.HashSet<System.Numerics.Complex>;
+using Region = System.Collections.Generic.HashSet<System.Numerics.Complex>;
+using System;
 
 [ProblemName("Garden Groups")]
 class Solution : Solver {
@@ -17,122 +16,116 @@ class Solution : Solver {
     Complex Left = -1;
     Complex Right = 1;
 
-    public object PartOne(string input) {
-         var map = GetMap(input);
-        var points = map.Keys.ToHashSet();
+    public object PartOne(string input) => CalculateFencePrice(input, discounted: false);
+    public object PartTwo(string input) => CalculateFencePrice(input, discounted: true);
 
-        var componentMap = new Dictionary<Complex, Component>();
-        var components = new List<Component>();
-
-        while (points.Any()) {
-            var component = Floodfill(map, points.First()).ToHashSet();
-            points.ExceptWith(component);
-
-            components.Add(component);
-            foreach (var pt in component) {
-                componentMap[pt] = component;
-            }
-        }
-
-        var borders = new Dictionary<Component, int>();
-        CollectBorders(componentMap, borders, Right, Down, Up, false);
-        CollectBorders(componentMap, borders, Right, Down, Down, false);
-        CollectBorders(componentMap, borders, Down, Right, Left, false);
-        CollectBorders(componentMap, borders, Down, Right, Right, false);
-
-        var res = 0;
-        foreach (var component in components) {
-            var area = component.Count;
-            var perimeter = borders[component];
-            res += area * perimeter;
-        }
-        return res;
+    int CalculateFencePrice(string input, bool discounted) {
+        var regions = GetRegions(input);
+        var perimeters = GetPerimeters(regions, discounted);
+        return regions.Values.Distinct().Sum(region => region.Count * perimeters[region]);
     }
 
-    public object PartTwo(string input) {
-        var map = GetMap(input);
-        var points = map.Keys.ToHashSet();
+    // Maps the positions of plants in a garden to their corresponding regions, grouping plants 
+    // of the same type into contiguous regions.
+    Dictionary<Complex, Region> GetRegions(string input) {
+        var lines = input.Split("\n");
 
-        var componentMap = new Dictionary<Complex, Component>();
-        var components = new List<Component>();
+        var garden = (
+            from y in Enumerable.Range(0, lines.Length)
+            from x in Enumerable.Range(0, lines[0].Length)
+            select new KeyValuePair<Complex, char>(x + y * Down, lines[y][x])
+        ).ToDictionary();
 
-        while (points.Any()) {
-            var component = Floodfill(map, points.First()).ToHashSet();
-            points.ExceptWith(component);
+        var res = new Dictionary<Complex, Region>();
 
-            components.Add(component);
-            foreach (var pt in component) {
-                componentMap[pt] = component;
-            }
-        }
+        // go over the positions of the garden and use a floodfill to determine the region
+        var positions = garden.Keys.ToHashSet();
+        while (positions.Any()) {
+            var pivot = positions.First();
+            var region = new Region { pivot };
 
-        var borders = new Dictionary<Component, int>();
-        CollectBorders(componentMap, borders, Right, Down, Up, true);
-        CollectBorders(componentMap, borders, Right, Down, Down, true);
-        CollectBorders(componentMap, borders, Down, Right, Left, true);
-        CollectBorders(componentMap, borders, Down, Right, Right, true);
+            var q = new Queue<Complex>();
+            q.Enqueue(pivot);
 
-        var res = 0;
-        foreach (var component in components) {
-            var area = component.Count;
-            var perimeter = borders[component];
-            res += area * perimeter;
-        }
-        return res;
-    }
+            var plant = garden[pivot];
 
-    void CollectBorders(Dictionary<Complex, Component> componentMap, Dictionary<Component, int> borders, Complex du, Complex dv, Complex look, bool b) {
-        var start = Complex.Zero;
-        var pt = start;
-        while (componentMap.ContainsKey(pt)) {
-            var found = false;
-            while (componentMap.ContainsKey(pt)) {
-                if (componentMap[pt] != componentMap.GetValueOrDefault(pt + look)) {
-                    if (!found) {
-                        borders[componentMap[pt]] = borders.GetValueOrDefault(componentMap[pt]) + 1;
-                        found = b & true;
+            while (q.Any()) {
+                var point = q.Dequeue();
+                res[point] = region;
+                positions.Remove(point);
+                foreach (var dir in new[] { Up, Down, Left, Right }) {
+                    if (!region.Contains(point + dir) && garden.GetValueOrDefault(point + dir) == plant) {
+                        region.Add(point + dir);
+                        q.Enqueue(point + dir);
                     }
-                } else {
-                    found = false;
                 }
+            }
+        }
+        return res;
+    }
 
-                if (componentMap[pt] != componentMap.GetValueOrDefault(pt + du)) {
-                    found = false;
+    // Calculates the perimeters of all regions at once (in segments or regular units)
+    Dictionary<Region, int> GetPerimeters(Dictionary<Complex, Region> map, bool segemented) {
+        var perimeters = new Dictionary<Region, int>();
+        foreach (var fence in GetFenceSegements(map)) {
+            var length = segemented ? 1 : fence.Count();
+            var region = map[fence.First()];
+            perimeters[region] = perimeters.GetValueOrDefault(region) + length;
+        }
+        return perimeters;
+    }
+
+    // Finds the positions of the straight fence segments
+    IEnumerable<IEnumerable<Complex>> GetFenceSegements(Dictionary<Complex, Region> map) {
+        // the loop scans the garden four times checking if the given position has a fence up, down, 
+        // left or right maintaining when a new fence segment is to be started
+
+        foreach (var (dir, look) in new[] { (Right, Up), (Right, Down), (Down, Left), (Down, Right) }) {
+            var fence = new List<Complex>();
+
+            foreach (var line in Scan(map, dir == Right)) {
+                foreach (var position in line) {
+                    // fence ends when:
+                    // - we step into a different region
+                    // - the position we are looking at belongs to the same region as our current region.
+                    if (map[position] != map.GetValueOrDefault(position - dir) ||
+                        map[position] == map.GetValueOrDefault(position + look)
+                    ) {
+                        if (fence.Any()) {
+                            yield return fence;
+                        }
+                        fence = new List<Complex>();
+                    }
+
+                    if (map[position] != map.GetValueOrDefault(position + look)) {
+                        fence.Add(position);
+                    }
                 }
+            }
 
+            if (fence.Any()) {
+                yield return fence;
+            }
+        }
+    }
+
+    // Returns horizontal or vertical scanlines of a map
+    IEnumerable<IEnumerable<Complex>> Scan<T>(Dictionary<Complex, T> map, bool horiz) {
+        var du = horiz ? Right : Down;
+        var dv = horiz ? Down : Right;
+
+        var pt0 = Complex.Zero;
+        while (map.ContainsKey(pt0)) {
+            var line = new List<Complex>();
+            var pt = pt0;
+            while (map.ContainsKey(pt)) {
+                line.Add(pt);
                 pt += du;
             }
-            start += dv;
-            pt = start;
+
+            yield return line;
+            pt0 += dv;
         }
     }
 
-    Component Floodfill(Map map, Complex start) {
-        // standard floodfill algorithm using a queue
-        var positions = new Queue<Complex>();
-        positions.Enqueue(start);
-        var component = new Component();
-        var plant = map[start];
-        while (positions.Any()) {
-            var point = positions.Dequeue();
-            component.Add(point);
-            foreach (var dir in new[] { Up, Down, Left, Right }) {
-                if (!component.Contains(point + dir) && map.GetValueOrDefault(point + dir) == plant) {
-                    positions.Enqueue(point + dir);
-                }
-            }
-        }
-        return component;
-    }
-
-    // store the points in a dictionary so that we can iterate over them and 
-    // to easily deal with points outside the area using GetValueOrDefault
-    Map GetMap(string input) {
-        var map = input.Split("\n");
-        return (
-            from y in Enumerable.Range(0, map.Length)
-            from x in Enumerable.Range(0, map[0].Length)
-            select new KeyValuePair<Complex, char>(x + y * Down, map[y][x])
-        ).ToImmutableDictionary();
-    }
 }
