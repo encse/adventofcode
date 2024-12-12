5c3bda7b7d349e04f484141eb48c58c1d2f9aeb7
Dávid Németh Cs <encse@csokavar.hu>
2024-12-12 12:49:27 +0100

    2024/12 with edge and corner detectors

--- 2024/Day12/README.md
+++ 2024/Day12/README.md
@@ -11,24 +11,25 @@ Here's an improved version with corrected grammar and flow:
 
 One can sense that the difficulty has ramped up today with a more complex problem involving area and perimeter calculations. 
 
-First we determine the connected components (regions) of plants of the 
-same type. This is done by picking a position of the garden and applying a standard flood-fill algorithm to it. The result is a _region_.
-As we process each region, we carefully remove all affected positions and then pick an untouched position to repeat the process. 
-In a few iterations, the entire garden is associated with the correct regions. The size of the regions corresponds to the _area_ 
-in question. This logic is implemented in the `GetRegions` function below.
-
+First we determine the connected components (regions) of plants of the same type. This is done by picking a position of the 
+garden and applying a standard flood-fill algorithm to it. The result is a _region_. As we process each region, we carefully 
+remove all affected positions and then pick an untouched position to repeat the process. In a few iterations, the entire 
+garden is associated with the correct regions. The size of the regions corresponds to the _area_  in question. This logic 
+is implemented in the `GetRegions` function below.
 
 The second part of the problem, however, is more intriguing: how do we calculate the lengths of the fences? In part 1, this is 
-relatively simple because we just iterate through each region and count the neighboring cells that belong to a different regions. This tells how many fence is needed. Unfortunately, this logic doesn't work for part 2.
+relatively simple because we just iterate through each region and count the neighboring cells that belong to a different regions. 
+This tells how many fence is needed. Unfortunately, this logic doesn't work for part 2...
 
-Initially, I attempted a "walk-around" approach, which involves finding a fence segment and tracing it like a line-following 
-robot while counting the turns. The challenge with this approach is that some regions have holes, that require fences as well and I didnt want to implement hole finding.
+Then I attempted a "walk-around" approach, which involves finding a fence segment and tracing it like a line-following 
+robot while counting the turns. The challenge with this approach is that some regions have holes, that require fences as well 
+and I didnt want to implement hole finding.
 
-I realized it is probably more straightforward to collect the fence segments along straight lines. For instance, if I start at the top-left corner and scan the map horizontally to the right, there is a long fence at the top of the map. A new segement starts when I step into a different region. On the next line, a similar check is performed: I determine if 
-a fence is needed above (due to a different region) or if a new region has been entered. This horizontal scanning is repeated 
-twice to detect fences above and below the positions, and a similar vertical scan is performed to identify fence segments on the left and right. You can find the implementation in the `GetFenceSegements` function.
+Later realized that it is probably more straightforward to collect the fence segments along straight lines scanning the garden 
+from top to bottom an right to left. This is how I solved the problem.
 
-`GetPerimeters` uses `GetFenceSegments` to calculate the total length of the fences for each region (in standard units 
-for part 1 or as the number of different segments for part 2).
+Finally went to reddit and read the hint: the number of segments equal to the number of corners... In other words difference
+between part 1 and 2 is very small. In part 1 we are building an edge detector which becomes a corner detector in part 2. I changed
+my implementation to that.
 
-This concludes day 12. I really enjoyed this one, although the solution became longer than I like it to be...
+This concludes our day 12. I'll take an extra ⭐ for learning something new again.
--- 2024/Day12/Solution.cs
+++ 2024/Day12/Solution.cs
@@ -16,29 +16,70 @@ class Solution : Solver {
     Complex Left = -1;
     Complex Right = 1;
 
-    public object PartOne(string input) => CalculateFencePrice(input, discounted: false);
-    public object PartTwo(string input) => CalculateFencePrice(input, discounted: true);
+    public object PartOne(string input) => CalculateFencePrice(input, FindEdges);
 
-    int CalculateFencePrice(string input, bool discounted) {
+    public object PartTwo(string input) => CalculateFencePrice(input, FindCorners);
+
+    int CalculateFencePrice(string input, MeasurePerimeter measure){
         var regions = GetRegions(input);
-        var perimeters = GetPerimeters(regions, discounted);
-        return regions.Values.Distinct().Sum(region => region.Count * perimeters[region]);
+        var res = 0;
+        foreach (var region in regions.Values.Distinct()) {
+            var perimeter = 0;
+            foreach (var pt in region) {
+                perimeter += measure(regions, pt);
+            }
+            res += region.Count() * perimeter;
+        }
+        return res;
+    }    
+   
+    delegate int MeasurePerimeter(Dictionary<Complex, Region> map, Complex pt);
+
+    int FindEdges(Dictionary<Complex, Region> map, Complex pt) {
+        var res = 0;
+        var region = map[pt];
+        foreach (var du in new[] { Right, Down, Left, Up}) {
+            // xy 
+            if (map.GetValueOrDefault(pt + du) != region) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    int FindCorners(Dictionary<Complex, Region> map, Complex pt) {
+        var res = 0;
+        var region = map[pt];
+        
+        // rotate du and dv and check for the 4 corner types
+        foreach (var (du, dv) in new[] { (Up, Right), (Right, Down), (Down, Left), (Left, Up) }) {
+            //  yy
+            //  xy
+            if (map.GetValueOrDefault(pt + du) != region &&  map.GetValueOrDefault(pt + dv) != region) {
+                res++; 
+            }
+
+            //  xy
+            //  xx
+            if (map.GetValueOrDefault(pt + du) == region && map.GetValueOrDefault(pt + dv) == region &&  map.GetValueOrDefault(pt + du + dv) != region) {
+                res++;
+            }
+        }
+        return res;
     }
 
     // Maps the positions of plants in a garden to their corresponding regions, grouping plants 
     // of the same type into contiguous regions.
     Dictionary<Complex, Region> GetRegions(string input) {
         var lines = input.Split("\n");
-
         var garden = (
             from y in Enumerable.Range(0, lines.Length)
             from x in Enumerable.Range(0, lines[0].Length)
             select new KeyValuePair<Complex, char>(x + y * Down, lines[y][x])
         ).ToDictionary();
 
-        var res = new Dictionary<Complex, Region>();
-
         // go over the positions of the garden and use a floodfill to determine the region
+        var res = new Dictionary<Complex, Region>();
         var positions = garden.Keys.ToHashSet();
         while (positions.Any()) {
             var pivot = positions.First();
@@ -62,70 +103,6 @@ class Solution : Solver {
             }
         }
         return res;
-    }
-
-    // Calculates the perimeters of all regions at once (in segments or regular units)
-    Dictionary<Region, int> GetPerimeters(Dictionary<Complex, Region> map, bool segemented) {
-        var perimeters = new Dictionary<Region, int>();
-        foreach (var fence in GetFenceSegements(map)) {
-            var length = segemented ? 1 : fence.Count();
-            var region = map[fence.First()];
-            perimeters[region] = perimeters.GetValueOrDefault(region) + length;
-        }
-        return perimeters;
-    }
-
-    // Finds the positions of the straight fence segments
-    IEnumerable<IEnumerable<Complex>> GetFenceSegements(Dictionary<Complex, Region> map) {
-        // the loop scans the garden four times checking if the given position has a fence up, down, 
-        // left or right maintaining when a new fence segment is to be started
-
-        foreach (var (dir, look) in new[] { (Right, Up), (Right, Down), (Down, Left), (Down, Right) }) {
-            var fence = new List<Complex>();
-
-            foreach (var line in Scan(map, dir == Right)) {
-                foreach (var position in line) {
-                    // fence ends when:
-                    // - we step into a different region
-                    // - the position we are looking at belongs to the same region as our current region.
-                    if (map[position] != map.GetValueOrDefault(position - dir) ||
-                        map[position] == map.GetValueOrDefault(position + look)
-                    ) {
-                        if (fence.Any()) {
-                            yield return fence;
-                        }
-                        fence = new List<Complex>();
-                    }
-
-                    if (map[position] != map.GetValueOrDefault(position + look)) {
-                        fence.Add(position);
-                    }
-                }
-            }
-
-            if (fence.Any()) {
-                yield return fence;
-            }
-        }
-    }
-
-    // Returns horizontal or vertical scanlines of a map
-    IEnumerable<IEnumerable<Complex>> Scan<T>(Dictionary<Complex, T> map, bool horiz) {
-        var du = horiz ? Right : Down;
-        var dv = horiz ? Down : Right;
-
-        var pt0 = Complex.Zero;
-        while (map.ContainsKey(pt0)) {
-            var line = new List<Complex>();
-            var pt = pt0;
-            while (map.ContainsKey(pt)) {
-                line.Add(pt);
-                pt += du;
-            }
-
-            yield return line;
-            pt0 += dv;
-        }
     }
 
 }
