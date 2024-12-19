2244f0654e5ec76864e17401a7a11bf582bbfe87
encse <encse@csokavar.hu>
2024-12-19 07:30:01 +0100

    2024/19

---
+++ 2024/Day19/README.md
@@ -0,0 +1,6 @@
+## --- Day 19: Linen Layout ---
+Today, The Historians take you up to the [hot springs](/2023/day/12) on Gear Island! Very [suspiciously](https://www.youtube.com/watch?v=ekL881PJMjI), absolutely nothing goes wrong as they begin their careful search of the vast field of helixes.
+
+Could this <em>finally</em> be your chance to visit the [onsen](https://en.wikipedia.org/wiki/Onsen) next door? Only one way to find out.
+
+_Visit the website for the full story and [full puzzle](https://adventofcode.com/2024/day/19) description._
---
+++ 2024/Day19/Solution.cs
@@ -0,0 +1,39 @@
+namespace AdventOfCode.Y2024.Day19;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Cache = System.Collections.Concurrent.ConcurrentDictionary<string, long>;
+
+[ProblemName("Linen Layout")]
+class Solution : Solver {
+
+    public object PartOne(string input) => MatchCounts(input).Count(c => c != 0);
+
+    public object PartTwo(string input) => MatchCounts(input).Sum();
+
+    IEnumerable<long> MatchCounts(string input) {
+        var blocks = input.Split("\n\n");
+        var towels = blocks[0].Split(", ");
+        return 
+            from pattern in  blocks[1].Split("\n") 
+            select MatchCount(towels, pattern, new Cache());
+    }
+
+    // computes the number of ways the pattern can be build up from the towels. 
+    // works recursively by matching the prefix of the pattern with each towel.
+    // a full match is found when the pattern becomes empty. the cache is applied 
+    // to _drammatically_ speed up execution
+    long MatchCount(string[] towels, string pattern, Cache cache) =>
+        cache.GetOrAdd(pattern, (pattern) => 
+            pattern switch {
+                "" => 1,
+                _  => (
+                    from towel in towels
+                    where pattern.StartsWith(towel)
+                    select MatchCount(towels, pattern[towel.Length..], cache)
+                ).Sum()
+            }
+        );
+}
