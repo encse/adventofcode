5cab3c2cffe6d2193f3cb4c54d25e3a835a7687b
Dávid Németh Cs <encse@csokavar.hu>
2024-12-25 07:53:21 +0100

    2024/25

--- 2024/Day25/README.md
+++ 2024/Day25/README.md
@@ -4,5 +4,3 @@ Out of ideas and time, The Historians agree that they should go back to check th
 When you get there, you are surprised to discover that the door to his office is <em>locked</em>! You can hear someone inside, but knocking yields no response. The locks on this floor are all fancy, expensive, virtual versions of [five-pin tumbler locks](https://en.wikipedia.org/wiki/Pin_tumbler_lock), so you contact North Pole security to see if they can help open the door.
 
 _Visit the website for the full story and [full puzzle](https://adventofcode.com/2024/day/25) description._
-
-A simple one for the last day to ease out. I used some linq as usual.
--- 2024/Day25/Solution.cs
+++ 2024/Day25/Solution.cs
@@ -7,18 +7,18 @@ using System.Linq;
 class Solution : Solver {
 
     public object PartOne(string input) {
-        int[] parsePattern(string[] lines) => 
-            Enumerable.Range(0, lines[0].Length).Select(x => 
+        int[] parsePattern(string[] lines) =>
+            Enumerable.Range(0, lines[0].Length).Select(x =>
                 Enumerable.Range(0, lines.Length).Count(y => lines[y][x] == '#')
             ).ToArray();
-        
-        bool match(int[] k, int[] l) => 
+
+        bool match(int[] k, int[] l) =>
             Enumerable.Range(0, k.Length).All(i => k[i] + l[i] <= 7);
 
-        var patterns = input.Split("\n\n").Select(b=>b.Split("\n"));
+        var patterns = input.Split("\n\n").Select(b => b.Split("\n"));
         var keys = patterns.Where(p => p[0][0] == '.').Select(parsePattern).ToList();
         var locks = patterns.Where(p => p[0][0] == '#').Select(parsePattern).ToList();
 
-        return keys.Sum(k => locks.Count(l => match(l,k)));
+        return keys.Sum(k => locks.Count(l => match(l, k)));
     }
 }
