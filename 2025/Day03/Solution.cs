a6fa88be96096504fcbc35fc4f1c2e8f16016b52
Dávid Németh Cs <encse@csokavar.hu>
2025-12-03 08:02:48 +0100

    2025/03 iterative

--- 2025/Day03/Solution.cs
+++ 2025/Day03/Solution.cs
@@ -1,6 +1,5 @@
 namespace AdventOfCode.Y2025.Day03;
 
-using System;
 using System.Linq;
 
 [ProblemName("Lobby")]
@@ -12,24 +11,16 @@ class Solution : Solver {
     public long MaxJoltSum(string input, int batteryCount) =>
         input.Split("\n").Select(bank => MaxJolt(bank, batteryCount)).Sum();
 
-    // Determines the maximum possible jolt value by selecting `batteryCount` 
-    // digits in descending order from `bank` while preserving order.
     long MaxJolt(string bank, int batteryCount) {
-        if (batteryCount == 0) {
-            return 0;
+        long res = 0L;
+        for (; batteryCount > 0; batteryCount--) {
+            // jump forward to the highest available digit in the bank, but keep 
+            // batteryCount digits in the suffix, so that we can select something 
+            // for those remaining batteries as well.
+            char jolt = bank[..^(batteryCount - 1)].Max();
+            bank = bank[(bank.IndexOf(jolt) + 1)..];
+            res = 10 * res + (jolt - '0');
         }
-        if (bank.Length < batteryCount) {
-            return -1;
-        }
-        for (int jolt = 9; jolt > 0; jolt--) {
-            var index = bank.IndexOf((char)(jolt + '0'));
-            if (index >= 0) {
-                var res = MaxJolt(bank[(index + 1)..], batteryCount - 1);
-                if (res >= 0) {
-                    return jolt * (long)Math.Pow(10, batteryCount-1) + res;
-                }
-            }
-        }
-        throw new Exception();
+        return res;
     }
 }
