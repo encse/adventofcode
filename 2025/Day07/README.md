61935b57f59476d39fa4c59db7913e37bb63d0af
Dávid Németh Cs <encse@csokavar.hu>
2025-12-07 07:20:40 +0100

    2025/07

---
+++ 2025/Day07/README.md
@@ -0,0 +1,6 @@
+## --- Day 7: Laboratories ---
+You thank the cephalopods for the help and exit the trash compactor, finding yourself in the [familiar](/2024/day/6) [halls](/2018/day/4) of a North Pole research wing.
+
+Based on the large sign that says "teleporter hub", they seem to be researching <em>teleportation</em>; you can't help but try it for yourself and step onto the large yellow teleporter pad.
+
+_Visit the website for the full story and [full puzzle](https://adventofcode.com/2025/day/7) description._
---
+++ 2025/Day07/Solution.cs
@@ -0,0 +1,43 @@
+namespace AdventOfCode.Y2025.Day07;
+
+using System.Linq;
+
+[ProblemName("Laboratories")]
+class Solution : Solver {
+
+    public object PartOne(string input) => RunManifold(input).splits;
+    public object PartTwo(string input) => RunManifold(input).timelines;
+    public (int splits, long timelines) RunManifold(string input) {
+        // Dynamic programming over the grid:
+        //
+        // Each cell in row i depends only on the values from row i-1 directly above it.
+        // We propagate a vector of "timeline counts" downward row by row instead of
+        // keeping a full 2D DP table, which reduces memory to O(columns).
+        //
+        // At forks ('^'), a timeline splits into left/right branches, and we count a
+        // "split" whenever an active timeline actually forks (>0 incoming paths).
+
+        var lines = input.Split("\n").Select(line => line.ToCharArray()).ToArray();
+        var crow = lines.Length;
+        var ccol = lines[0].Length;
+
+        var splits = 0;
+        var timelines = new long[ccol];
+        for (int irow = 0; irow < crow; irow++) {
+            var nextTimelines = new long[ccol];
+            for (var icol = 0; icol < ccol; icol++) {
+                if (lines[irow][icol] == '^') {
+                    splits += timelines[icol] > 0 ? 1 : 0;
+                    nextTimelines[icol - 1] += timelines[icol];
+                    nextTimelines[icol + 1] += timelines[icol];
+                } else if (lines[irow][icol] == 'S') {
+                    nextTimelines[icol] = 1;
+                } else {
+                    nextTimelines[icol] += timelines[icol];
+                }
+            }
+            timelines = nextTimelines;
+        }
+        return (splits, timelines.Sum());
+    }
+}
