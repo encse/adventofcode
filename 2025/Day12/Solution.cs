9e8d960543cae5e577793ce739e761f4bf74e4d1
Dávid Németh Cs. <encse@csokavar.hu>
2025-12-26 06:00:50 +0100

    Clarify comment about packing in PartOne method
    Updated comment for clarity regarding packing.

--- 2025/Day12/Solution.cs
+++ 2025/Day12/Solution.cs
@@ -9,7 +9,7 @@ class Solution : Solver {
 
     public object PartOne(string input) {
         // It's enough to check if the 3x3 area of the presents is less than 
-        // the area under 🎄 tree. No packing is required.
+        // the area under 🎄 tree. No packing required.
         return input.Split("\n\n").Last()
             .Split("\n")
             .Select(line => Regex.Matches(line, @"\d+").Select(m => int.Parse(m.Value)).ToArray())
